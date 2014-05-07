bf67774e63662ecb67b5f5ecd62f34fa971defa9
Martin <martin@jive.ly>
2014-05-07 15:39:10 +0100

    Added to do list :-)

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ import(
 	"github.com/Sirupsen/logrus"
 )
 
+/*
+TODO: Configuration loader
+TODO: Configuration: Error template file path
+TODO: Configuration: Set forwarding URL
+TODO: Set launch port
+TODO: Set configuration file (Command line)
+TODO: Configuration: set redis DB details
+TODO: Redis storage manager
+TODO: API endpoints for management functions: AddKey, RevokeKey, UpdateKey, GetKeyDetails
+TODO: Secure API endpoints (perhaps with just a shared secret, should be internally used anyway)
+TODO: Configuration: Set shared secret
+*/
+
 var log = logrus.New()
 var authManager = AuthorisationManager{MockStorageManager{map[string]string{"1234": "{\"LastCheck\":1399469149,\"Allowance\":5.0,\"Rate\":1.0,\"Per\":1.0}"}}}
 var sessionLimiter = SessionLimiter{}
