3387a0ea78429c0120fd1e79ef50e0be6de3b0f2
Martin <martin@jive.ly>
2014-06-11 12:10:32 +0100

    Moved a bunch of path based logic into new ApiSpec object, multi defs tests run, but not integrated.

---
+++ api_definition.go
@@ -0,0 +1,285 @@
+package main
+
+import (
+	"regexp"
+	"time"
+	"net/http"
+)
+
+type ApiDefinition struct {
+	Name string `json:"name"`
+	VersionDefinition struct {
+		Location string `json:"location"`
+		Key string `json:"key"`
+	} `json:"definition"`
+	VersionData struct {
+		NotVersioned bool `json:"not_versioned"`
+		Versions map[string]VersionInfo `json:"versions"`
+	} `json:"version_data"`
+}
+
+type VersionInfo struct {
+	Name string `json:"name"`
+	Expires string `json:"expires"`
+	Proxy struct {
+		ListenPath string `json:"listen_path"`
+		TargetUrl string `json:"target_url"`
+	} `json:"proxy"`
+	Auth struct {
+		AuthHeaderName string `json:"auth_header_name"`
+	} `json:"auth"`
+	Paths struct {
+		Ignored []string `json:"ignored"`
+		WhiteList []string `json:"white_list"`
+		BlackList []string `json:"black_list"`
+	} `json:"paths"`
+}
+
+type UrlStatus int
+
+const (
+	Ignored UrlStatus = 1
+	WhiteList UrlStatus = 2
+	BlackList UrlStatus = 3
+)
+
+type RequestStatus string
+const (
+	VersionNotFound RequestStatus = "Version information not found"
+	VersionDoesNotExist RequestStatus = "This API version doesn't seem to exist"
+	VersionPathsNotFound RequestStatus = "Path information could not be found for version"
+	VersionWhiteListStatusNotFound = "WhiteListStatus for path not found"
+	VersionExpired RequestStatus = "Api Version has expired, please check documentation or contact administrator"
+	EndPointNotAllowed RequestStatus = "Requested endpoint is forbidden"
+	GeneralFailure RequestStatus = "An error occured that should have not been possible"
+	StatusOkAndIgnore RequestStatus = "Everything OK, passing and not filtering"
+	StatusOk RequestStatus = "Everything OK, passing"
+)
+
+type UrlSpec struct {
+	Spec *regexp.Regexp
+	Status UrlStatus
+}
+
+type ApiSpec struct {
+	ApiDefinition
+	RxPaths map[string][]UrlSpec
+	WhiteListEnabled map[string]bool
+}
+
+type ApiDefinitionLoader struct {}
+
+func (a *ApiDefinitionLoader) LoadDefinitions(definitionFolder string) []ApiSpec {
+	// Grab json files from directory
+	// Check if white list and black list are being used (only one can be used at a time)
+	// If White list is being used, set flag so we don't let requests through unless on list
+	// If only black list and ignored are being used, all requests are filtered except these ones.
+
+	return []ApiSpec{}
+}
+
+func (a *ApiDefinitionLoader) getPathSpecs(apiVersionDef VersionInfo) ([]UrlSpec, bool) {
+	ignoredPaths := a.CompilePathSpec(apiVersionDef.Paths.Ignored, Ignored)
+	blackListPaths := a.CompilePathSpec(apiVersionDef.Paths.BlackList, BlackList)
+	whiteListPaths := a.CompilePathSpec(apiVersionDef.Paths.WhiteList, WhiteList)
+
+
+	combinedPath := []UrlSpec{}
+	combinedPath = append(combinedPath, ignoredPaths...)
+	combinedPath = append(combinedPath, blackListPaths...)
+	combinedPath = append(combinedPath, whiteListPaths...)
+
+	if len(whiteListPaths) > 0 {
+		return combinedPath, true
+	}
+
+	return combinedPath, false
+}
+
+func (a *ApiDefinitionLoader) CompilePathSpec(paths []string, specType UrlStatus) []UrlSpec {
+
+	// transform a configuration URL into an array of URLSpecs
+	// This way we can iterate the whole array once, on match we break with status
+	apiLangIdsRegex, _ := regexp.Compile("{(.*?)}")
+	thisUrlSpec := []UrlSpec{}
+
+	for _, stringSpec := range paths {
+		asRegexStr := apiLangIdsRegex.ReplaceAllString(stringSpec, "(.*?)")
+		asRegex, _ := regexp.Compile(asRegexStr)
+
+		newSpec := UrlSpec{}
+		newSpec.Spec = asRegex
+		newSpec.Status = specType
+		thisUrlSpec = append(thisUrlSpec, newSpec)
+	}
+
+	return thisUrlSpec
+}
+
+func (a *ApiSpec) IsUrlAllowedAndIgnored(url string, RxPaths []UrlSpec, WhiteListStatus bool) (bool, bool) {
+	// Check if ignored
+	for _, v := range RxPaths {
+		match := v.Spec.MatchString(url)
+		if match {
+			if v.Status == Ignored {
+				// Let it pass, and do not check auth
+				return true, true
+			} else if v.Status == BlackList {
+				// Matched  a blacklist URL, disallow access and filter (irrelevant here)
+				return false, false
+			} else if v.Status == WhiteList {
+				// Matched whitelist, allow request but filter
+				return true, false
+			} else {
+				// Should not occur, something has gone wrong
+				log.Error("URL Status was not one of Ignored, Blacklist or WhiteList! Blocking.")
+				return false, false
+			}
+		}
+	}
+
+	// Nothing matched - should we still let it through?
+	if WhiteListStatus {
+		// We have a whitelist, nothing gets through unless specifically defined
+		return false, false
+	} else {
+		// No whitelist, but also not in any of the other lists, let it through and filter
+		return true, false
+	}
+}
+
+func (a *ApiSpec) getVersionFromRequest(r *http.Request) string {
+	if a.ApiDefinition.VersionDefinition.Location == "header" {
+		versionHeaderVal := r.Header.Get(a.ApiDefinition.VersionDefinition.Key)
+		if versionHeaderVal != "" {
+			return versionHeaderVal
+		} else {
+			return ""
+		}
+	} else if a.ApiDefinition.VersionDefinition.Location == "url" {
+		// TODO - URL MATCH
+		return ""
+	} else {
+		return ""
+	}
+
+	return ""
+}
+
+func (a *ApiSpec) IsThisApiVersionExpired(versionDef VersionInfo) bool {
+	// Never expires
+	if versionDef.Expires == "-1" {
+		return false
+	}
+
+	// otherwise - calculate the time
+	t, err := time.Parse("2006-01-02 15:04", versionDef.Expires)
+	if err != nil {
+		log.Error("Could not parse expiry date for API, dissallow")
+		log.Error(err)
+		return true
+	} else {
+		remaining := time.Since(t)
+		if remaining < 0 {
+			// It's in the future, keep going
+			return false
+		} else {
+			// It's in the past, expire
+			return true
+		}
+	}
+}
+
+func (a *ApiSpec) IsRequestValid(r *http.Request) (bool, RequestStatus) {
+	versionMetaData, versionPaths, whiteListStatus, stat := a.GetVersionData(r)
+
+	// Screwed up version info - fail and pass through
+	if stat != StatusOk {
+		return false, stat
+	}
+
+	// Is the API version expired?
+	if a.IsThisApiVersionExpired(versionMetaData) == true {
+		// Expired - fail
+		return false, VersionExpired
+	}
+
+	// not expired, let's check path info
+	allowURL, ignore := a.IsUrlAllowedAndIgnored(r.URL.Path, versionPaths, whiteListStatus)
+	if !allowURL {
+		return false, EndPointNotAllowed
+	}
+
+	if ignore {
+		return true, StatusOkAndIgnore
+	}
+
+	return true, StatusOk
+
+}
+
+func (a *ApiSpec) GetVersionData(r *http.Request) (VersionInfo, []UrlSpec, bool, RequestStatus) {
+	var thisVersion = VersionInfo{}
+	var versionKey string
+	var versionRxPaths = []UrlSpec{}
+	var versionWLStatus bool
+
+	// Are we versioned?
+	if a.ApiDefinition.VersionData.NotVersioned {
+		// Get the first one in the list
+		for k, v := range a.ApiDefinition.VersionData.Versions {
+			versionKey = k
+			thisVersion = v
+			break
+		}
+	} else {
+		// Extract Version Info
+		versionKey = a.getVersionFromRequest(r)
+		if versionKey == "" {
+			return thisVersion, versionRxPaths, versionWLStatus, VersionNotFound
+		}
+
+		// Load Version Data - General
+		var ok bool
+		thisVersion, ok =  a.ApiDefinition.VersionData.Versions[versionKey]
+		if !ok {
+			return thisVersion, versionRxPaths, versionWLStatus, VersionDoesNotExist
+		}
+
+
+		// Load path data and whitelist data for version
+		RxPaths, rxOk := a.RxPaths[versionKey]
+		WhiteListStatus, wlOk := a.WhiteListEnabled[versionKey]
+
+		if !rxOk {
+			log.Error("no RX Paths found for version")
+			log.Error(versionKey)
+			return thisVersion, versionRxPaths, versionWLStatus, VersionDoesNotExist
+		}
+
+		if !wlOk {
+			log.Error("No whitelist data found")
+			return thisVersion, versionRxPaths, versionWLStatus, VersionWhiteListStatusNotFound
+		}
+
+		versionRxPaths = RxPaths
+		versionWLStatus = WhiteListStatus
+
+		return thisVersion, versionRxPaths, versionWLStatus, StatusOk
+	}
+
+	return thisVersion, versionRxPaths, versionWLStatus, GeneralFailure
+}
+
+/*
+
+Get /api/blah version=1
+Extract Version Info
+v1
+Load Version Data for v1
+Pull RXPaths for version info
+Pull White-list bool for versionInfo
+Compare route to RxPaths
+Route matched? -> Check status (White, black ignore)
+Route not matched -> Check Whitelist status -> action
+*/
---
+++ tests/api_definition_test.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"testing"
+	"net/url"
+	"net/http"
+)
+
+func createDefinition() ApiSpec {
+	var thisDef = ApiDefinition{}
+	var v1 = VersionInfo{}
+	var thisSpec = ApiSpec{}
+	var thisLoader = ApiDefinitionLoader{}
+
+	thisDef.Name = "Test API"
+	thisDef.VersionDefinition.Key = "version"
+	thisDef.VersionDefinition.Location = "header"
+	thisDef.VersionData.NotVersioned = false
+
+	v1.Name = "v1"
+	v1.Auth.AuthHeaderName = "authorization"
+	v1.Expires = "2006-01-02 15:04" //TODO: Change this
+	v1.Proxy.ListenPath = "/v1"
+	v1.Proxy.TargetUrl = "http://lonelycode.com"
+	v1.Paths.Ignored = []string{"/v1/ignored/noregex", "/v1/ignored/with_id/{id}"}
+	v1.Paths.BlackList = []string{"v1/disallowed/blacklist/literal", "v1/disallowed/blacklist/{id}"}
+	v1.Paths.WhiteList = []string{"v1/disallowed/whitelist/literal", "v1/disallowed/whitelist/{id}"}
+
+	thisDef.VersionData.Versions = make(map[string]VersionInfo)
+	thisDef.VersionData.Versions[v1.Name] = v1
+
+	thisSpec.ApiDefinition = thisDef
+
+	thisSpec.RxPaths = make(map[string][]UrlSpec)
+	thisSpec.WhiteListEnabled = make(map[string]bool)
+
+	pathSpecs, whiteListSpecs := thisLoader.getPathSpecs(v1)
+	thisSpec.RxPaths[v1.Name] = pathSpecs
+
+	thisSpec.WhiteListEnabled[v1.Name] = whiteListSpecs
+
+	return thisSpec
+}
+
+func createNonExpiringDefinition() ApiSpec {
+	var thisDef = ApiDefinition{}
+	var v1 = VersionInfo{}
+	var thisSpec = ApiSpec{}
+	var thisLoader = ApiDefinitionLoader{}
+
+	thisDef.Name = "Test API"
+	thisDef.VersionDefinition.Key = "version"
+	thisDef.VersionDefinition.Location = "header"
+	thisDef.VersionData.NotVersioned = false
+
+	v1.Name = "v1"
+	v1.Auth.AuthHeaderName = "authorization"
+	v1.Expires = "3000-01-02 15:04" //TODO: Change this
+	v1.Proxy.ListenPath = "/v1"
+	v1.Proxy.TargetUrl = "http://lonelycode.com"
+	v1.Paths.Ignored = []string{"/v1/ignored/noregex", "/v1/ignored/with_id/{id}"}
+	v1.Paths.BlackList = []string{"v1/disallowed/blacklist/literal", "v1/disallowed/blacklist/{id}"}
+	v1.Paths.WhiteList = []string{"v1/allowed/whitelist/literal", "v1/allowed/whitelist/{id}"}
+
+	thisDef.VersionData.Versions = make(map[string]VersionInfo)
+	thisDef.VersionData.Versions[v1.Name] = v1
+
+	thisSpec.ApiDefinition = thisDef
+
+	thisSpec.RxPaths = make(map[string][]UrlSpec)
+	thisSpec.WhiteListEnabled = make(map[string]bool)
+
+	pathSpecs, whiteListSpecs := thisLoader.getPathSpecs(v1)
+	thisSpec.RxPaths[v1.Name] = pathSpecs
+
+	thisSpec.WhiteListEnabled[v1.Name] = whiteListSpecs
+
+	return thisSpec
+}
+
+func createNonExpiringMultiDefinition() ApiSpec {
+	var thisDef = ApiDefinition{}
+	var v1 = VersionInfo{}
+	var v2 = VersionInfo{}
+	var thisSpec = ApiSpec{}
+	var thisLoader = ApiDefinitionLoader{}
+
+	thisDef.Name = "Test API"
+	thisDef.VersionDefinition.Key = "version"
+	thisDef.VersionDefinition.Location = "header"
+	thisDef.VersionData.NotVersioned = false
+
+	v1.Name = "v1"
+	v1.Auth.AuthHeaderName = "authorization"
+	v1.Expires = "3000-01-02 15:04"
+	v1.Proxy.ListenPath = "/v1"
+	v1.Proxy.TargetUrl = "http://lonelycode.com"
+	v1.Paths.Ignored = []string{"/v1/ignored/noregex", "/v1/ignored/with_id/{id}"}
+	v1.Paths.BlackList = []string{"v1/disallowed/blacklist/literal", "v1/disallowed/blacklist/{id}"}
+	v1.Paths.WhiteList = []string{"v1/allowed/whitelist/literal", "v1/allowed/whitelist/{id}"}
+
+	v2.Name = "v2"
+	v2.Auth.AuthHeaderName = "authorization"
+	v2.Expires = "3000-01-02 15:04"
+	v2.Proxy.ListenPath = "/v2"
+	v2.Proxy.TargetUrl = "http://lonelycode.com"
+	v2.Paths.Ignored = []string{"/v1/ignored/noregex", "/v1/ignored/with_id/{id}"}
+	v2.Paths.BlackList = []string{"v1/disallowed/blacklist/literal"}
+	v2.Paths.WhiteList = []string{}
+
+	thisDef.VersionData.Versions = make(map[string]VersionInfo)
+	thisDef.VersionData.Versions[v1.Name] = v1
+	thisDef.VersionData.Versions[v2.Name] = v2
+
+	thisSpec.ApiDefinition = thisDef
+
+	thisSpec.RxPaths = make(map[string][]UrlSpec)
+	thisSpec.WhiteListEnabled = make(map[string]bool)
+
+	pathSpecs, whiteListSpecs := thisLoader.getPathSpecs(v1)
+	pathSpecs2, whiteListSpecs2 := thisLoader.getPathSpecs(v2)
+
+	thisSpec.RxPaths[v1.Name] = pathSpecs
+	thisSpec.WhiteListEnabled[v1.Name] = whiteListSpecs
+
+	thisSpec.RxPaths[v2.Name] = pathSpecs2
+	thisSpec.WhiteListEnabled[v2.Name] = whiteListSpecs2
+
+	return thisSpec
+}
+
+func TestExpiredRequest(t *testing.T) {
+	uri := "/v1/bananaphone"
+	method := "GET"
+
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	req.Header.Add("version", "v1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	thisSpec := createDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as expiry date is in the past!")
+	}
+
+	if status != VersionExpired {
+		t.Error("Request should return expired status!")
+		t.Error(status)
+	}
+}
+
+func TestMissingVersion(t *testing.T) {
+	uri := "/v1/bananaphone"
+	method := "GET"
+
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	thisSpec := createDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as there is no version number!")
+	}
+
+	if status != VersionNotFound {
+		t.Error("Request should return version not found status!")
+		t.Error(status)
+	}
+}
+
+func TestWrongVersion(t *testing.T) {
+	uri := "/v1/bananaphone"
+	method := "GET"
+
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v2")
+
+	thisSpec := createDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as version number is wrong!")
+	}
+
+	if status != VersionDoesNotExist {
+		t.Error("Request should return version doesn't exist status!")
+		t.Error(status)
+	}
+}
+
+func TestBlacklistLinks(t *testing.T) {
+	uri := "v1/disallowed/blacklist/literal"
+	method := "GET"
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v1")
+
+	thisSpec := createNonExpiringDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as URL is blacklisted!")
+	}
+
+	if status != EndPointNotAllowed {
+		t.Error("Request should return endpoint disallowed status!")
+		t.Error(status)
+	}
+
+	uri = "v1/disallowed/blacklist/abacab12345"
+	method = "GET"
+	param = make(url.Values)
+	req, err = http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v1")
+
+	ok, status = thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as URL (with dynamic ID) is blacklisted!")
+	}
+
+	if status != EndPointNotAllowed {
+		t.Error("Request should return endpoint disallowed status for regex blacklists too!")
+		t.Error(status)
+	}
+}
+
+func TestWhiteLIstLinks(t *testing.T) {
+	uri := "v1/allowed/whitelist/literal"
+	method := "GET"
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v1")
+
+	thisSpec := createNonExpiringDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok != true {
+		t.Error("Request should be OK as URL is whitelisted!")
+	}
+
+	if status != StatusOk {
+		t.Error("Request should return StatusOk!")
+		t.Error(status)
+	}
+
+	uri = "v1/allowed/whitelist/12345abans"
+	method = "GET"
+	param = make(url.Values)
+	req, err = http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v1")
+
+	ok, status = thisSpec.IsRequestValid(req)
+	if ok != true {
+		t.Error("Request should be OK as URL is whitelisted (regex)!")
+	}
+
+	if status != StatusOk {
+		t.Error("Regex whitelist Request should return StatusOk!")
+		t.Error(status)
+	}
+}
+
+func TestWhiteListBlock(t *testing.T) {
+	uri := "v1/allowed/bananaphone"
+	method := "GET"
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v1")
+
+	thisSpec := createNonExpiringDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as things not in whitelist should be rejected!")
+	}
+
+	if status != EndPointNotAllowed {
+		t.Error("Request should return EndPointNotAllowed!")
+		t.Error(status)
+	}
+}
+
+func TestIgnored(t *testing.T) {
+	uri := "/v1/ignored/noregex"
+	method := "GET"
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v1")
+
+	thisSpec := createNonExpiringDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok != true {
+		t.Error("Request should pass, URL is ignored")
+	}
+
+	if status != StatusOkAndIgnore {
+		t.Error("Request should return StatusOkAndIgnore!")
+		t.Error(status)
+	}
+}
+
+
+func TestBlacklistLinksMulti(t *testing.T) {
+	uri := "v1/disallowed/blacklist/literal"
+	method := "GET"
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v2")
+
+	thisSpec := createNonExpiringMultiDefinition()
+
+	ok, status := thisSpec.IsRequestValid(req)
+	if ok == true {
+		t.Error("Request should fail as URL is blacklisted!")
+	}
+
+	if status != EndPointNotAllowed {
+		t.Error("Request should return endpoint disallowed status!")
+		t.Error(status)
+	}
+
+	uri = "v1/disallowed/blacklist/abacab12345"
+	method = "GET"
+	param = make(url.Values)
+	req, err = http.NewRequest(method, uri+param.Encode(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("version", "v2")
+
+	ok, status = thisSpec.IsRequestValid(req)
+	if ok != true {
+		t.Error("Request should be OK as in v2 this URL is not blacklisted")
+		t.Error(thisSpec.RxPaths["v2"])
+	}
+
+	if status != StatusOk {
+		t.Error("Request should return StatusOK as URL not blacklisted!")
+		t.Error(status)
+	}
+}
