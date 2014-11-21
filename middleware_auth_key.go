2c18dabeb41794a8a330ba21c7986cc3e511ac9f
Martin Buhr <martin@jive.ly>
2014-11-21 15:50:14 +0000

    Moved some middleware data back into main

--- middleware_auth_key.go
+++ middleware_auth_key.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/context"
-	"github.com/mitchellh/mapstructure"
 )
 
 // KeyExists will check if the key being used to access the API is in the request data,
@@ -15,32 +14,17 @@ type AuthKey struct {
 	TykMiddleware
 }
 
-type AuthKeyConfiguration struct {
-	Auth struct {
-		AuthHeaderName string `mapstructure:"auth_header_name" bson:"auth_header_name" json:"auth_header_name"`
-	} `mapstructure:"auth" bson:"auth" json:"auth"`
-}
-
 func (k AuthKey) New() {}
 
 // GetConfig retrieves the configuration from the API config
 func (k *AuthKey) GetConfig() (interface{}, error) {
-	var thisModuleConfig AuthKeyConfiguration
-
-	err := mapstructure.Decode(k.TykMiddleware.Spec.APIDefinition.RawData, &thisModuleConfig)
-	if err != nil {
-		log.Error(err)
-		return nil, err
-	}
-
-	return thisModuleConfig, nil
+	return k.TykMiddleware.Spec.APIDefinition.Auth, nil
 }
 
 func (k *AuthKey) ProcessRequest(w http.ResponseWriter, r *http.Request, configuration interface{}) (error, int) {
-	var thisConfig AuthKeyConfiguration
-	thisConfig = configuration.(AuthKeyConfiguration)
+	thisConfig := k.TykMiddleware.Spec.APIDefinition.Auth
 
-	authHeaderValue := r.Header.Get(thisConfig.Auth.AuthHeaderName)
+	authHeaderValue := r.Header.Get(thisConfig.AuthHeaderName)
 	if authHeaderValue == "" {
 		// No header value, fail
 		log.WithFields(logrus.Fields{
