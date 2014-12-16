2084b8f3cd163631783426a3565b007b13241bf5
Martin Buhr <martin@jive.ly>
2014-12-16 16:47:11 +0000

    Basic auth - more robust tests added

--- middleware_basic_auth_test.go
+++ middleware_basic_auth_test.go
@@ -116,3 +116,218 @@ func TestBasicAuthSession(t *testing.T) {
 		t.Error("Initial request failed with non-200 code, should have gone through!: \n", recorder.Code)
 	}
 }
+
+func TestBasicAuthBadFormatting(t *testing.T) {
+	spec := createDefinitionFromString(basicAuthDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	spec.Init(&redisStore, &redisStore, healthStore)
+	thisSession := createBasicAuthSession()
+	username := "4321"
+	password := "TEST"
+	// Basic auth sessions are stored as {org-id}{username}, so we need to append it here when we create the session.
+	spec.SessionManager.UpdateSession("default4321", thisSession, 60)
+
+	to_encode := strings.Join([]string{username, password}, "-")
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(to_encode))
+	uri := "/"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", encodedPass))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain := getBasicAuthChain(spec)
+	chain.ServeHTTP(recorder, req)
+
+	if recorder.Code == 200 {
+		t.Error("Request should have failed and returned non-200 code!: \n", recorder.Code)
+	}
+
+	if recorder.Code != 400 {
+		t.Error("Request didn't return 400 code!: \n", recorder.Code)
+	}
+}
+
+func TestBasicAuthBadData(t *testing.T) {
+	spec := createDefinitionFromString(basicAuthDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	spec.Init(&redisStore, &redisStore, healthStore)
+	thisSession := createBasicAuthSession()
+	// Basic auth sessions are stored as {org-id}{username}, so we need to append it here when we create the session.
+	spec.SessionManager.UpdateSession("default4321", thisSession, 60)
+
+	to_encode := "ldhflsdflksjfdlksjflksdjlskdjflkjsfd:::jhdsgfkjahsgdkhasdgjhgasdjhads:::aksdakjsdh:adskasdkjhasdkjhad-asdads"
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(to_encode))
+	uri := "/"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", encodedPass))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain := getBasicAuthChain(spec)
+	chain.ServeHTTP(recorder, req)
+
+	if recorder.Code == 200 {
+		t.Error("Request should have failed and returned non-200 code!: \n", recorder.Code)
+	}
+
+	if recorder.Code != 400 {
+		t.Error("Request didn't return 400 code!: \n", recorder.Code)
+	}
+}
+
+func TestBasicAuthBadOverFormatting(t *testing.T) {
+	spec := createDefinitionFromString(basicAuthDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	spec.Init(&redisStore, &redisStore, healthStore)
+	thisSession := createBasicAuthSession()
+	username := "4321"
+	password := "TEST"
+	// Basic auth sessions are stored as {org-id}{username}, so we need to append it here when we create the session.
+	spec.SessionManager.UpdateSession("default4321", thisSession, 60)
+
+	to_encode := strings.Join([]string{username, password, "banana"}, ":")
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(to_encode))
+	uri := "/"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", encodedPass))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain := getBasicAuthChain(spec)
+	chain.ServeHTTP(recorder, req)
+
+	if recorder.Code == 200 {
+		t.Error("Request should have failed and returned non-200 code!: \n", recorder.Code)
+	}
+
+	if recorder.Code != 400 {
+		t.Error("Request didn't return 400 code!: \n", recorder.Code)
+	}
+}
+
+func TestBasicAuthWrongUser(t *testing.T) {
+	spec := createDefinitionFromString(basicAuthDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	spec.Init(&redisStore, &redisStore, healthStore)
+	thisSession := createBasicAuthSession()
+	password := "TEST"
+	// Basic auth sessions are stored as {org-id}{username}, so we need to append it here when we create the session.
+	spec.SessionManager.UpdateSession("default4321", thisSession, 60)
+
+	to_encode := strings.Join([]string{"1234", password}, ":")
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(to_encode))
+	uri := "/"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", encodedPass))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain := getBasicAuthChain(spec)
+	chain.ServeHTTP(recorder, req)
+
+	if recorder.Code == 200 {
+		t.Error("Request should have failed and returned non-200 code!: \n", recorder.Code)
+	}
+
+	if recorder.Code != 403 {
+		t.Error("Request should have returned 403 code!: \n", recorder.Code)
+	}
+}
+
+func TestBasicMissingHeader(t *testing.T) {
+	spec := createDefinitionFromString(basicAuthDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	spec.Init(&redisStore, &redisStore, healthStore)
+	thisSession := createBasicAuthSession()
+
+	// Basic auth sessions are stored as {org-id}{username}, so we need to append it here when we create the session.
+	spec.SessionManager.UpdateSession("default4321", thisSession, 60)
+
+	uri := "/"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain := getBasicAuthChain(spec)
+	chain.ServeHTTP(recorder, req)
+
+	if recorder.Code == 200 {
+		t.Error("Request should have failed and returned non-200 code!: \n", recorder.Code)
+	}
+
+	if recorder.Code != 401 {
+		t.Error("Request should have returned 401 code!: \n", recorder.Code)
+	}
+}
+
+func TestBasicAuthWrongPassword(t *testing.T) {
+	spec := createDefinitionFromString(basicAuthDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	spec.Init(&redisStore, &redisStore, healthStore)
+	thisSession := createBasicAuthSession()
+	username := "4321"
+
+	// Basic auth sessions are stored as {org-id}{username}, so we need to append it here when we create the session.
+	spec.SessionManager.UpdateSession("default4321", thisSession, 60)
+
+	to_encode := strings.Join([]string{username, "WRONGPASSTEST"}, ":")
+	encodedPass := base64.StdEncoding.EncodeToString([]byte(to_encode))
+	uri := "/"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+	param := make(url.Values)
+	req, err := http.NewRequest(method, uri+param.Encode(), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", encodedPass))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain := getBasicAuthChain(spec)
+	chain.ServeHTTP(recorder, req)
+
+	if recorder.Code == 200 {
+		t.Error("Request should have failed and returned non-200 code!: \n", recorder.Code)
+	}
+
+	if recorder.Code != 403 {
+		t.Error("Request should have returned 403 code!: \n", recorder.Code)
+	}
+}
