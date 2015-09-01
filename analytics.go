0b245033961e00e93e87bf11f251c418960989a3
Martin Buhr <martin@tyk.io>
2015-09-01 05:23:24 -0400

    Added RPC tag

--- analytics.go
+++ analytics.go
@@ -83,6 +83,11 @@ func (r RedisAnalyticsHandler) RecordHit(thisRecord AnalyticsRecord) error {
 	// If we are obfuscating API Keys, store the hashed representation (config check handled in hashing function)
 	thisRecord.APIKey = publicHash(thisRecord.APIKey)
 
+	if config.SlaveOptions.UseRPC {
+		// Extend tag list to include this data so wecan segment by node if necessary
+		thisRecord.Tags = append([]string{"tyk-hybrid-rpc"})
+	}
+
 	if config.DBAppConfOptions.NodeIsSegmented {
 		// Extend tag list to include this data so wecan segment by node if necessary
 		thisRecord.Tags = append(thisRecord.Tags, config.DBAppConfOptions.Tags...)
