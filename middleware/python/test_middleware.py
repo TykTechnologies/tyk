da7a38417f9082a4cc9e9133349496de17a67f38
Matias Insaurralde <matias@insaurral.de>
2016-07-25 15:56:01 -0400

    Triggering an event from Python middleware

--- middleware/python/test_middleware.py
+++ middleware/python/test_middleware.py
@@ -5,6 +5,7 @@ from tyk.gateway import TykGateway as tyk
 def AddSomeHeader(request, session):
     request['SetHeaders']['SomeHeader'] = 'python'
     tyk.store_data( "cool_key", "cool_value", 300 )
+    tyk.trigger_event( "a_middleware_event", "additional_data" )
     return request, session
 
 def NotARealHandler():
