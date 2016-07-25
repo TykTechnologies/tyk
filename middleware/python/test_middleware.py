1040e17e006fef93d990a6212e8be06e0a818c2f
Matias Insaurralde <matias@insaurral.de>
2016-07-25 12:28:41 -0400

    Update middleware syntax with "TykGateway"

--- middleware/python/test_middleware.py
+++ middleware/python/test_middleware.py
@@ -1,9 +1,10 @@
 from tyk.decorators import *
+from tyk.gateway import TykGateway as tyk
 
 @Pre
 def AddSomeHeader(request, session):
-    # request['Body'] = 'tyk=python'
-    request['SetHeaders']['SomeHeader'] = 'python2'
+    request['SetHeaders']['SomeHeader'] = 'python'
+    tyk.store_data( "cool_key", "cool_value", 300 )
     return request, session
 
 def NotARealHandler():
