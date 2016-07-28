21b1647454ff9976029a9bc1113e06956393e161
Matias Insaurralde <matias@insaurral.de>
2016-07-28 03:02:03 -0400

    Updating test_middleware to print a session field

--- middleware/python/test_middleware.py
+++ middleware/python/test_middleware.py
@@ -2,18 +2,24 @@ from tyk.decorators import *
 from tyk.gateway import TykGateway as tyk
 
 @Pre
-def ModifyBody(request, session, spec):
-    request['SetHeaders']['SomeHeader'] = 'python'
+def ModifyRequest(request, session, spec):
+    # request['SetHeaders']['SomeHeader'] = 'python'
     tyk.store_data( "cool_key", "cool_value", 300 )
     tyk.trigger_event( "a_middleware_event", "additional_data" )
-    request['Body'] = "modified_body=1"
+
+    # request['Body'] = "modified_body=1"
+
+    request.add_header("custom_header", "custom_value")
+    request.delete_header("Accept")
+
     return request, session
 
 @Post
-def AddSomeHeader2(request, session, spec):
-    print("middleware#AddSomeheader2")
-    print("request = ", request)
-    print("session = ", session)
+def AddSomeHeader(request, session, spec):
+    print("session = ", session, session.__dict__)
+
+    print(session.allowance)
+
     return request, session
 
 def NotARealHandler():
