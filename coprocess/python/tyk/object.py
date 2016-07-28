2b363ac6e399d1e28bf9922cd5b0ce668e7a6205
Matias Insaurralde <matias@insaurral.de>
2016-07-28 02:59:45 -0400

    Wrapping session & request

--- coprocess/python/tyk/object.py
+++ coprocess/python/tyk/object.py
@@ -1,14 +1,23 @@
 import json
 
+from tyk.session import TykSession
+from tyk.request import TykCoProcessRequest
+
 class TykCoProcessObject:
     def __init__(self, object_json):
         self.object = json.loads(object_json)
-        self.request = self.object['request']
-        self.session = self.object['session']
+
+        self.request = TykCoProcessRequest(self.object['request'])
+        self.session = TykSession(self.object['session'])
+
         self.spec = self.object['spec']
         self.hook_type = self.object['hook_type']
 
+    def add_header(self, key, value):
+        self.request.SetHeaders[key] = value
+
     def dump(self):
-        self.object['request'] = self.request
-        self.object['session'] = self.session
+        self.object['request'] = self.request.__dict__
+        self.object['session'] = self.session.__dict__
+        
         return json.dumps(self.object)
