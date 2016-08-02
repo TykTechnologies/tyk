396bc0d81eb01872c5c85b7434f25b8567011d38
Matias Insaurralde <matias@insaurral.de>
2016-08-02 13:22:58 -0400

    Updating test_auth_middleware

--- middleware/python/test_auth_middleware.py
+++ middleware/python/test_auth_middleware.py
@@ -4,11 +4,21 @@ from gateway import TykGateway as tyk
 @CustomKeyCheck
 def MyKeyCheck(request, session, metadata, spec):
     print("Running MyKeyCheck?")
+
     print("request:", request)
     print("session:", session)
     print("spec:", spec)
-    session.rate = 1000
-    session.per = 1
-    print("session.__dict__ = ", session.__dict__)
-    metadata['token'] = "mytoken"
+
+    valid_token = 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d'
+    request_token = request.get_header('Authorization')
+
+    if request_token == valid_token:
+        print("Token is OK")
+        session.rate = 1000
+        session.per = 1
+        metadata['token'] = "mytoken"
+    else:
+        print("Token is WRONG")
+        request.return_overrides = { 'response_code': 401, 'response_error': 'Not authorized (by the Python middleware)' }
+
     return request, session, metadata
