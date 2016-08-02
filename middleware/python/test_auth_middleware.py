659416966b42eee0fda3e189ace5c588bf48a6e3
Matias Insaurralde <matias@insaurral.de>
2016-08-02 12:58:04 -0400

    Setting token from middleware

--- middleware/python/test_auth_middleware.py
+++ middleware/python/test_auth_middleware.py
@@ -2,9 +2,13 @@ from tyk.decorators import *
 from gateway import TykGateway as tyk
 
 @CustomKeyCheck
-def MyKeyCheck(request, session, spec):
+def MyKeyCheck(request, session, metadata, spec):
     print("Running MyKeyCheck?")
     print("request:", request)
     print("session:", session)
     print("spec:", spec)
-    return request, session
+    session.rate = 1000
+    session.per = 1
+    print("session.__dict__ = ", session.__dict__)
+    metadata['token'] = "mytoken"
+    return request, session, metadata
