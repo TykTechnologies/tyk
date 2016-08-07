3090f80fb75e28d76a2a9f5e25c507d095a695c8
Matias Insaurralde <matias@insaurral.de>
2016-08-07 02:53:31 -0400

    Use float in session fields

--- middleware/python/test_auth_middleware.py
+++ middleware/python/test_auth_middleware.py
@@ -12,10 +12,12 @@ def MyKeyCheck(request, session, metadata, spec):
     valid_token = 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d'
     request_token = request.get_header('Authorization')
 
+    print("(python) request_token =", request_token)
+
     if request_token == valid_token:
         print("Token is OK")
-        session.rate = 1000
-        session.per = 1
+        session.rate = 1000.0
+        session.per = 1.0
         metadata['token'] = "mytoken"
     else:
         print("Token is WRONG")
