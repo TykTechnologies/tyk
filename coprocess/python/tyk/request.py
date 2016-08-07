00d87354d0c4aade5dc2be855c4a5396c6d005f0
Matias Insaurralde <matias@insaurral.de>
2016-08-06 20:45:24 -0400

    Updating request module to use tuples (for delete_headers and delete_params)

--- coprocess/python/tyk/request.py
+++ coprocess/python/tyk/request.py
@@ -2,15 +2,15 @@ class TykCoProcessRequest:
     def __init__(self, request):
         self.__dict__ = request
     def add_header(self, key, value):
-        self.SetHeaders[key] = value
+        self.set_headers[key] = value
     def delete_header(self, key):
-        self.DeleteHeaders.append(key)
+        self.delete_headers = self.delete_headers + (key,)
     def add_param(self, key, value):
-        self.AddParams[key] = value
+        self.add_params[key] = value
     def delete_param(self, key):
-        self.DeleteParams.append(key)
+        self.delete_params = self.delete_params + (key,)
     def get_header(self, key):
-        if key in self.Headers:
-            return self.Headers[key][0]
+        if key in self.headers:
+            return self.headers[key][0]
         else:
             return None
