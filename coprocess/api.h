909497caef34f75a087f13c3e70867cf4566c81a
Matias Insaurralde <matias@insaurral.de>
2016-08-06 20:38:30 -0400

    Implementing a C struct to hold msgp object information (data pointer & length)

--- coprocess/api.h
+++ coprocess/api.h
@@ -1,9 +1,15 @@
 #ifndef TYK_COPROCESS_API
 #define TYK_COPROCESS_API
 
+struct CoProcessObject {
+  void* p_data;
+  int length;
+};
+
 extern void TykStoreData(char* key, char* value, int ttl);
 extern char* TykGetData(char* key);
 extern void TykTriggerEvent(char* event_name, char* payload);
 
 extern void CoProcess_Log(char *msg, char *level);
+
 #endif
