2b2c20468f01f2ecaa0d2129ca36478ba72ca945
Matias Insaurralde <matias@insaurral.de>
2016-08-09 02:41:21 -0400

    Updating test middleware

--- middleware/python/test_middleware.py
+++ middleware/python/test_middleware.py
@@ -2,14 +2,24 @@ from tyk.decorators import *
 from gateway import TykGateway as tyk
 
 @Pre
-def ModifyRequest(request, session, spec):
-    print("about to store cool_key!")
-    tyk.store_data( "cool_key", "not_so_cool", 300 )
-    print("about to fetch cool_key")
-    val = tyk.get_data("cool_key")
-    print("val = ", val)
-    tyk.store_data( "expire_soon", "not_so_cool", 5 )
+def CallTykAPI(request, session, spec):
+    print("test_middleware: CallTykAPI")
+
+    print("test_middleware: tyk.store_data")
+    tyk.store_data( "cool_key", "cool_value", 300 )
+
+    print("test_middleware: tyk.get_data")
+    print("test_middleware: ", tyk.get_data("cool_key") )
+
+    print("test_middleware: tyk.trigger_event")
     tyk.trigger_event( "a_middleware_event", "additional_data" )
+
+    # TODO: what to do when the function doesn't return (or returns wrong values)?
+    return request, session
+
+@Pre
+def ModifyRequest(request, session, spec):
+    print("test_middleware: ModifyRequest")
 
     request.add_header("custom_header", "custom_value")
     request.delete_header("Accept")
@@ -20,15 +30,16 @@ def ModifyRequest(request, session, spec):
     return request, session
 
 @Post
-def AddSomeHeader(request, session, spec):
-    print("session = ", session, session.__dict__)
-    print(session.allowance)
+def PrintSession(request, session, spec):
+    print("test_middleware: PrintSession")
+    print("session = ", session, session.__object__)
+    # print(session.allowance)
 
     return request, session
 
 @PostKeyAuth
 def Something(request, session, spec):
-    print("post key auth?")
+    print("test_middleware: PostKeyAuth")
 
     return request, session
 
