348a4e77089a4a82eac4c15cd575c3d2b0060826
Matias Insaurralde <matias@insaurral.de>
2016-08-10 05:24:37 -0400

    Simplified middleware

---
+++ middleware/python/my_middleware.py
@@ -0,0 +1,17 @@
+from tyk.decorators import *
+from gateway import TykGateway as tyk
+
+@Pre
+def MyPreMiddleware(request, session, spec):
+    print("my_middleware: MyPreMiddleware")
+    return request, session
+
+@Pre
+def AnotherPreMiddleware(request, session, spec):
+    print("my_middleware: AnotherPreMiddleware")
+    return request, session
+
+@Post
+def MyPostMiddleware(request, session, spec):
+    print("my_middleware: MyPostMiddleware")
+    return request, session
