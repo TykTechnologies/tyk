46e60df592484d5b7fc44048013cc1b60d8bff83
Matias Insaurralde <matias@insaurral.de>
2016-08-12 02:03:45 -0400

    Removing previous key check middleware

--- middleware/python/my_middleware.py
+++ middleware/python/my_middleware.py
@@ -15,28 +15,4 @@ def AnotherPreMiddleware(request, session, spec):
 def MyPostMiddleware(request, session, spec):
     print("my_middleware: MyPostMiddleware")
     return request, session
-
-@CustomKeyCheck
-def MyAuthCheck(request, session, metadata, spec):
-    print("my_middleware.py: MyAuthCheck")
-
-    print("test_auth_middleware - Request:", request)
-    print("test_auth_middleware - Session:", session)
-    print("test_auth_middleware - Spec:", spec)
-
-    valid_token = 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d'
-    request_token = request.get_header('Authorization')
-
-    print("test_auth_middleware - Request Token:", request_token)
-
-    if request_token == valid_token:
-        print("test_auth_middleware: Valid token")
-        session.object.rate = 1000.0
-        session.object.per = 1.0
-        metadata['token'] = 'mytoken'
-    else:
-        print("test_auth_middleware: Invalid token")
-        request.__object__.return_overrides.response_code = 401
-        request.__object__.return_overrides.response_error = 'Not authorized (Python middleware)'
-
-    return request, session, metadata
+    
