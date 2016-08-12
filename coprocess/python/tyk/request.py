12380599e986a6de452eb84a23c7921e4f9b85a2
Matias Insaurralde <matias@insaurral.de>
2016-08-11 20:43:56 -0400

    Updating test middleware

--- coprocess/python/tyk/request.py
+++ coprocess/python/tyk/request.py
@@ -1,16 +1,16 @@
 class TykCoProcessRequest():
     def __init__(self, request):
-        self.__object__ = request
+        self.object = request
     def add_header(self, key, value):
-        self.__object__.set_headers[key] = value
+        self.object.set_headers[key] = value
     def delete_header(self, key):
-        self.__object__.delete_headers.append(key)
+        self.object.delete_headers.append(key)
     def add_param(self, key, value):
-        self.__object__.add_params[key] = value
+        self.object.add_params[key] = value
     def delete_param(self, key):
-        self.__object__.delete_params.append(key)
+        self.object.delete_params.append(key)
     def get_header(self, key):
-        if key in self.__object__.headers:
-            return self.__object__.headers[key]
+        if key in self.object.headers:
+            return self.object.headers[key]
         else:
             return None
--- middleware/python/test_auth_middleware.py
+++ middleware/python/test_auth_middleware.py
@@ -1,8 +1,10 @@
 from tyk.decorators import *
 from gateway import TykGateway as tyk
 
+from tyk.session import AccessSpec, AccessDefinition, BasicAuthData, JWTData, Monitor
+
 @CustomKeyCheck
-def MyKeyCheck(request, session, metadata, spec):
+def MyAuthCheck(request, session, metadata, spec):
     print("test_auth_middleware: MyKeyCheck")
 
     print("test_auth_middleware - Request:", request)
@@ -16,12 +18,19 @@ def MyKeyCheck(request, session, metadata, spec):
 
     if request_token == valid_token:
         print("test_auth_middleware: Valid token")
-        session.object.rate = 1000.0
-        session.object.per = 1.0
+        session.rate = 1000.0
+        session.per = 1.0
+
+        spec = AccessSpec( url="http://test_url", methods=["GET", "POST"])
+        definition = AccessDefinition( api_name="My API", api_id="api_01", allowed_urls=[spec])
+        session.access_rights["key"].CopyFrom(definition)
+
+        print("Returning session:", session)
+
         metadata['token'] = 'mytoken'
     else:
         print("test_auth_middleware: Invalid token")
-        request.__object__.return_overrides.response_code = 401
-        request.__object__.return_overrides.response_error = 'Not authorized (Python middleware)'
+        request.object.return_overrides.response_code = 401
+        request.object.return_overrides.response_error = 'Not authorized (Python middleware)'
 
     return request, session, metadata
