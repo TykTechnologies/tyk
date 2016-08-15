c889dfdb82d7cbfb95afc6c2232632d79368e66d
Matias Insaurralde <matias@insaurral.de>
2016-08-15 11:40:03 -0400

    Updating my_handler

--- event_handlers/my_handler.py
+++ event_handlers/my_handler.py
@@ -2,7 +2,11 @@ from tyk.decorators import Event
 
 @Event
 def my_handler(event, spec):
-    print("Receiving event", event)
-    print("And spec", spec)
+    print("-- my_handler:")
+    print(" Event:", event, "\n")
+    print(" Spec:", spec, "\n")
+
+def some_other_stuff():
+    pass
 
 print(1)
