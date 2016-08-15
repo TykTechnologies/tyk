9ffccddec4c485265ef198a602248726639da909
Matias Insaurralde <matias@insaurral.de>
2016-08-15 11:38:25 -0400

    Extends TykDispatcher to load and call event handlers. Reloading is implemented too!

--- coprocess/python/dispatcher.py
+++ coprocess/python/dispatcher.py
@@ -4,21 +4,27 @@ from os import getcwd, chdir, path
 import tyk
 from tyk.middleware import TykMiddleware
 from tyk.object import TykCoProcessObject
+from tyk.event import TykEvent, TykEventHandler
 
 from gateway import TykGateway as tyk
 
 class TykDispatcher:
     '''A simple dispatcher'''
 
-    def __init__(self, middleware_path):
+    def __init__(self, middleware_path, event_handler_path):
         tyk.log( "Initializing dispatcher", "info" )
+
+        self.event_handler_path = path.join(event_handler_path, '*.py')
+        self.event_handlers = {}
+        self.load_event_handlers()
+
         self.middleware_path = path.join(middleware_path, '*.py')
         self.middlewares = []
         self.hook_table = {}
         self.load_middlewares()
 
-    def get_modules(self):
-        files = glob(self.middleware_path)
+    def get_modules(self, the_path):
+        files = glob(the_path)
         files = [ path.basename( f.replace('.py', '') ) for f in files ]
         return files
 
@@ -34,7 +40,7 @@ class TykDispatcher:
     def load_middlewares(self):
         tyk.log( "Loading middlewares.", "debug" )
         # chdir(self.middleware_path)
-        for module_name in self.get_modules():
+        for module_name in self.get_modules(self.middleware_path):
             middleware = self.find_middleware(module_name)
             if middleware:
                 middleware.reload()
@@ -43,6 +49,14 @@ class TykDispatcher:
                 self.middlewares.append(middleware)
         self.update_hook_table()
 
+    def purge_middlewares(self):
+        tyk.log( "Purging middlewares.", "debug" )
+        available_modules = self.get_modules(self.middleware_path)
+        for middleware in self.middlewares:
+            if not middleware.filepath in available_modules:
+                tyk.log( "Purging middleware: '{0}'".format(middleware.filepath), "warning" )
+                self.middlewares.remove(middleware)
+
     def update_hook_table(self):
         self.hook_table = {}
         for middleware in self.middlewares:
@@ -50,19 +64,6 @@ class TykDispatcher:
                 for handler in middleware.handlers[hook_type]:
                     handler.middleware = middleware
                     self.hook_table[handler.name] = handler
-
-    def purge_middlewares(self):
-        tyk.log( "Purging middlewares.", "debug" )
-        available_modules = self.get_modules()
-        for middleware in self.middlewares:
-            if not middleware.filepath in available_modules:
-                tyk.log( "Purging middleware: '{0}'".format(middleware.filepath), "warning" )
-                self.middlewares.remove(middleware)
-
-    def reload(self):
-        tyk.log( "Reloading middlewares.", "info" )
-        self.purge_middlewares()
-        self.load_middlewares()
 
     def find_hook_by_type_and_name(self, hook_type, hook_name):
         found_middleware, matching_hook_handler = None, None
@@ -91,5 +92,40 @@ class TykDispatcher:
                 tyk.log( "Can't dispatch '{0}', hook is not defined.".format(object.hook_name), "error")
             return object.dump()
         except:
-            tyk.log_error( "Can't dispatch, protocol error:" )
+            tyk.log_error( "Can't dispatch, error:" )
             return object_msg
+
+    def purge_event_handlers(self):
+        tyk.log( "Purging event handlers.", "debug" )
+        self.event_handlers = []
+
+    def load_event_handlers(self):
+        tyk.log( "Loading event handlers.", "debug" )
+        for module_name in self.get_modules(self.event_handler_path):
+            event_handlers = TykEventHandler.from_module(module_name)
+            for event_handler in event_handlers:
+                self.event_handlers[event_handler.name] = event_handler
+
+    def find_event_handler(self, handler_name):
+        handler = None
+        if handler_name in self.event_handlers:
+            handler = self.event_handlers[handler_name]
+        return handler
+
+    def dispatch_event(self, event_json):
+        try:
+            event = TykEvent(event_json)
+            event_handler = self.find_event_handler(event.handler_name)
+            if event_handler:
+                event_handler.process(event)
+        except:
+            tyk.log_error( "Can't dispatch, error:")
+
+    def reload(self):
+        tyk.log( "Reloading event handlers and middlewares.", "info" )
+
+        self.purge_event_handlers()
+        self.load_event_handlers()
+
+        self.purge_middlewares()
+        self.load_middlewares()
