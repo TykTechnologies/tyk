42781e61b58fab84f9159a564f6c5f8de9f2b20c
Matias Insaurralde <matias@insaurral.de>
2016-08-17 12:02:13 -0400

    Implementing TestDispatcher.Dispatch

--- coprocess/test/dispatcher.go
+++ coprocess/test/dispatcher.go
@@ -2,33 +2,37 @@ package coprocess_test
 
 /*
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../sds/sds.h"
 
 #include "../api.h"
 
 static struct CoProcessMessage* TestDispatchHook(struct CoProcessMessage* object) {
+	struct CoProcessMessage* outputObject = malloc(sizeof *outputObject);
+	printf("TestDispatchHook %s\n", object->p_data);
   return object;
 };
 
 */
 import "C"
 
-type CoProcessDispatcher interface {
-	Dispatch(*C.struct_CoProcessMessage) *C.struct_CoProcessMessage
-	DispatchEvent([]byte)
-	Reload()
-}
+import(
+	"unsafe"
+	"github.com/golang/protobuf/proto"
+	"github.com/TykTechnologies/tyk/coprocess"
+	"fmt"
+)
 
 type TestDispatcher struct {
-	CoProcessDispatcher
+	coprocess.Dispatcher
 }
 
-func (d *TestDispatcher) Dispatch(objectPtr *C.struct_CoProcessMessage) *C.struct_CoProcessMessage {
+func (d *TestDispatcher) Dispatch(objectPtr unsafe.Pointer) unsafe.Pointer {
 	var newObjectPtr *C.struct_CoProcessMessage
 	newObjectPtr = C.TestDispatchHook(objectPtr)
 
-	return newObjectPtr
+	return unsafe.Pointer(newObjectPtr)
 }
 
 func (d *TestDispatcher) DispatchEvent(eventJSON []byte) {
@@ -45,7 +49,38 @@ func (d *TestDispatcher) Reload() {
 	// C.Python_ReloadDispatcher()
 }
 
-func NewTestDispatcher() (dispatcher CoProcessDispatcher, err error) {
+func NewDispatcher() (dispatcher coprocess.Dispatcher, err error) {
 	d := &TestDispatcher{}
 	return d, nil
+}
+
+func ToCoProcessMessage(object *coprocess.Object) unsafe.Pointer {
+	  objectMsg, _ := proto.Marshal(object)
+		fmt.Println("MARSHAL", objectMsg)
+	  objectMsgStr := string(objectMsg)
+
+	  var CObjectStr *C.char
+	  CObjectStr = C.CString(objectMsgStr)
+
+	  var messagePtr *C.struct_CoProcessMessage
+
+	  messagePtr = (*C.struct_CoProcessMessage)(C.malloc(C.size_t(unsafe.Sizeof(C.struct_CoProcessMessage{}))))
+	  messagePtr.p_data = unsafe.Pointer(CObjectStr)
+	  messagePtr.length = C.int(len(objectMsg))
+
+		return unsafe.Pointer(messagePtr)
+}
+
+func ToCoProcessObject(messagePtr unsafe.Pointer) *coprocess.Object {
+	var message *C.struct_CoProcessMessage
+	message = (*C.struct_CoProcessMessage)(messagePtr)
+
+	var object *coprocess.Object
+	object = &coprocess.Object{}
+
+	objectBytes := C.GoBytes(message.p_data, message.length)
+
+	proto.Unmarshal(objectBytes, object)
+
+	return object
 }
--- coprocess_test.go
+++ coprocess_test.go
@@ -3,21 +3,11 @@
 package main
 
 import(
-  "net/http"
-  "net/http/httptest"
-  "io/ioutil"
-  "path"
-  "os"
-  "strings"
   "testing"
-  "bytes"
-  "net/url"
+  "unsafe"
 
-  "github.com/justinas/alice"
-
-  "github.com/TykTechnologies/tykcommon"
+  // "github.com/TykTechnologies/tykcommon"
   "github.com/TykTechnologies/tyk/coprocess"
-
   "github.com/TykTechnologies/tyk/coprocess/test"
 )
 
@@ -25,185 +15,23 @@ const(
   baseMiddlewarePath = "middleware/python"
 )
 
-var basicCoProcessPreDef string = `
 
-	{
-		"name": "Tyk Test API - IPCONF Fail",
-		"api_id": "1",
-		"org_id": "default",
-		"definition": {
-			"location": "header",
-			"key": "version"
-		},
-		"auth": {
-			"auth_header_name": "authorization"
-		},
-		"version_data": {
-			"not_versioned": true,
-			"versions": {
-				"v1": {
-					"name": "v1",
-					"expires": "2100-01-02 15:04",
-					"use_extended_paths": true,
-					"paths": {
-						"ignored": [],
-						"white_list": [],
-						"black_list": []
-					}
-				}
-			}
-		},
-    "custom_middleware": {
-      "pre": [
-        {
-          "name": "MyPreMiddleware",
-          "require_session": false
-        }
-      ],
-      "driver": "python"
-    },
-		"proxy": {
-			"listen_path": "/v1",
-			"target_url": "http://izumi.tykbeta.com",
-			"strip_listen_path": false
-		}
-	}
-`
+var TestDispatcher, _ = coprocess_test.NewDispatcher()
 
-var basicCoProcessOttoDef string = `
+func TestCoProcessDispatch(t *testing.T) {
+  var object, newObject *coprocess.Object
+  var messagePtr, newMessagePtr unsafe.Pointer
 
-	{
-		"name": "Tyk Test API - IPCONF Fail",
-		"api_id": "1",
-		"org_id": "default",
-		"definition": {
-			"location": "header",
-			"key": "version"
-		},
-		"auth": {
-			"auth_header_name": "authorization"
-		},
-		"version_data": {
-			"not_versioned": true,
-			"versions": {
-				"v1": {
-					"name": "v1",
-					"expires": "2100-01-02 15:04",
-					"use_extended_paths": true,
-					"paths": {
-						"ignored": [],
-						"white_list": [],
-						"black_list": []
-					}
-				}
-			}
-		},
-    "custom_middleware": {
-      "pre": [
-        {
-          "name": "samplePreProcessMiddleware",
-          "path": "middleware/sample_pre.js",
-          "require_session": false
-        }
-      ]
-    },
-		"proxy": {
-			"listen_path": "/v1",
-			"target_url": "http://izumi.tykbeta.com",
-			"strip_listen_path": false
-		}
-	}
-`
-
-var pythonTestPreMiddleware string = `
-from tyk.decorators import *
-from gateway import TykGateway as tyk
-
-@Pre
-def MyPreMiddleware(request, session, spec):
-    print("my_middleware: MyPreMiddleware")
-    return request, session
-`
-
-var _ = writeTestMiddleware("cp_test_test_middleware.py", pythonTestPreMiddleware)
-
-var TestDispatcher, _ = coprocess_test.NewTestDispatcher()
-
-func writeTestMiddleware(filename string, testMiddleware string) bool {
-  middlewarePath := path.Join("middleware/python", filename)
-  ioutil.WriteFile(middlewarePath, []byte(testMiddleware), 0644)
-  return true
-}
-
-func removeTestMiddlewares() {
-  files , _ := ioutil.ReadDir(baseMiddlewarePath)
-  for _, f := range files {
-    isTestMiddleware := strings.Index(f.Name(), "cp_test")
-    middlewarePath := path.Join(baseMiddlewarePath, f.Name())
-
-    if isTestMiddleware == 0 {
-      os.Remove(middlewarePath)
-    }
-  }
-}
-
-func getCoProcessChain(spec APISpec, hookName string, hookType coprocess.HookType, driver tykcommon.MiddlewareDriver) http.Handler {
-	remote, _ := url.Parse(spec.Proxy.TargetURL)
-	proxy := TykNewSingleHostReverseProxy(remote, &spec)
-	proxyHandler := http.HandlerFunc(ProxyHandler(proxy, &spec))
-	tykMiddleware := &TykMiddleware{&spec, proxy}
-	chain := alice.New(
-    CreateCoProcessMiddleware(hookName, hookType, driver, tykMiddleware),
-		CreateMiddleware(&IPWhiteListMiddleware{tykMiddleware}, tykMiddleware),
-		CreateMiddleware(&AuthKey{tykMiddleware}, tykMiddleware),
-		CreateMiddleware(&VersionCheck{TykMiddleware: tykMiddleware}, tykMiddleware),
-		CreateMiddleware(&KeyExpired{tykMiddleware}, tykMiddleware),
-		CreateMiddleware(&AccessRightsCheck{tykMiddleware}, tykMiddleware),
-		CreateMiddleware(&RateLimitAndQuotaCheck{tykMiddleware}, tykMiddleware)).Then(proxyHandler)
-
-	return chain
-}
-
-func MakeCoProcessSampleAPI(apiTestDef string) *APISpec {
-	log.Debug("CREATING TEMPORARY API FOR IP WHITELIST")
-	thisSpec := createDefinitionFromString(apiTestDef)
-	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
-	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
-	orgStore := &RedisStorageManager{KeyPrefix: "orgKey."}
-	thisSpec.Init(&redisStore, &redisStore, healthStore, orgStore)
-	return &thisSpec
-}
-
-func TestCoProcessMiddleware(t *testing.T) {
-
-  writeTestMiddleware("cp_test_test_middleware.py", pythonTestPreMiddleware)
-
-  spec := MakeCoProcessSampleAPI(basicCoProcessPreDef)
-
-  thisSession := createNonThrottledSession()
-  spec.SessionManager.UpdateSession("abc", thisSession, 60)
-
-  uri := "/headers"
-  method := "GET"
-
-  recorder := httptest.NewRecorder()
-
-  param := make(url.Values)
-
-  req, err := http.NewRequest(method, uri,  bytes.NewBufferString(param.Encode()) )
-  req.Header.Add("authorization", "abc")
-
-  if err != nil {
-    t.Fatal(err)
+  object = &coprocess.Object{
+    HookType: coprocess.HookType_Pre,
+    HookName: "test_hook",
   }
 
-  chain := getCoProcessChain(*spec, "abc", coprocess.HookType_Pre, "python")
+  messagePtr = coprocess_test.ToCoProcessMessage(object)
+  newMessagePtr = TestDispatcher.Dispatch(messagePtr)
 
-  chain.ServeHTTP(recorder, req)
+  newObject = coprocess_test.ToCoProcessObject(newMessagePtr)
 
-  if recorder.Code != 200 {
-    t.Error("Invalid response code, should be 200:  \n", recorder.Code, recorder.Body, req.RemoteAddr)
-  }
+  t.Log(newObject)
 
-  removeTestMiddlewares()
 }
