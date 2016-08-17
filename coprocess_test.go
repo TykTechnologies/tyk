650acc85b2d2be5039792d5e62042558550c2bb5
Matias Insaurralde <matias@insaurral.de>
2016-08-17 13:22:25 -0400

    Serialization test

--- coprocess_test.go
+++ coprocess_test.go
@@ -6,6 +6,8 @@ import(
   "testing"
   "unsafe"
 
+  "github.com/golang/protobuf/proto"
+
   // "github.com/TykTechnologies/tykcommon"
   "github.com/TykTechnologies/tyk/coprocess"
   "github.com/TykTechnologies/tyk/coprocess/test"
@@ -17,6 +19,8 @@ const(
 
 
 var TestDispatcher, _ = coprocess_test.NewDispatcher()
+
+/* Dispatcher logic */
 
 func TestCoProcessDispatch(t *testing.T) {
   var object, newObject *coprocess.Object
@@ -34,4 +38,32 @@ func TestCoProcessDispatch(t *testing.T) {
 
   t.Log(newObject)
 
+}
+
+/* Serialization, CP Objects */
+
+func TestCoProcessSerialization(t *testing.T) {
+  var object *coprocess.Object
+
+  object = &coprocess.Object{
+    HookType: coprocess.HookType_Pre,
+    HookName: "test_hook",
+  }
+
+  data, err := proto.Marshal(object)
+
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  var messagePtr unsafe.Pointer
+  messagePtr = coprocess_test.ToCoProcessMessage(object)
+
+  var length int
+  length = coprocess_test.TestMessageLength(messagePtr)
+
+  if len(data) != length {
+    err := "The length of the serialized object doesn't match."
+    t.Fatal(err)
+  }
 }
