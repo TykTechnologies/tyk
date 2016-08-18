03dbc3f31b5861cb14ee9809c8a7156b9448d552
Matias Insaurralde <matias@insaurral.de>
2016-08-18 02:29:43 -0400

    Test coverage for CP API, middleware, reload, etc.

--- coprocess_test.go
+++ coprocess_test.go
@@ -3,67 +3,280 @@
 
 package main
 
-import(
-  "testing"
-  "unsafe"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"unsafe"
 
-  "github.com/golang/protobuf/proto"
-
-  // "github.com/TykTechnologies/tykcommon"
-  "github.com/TykTechnologies/tyk/coprocess"
+	"github.com/TykTechnologies/tyk/coprocess"
+	"github.com/TykTechnologies/tykcommon"
+	"github.com/golang/protobuf/proto"
+	"github.com/justinas/alice"
 )
 
-const(
-  baseMiddlewarePath = "middleware/python"
+const (
+	baseMiddlewarePath = "middleware/python"
 )
 
 var CoProcessName = "test"
 var thisTestDispatcher, _ = NewCoProcessDispatcher()
 
-/* Dispatcher logic */
+/* Dispatcher functions */
 
 func TestCoProcessDispatch(t *testing.T) {
-  var object, newObject *coprocess.Object
-  var messagePtr, newMessagePtr unsafe.Pointer
+	var object, newObject *coprocess.Object
+	var messagePtr, newMessagePtr unsafe.Pointer
 
-  object = &coprocess.Object{
-    HookType: coprocess.HookType_Pre,
-    HookName: "test_hook",
-  }
+	object = &coprocess.Object{
+		HookType: coprocess.HookType_Pre,
+		HookName: "test",
+	}
 
-  messagePtr = thisTestDispatcher.ToCoProcessMessage(object)
-  newMessagePtr = thisTestDispatcher.Dispatch(messagePtr)
+	messagePtr = thisTestDispatcher.ToCoProcessMessage(object)
+	newMessagePtr = thisTestDispatcher.Dispatch(messagePtr)
 
-  newObject = thisTestDispatcher.ToCoProcessObject(newMessagePtr)
+	newObject = thisTestDispatcher.ToCoProcessObject(newMessagePtr)
 
-  t.Log(newObject)
+	t.Log(newObject)
 
+}
+
+func TestCoProcessDispatchEvent(t *testing.T) {
+}
+
+// Makes sense when testing with -timeout
+func TestCoProcessReload(t *testing.T) {
+	ReloadURLStructure()
+	<-CoProcessReload
 }
 
 /* Serialization, CP Objects */
 
 func TestCoProcessSerialization(t *testing.T) {
-  var object *coprocess.Object
+	var object *coprocess.Object
 
-  object = &coprocess.Object{
-    HookType: coprocess.HookType_Pre,
-    HookName: "test_hook",
-  }
+	object = &coprocess.Object{
+		HookType: coprocess.HookType_Pre,
+		HookName: "test_hook",
+	}
 
-  data, err := proto.Marshal(object)
+	data, err := proto.Marshal(object)
 
-  if err != nil {
-    t.Fatal(err)
-  }
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  var messagePtr unsafe.Pointer
-  messagePtr = thisTestDispatcher.ToCoProcessMessage(object)
+	var messagePtr unsafe.Pointer
+	messagePtr = thisTestDispatcher.ToCoProcessMessage(object)
 
-  var length int
-  length = thisTestDispatcher.TestMessageLength(messagePtr)
+	var length int
+	length = thisTestDispatcher.TestMessageLength(messagePtr)
 
-  if len(data) != length {
-    err := "The length of the serialized object doesn't match."
-    t.Fatal(err)
-  }
+	if len(data) != length {
+		err := "The length of the serialized object doesn't match."
+		t.Fatal(err)
+	}
 }
+
+/* Gateway API */
+
+func TestCoProcessGetSetData(t *testing.T) {
+	key := "testkey"
+	value := "testvalue"
+	ttl := 1000
+
+	TestTykStoreData(key, value, ttl)
+
+	retrievedValue := TestTykGetData("testkey")
+
+	if retrievedValue != value {
+		err := "Couldn't retrieve key value using CP API"
+		t.Fatal(err)
+	}
+
+}
+
+func TestCoProcessTykTriggerEvent(t *testing.T) {
+	TestTykTriggerEvent("testevent", "testpayload")
+}
+
+/* Middleware */
+
+type HttpbinGetResponse struct {
+	Params  map[string]string `json:"args"`
+	Headers map[string]string `json:"headers"`
+	Origin  string            `json:"origin"`
+	Url     string            `json:"url"`
+}
+
+type HttpbinHeadersResponse struct {
+	Headers map[string]string `json:"headers"`
+}
+
+func MakeCoProcessSampleAPI(apiTestDef string) *APISpec {
+	log.Debug("CREATING TEMPORARY API FOR COPROCESS TEST")
+	thisSpec := createDefinitionFromString(apiTestDef)
+	redisStore := RedisStorageManager{KeyPrefix: "apikey-"}
+	healthStore := &RedisStorageManager{KeyPrefix: "apihealth."}
+	orgStore := &RedisStorageManager{KeyPrefix: "orgKey."}
+	thisSpec.Init(&redisStore, &redisStore, healthStore, orgStore)
+	return &thisSpec
+}
+
+func BuildCoProcessChain(spec *APISpec, hookName string, hookType coprocess.HookType, driver tykcommon.MiddlewareDriver) http.Handler {
+	remote, _ := url.Parse(spec.Proxy.TargetURL)
+	proxy := TykNewSingleHostReverseProxy(remote, spec)
+	proxyHandler := http.HandlerFunc(ProxyHandler(proxy, spec))
+	tykMiddleware := &TykMiddleware{spec, proxy}
+	mw := CreateCoProcessMiddleware(hookName, hookType, driver, tykMiddleware)
+	chain := alice.New(mw).Then(proxyHandler)
+	return chain
+}
+
+func TestCoProcessMiddleware(t *testing.T) {
+	spec := MakeCoProcessSampleAPI(basicCoProcessDef)
+
+	chain := BuildCoProcessChain(spec, "hook_test", coprocess.HookType_Pre, tykcommon.MiddlewareDriver("python"))
+
+	thisSession := createNonThrottledSession()
+	spec.SessionManager.UpdateSession("abc", thisSession, 60)
+
+	uri := "/headers"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+
+	param := make(url.Values)
+
+	req, err := http.NewRequest(method, uri, bytes.NewBufferString(param.Encode()))
+	req.Header.Add("authorization", "abc")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain.ServeHTTP(recorder, req)
+
+}
+
+func TestCoProcessObjectPostProcess(t *testing.T) {
+	spec := MakeCoProcessSampleAPI(basicCoProcessDef)
+
+	chain := BuildCoProcessChain(spec, "hook_test_object_postprocess", coprocess.HookType_Pre, tykcommon.MiddlewareDriver("python"))
+
+	thisSession := createNonThrottledSession()
+	spec.SessionManager.UpdateSession("abc", thisSession, 60)
+
+	uri := "/headers"
+	method := "GET"
+
+	recorder := httptest.NewRecorder()
+
+	param := make(url.Values)
+
+	req, err := http.NewRequest(method, uri, bytes.NewBufferString(param.Encode()))
+	req.Header.Add("authorization", "abc")
+	req.Header.Add("Deletethisheader", "value")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain.ServeHTTP(recorder, req)
+
+	headersResponse := HttpbinHeadersResponse{}
+	err = json.Unmarshal(recorder.Body.Bytes(), &headersResponse)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if headersResponse.Headers["Test"] != "value" {
+		t.Fatal("ObjectPostProcess couldn't add a header.")
+	}
+
+	if headersResponse.Headers["Deletethisheader"] != "" {
+		t.Fatal("ObjectPostProcess couldn't delete a header.")
+	}
+
+	recorder = httptest.NewRecorder()
+
+	uri = "/get?a=a_value&b=123&remove=3"
+	getReq, err := http.NewRequest(method, uri, bytes.NewBufferString(param.Encode()))
+	getReq.Header.Add("authorization", "abc")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chain.ServeHTTP(recorder, getReq)
+
+	getResponse := HttpbinGetResponse{}
+	err = json.Unmarshal(recorder.Body.Bytes(), &getResponse)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if getResponse.Params["a"] != "a_value" || getResponse.Params["b"] != "123" {
+		t.Fatal("The original parameters don't match.")
+	}
+
+	if getResponse.Params["remove"] != "" {
+		t.Fatal("ObjectPostProcess couldn't remove a parameter.")
+	}
+
+	if getResponse.Params["customparam"] != "customvalue" {
+		t.Fatal("ObjectPostProcess couldn't set custom parameters.")
+	}
+
+}
+
+var basicCoProcessDef string = `
+
+	{
+		"name": "Tyk Test API - IPCONF Fail",
+		"api_id": "1",
+		"org_id": "default",
+		"definition": {
+			"location": "header",
+			"key": "version"
+		},
+		"auth": {
+			"auth_header_name": "authorization"
+		},
+		"version_data": {
+			"not_versioned": true,
+			"versions": {
+				"v1": {
+					"name": "v1",
+					"expires": "2100-01-02 15:04",
+					"use_extended_paths": true,
+					"paths": {
+						"ignored": [],
+						"white_list": [],
+						"black_list": []
+					}
+				}
+			}
+		},
+    "custom_middleware": {
+      "pre": [
+        {
+          "name": "MyPreMiddleware",
+          "require_session": false
+        }
+      ],
+      "driver": "python"
+    },
+		"proxy": {
+			"listen_path": "/v1",
+			"target_url": "http://izumi.tykbeta.com",
+			"strip_listen_path": false
+		}
+	}
+`
--- coprocess_test_helpers.go
+++ coprocess_test_helpers.go
@@ -11,25 +11,37 @@ package main
 
 #include "coprocess/api.h"
 
+void applyTestHooks();
+
 static int TestMessageLength(struct CoProcessMessage* object) {
 	return object->length;
 }
 
 static struct CoProcessMessage* TestDispatchHook(struct CoProcessMessage* object) {
 	struct CoProcessMessage* outputObject = malloc(sizeof *outputObject);
-	printf("TestDispatchHook %s\n", object->p_data);
-  return object;
+
+	printf("TestDispatchHook: %p\n", object);
+
+	outputObject->p_data = object->p_data;
+	outputObject->length = object->length;
+
+	applyTestHooks(outputObject);
+
+  return outputObject;
 };
 
 */
 import "C"
 
-import(
+import (
+	"strings"
 	"unsafe"
-	"github.com/golang/protobuf/proto"
+
 	"github.com/TykTechnologies/tyk/coprocess"
-	"fmt"
+	"github.com/golang/protobuf/proto"
 )
+
+var CoProcessReload = make(chan bool)
 
 type TestDispatcher struct {
 	coprocess.Dispatcher
@@ -46,16 +58,17 @@ func (d *TestDispatcher) Dispatch(objectPtr unsafe.Pointer) unsafe.Pointer {
 
 func (d *TestDispatcher) DispatchEvent(eventJSON []byte) {
 	/*
-	var CEventJSON *C.char
-	CEventJSON = C.CString(string(eventJSON))
-	C.Python_DispatchEvent(CEventJSON)
-	C.free(unsafe.Pointer(CEventJSON))
+		var CEventJSON *C.char
+		CEventJSON = C.CString(string(eventJSON))
+		C.Python_DispatchEvent(CEventJSON)
+		C.free(unsafe.Pointer(CEventJSON))
 	*/
 	return
 }
 
 func (d *TestDispatcher) Reload() {
-	// C.Python_ReloadDispatcher()
+	CoProcessReload <- true
+	return
 }
 
 /* General test helpers */
@@ -66,20 +79,20 @@ func NewCoProcessDispatcher() (dispatcher *TestDispatcher, err error) {
 }
 
 func (d *TestDispatcher) ToCoProcessMessage(object *coprocess.Object) unsafe.Pointer {
-	  objectMsg, _ := proto.Marshal(object)
-		fmt.Println("MARSHAL", objectMsg)
-	  objectMsgStr := string(objectMsg)
+	objectMsg, _ := proto.Marshal(object)
 
-	  var CObjectStr *C.char
-	  CObjectStr = C.CString(objectMsgStr)
+	objectMsgStr := string(objectMsg)
 
-	  var messagePtr *C.struct_CoProcessMessage
+	var CObjectStr *C.char
+	CObjectStr = C.CString(objectMsgStr)
 
-	  messagePtr = (*C.struct_CoProcessMessage)(C.malloc(C.size_t(unsafe.Sizeof(C.struct_CoProcessMessage{}))))
-	  messagePtr.p_data = unsafe.Pointer(CObjectStr)
-	  messagePtr.length = C.int(len(objectMsg))
+	var messagePtr *C.struct_CoProcessMessage
 
-		return unsafe.Pointer(messagePtr)
+	messagePtr = (*C.struct_CoProcessMessage)(C.malloc(C.size_t(unsafe.Sizeof(C.struct_CoProcessMessage{}))))
+	messagePtr.p_data = unsafe.Pointer(CObjectStr)
+	messagePtr.length = C.int(len(objectMsg))
+
+	return unsafe.Pointer(messagePtr)
 }
 
 func (d *TestDispatcher) ToCoProcessObject(messagePtr unsafe.Pointer) *coprocess.Object {
@@ -103,4 +116,63 @@ func (d *TestDispatcher) TestMessageLength(messagePtr unsafe.Pointer) (length in
 	length = int(C.TestMessageLength(message))
 
 	return length
+}
+
+func TestTykStoreData(key string, value string, ttl int) {
+	Ckey := C.CString(key)
+	Cvalue := C.CString(value)
+	Cttl := C.int(ttl)
+	TykStoreData(Ckey, Cvalue, Cttl)
+}
+
+func TestTykGetData(key string) string {
+	Ckey := C.CString(key)
+	Cvalue := TykGetData(Ckey)
+	return C.GoString(Cvalue)
+}
+
+/* Events */
+
+func TestTykTriggerEvent(eventName string, eventPayload string) {
+	CeventName := C.CString(eventName)
+	CeventPayload := C.CString(eventPayload)
+	TykTriggerEvent(CeventName, CeventPayload)
+}
+
+/* Middleware */
+
+//export applyTestHooks
+func applyTestHooks(objectPtr unsafe.Pointer) {
+	var objectStruct *C.struct_CoProcessMessage
+	objectStruct = (*C.struct_CoProcessMessage)(objectPtr)
+
+	objectBytes := C.GoBytes(objectStruct.p_data, objectStruct.length)
+
+	object := &coprocess.Object{}
+	proto.Unmarshal(objectBytes, object)
+
+	if strings.Index(object.HookName, "hook_test") != 0 {
+		return
+	}
+
+	switch object.HookName {
+	case "hook_test_object_postprocess":
+		object.Request.SetHeaders = map[string]string{
+			"test": "value",
+		}
+		object.Request.DeleteHeaders = []string{"Deletethisheader"}
+
+		object.Request.AddParams = map[string]string{
+			"customparam": "customvalue",
+		}
+		object.Request.DeleteParams = []string{"remove"}
+	}
+
+	newObject, _ := proto.Marshal(object)
+
+	newObjectBytes := C.CBytes(newObject)
+	newObjectLength := C.int(len(newObject))
+
+	objectStruct.p_data = unsafe.Pointer(newObjectBytes)
+	objectStruct.length = newObjectLength
 }
