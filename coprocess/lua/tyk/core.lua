d5b60513ab2c21ff15f16a2215485dc08ee29bbc
Matias Insaurralde <matias@insaurral.de>
2016-08-25 12:21:09 -0400

    Updating Lua core script.

--- coprocess/lua/tyk/core.lua
+++ coprocess/lua/tyk/core.lua
@@ -1,6 +1,14 @@
 print("Loading core")
 
-function dispatch(buf)
-  print("lua_dispatch!")
-  return buf, #buf
+local cjson = require "cjson"
+
+function dispatch(raw_object)
+  object = cjson.decode(raw_object)
+  -- print(object['request'])
+  object['request']['set_headers'] = {}
+  object['request']['set_headers']["testkey"] = "testvalue"
+  object['request']['return_overrides']['response_code'] = 500
+  new_object = cjson.encode(object)
+  -- print("new_object =", new_object)
+  return new_object, #new_object
 end
