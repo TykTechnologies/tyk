c667068dbcb81ac02b31322158efbf4b201590f5
Matias Insaurralde <matias@insaurral.de>
2016-08-26 13:53:07 -0400

    Setting up a table for Lua helpers.

--- coprocess/lua/tyk/core.lua
+++ coprocess/lua/tyk/core.lua
@@ -2,6 +2,14 @@ print("Loading core")
 
 local cjson = require "cjson"
 
+-- TODO: Cache additional modules
+tyk = {
+  req=require("coprocess.lua.tyk.request")
+}
+
+-- Make the current object accessible for helpers.
+object = nil
+
 function dispatch(raw_object)
   object = cjson.decode(raw_object)
 
@@ -17,18 +25,17 @@ function dispatch(raw_object)
 
   -- Call the hook and return a serialized version of the modified object.
   if hook_f then
-    local request, session, spec, metadata
+    local new_request, new_session, metadata
 
     if custom_key_auth then
-      request, session, metadata, spec = hook_f(object['request'], object['session'], object['metadata'], object['spec'])
+      new_request, new_session, metadata = hook_f(object['request'], object['session'], object['metadata'], object['spec'])
     else
-      request, session, spec = hook_f(object['request'], object['session'], object['spec'])
+      new_request, new_session = hook_f(object['request'], object['session'], object['spec'])
     end
 
     -- Modify the CP object.
-    object['request'] = request
-    object['session'] = session
-    object['spec'] = spec
+    object['request'] = new_request
+    object['session'] = new_session
     object['metadata'] = metadata
 
     raw_new_object = cjson.encode(object)
