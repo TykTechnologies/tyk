b874f07c5e33936cfa46565cd13017fbc99e9321
Matias Insaurralde <matias@insaurral.de>
2016-08-26 11:38:44 -0400

    Updating Lua core module to dispatch requests based on hook name.

--- coprocess/lua/tyk/core.lua
+++ coprocess/lua/tyk/core.lua
@@ -4,8 +4,21 @@ local cjson = require "cjson"
 
 function dispatch(raw_object)
   object = cjson.decode(raw_object)
-  object['request']['set_headers'] = {}
-  object['request']['set_headers']["luaheader"] = "luavalue"
-  new_object = cjson.encode(object)
-  return new_object, #new_object
+
+  -- Environment reference to hook.
+  hook_name = object['hook_name']
+  hook_f = _G[hook_name]
+
+  -- Call the hook and return a serialized version of the modified object.
+  if hook_f then
+    new_object = hook_f(object)
+    raw_new_object = cjson.encode(new_object)
+    return raw_new_object, #raw_new_object
+
+  -- Return the original object and print an error.
+  else
+    print("Lua: hook doesn't exist!")
+    return raw_object, #raw_object
+  end
+
 end
