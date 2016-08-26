dbd4c00b93fff926175ac7cbf86c1887c9523ad9
Matias Insaurralde <matias@insaurral.de>
2016-08-26 09:28:58 -0400

    Remove return overrides.

--- coprocess/lua/tyk/core.lua
+++ coprocess/lua/tyk/core.lua
@@ -4,11 +4,8 @@ local cjson = require "cjson"
 
 function dispatch(raw_object)
   object = cjson.decode(raw_object)
-  -- print(object['request'])
   object['request']['set_headers'] = {}
   object['request']['set_headers']["testkey"] = "testvalue"
-  object['request']['return_overrides']['response_code'] = 500
   new_object = cjson.encode(object)
-  -- print("new_object =", new_object)
   return new_object, #new_object
 end
