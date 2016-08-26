3facf348e0e79aca16bb8dc5df1ae0215471b3f9
Matias Insaurralde <matias@insaurral.de>
2016-08-26 13:30:51 -0400

    Extend the Lua dispatcher to support custom auth hooks.

--- coprocess/lua/tyk/core.lua
+++ coprocess/lua/tyk/core.lua
@@ -8,11 +8,31 @@ function dispatch(raw_object)
   -- Environment reference to hook.
   hook_name = object['hook_name']
   hook_f = _G[hook_name]
+  is_custom_key_auth = false
+
+  -- Set a flag if this is a custom key auth hook.
+  if object['hook_type'] == 4 then
+    is_custom_key_auth = true
+  end
 
   -- Call the hook and return a serialized version of the modified object.
   if hook_f then
-    new_object = hook_f(object)
-    raw_new_object = cjson.encode(new_object)
+    local request, session, spec, metadata
+
+    if custom_key_auth then
+      request, session, metadata, spec = hook_f(object['request'], object['session'], object['metadata'], object['spec'])
+    else
+      request, session, spec = hook_f(object['request'], object['session'], object['spec'])
+    end
+
+    -- Modify the CP object.
+    object['request'] = request
+    object['session'] = session
+    object['spec'] = spec
+    object['metadata'] = metadata
+
+    raw_new_object = cjson.encode(object)
+
     return raw_new_object, #raw_new_object
 
   -- Return the original object and print an error.
