ee2174095c01d002198bbf8e8552f6bdd0098638
Matias Insaurralde <matias@insaurral.de>
2016-08-27 15:06:29 -0400

    Simplify test middleware.

--- middleware/lua/my_middleware.lua
+++ middleware/lua/my_middleware.lua
@@ -1,11 +1,6 @@
-function MyPostMiddleware(request, session, spec)
-  print("MyPostMiddleware, request=", request, "session=", session, "spec=", spec)
+  function MyPreMiddleware(request, session, spec)
+  print("MyPreMiddleware, request=", request, "session=", session, "spec=", spec)
   tyk.req.set_header("myluaheader", "myluavalue")
-  print("User-Agent header:", tyk.header["User-Agent"])
+  -- print("User-Agent header:", tyk.header["User-Agent"])
   return request, session
-end
-
-function MyAuthCheck(request, session, metadata, spec)
-  print("MyPostMiddleware, request=", request, "session=", session, "metadata=", metadata, "spec=", spec)
-  return request, session, metadata
 end
