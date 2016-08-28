af5fc7e5a7d76d5fe3749bd9e368f1382d30ef73
Martin Buhr <martin@tyk.io>
2016-08-28 12:03:52 -0400

    Added LRU Cache to expire buckets

--- rate_limiter.go
+++ rate_limiter.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/TykTechnologies/leakybucket"
-	"github.com/TykTechnologies/leakybucket/memory"
+	"github.com/TykTechnologies/leakybucket/memorycache"
 )
 
 var BucketStore leakybucket.Storage
 
 func InitBucketStore() {
-	BucketStore = memory.New()
+	BucketStore = memorycache.New()
 }
