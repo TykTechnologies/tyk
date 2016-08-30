6c5fa3bbbb9f645b63fdd3f23e69787a201778dc
Matias Insaurralde <matias@insaurral.de>
2016-08-30 09:57:43 -0400

    Header cleanup.

--- coprocess/lua/binding.h
+++ coprocess/lua/binding.h
@@ -7,15 +7,11 @@
 #include <lauxlib.h>
 
 static void LuaInit();
-static void LuaReload();
-// static void Node_SetEnv(char*);
 
 static struct CoProcessMessage* LuaDispatchHook(struct CoProcessMessage*);
 static void LuaDispatchEvent(char*);
 
 void LoadCachedMiddleware(void*);
 void LoadCachedModules(void*);
-// void LoadCachedModules(*lua_State);
-// static void Python_DispatchEvent(char*);
 
 #endif
