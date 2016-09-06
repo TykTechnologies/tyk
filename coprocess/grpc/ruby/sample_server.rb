37f787d5798df55c399015825169972c1799d73a
Matias Insaurralde <matias@insaurral.de>
2016-09-06 11:33:54 +0100

    Sample gRPC-Ruby dispatcher.

--- coprocess/grpc/ruby/sample_server.rb
+++ coprocess/grpc/ruby/sample_server.rb
@@ -1,10 +1,27 @@
 require '../../bindings/ruby/dispatcher'
 
 class SampleServer < Coprocess::Dispatcher::Service
-  def dispatch(request, _unused_call)
-    print request.inspect
-    puts
-    return request
+
+  def MyPreMiddleware(coprocess_object)
+    coprocess_object.request.set_headers["rubyheader"] = "rubyvalue"
+    return coprocess_object
+  end
+
+  # Implements a dynamic dispatcher, this class should implement your hooks.
+  def dispatch(coprocess_object, _unused_call)
+    begin
+      if self.respond_to?(coprocess_object.hook_name)
+        coprocess_object = self.send(coprocess_object.hook_name, coprocess_object)
+      else
+        raise Coprocess::Dispatcher::HookNotImplemented
+      end
+    rescue Coprocess::Dispatcher::HookNotImplemented
+      puts "Hook not implemented: #{coprocess_object.hook_name}"
+    rescue Exception => e
+      puts "Couldn't dispatch: #{e}"
+    end
+
+    return coprocess_object
   end
 end
 
