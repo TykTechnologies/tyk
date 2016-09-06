235adbdf4e33e2f9d2b090005ad4af19a85d7ecb
Matias Insaurralde <matias@insaurral.de>
2016-09-06 18:39:34 +0100

    Use AppendMiddleware for appending ID extractor.

--- main.go
+++ main.go
@@ -984,8 +984,7 @@ func loadApps(APISpecs *[]*APISpec, Muxer *mux.Router) {
 					log.WithFields(logrus.Fields{
 						"prefix": "coprocess",
 					}).Debug("----> Registering coprocess middleware, hook name: ", mwAuthCheckFunc.Name, "hook type: CustomKeyCheck", ", driver: ", mwDriver)
-
-					authArray = append(authArray, CreateIdExtractorMiddleware(mwDriver, tykMiddleware))
+					AppendMiddleware(&authArray, &IdExtractorMiddleware{tykMiddleware}, tykMiddleware)
 					authArray = append(authArray, CreateCoProcessMiddleware(mwAuthCheckFunc.Name, coprocess.HookType_CustomKeyCheck, mwDriver, tykMiddleware))
 				}
 
