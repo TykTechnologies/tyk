8818aeed831f95490ae1c8c7c4618e3161c0ed91
Matias Insaurralde <matias@insaurral.de>
2016-09-06 13:00:41 +0100

    Enhance dispatcher comments.

--- coprocess/dispatcher.go
+++ coprocess/dispatcher.go
@@ -17,11 +17,20 @@ const(
 	ProtobufMessage
 )
 
-// CoProcessDispatcher defines a basic interface for the CP dispatcher, check PythonDispatcher for reference.
+// Dispatcher defines a basic interface for the CP dispatcher, check PythonDispatcher for reference.
 type Dispatcher interface {
+	// Dispatch takes and returns a pointer to a CoProcessMessage struct, see coprocess/api.h for details. This is used by CP bindings.
 	Dispatch(unsafe.Pointer) unsafe.Pointer
-	DispatchObject(*Object) *Object
+
+	// DispatchEvent takes an event JSON, as bytes. Doesn't return.
 	DispatchEvent([]byte)
+
+	// DispatchObject takes and returns a coprocess.Object pointer, this is used by gRPC.
+	DispatchObject(*Object) *Object
+
+	// LoadModules is called the first time a CP binding starts. Used by Lua.
 	LoadModules()
+
+	// Reload is called when a hot reload is triggered. Used by all the CPs.
 	Reload()
 }
