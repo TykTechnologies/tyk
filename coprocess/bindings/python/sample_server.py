d23613ac575e7fe2a3e65a1bcd8467bf5ab02d47
Matias Insaurralde <matias@insaurral.de>
2016-09-06 11:34:55 +0100

    Updating gRPC-Python sample.

--- coprocess/bindings/python/sample_server.py
+++ coprocess/bindings/python/sample_server.py
@@ -7,28 +7,21 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 from concurrent import futures
 
 def MyPreMiddleware(coprocess_object):
-  print("You're calling MyPreMiddleware")
   coprocess_object.request.set_headers["myheader"] = "myvalue"
   return coprocess_object
 
 def MyPostMiddleware(coprocess_object):
-  print("You're calling MyPostMiddleware")
   coprocess_object.request.set_headers["anotherheader"] = "anothervalue"
   return coprocess_object
 
 class MyDispatcher(coprocess_object_pb2.DispatcherServicer):
   def Dispatch(self, coprocess_object, context):
-    print("dispatch, hook_name =", coprocess_object.hook_name)
-
-    print("original object =", coprocess_object)
 
     if coprocess_object.hook_name == "MyPreMiddleware":
         coprocess_object = MyPreMiddleware(coprocess_object)
 
     if coprocess_object.hook_name == "MyPostMiddleware":
         coprocess_object = MyPostMiddleware(coprocess_object)
-
-    print("new object =", coprocess_object)
 
     return coprocess_object
 
