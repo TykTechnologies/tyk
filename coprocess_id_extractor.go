5c43973d91bc613cc2c95d67914b073249e1f3f9
Matias Insaurralde <matias@insaurral.de>
2016-09-06 17:19:22 +0100

    Push additional CP middleware.

---
+++ coprocess_id_extractor.go
@@ -0,0 +1,59 @@
+// +build coprocess
+
+package main
+
+import(
+  "github.com/Sirupsen/logrus"
+  // "github.com/gorilla/context"
+  "github.com/mitchellh/mapstructure"
+  "github.com/TykTechnologies/tykcommon"
+
+  "net/http"
+)
+
+// IdExtractorMiddleware is the basic CP middleware struct.
+type IdExtractorMiddleware struct {
+	*TykMiddleware
+	MiddlewareDriver tykcommon.MiddlewareDriver
+}
+
+func CreateIdExtractorMiddleware(mwDriver tykcommon.MiddlewareDriver, tykMwSuper *TykMiddleware) func(http.Handler) http.Handler {
+	dMiddleware := &IdExtractorMiddleware{
+		TykMiddleware:    tykMwSuper,
+		MiddlewareDriver: mwDriver,
+	}
+
+	return CreateMiddleware(dMiddleware, tykMwSuper)
+}
+
+// CoProcessMiddlewareConfig holds the middleware configuration.
+type IdExtractorMiddlewareConfig struct {
+	ConfigData map[string]string `mapstructure:"config_data" bson:"config_data" json:"config_data"`
+}
+
+// New lets you do any initialisations for the object can be done here
+func (m *IdExtractorMiddleware) New() {}
+
+// GetConfig retrieves the configuration from the API config - we user mapstructure for this for simplicity
+func (m *IdExtractorMiddleware) GetConfig() (interface{}, error) {
+	var thisModuleConfig IdExtractorMiddlewareConfig
+
+	err := mapstructure.Decode(m.TykMiddleware.Spec.APIDefinition.RawData, &thisModuleConfig)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"prefix": "jsvm",
+		}).Error(err)
+		return nil, err
+	}
+
+	return thisModuleConfig, nil
+}
+
+func (m *IdExtractorMiddleware) IsEnabledForSpec() bool {
+	return true
+}
+
+func (m *IdExtractorMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Request, configuration interface{}) (error, int) {
+  log.Println("*** IdExtractorMiddleware")
+  return nil, 200
+}
