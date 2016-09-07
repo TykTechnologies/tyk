2b0b8e8d2c0d2d5c74e0efdcc522e1f3781b8b40
Matias Insaurralde <matias@insaurral.de>
2016-09-07 11:03:22 +0100

    Implementing basic ID extractor logic.

--- coprocess_id_extractor.go
+++ coprocess_id_extractor.go
@@ -6,15 +6,39 @@ import (
 	"github.com/Sirupsen/logrus"
 	// "github.com/gorilla/context"
 	"github.com/mitchellh/mapstructure"
-	// "github.com/TykTechnologies/tykcommon"
+	"github.com/TykTechnologies/tykcommon"
 
+	"crypto/md5"
 	"net/http"
+	"errors"
+	"fmt"
 )
 
 // IdExtractorMiddleware is the basic CP middleware struct.
 type IdExtractorMiddleware struct {
 	*TykMiddleware
 }
+
+type IdExtractor interface {
+	Extract(interface{}) string
+}
+
+type ValueExtractor struct {
+	Config *tykcommon.MiddlewareIdExtractor
+}
+
+func(e *ValueExtractor) Extract(input interface{}) string {
+	headerValue := input.(string)
+	return headerValue
+}
+
+/*
+"extract_from": "header",
+"extract_with": "value",
+"extractor_config": {
+	"header_name": "Authorization"
+}
+*/
 
 func CreateIdExtractorMiddleware(tykMwSuper *TykMiddleware) func(http.Handler) http.Handler {
 	dMiddleware := &IdExtractorMiddleware{
@@ -57,5 +81,61 @@ func (m *IdExtractorMiddleware) IsEnabledForSpec() bool {
 
 func (m *IdExtractorMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Request, configuration interface{}) (error, int) {
 	log.Println("*** IdExtractorMiddleware")
+	log.Println("*** configuration", configuration)
+	log.Println("*** spec (IdExtractorMiddleware)", m.TykMiddleware.Spec.CustomMiddleware.IdExtractor.ExtractorConfig)
+
+	log.Println("*** extractwith", m.TykMiddleware.Spec.CustomMiddleware.IdExtractor.ExtractWith)
+
+	var thisExtractor IdExtractor
+	var extractorOutput, tokenID, SessionID string
+
+	// Initialize a extractor based on the API spec.
+	switch m.TykMiddleware.Spec.CustomMiddleware.IdExtractor.ExtractWith {
+	case tykcommon.ValueExtractor:
+		log.Println("Is a ValueExtractor.")
+		thisExtractor = &ValueExtractor{
+			Config: &m.TykMiddleware.Spec.CustomMiddleware.IdExtractor,
+		}
+	}
+
+	// Check the extractor source, take the value and perform the extraction.
+	switch m.TykMiddleware.Spec.CustomMiddleware.IdExtractor.ExtractFrom {
+	case tykcommon.HeaderSource:
+		var headerName, headerValue string
+
+		// TODO: check if header_name is set
+		headerName = m.TykMiddleware.Spec.CustomMiddleware.IdExtractor.ExtractorConfig["header_name"].(string)
+		headerValue = r.Header.Get(headerName)
+
+		if headerValue == "" {
+			log.WithFields(logrus.Fields{
+				"path":   r.URL.Path,
+				"origin": GetIPFromRequest(r),
+			}).Info("Attempted access with malformed header, no auth header found.")
+
+			log.Debug("Looked in: ", headerName)
+			log.Debug("Raw data was: ", headerValue)
+			log.Debug("Headers are: ", r.Header)
+
+			// m.reportLoginFailure(tykId, r)
+			return errors.New("Authorization field missing"), 400
+		}
+
+		log.Println("Uses a HeaderSource.")
+		// TODO: check if header_name setting exists!
+		rawHeader := r.Header.Get(headerName)
+		log.Println("rawHeader is", rawHeader)
+		extractorOutput = thisExtractor.Extract(rawHeader)
+	}
+
+	// Prepare a session ID.
+
+	data := []byte(extractorOutput)
+	tokenID = fmt.Sprintf("%x", md5.Sum(data))
+	SessionID = m.TykMiddleware.Spec.OrgID + tokenID
+
+	fmt.Println("tokenID is", tokenID)
+	fmt.Println("SessionID is", SessionID)
+
 	return nil, 200
 }
