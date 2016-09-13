dcdb1d3ba0fe45ac345e1268d0d929ce43ea4bfb
Matias Insaurralde <matias@insaurral.de>
2016-09-13 15:47:13 +0100

    Removing unsafe/C based dispatcher from main CP file.

--- coprocess.go
+++ coprocess.go
@@ -5,23 +5,6 @@
 
 package main
 
-/*
-#cgo python CFLAGS: -DENABLE_PYTHON
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "coprocess/sds/sds.h"
-
-#include "coprocess/api.h"
-
-#ifdef ENABLE_PYTHON
-#include "coprocess/python/dispatcher.h"
-#include "coprocess/python/binding.h"
-#endif
-
-*/
-import "C"
-
 import (
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/context"
@@ -30,14 +13,10 @@ import (
 	"github.com/TykTechnologies/tyk/coprocess"
 	"github.com/TykTechnologies/tykcommon"
 
-	"encoding/json"
-	"github.com/golang/protobuf/proto"
-
 	"bytes"
 	"errors"
 	"io/ioutil"
 	"net/http"
-	"unsafe"
 )
 
 // EnableCoProcess will be overridden by config.EnableCoProcess.
@@ -163,54 +142,6 @@ func (c *CoProcessor) ObjectPostProcess(object *coprocess.Object, r *http.Reques
 	}
 
 	r.URL.RawQuery = values.Encode()
-}
-
-// Dispatch prepares a CoProcessMessage, sends it to the GlobalDispatcher and gets a reply.
-func (c *CoProcessor) Dispatch(object *coprocess.Object) *coprocess.Object {
-
-	var objectMsg []byte
-
-	if MessageType == coprocess.ProtobufMessage {
-		objectMsg, _ = proto.Marshal(object)
-	} else if MessageType == coprocess.JsonMessage {
-		objectMsg, _ = json.Marshal(object)
-	}
-
-	if CoProcessName == "grpc" {
-		object = GlobalDispatcher.DispatchObject(object)
-		return object
-	}
-
-	objectMsgStr := string(objectMsg)
-
-	var CObjectStr *C.char
-	CObjectStr = C.CString(objectMsgStr)
-
-	var objectPtr *C.struct_CoProcessMessage
-
-	objectPtr = (*C.struct_CoProcessMessage)(C.malloc(C.size_t(unsafe.Sizeof(C.struct_CoProcessMessage{}))))
-	objectPtr.p_data = unsafe.Pointer(CObjectStr)
-	objectPtr.length = C.int(len(objectMsg))
-
-	var newObjectPtr *C.struct_CoProcessMessage
-	newObjectPtr = (*C.struct_CoProcessMessage)(GlobalDispatcher.Dispatch(unsafe.Pointer(objectPtr)))
-
-	var newObjectBytes []byte
-	newObjectBytes = C.GoBytes(newObjectPtr.p_data, newObjectPtr.length)
-
-	newObject := &coprocess.Object{}
-
-	if MessageType == coprocess.ProtobufMessage {
-		proto.Unmarshal(newObjectBytes, newObject)
-	} else if MessageType == coprocess.JsonMessage {
-		json.Unmarshal(newObjectBytes, newObject)
-	}
-
-	C.free(unsafe.Pointer(CObjectStr))
-	C.free(unsafe.Pointer(objectPtr))
-	C.free(unsafe.Pointer(newObjectPtr))
-
-	return newObject
 }
 
 // CoProcessInit creates a new CoProcessDispatcher, it will be called when Tyk starts.
