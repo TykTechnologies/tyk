598be6dffbc3ade26a1180ff3be60f074a66a18d
Matias Insaurralde <matias@insaurral.de>
2016-09-13 12:48:57 +0100

    Move CP log function.

--- coprocess_api.go
+++ coprocess_api.go
@@ -57,3 +57,30 @@ func TykTriggerEvent(CEventName *C.char, CPayload *C.char) {
 		Message: payload,
 	})
 }
+
+// CoProcessLog is a bridge for using Tyk log from CP.
+//export CoProcessLog
+func CoProcessLog(CMessage *C.char, CLogLevel *C.char) {
+	var message, logLevel string
+	message = C.GoString(CMessage)
+	logLevel = C.GoString(CLogLevel)
+
+	switch logLevel {
+	case "debug":
+		log.WithFields(logrus.Fields{
+			"prefix": CoProcessName,
+		}).Debug(message)
+	case "error":
+		log.WithFields(logrus.Fields{
+			"prefix": CoProcessName,
+		}).Error(message)
+	case "warning":
+		log.WithFields(logrus.Fields{
+			"prefix": CoProcessName,
+		}).Warning(message)
+	default:
+		log.WithFields(logrus.Fields{
+			"prefix": CoProcessName,
+		}).Info(message)
+	}
+}
