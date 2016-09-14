2c062652be74c9c8dd20df7c6f6f6c66b2498c49
Matias Insaurralde <matias@insaurral.de>
2016-09-14 14:38:02 +0100

    Regex extractor spec.

--- coprocess_id_extractor_test.go
+++ coprocess_id_extractor_test.go
@@ -101,7 +101,71 @@ func TestValueExtractorRequirements(t *testing.T) {
 var IdExtractorCoProcessDef string = `
 
 	{
-		"name": "Tyk Test API - IPCONF Fail",
+		"name": "Tyk Test API",
+		"api_id": "1",
+		"org_id": "default",
+		"definition": {
+			"location": "header",
+			"key": "version"
+		},
+		"auth": {
+			"auth_header_name": "authorization"
+		},
+		"version_data": {
+			"not_versioned": true,
+			"versions": {
+				"v1": {
+					"name": "v1",
+					"expires": "2100-01-02 15:04",
+					"use_extended_paths": true,
+					"paths": {
+						"ignored": [],
+						"white_list": [],
+						"black_list": []
+					}
+				}
+			}
+		},
+		"event_handlers": {
+			"events": {
+				"AuthFailure": [
+								{
+										"handler_name":"cp_dynamic_handler",
+										"handler_meta": {
+												"name": "my_handler"
+										}
+								}
+						]
+			}
+		},
+		"custom_middleware": {
+      "pre": [
+        {
+          "name": "MyPreMiddleware",
+          "require_session": false
+        }
+      ],
+      "id_extractor": {
+        "extract_from": "header",
+        "extract_with": "value",
+        "extractor_config": {
+          "header_name": "Authorization"
+        }
+      },
+      "driver": "grpc"
+    },
+		"proxy": {
+			"listen_path": "/v1",
+			"target_url": "http://httpbin.org",
+			"strip_listen_path": false
+		}
+	}
+`
+
+var RegexExtractor string = `
+
+	{
+		"name": "Tyk Test API - ValueExtractor/XPath",
 		"api_id": "1",
 		"org_id": "default",
 		"definition": {
