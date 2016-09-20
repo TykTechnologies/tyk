bacbf6616a1cf9432e38a93a007c325bdf40ab01
Matias Insaurralde <matias@insaurral.de>
2016-09-20 19:39:33 +0200

    Fix tests.

--- coprocess_bundle_test.go
+++ coprocess_bundle_test.go
@@ -24,12 +24,12 @@ func TestZipBundleSaver(t *testing.T) {
 }
 
 func TestFetchBundle(t *testing.T) {
-	var testBundleSaver BundleSaver
-	testBundleSaver = &ZipBundleSaver{}
+	// var testBundleSaver BundleSaver
+	// testBundleSaver = &ZipBundleSaver{}
 }
 
 func TestSaveBundle(t *testing.T) {
 	// var testBundleFormat = "zip"
-	var testBundleSaver BundleSaver
-	testBundleSaver = &ZipBundleSaver{}
+	// var testBundleSaver BundleSaver
+	// testBundleSaver = &ZipBundleSaver{}
 }
