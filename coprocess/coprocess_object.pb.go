5f9cafe624d91dd70e4fe7ae3c100503699a86d7
Matias Insaurralde <matias@insaurral.de>
2016-11-08 09:05:26 -0300

    Updating Protobuf bindings.

--- coprocess/coprocess_object.pb.go
+++ coprocess/coprocess_object.pb.go
@@ -89,7 +89,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion3
+const _ = grpc.SupportPackageIsVersion4
 
 // Client API for Dispatcher service
 
@@ -185,7 +185,7 @@ var _Dispatcher_serviceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: fileDescriptor2,
+	Metadata: "coprocess_object.proto",
 }
 
 func init() { proto.RegisterFile("coprocess_object.proto", fileDescriptor2) }
