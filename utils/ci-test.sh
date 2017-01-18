1b139e16fbed760c7d66f4f6b26d4d7b97202c21
Daniel Martí <mvdan@mvdan.cc>
2017-01-18 15:14:52 +0000

    utils/ci-test: enforce go vet
    Both with and without the coprocess tag for now, like the tests.
    
    Fixes #346.

--- utils/ci-test.sh
+++ utils/ci-test.sh
@@ -4,8 +4,13 @@ set -e
 
 # print a command and execute it
 show() {
-    echo "$@"
-    eval "$@"
+	echo "$@"
+	eval "$@"
+}
+
+fatal() {
+	echo "$@"
+	exit 1
 }
 
 PKGS="$(go list ./... | grep -v /vendor/)"
@@ -13,16 +18,17 @@ PKGS="$(go list ./... | grep -v /vendor/)"
 show go test -v $PKGS
 show go test -v -tags coprocess $PKGS
 
+show go vet -v $PKGS
+show go vet -v -tags coprocess $PKGS
+
 GOFILES=$(find * -name '*.go' -not -path 'vendor/*')
 
 FMT_FILES="$(gofmt -s -l $GOFILES)"
 if [[ -n $FMT_FILES ]]; then
-	echo "Run 'gofmt -s -w' on these files:\n$FMT_FILES"
-	exit 1
+	fatal "Run 'gofmt -s -w' on these files:\n$FMT_FILES"
 fi
 
 IMP_FILES="$(goimports -l $GOFILES)"
 if [[ -n $IMP_FILES ]]; then
-	echo "Run 'goimports -w' on these files:\n$IMP_FILES"
-	exit 1
+	fatal "Run 'goimports -w' on these files:\n$IMP_FILES"
 fi
