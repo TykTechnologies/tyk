TykTechnologies/tyk

commit 6c6293713d2a807a20fef68e2ce4155887f554b0

Make the default log level for tests be ERROR
Can still be overwritten via TYK_LOGLEVEL, see .travis.yml.

Updates #330.

main.go
@@ -63,8 +63,10 @@ var (
 	NodeID string
 )
 
-// Generic system error
+var runningTests = false
+
 const (
+	// Generic system error
 	E_SYSTEM_ERROR = "{\"status\": \"system error, please contact administrator\"}"
 	OAUTH_PREFIX   = "oauth-data."
 )
@@ -894,6 +896,19 @@ func initialiseSystem(arguments map[string]interface{}) {
 
 	}
 
+	if runningTests && os.Getenv("TYK_LOGLEVEL") == "" {
+		// `go test` without TYK_LOGLEVEL override
+		log.Level = logrus.ErrorLevel
+	} else if dbg, _ := arguments["--debug"]; dbg == true {
+		log.Level = logrus.DebugLevel
+		log.WithFields(logrus.Fields{
+			"prefix": "main",
+		}).Debug("Enabling debug-level output")
+	} else {
+		// default is info
+		log.Level = logrus.InfoLevel
+	}
+
 	filename := "/etc/tyk/tyk.conf"
 	value, _ := arguments["--conf"]
 	if value != nil {
@@ -934,15 +949,6 @@ func initialiseSystem(arguments map[string]interface{}) {
 
 	doMemoryProfile, _ = arguments["--memprofile"].(bool)
 	doCpuProfile, _ = arguments["--cpuprofile"].(bool)
-
-	doDebug, _ := arguments["--debug"]
-	log.Level = logrus.InfoLevel
-	if doDebug == true {
-		log.Level = logrus.DebugLevel
-		log.WithFields(logrus.Fields{
-			"prefix": "main",
-		}).Debug("Enabling debug-level output")
-	}
 
 	// Enable all the loggers
 	setupLogger()

.travis.yml
@@ -1,6 +1,10 @@
 sudo: false
 language: go
 
+env:
+  global:
+    - TYK_LOGLEVEL=info
+
 matrix:
   include:
     - go: 1.6.x

gateway_test.go
@@ -17,6 +17,7 @@ import (
 
 func init() {
 	fmt.Println("THIS IS THE TEST SETUP INIT")
+	runningTests = true
 	initialiseSystem(map[string]interface{}{})
 }
 

commit ac001ef44b90fe8fc491a50f8a9cf04709886e48

Don't overwrite log.Level with InfoLevel
When we run GetLogger(), that func already reads TYK_LOGLEVEL to set the
proper value, defaulting to InfoLevel. If we overwrite it here, we are
ignoring TYK_LOGLEVEL. For example:

	$ TYK_LOGLEVEL=error go test
	[prints all logs, including INFO]

I introduced this regression in 6c629371.

Fixes #466.

main.go
@@ -900,9 +900,6 @@ func initialiseSystem(arguments map[string]interface{}) {
 		log.WithFields(logrus.Fields{
 			"prefix": "main",
 		}).Debug("Enabling debug-level output")
-	} else {
-		// default is info
-		log.Level = logrus.InfoLevel
 	}
 
 	filename := "/etc/tyk/tyk.conf"