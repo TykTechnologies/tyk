aabb7459b3a1184eccc17d9530795205fc239c13
Daniel Martí <mvdan@mvdan.cc>
2017-02-13 11:11:45 +0000

    Refactor url rewrite tests into sub-tests
    Greatly reduces duplicated code.
    
     $ go test -run TestRewriter -v
    === RUN   TestRewriter
    === RUN   TestRewriter/Straight
    === RUN   TestRewriter/OneVal
    === RUN   TestRewriter/ThreeVals
    === RUN   TestRewriter/Reverse
    === RUN   TestRewriter/Missing
    === RUN   TestRewriter/MissingAgain
    === RUN   TestRewriter/QS
    === RUN   TestRewriter/QS2
    --- PASS: TestRewriter (0.00s)

--- middleware_url_rewrite_test.go
+++ middleware_url_rewrite_test.go
@@ -7,193 +7,66 @@ import (
 )
 
 func TestRewriter(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/straight/rewrite",
-		RewriteTo:    "change/to/me",
+	tests := []struct {
+		name        string
+		pattern, to string
+		in, want    string
+	}{
+		{
+			"Straight",
+			"test/straight/rewrite", "change/to/me",
+			"test/straight/rewrite", "change/to/me",
+		},
+		{
+			"OneVal",
+			"test/val/(.*)", "change/to/$1",
+			"test/val/VALUE", "change/to/VALUE",
+		},
+		{
+			"ThreeVals",
+			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$1/$2/$3",
+			"test/val/ONE/space/TWO/and/then/THREE", "change/to/ONE/TWO/THREE",
+		},
+		{
+			"Reverse",
+			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$3/$2/$1",
+			"test/val/ONE/space/TWO/and/then/THREE", "change/to/THREE/TWO/ONE",
+		},
+		{
+			"Missing",
+			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$1/$2",
+			"test/val/ONE/space/TWO/and/then/THREE", "change/to/ONE/TWO",
+		},
+		{
+			"MissingAgain",
+			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$3/$1",
+			"test/val/ONE/space/TWO/and/then/THREE", "change/to/THREE/ONE",
+		},
+		{
+			"QS",
+			"(.*)", "$1&newParam=that",
+			"foo/bar?param1=this", "foo/bar?param1=this&newParam=that",
+		},
+		{
+			"QS2",
+			"test/val/(.*)/space/(.*)/and/then(.*)", "change/to/$2/$1$3",
+			"test/val/ONE/space/TWO/and/then?param1=this", "change/to/TWO/ONE?param1=this",
+		},
 	}
-
-	inbound := "test/straight/rewrite"
-	expected := "change/to/me"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithOneVal(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/val/(.*)",
-		RewriteTo:    "change/to/$1",
-	}
-
-	inbound := "test/val/VALUE"
-	expected := "change/to/VALUE"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithThreeVals(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/val/(.*)/space/(.*)/and/then/(.*)",
-		RewriteTo:    "change/to/$1/$2/$3",
-	}
-
-	inbound := "test/val/ONE/space/TWO/and/then/THREE"
-	expected := "change/to/ONE/TWO/THREE"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithReverse(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/val/(.*)/space/(.*)/and/then/(.*)",
-		RewriteTo:    "change/to/$3/$2/$1",
-	}
-
-	inbound := "test/val/ONE/space/TWO/and/then/THREE"
-	expected := "change/to/THREE/TWO/ONE"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithMissing(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/val/(.*)/space/(.*)/and/then/(.*)",
-		RewriteTo:    "change/to/$1/$2",
-	}
-
-	inbound := "test/val/ONE/space/TWO/and/then/THREE"
-	expected := "change/to/ONE/TWO"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithMissingAgain(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/val/(.*)/space/(.*)/and/then/(.*)",
-		RewriteTo:    "change/to/$3/$1",
-	}
-
-	inbound := "test/val/ONE/space/TWO/and/then/THREE"
-	expected := "change/to/THREE/ONE"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithQS(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "(.*)",
-		RewriteTo:    "$1&newParam=that",
-	}
-
-	inbound := "foo/bar?param1=this"
-	expected := "foo/bar?param1=this&newParam=that"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
-	}
-}
-
-func TestRewriterWithQS2(t *testing.T) {
-	rw := URLRewriter{}
-
-	testConf := apidef.URLRewriteMeta{
-		Path:         "",
-		Method:       "",
-		MatchPattern: "test/val/(.*)/space/(.*)/and/then(.*)",
-		RewriteTo:    "change/to/$2/$1$3",
-	}
-
-	inbound := "test/val/ONE/space/TWO/and/then?param1=this"
-	expected := "change/to/TWO/ONE?param1=this"
-
-	val, err := rw.Rewrite(&testConf, inbound, false, nil)
-
-	if err != nil {
-		t.Error("Compile failed: ", err)
-	}
-
-	if val != expected {
-		t.Errorf("Transform failed, expected: %v, got: %v ", expected, val)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			rw := URLRewriter{}
+			testConf := apidef.URLRewriteMeta{
+				MatchPattern: tc.pattern,
+				RewriteTo:    tc.to,
+			}
+			got, err := rw.Rewrite(&testConf, tc.in, false, nil)
+			if err != nil {
+				t.Error("compile failed:", err)
+			}
+			if got != tc.want {
+				t.Errorf("rewrite failed, want %q, got %q", tc.want, got)
+			}
+		})
 	}
 }
