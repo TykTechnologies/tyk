105a6238e9a6f81ae83247165fe22cebc17b0c7a
Daniel Martí <mvdan@mvdan.cc>
2017-04-19 16:40:55 +0100

    Merge response_middleware.go into middleware.go
    The former was tiny and only used in main.go.
    
    Updates #334.

--- middleware.go
+++ middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 	"time"
@@ -123,4 +124,36 @@ func CheckETEnabled(tykMwSuper *TykMiddleware) (used bool) {
 		}
 	}
 	return
+}
+
+var responseProcessors = map[string]TykResponseHandler{
+	"header_injector":         HeaderInjector{},
+	"response_body_transform": ResponseTransformMiddleware{},
+	"header_transform":        HeaderTransform{},
+}
+
+type TykResponseHandler interface {
+	HandleResponse(http.ResponseWriter, *http.Response, *http.Request, *SessionState) error
+	New(interface{}, *APISpec) (TykResponseHandler, error)
+}
+
+func GetResponseProcessorByName(name string) (TykResponseHandler, error) {
+	processor, ok := responseProcessors[name]
+	if !ok {
+		return nil, errors.New("Not found")
+	}
+
+	return processor, nil
+
+}
+
+type ResponseChain struct{}
+
+func (r ResponseChain) Go(chain []TykResponseHandler, rw http.ResponseWriter, res *http.Response, req *http.Request, ses *SessionState) error {
+	for _, rh := range chain {
+		if err := rh.HandleResponse(rw, res, req, ses); err != nil {
+			return err
+		}
+	}
+	return nil
 }
