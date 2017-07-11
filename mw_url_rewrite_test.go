25edaf0d26d5a38fa95266029e12056a9ce08453
Daniel Martí <mvdan@mvdan.cc>
2017-07-11 09:59:37 +0200

    mw: simplify urlRewrite func signature
    Reuse r for the path. Simplify the tests too.
    
    The test request paths must start with a slash to be valid requests,
    otherwise httptest panics.

--- mw_url_rewrite_test.go
+++ mw_url_rewrite_test.go
@@ -15,43 +15,43 @@ func TestRewriter(t *testing.T) {
 	}{
 		{
 			"Straight",
-			"test/straight/rewrite", "change/to/me",
-			"test/straight/rewrite", "change/to/me",
+			"/test/straight/rewrite", "/change/to/me",
+			"/test/straight/rewrite", "/change/to/me",
 		},
 		{
 			"OneVal",
 			"test/val/(.*)", "change/to/$1",
-			"test/val/VALUE", "change/to/VALUE",
+			"/test/val/VALUE", "change/to/VALUE",
 		},
 		{
 			"ThreeVals",
-			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$1/$2/$3",
-			"test/val/ONE/space/TWO/and/then/THREE", "change/to/ONE/TWO/THREE",
+			"/test/val/(.*)/space/(.*)/and/then/(.*)", "/change/to/$1/$2/$3",
+			"/test/val/ONE/space/TWO/and/then/THREE", "/change/to/ONE/TWO/THREE",
 		},
 		{
 			"Reverse",
-			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$3/$2/$1",
-			"test/val/ONE/space/TWO/and/then/THREE", "change/to/THREE/TWO/ONE",
+			"/test/val/(.*)/space/(.*)/and/then/(.*)", "/change/to/$3/$2/$1",
+			"/test/val/ONE/space/TWO/and/then/THREE", "/change/to/THREE/TWO/ONE",
 		},
 		{
 			"Missing",
-			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$1/$2",
-			"test/val/ONE/space/TWO/and/then/THREE", "change/to/ONE/TWO",
+			"/test/val/(.*)/space/(.*)/and/then/(.*)", "/change/to/$1/$2",
+			"/test/val/ONE/space/TWO/and/then/THREE", "/change/to/ONE/TWO",
 		},
 		{
 			"MissingAgain",
-			"test/val/(.*)/space/(.*)/and/then/(.*)", "change/to/$3/$1",
-			"test/val/ONE/space/TWO/and/then/THREE", "change/to/THREE/ONE",
+			"/test/val/(.*)/space/(.*)/and/then/(.*)", "/change/to/$3/$1",
+			"/test/val/ONE/space/TWO/and/then/THREE", "/change/to/THREE/ONE",
 		},
 		{
 			"QS",
 			"(.*)", "$1&newParam=that",
-			"foo/bar?param1=this", "foo/bar?param1=this&newParam=that",
+			"/foo/bar?param1=this", "/foo/bar?param1=this&newParam=that",
 		},
 		{
 			"QS2",
-			"test/val/(.*)/space/(.*)/and/then(.*)", "change/to/$2/$1$3",
-			"test/val/ONE/space/TWO/and/then?param1=this", "change/to/TWO/ONE?param1=this",
+			"/test/val/(.*)/space/(.*)/and/then(.*)", "/change/to/$2/$1$3",
+			"/test/val/ONE/space/TWO/and/then?param1=this", "/change/to/TWO/ONE?param1=this",
 		},
 	}
 	for _, tc := range tests {
@@ -60,8 +60,8 @@ func TestRewriter(t *testing.T) {
 				MatchPattern: tc.pattern,
 				RewriteTo:    tc.to,
 			}
-			r := httptest.NewRequest("GET", "/", nil)
-			got, err := urlRewrite(&testConf, tc.in, r)
+			r := httptest.NewRequest("GET", tc.in, nil)
+			got, err := urlRewrite(&testConf, r)
 			if err != nil {
 				t.Error("compile failed:", err)
 			}
