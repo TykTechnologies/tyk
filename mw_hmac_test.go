8555b7ece2d0ab069fff232dcc216c2c61173162
Daniel Martí <mvdan@mvdan.cc>
2017-07-11 16:42:37 +0200

    api: deduplicate baseMid from mw chain lines
    This was all because CreateMiddleware needed access to Spec, which is in
    BaseMiddleware.
    
    Make it accessible in the middleware interface instead with Base().
    Remove the BaseMiddleware parameters where funcs already get a
    TykMiddleware.

--- mw_hmac_test.go
+++ mw_hmac_test.go
@@ -59,12 +59,12 @@ func getHMACAuthChain(spec *APISpec) http.Handler {
 	proxyHandler := ProxyHandler(proxy, spec)
 	baseMid := &BaseMiddleware{spec, proxy}
 	chain := alice.New(
-		CreateMiddleware(&IPWhiteListMiddleware{baseMid}, baseMid),
-		CreateMiddleware(&HMACMiddleware{BaseMiddleware: baseMid}, baseMid),
-		CreateMiddleware(&VersionCheck{BaseMiddleware: baseMid}, baseMid),
-		CreateMiddleware(&KeyExpired{baseMid}, baseMid),
-		CreateMiddleware(&AccessRightsCheck{baseMid}, baseMid),
-		CreateMiddleware(&RateLimitAndQuotaCheck{baseMid}, baseMid)).Then(proxyHandler)
+		CreateMiddleware(&IPWhiteListMiddleware{baseMid}),
+		CreateMiddleware(&HMACMiddleware{BaseMiddleware: baseMid}),
+		CreateMiddleware(&VersionCheck{BaseMiddleware: baseMid}),
+		CreateMiddleware(&KeyExpired{baseMid}),
+		CreateMiddleware(&AccessRightsCheck{baseMid}),
+		CreateMiddleware(&RateLimitAndQuotaCheck{baseMid})).Then(proxyHandler)
 
 	return chain
 }
