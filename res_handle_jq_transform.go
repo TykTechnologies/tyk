a50375452c8dd941760544f35a2726447cc50fcb
Julien Barbot <jubarbot@cisco.com>
2017-07-27 17:22:36 +0200

    Improve error checking on JQ transformations

--- res_handle_jq_transform.go
+++ res_handle_jq_transform.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"github.com/mitchellh/mapstructure"
 	"io/ioutil"
 	"net/http"
@@ -42,15 +43,20 @@ func (h *ResponseTransformJQMiddleware) HandleResponse(rw http.ResponseWriter, r
 
 	t := meta.(*TransformJQSpec)
 
-	t.JQFilter.HandleJson(string(body))
-	t.JQFilter.Next()
-	transformed := t.JQFilter.ValueJson()
-
-	//transformed := "{\"name\": \"hello\"}"
-	bodyBuffer := bytes.NewBufferString(transformed)
-	res.Header.Set("Content-Length", strconv.Itoa(bodyBuffer.Len()))
-	res.ContentLength = int64(bodyBuffer.Len())
-	res.Body = ioutil.NopCloser(bodyBuffer)
+	err = t.JQFilter.HandleJson(string(body))
+	if err != nil {
+		return errors.New("Response returned by upstream server is not a valid JSON")
+	}
+	filterResult := t.JQFilter.Next()
+	if filterResult {
+		transformed := t.JQFilter.ValueJson()
+		bodyBuffer := bytes.NewBufferString(transformed)
+		res.Header.Set("Content-Length", strconv.Itoa(bodyBuffer.Len()))
+		res.ContentLength = int64(bodyBuffer.Len())
+		res.Body = ioutil.NopCloser(bodyBuffer)
+	} else {
+		errors.New("Error while applying JQ filter to upstream response")
+	}
 
 	return nil
 }
