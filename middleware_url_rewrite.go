c62f0d58c3b1ba460313d072e1c677d94cf50f11
Daniel Martí <mvdan@mvdan.cc>
2017-07-28 16:22:27 +0200

    session: make MetaData a map[string]string
    That's all we support and what the documentation says. We already
    enforce this on master.
    
    This way, if someone tries to create a key with metadata that isn't an
    object or contains something other than keys, the key creation will just
    error.
    
    Fixes #944.

--- middleware_url_rewrite.go
+++ middleware_url_rewrite.go
@@ -113,34 +113,10 @@ func (u URLRewriter) Rewrite(thisMeta *tykcommon.URLRewriteMeta, path string, us
 			contextKey := strings.Replace(v[0], "$tyk_meta.", "", 1)
 			log.Debug("Replacing: ", v[0])
 
-			tempVal, ok := thisSessionState.MetaData.(map[string]interface{})[contextKey]
+			tempVal, ok := thisSessionState.MetaData[contextKey]
 			if ok {
-				var nVal string
-				if ok {
-					switch tempVal.(type) {
-					case string:
-						nVal = tempVal.(string)
-					case []string:
-						nVal = strings.Join(tempVal.([]string), ",")
-						// Remove empty start
-						nVal = strings.TrimPrefix(nVal, ",")
-					case url.Values:
-						end := len(tempVal.(url.Values))
-						i := 0
-						nVal = ""
-						for key, val := range tempVal.(url.Values) {
-							nVal += key + ":" + strings.Join(val, ",")
-							if i < end-1 {
-								nVal += ";"
-							}
-							i++
-						}
-					default:
-						log.Error("Context variable type is not supported: ", reflect.TypeOf(tempVal))
-					}
-					newpath = strings.Replace(newpath, string(v[0]), url.QueryEscape(nVal), -1)
-				}
-
+				nVal := tempVal
+				newpath = strings.Replace(newpath, string(v[0]), url.QueryEscape(nVal), -1)
 			}
 
 		}
