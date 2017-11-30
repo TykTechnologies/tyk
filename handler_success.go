TykTechnologies/tyk

commit d847b5e3471a1d3c5f8a164360935f92ecc033d3

Don't check http request/response copies for nil
The copy funcs can't return nil, and if RecordDetail(r) is true we are
already passing a non-nil copy.

handler_success.go
@@ -239,18 +239,14 @@ func (s *SuccessHandler) RecordHit(w http.ResponseWriter, r *http.Request, timin
 		rawRequest := ""
 		rawResponse := ""
 		if RecordDetail(r) {
-			if requestCopy != nil {
-				// Get the wire format representation
-				var wireFormatReq bytes.Buffer
-				requestCopy.Write(&wireFormatReq)
-				rawRequest = base64.StdEncoding.EncodeToString(wireFormatReq.Bytes())
-			}
-			if responseCopy != nil {
-				// Get the wire format representation
-				var wireFormatRes bytes.Buffer
-				responseCopy.Write(&wireFormatRes)
-				rawResponse = base64.StdEncoding.EncodeToString(wireFormatRes.Bytes())
-			}
+			// Get the wire format representation
+			var wireFormatReq bytes.Buffer
+			requestCopy.Write(&wireFormatReq)
+			rawRequest = base64.StdEncoding.EncodeToString(wireFormatReq.Bytes())
+			// Get the wire format representation
+			var wireFormatRes bytes.Buffer
+			responseCopy.Write(&wireFormatRes)
+			rawResponse = base64.StdEncoding.EncodeToString(wireFormatRes.Bytes())
 		}
 
 		trackThisEndpoint, ok := context.GetOk(r, TrackThisEndpoint)

handler_error.go
@@ -120,20 +120,14 @@ func (e *ErrorHandler) HandleError(w http.ResponseWriter, r *http.Request, errMs
 			tags = sessionState.(SessionState).Tags
 		}
 
-		var requestCopy *http.Request
-		if RecordDetail(r) {
-			requestCopy = CopyHttpRequest(r)
-		}
-
 		rawRequest := ""
 		rawResponse := ""
 		if RecordDetail(r) {
-			if requestCopy != nil {
-				// Get the wire format representation
-				var wireFormatReq bytes.Buffer
-				requestCopy.Write(&wireFormatReq)
-				rawRequest = base64.StdEncoding.EncodeToString(wireFormatReq.Bytes())
-			}
+			requestCopy := CopyHttpRequest(r)
+			// Get the wire format representation
+			var wireFormatReq bytes.Buffer
+			requestCopy.Write(&wireFormatReq)
+			rawRequest = base64.StdEncoding.EncodeToString(wireFormatReq.Bytes())
 		}
 
 		trackThisEndpoint, ok := context.GetOk(r, TrackThisEndpoint)

middleware_context_vars.go
@@ -40,29 +40,27 @@ func (m *MiddlewareContextVars) ProcessRequest(w http.ResponseWriter, r *http.Re
 	copiedRequest := CopyHttpRequest(r)
 	contextDataObject := make(map[string]interface{})
 
-	if copiedRequest != nil {
-		copiedRequest.ParseForm()
+	copiedRequest.ParseForm()
 
-		// Form params (map[string][]string)
-		contextDataObject["request_data"] = copiedRequest.Form
+	// Form params (map[string][]string)
+	contextDataObject["request_data"] = copiedRequest.Form
 
-		contextDataObject["headers"] = map[string][]string(copiedRequest.Header)
+	contextDataObject["headers"] = map[string][]string(copiedRequest.Header)
 
-		for hname, vals := range copiedRequest.Header {
-			n := "headers_" + strings.Replace(hname, "-", "_", -1)
-			contextDataObject[n] = vals[0]
-		}
-
-		// Path parts
-		segmentedPathArray := strings.Split(copiedRequest.URL.Path, "/")
-		contextDataObject["path_parts"] = segmentedPathArray
-
-		// path data
-		contextDataObject["path"] = copiedRequest.URL.Path
-
-		// IP:Port
-		contextDataObject["remote_addr"] = copiedRequest.RemoteAddr
+	for hname, vals := range copiedRequest.Header {
+		n := "headers_" + strings.Replace(hname, "-", "_", -1)
+		contextDataObject[n] = vals[0]
 	}
+
+	// Path parts
+	segmentedPathArray := strings.Split(copiedRequest.URL.Path, "/")
+	contextDataObject["path_parts"] = segmentedPathArray
+
+	// path data
+	contextDataObject["path"] = copiedRequest.URL.Path
+
+	// IP:Port
+	contextDataObject["remote_addr"] = copiedRequest.RemoteAddr
 
 	context.Set(r, ContextData, contextDataObject)
 

commit 71faded909bd0bcf6fae6af6c469cec2e1e8fa09

analytics: don't panic on nil response copy
This was a regression introduced by myself in d847b5e3. I was wrong in
saying that these were never nil. In one scenario, responseCopy can be
nil - see the added comments.

Perhaps having a non-nil response copy here would be a better approach,
but do the safer revert for now and leave it as a TODO for a later time.

Fixes #1327.

handler_success.go
@@ -110,10 +110,19 @@ func (s *SuccessHandler) RecordHit(r *http.Request, timing int64, code int, requ
 			var wireFormatReq bytes.Buffer
 			requestCopy.Write(&wireFormatReq)
 			rawRequest = base64.StdEncoding.EncodeToString(wireFormatReq.Bytes())
-			// Get the wire format representation
-			var wireFormatRes bytes.Buffer
-			responseCopy.Write(&wireFormatRes)
-			rawResponse = base64.StdEncoding.EncodeToString(wireFormatRes.Bytes())
+			// responseCopy, unlike requestCopy, can be nil
+			// here - if the response was cached in
+			// mw_redis_cache, RecordHit gets passed a nil
+			// response copy.
+			// TODO: pass a copy of the cached response in
+			// mw_redis_cache instead? is there a reason not
+			// to include that in the analytics?
+			if responseCopy != nil {
+				// Get the wire format representation
+				var wireFormatRes bytes.Buffer
+				responseCopy.Write(&wireFormatRes)
+				rawResponse = base64.StdEncoding.EncodeToString(wireFormatRes.Bytes())
+			}
 		}
 
 		trackEP := false