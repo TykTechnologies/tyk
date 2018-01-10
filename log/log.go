41c8ff3a2fdafb768c275c3f3c5e7dc5fcc307b5
Ilya Danilkin <nezorflame@gmail.com>
2018-01-10 20:49:34 +0300

    fix missing tykcommon-logger package

--- log/log.go
+++ log/log.go
@@ -1,4 +1,4 @@
-package logger
+package log
 
 import (
 	"os"
@@ -14,33 +14,25 @@ var rawLog = logrus.New()
 type RawFormatter struct{}
 
 func (f *RawFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-    return []byte(entry.Message), nil
+	return []byte(entry.Message), nil
 }
 
 func init() {
 	log.Formatter = new(prefixed.TextFormatter)
-    rawLog.Formatter = new(RawFormatter)
+	rawLog.Formatter = new(RawFormatter)
 }
 
-func GetLogger() *logrus.Logger {
-	level := os.Getenv("TYK_LOGLEVEL")
-	if level == "" {
-		level = "info"
-	}
-
-	switch strings.ToLower(level) {
-	case "warn":
-		log.Level = logrus.WarnLevel
-	case "info":
-		log.Level = logrus.InfoLevel
-	case "debug":
-		log.Level = logrus.DebugLevel
+func Get() *logrus.Logger {
+	switch strings.ToLower(os.Getenv("TYK_LOGLEVEL")) {
 	case "error":
 		log.Level = logrus.ErrorLevel
+	case "warn":
+		log.Level = logrus.WarnLevel
+	case "debug":
+		log.Level = logrus.DebugLevel
 	default:
 		log.Level = logrus.InfoLevel
 	}
-
 	return log
 }
 
