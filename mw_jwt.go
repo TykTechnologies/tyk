76bffaf05953d86752501128f7f6632995abf909
Leonid Bugaev <leonsbox@gmail.com>
2018-01-12 20:47:05 +0200

    Fix when JWK url is base64 encoded
    Added additional check for URL when payload is decoded
    
    Additionally added test helpers for JWK tests

--- mw_jwt.go
+++ mw_jwt.go
@@ -122,7 +122,6 @@ func (k *JWTMiddleware) getSecret(token *jwt.Token) ([]byte, error) {
 	config := k.Spec.APIDefinition
 	// Check for central JWT source
 	if config.JWTSource != "" {
-
 		// Is it a URL?
 		if httpScheme.MatchString(config.JWTSource) {
 			secret, err := k.getSecretFromURL(config.JWTSource, token.Header["kid"].(string), k.Spec.JWTSigningMethod)
@@ -138,6 +137,17 @@ func (k *JWTMiddleware) getSecret(token *jwt.Token) ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
+
+		// Is decoded url too?
+		if httpScheme.MatchString(string(decodedCert)) {
+			secret, err := k.getSecretFromURL(string(decodedCert), token.Header["kid"].(string), k.Spec.JWTSigningMethod)
+			if err != nil {
+				return nil, err
+			}
+
+			return secret, nil
+		}
+
 		return decodedCert, nil
 	}
 
