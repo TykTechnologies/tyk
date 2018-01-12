d6d6c4d4b58f4a75ce401a0b708a527fbf2fbf34
Yaara Letz <3155222+letzya@users.noreply.github.com>
2018-01-12 11:50:33 +0000

    Changes after rebase against master.

--- mw_jwt.go
+++ mw_jwt.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/dgrijalva/jwt-go"
 	cache "github.com/pmylund/go-cache"
 
@@ -171,16 +170,19 @@ func (k *JWTMiddleware) getSecret(token *jwt.Token) ([]byte, error) {
 	return []byte(session.JWTData.Secret), nil
 }
 
+func (k *JWTMiddleware) getPolicyIDFromToken(token *jwt.Token) (string, bool) {
+	policyID, foundPolicy := token.Claims.(jwt.MapClaims)[k.Spec.JWTPolicyFieldName].(string)
+	if !foundPolicy {
+		log.Error("Could not identify a policy to apply to this token from field!")
+		return "", false
+	}
+
+	return policyID, true
+}
+
 func (k *JWTMiddleware) getBasePolicyID(token *jwt.Token) (string, bool) {
 	if k.Spec.JWTPolicyFieldName != "" {
-		basePolicyID, foundPolicy := token.Claims.(jwt.MapClaims)[k.Spec.JWTPolicyFieldName].(string)
-		if !foundPolicy {
-			log.Error("Could not identify a policy to apply to this token from field!")
-			return "", false
-		}
-
-		return basePolicyID, true
-
+		return k.getPolicyIDFromToken(token)
 	} else if k.Spec.JWTClientIDBaseField != "" {
 		clientID, clientIDFound := token.Claims.(jwt.MapClaims)[k.Spec.JWTClientIDBaseField].(string)
 		if !clientIDFound {
@@ -238,6 +240,7 @@ func (k *JWTMiddleware) processCentralisedJWT(r *http.Request, token *jwt.Token)
 		// We need a base policy as a template, either get it from the token itself OR a proxy client ID within Tyk
 		basePolicyID, foundPolicy := k.getBasePolicyID(token)
 		if !foundPolicy {
+			k.reportLoginFailure(baseFieldData, r)
 			return errors.New("Key not authorized: no matching policy found"), 403
 		}
 
@@ -266,6 +269,25 @@ func (k *JWTMiddleware) processCentralisedJWT(r *http.Request, token *jwt.Token)
 		k.reportLoginFailure(baseFieldData, r)
 		log.Error("Could not find a valid policy to apply to this token!")
 		return errors.New("Key not authorized: no matching policy"), 403
+	} else if k.Spec.JWTPolicyFieldName != "" {
+		// extract policy ID from JWT token
+		policyID, foundPolicy := k.getPolicyIDFromToken(token)
+		if !foundPolicy {
+			k.reportLoginFailure(baseFieldData, r)
+			return errors.New("Key not authorized: no matching policy found"), 403
+		}
+		// check if policy in session is the same as policy set in the given token
+		pols := session.PolicyIDs()
+		if len(pols) < 1 {
+			k.reportLoginFailure(baseFieldData, r)
+			log.Error("No policies for the found session. Failing Request.")
+			return errors.New("Key not authorized: no matching policy found"), 403
+		}
+		if pols[0] != policyID {
+			k.reportLoginFailure(baseFieldData, r)
+			log.Error("Policy ID found in active session is not the same as policy in token!")
+			return errors.New("Key not authorized: no matching policy"), 403
+		}
 	}
 
 	log.Debug("Key found")
@@ -330,10 +352,8 @@ func (k *JWTMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Request, _
 
 	if rawJWT == "" {
 		// No header value, fail
-		log.WithFields(logrus.Fields{
-			"path":   r.URL.Path,
-			"origin": requestIP(r),
-		}).Info("Attempted access with malformed header, no JWT auth header found.")
+		logEntry := getLogEntryForRequest(r, "", nil)
+		logEntry.Info("Attempted access with malformed header, no JWT auth header found.")
 
 		log.Debug("Looked in: ", config.AuthHeaderName)
 		log.Debug("Raw data was: ", rawJWT)
@@ -398,16 +418,11 @@ func (k *JWTMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Request, _
 		// No, let's try one-to-one mapping
 		return k.processOneToOneTokenMap(r, token)
 	}
-	log.WithFields(logrus.Fields{
-		"path":   r.URL.Path,
-		"origin": requestIP(r),
-	}).Info("Attempted JWT access with non-existent key.")
+	logEntry := getLogEntryForRequest(r, "", nil)
+	logEntry.Info("Attempted JWT access with non-existent key.")
 
 	if err != nil {
-		log.WithFields(logrus.Fields{
-			"path":   r.URL.Path,
-			"origin": requestIP(r),
-		}).Error("JWT validation error: ", err)
+		logEntry.Error("JWT validation error: ", err)
 	}
 
 	k.reportLoginFailure(tykId, r)
