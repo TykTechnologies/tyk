fa97a1f9692f8d68bb8886b48729582c808234f4
joshblakeley <jtblakeley@gmail.com>
2018-02-13 11:06:46 +0000

    batch requests client checks for ssl config and add test

--- batch_requests_test.go
+++ batch_requests_test.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/TykTechnologies/tyk/config"
 	"github.com/TykTechnologies/tyk/test"
 )
 
@@ -64,4 +72,123 @@ func TestBatch(t *testing.T) {
 			t.Error("Url order not match:", string(batchResponse[0]["relative_url"]))
 		}
 	}
+}
+
+var virtBatchTest = `function batchTest (request, session, config) {
+    // Set up a response object
+    var response = {
+        Body: "",
+        Headers: {
+            "content-type": "application/json"
+        },
+        Code: 202
+    }
+    
+    // Batch request
+    var batch = {
+        "requests": [
+			{
+				"method": "GET",
+				"headers": {},
+                "body": "",
+				"relative_url": "{upstream_URL}"
+			},
+			{
+				"method": "GET",
+				"headers": {},
+                "body": "",
+				"relative_url": "{upstream_URL}"
+			}
+		],
+        "suppress_parallel_execution": false
+    }
+	
+    var newBody = TykBatchRequest(JSON.stringify(batch))
+	var asJS = JSON.parse(newBody)
+	for (var i in asJS) {
+		if (asJS[i].code == 0){
+			response.Code = 404
+		}	
+    }
+    return TykJsResponse(response, session.meta_data)
+	
+}`
+
+func TestSSLBatch(t *testing.T) {
+
+	_, _, combinedClientPEM, clientCert := genCertificate(&x509.Certificate{})
+	clientCert.Leaf, _ = x509.ParseCertificate(clientCert.Certificate[0])
+
+	upstream := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}))
+
+	// Mutual TLS protected upstream
+	pool := x509.NewCertPool()
+	pool.AddCert(clientCert.Leaf)
+	upstream.TLS = &tls.Config{
+		ClientAuth:         tls.RequireAndVerifyClientCert,
+		ClientCAs:          pool,
+		InsecureSkipVerify: true,
+	}
+
+	upstream.StartTLS()
+	defer upstream.Close()
+
+	_, _, combinedPEM, _ := genServerCertificate()
+	serverCertID, _ := CertificateManager.Add(combinedPEM, "")
+	defer CertificateManager.Delete(serverCertID)
+
+	clientCertID, _ := CertificateManager.Add(combinedClientPEM, "")
+	defer CertificateManager.Delete(clientCertID)
+
+	virtBatchTest = strings.Replace(virtBatchTest, "{upstream_URL}", upstream.URL, 2)
+	defer upstream.Close()
+	log.Debug(upstream.URL)
+	upstreamHost := strings.TrimPrefix(upstream.URL, "https://")
+
+	config.Global.Security.Certificates.Upstream = map[string]string{upstreamHost: clientCertID}
+	config.Global.HttpServerOptions.UseSSL = true
+	config.Global.HttpServerOptions.SSLCertificates = []string{serverCertID}
+	config.Global.ProxySSLInsecureSkipVerify = true
+
+	defer resetTestConfig()
+
+	ts := newTykTestServer()
+	defer ts.Close()
+
+	buildAndLoadAPI(func(spec *APISpec) {
+		spec.Proxy.ListenPath = "/"
+		virtualMeta := apidef.VirtualMeta{
+			ResponseFunctionName: "batchTest",
+			FunctionSourceType:   "blob",
+			FunctionSourceURI:    base64.StdEncoding.EncodeToString([]byte(virtBatchTest)),
+			Path:                 "/virt",
+			Method:               "GET",
+		}
+		v := spec.VersionData.Versions["v1"]
+		v.UseExtendedPaths = true
+		v.ExtendedPaths = apidef.ExtendedPathsSet{
+			Virtual: []apidef.VirtualMeta{virtualMeta},
+		}
+		spec.VersionData.Versions["v1"] = v
+	})
+	client := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true,
+	}}}
+
+	t.Run("Skip verification", func(t *testing.T) {
+		ts.Run(t, test.TestCase{
+			Path: "/virt", Code: 202, Client: client,
+		})
+	})
+
+	t.Run("Verification required", func(t *testing.T) {
+
+		config.Global.ProxySSLInsecureSkipVerify = false
+
+		ts.Run(t, test.TestCase{
+			Path: "/virt", Code: 404, Client: client,
+		})
+	})
+
 }
