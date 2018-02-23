23c32e478c35ef7d48792ce4c10bccec797e07d9
Luan van Pletsen <luanvanpleten@msn.com>
2018-02-23 11:19:50 +0000

    Rename ottoAuthExample to match camelCase

--- middleware/ottoAuthExample.js
+++ middleware/ottoAuthExample.js
@@ -1,26 +1,26 @@
 log("====> JS Auth initialising");
 
-var OttoAuthExample = new TykJS.TykMiddleware.NewMiddleware({});
+var ottoAuthExample = new TykJS.TykMiddleware.NewMiddleware({});
 
-OttoAuthExample.NewProcessRequest(function(request, session) {
-    log("----> Running OttoAuthExample JSVM Auth Middleware")
-   
+ottoAuthExample.NewProcessRequest(function(request, session) {
+    log("----> Running ottoAuthExample JSVM Auth Middleware")
+
     var thisToken = request.Params["auth"];
-   
+
     if (thisToken == undefined) {
         // no token at all?
         request.ReturnOverrides.ResponseCode = 401
         request.ReturnOverrides.ResponseError = 'Header missing (JS middleware)'
-        return OttoAuthExample.ReturnData(request, {});
+        return ottoAuthExample.ReturnData(request, {});
     }
- 
+
     if (thisToken != "foobar") {
         request.ReturnOverrides.ResponseCode = 401
         request.ReturnOverrides.ResponseError = 'Not authorized (JS middleware)'
-        return OttoAuthExample.ReturnData(request, {});
-    } 
+        return ottoAuthExample.ReturnData(request, {});
+    }
 
-    var thisSession = {    
+    var thisSession = {
         "allowance": 100,
         "rate": 100,
         "per": 1,
@@ -28,9 +28,9 @@ OttoAuthExample.NewProcessRequest(function(request, session) {
         "quota_renews": 1406121006,
         "access_rights": {}
     };
-    
-    return OttoAuthExample.ReturnAuthData(request, thisSession);
+
+    return ottoAuthExample.ReturnAuthData(request, thisSession);
 });
- 
+
 // Ensure init with a post-declaration log message
 log("====> JS Auth initialised");
