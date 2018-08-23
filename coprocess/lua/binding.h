56aedb085a13b1bda0854b943b7ecf675fb17d3f
Matias Insaurralde <matias@insaurral.de>
2018-08-23 05:50:16 -0400

    coprocess: modify Lua code to match dispatcher arguments

--- coprocess/lua/binding.h
+++ coprocess/lua/binding.h
@@ -8,7 +8,7 @@
 
 static void LuaInit();
 
-static struct CoProcessMessage* LuaDispatchHook(struct CoProcessMessage*);
+static void LuaDispatchHook(struct CoProcessMessage*, struct CoProcessMessage*);
 static void LuaDispatchEvent(char*);
 
 void LoadCachedMiddleware(void*);
