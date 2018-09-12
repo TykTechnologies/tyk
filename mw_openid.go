0f9435c7591eb3abd9389b9758b9020c6f7c66d4
joshblakeley <jtblakeley@gmail.com>
2018-09-12 15:12:41 +0100

    check for policy change and apply to access rights each request

--- mw_openid.go
+++ mw_openid.go
@@ -152,6 +152,7 @@ func (k *OpenIDMW) ProcessRequest(w http.ResponseWriter, r *http.Request, _ inte
 	data := []byte(ouser.ID)
 	keyID := fmt.Sprintf("%x", md5.Sum(data))
 	sessionID := generateToken(k.Spec.OrgID, keyID)
+
 	if k.Spec.OpenIDOptions.SegregateByClient {
 		// We are segregating by client, so use it as part of the internal token
 		logger.Debug("Client ID:", clientID)
@@ -183,6 +184,12 @@ func (k *OpenIDMW) ProcessRequest(w http.ResponseWriter, r *http.Request, _ inte
 
 		// Update the session in the session manager in case it gets called again
 		logger.Debug("Policy applied to key")
+	}
+	// apply new policy to session if any and update session
+	session.SetPolicies(policyID)
+	if err := k.ApplyPolicies(sessionID, &session); err != nil {
+		k.Logger().WithError(err).Error("Could not apply new policy from OIDC client to session")
+		return errors.New("Key not authorized: could not apply new policy"), http.StatusForbidden
 	}
 
 	// 4. Set session state on context, we will need it later
