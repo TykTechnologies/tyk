3bbca10dafe9d2c293c6643baba40a1429c16a01
Leonid Bugaev <leonsbox@gmail.com>
2018-11-13 19:31:52 +0300

    Added new `health_check_endpoint_name` option
    At the moment we expose fixed “/hello” endpoint name for gateway/api
    healtchecks. It can cause issues from security point of view, since
    expose which Gateway is used to handle traffic. Should be a way to
    rename it to some secret URL.

--- main.go
+++ main.go
@@ -879,6 +879,10 @@ func afterConfSetup(conf *config.Config) {
 	rpc.GlobalRPCCallTimeout = time.Second * time.Duration(conf.SlaveOptions.CallTimeout)
 	initGenericEventHandlers(conf)
 	regexp.ResetCache(time.Second*time.Duration(conf.RegexpCacheExpire), !conf.DisableRegexpCache)
+
+	if conf.HealthCheckEndpointName == "" {
+		conf.HealthCheckEndpointName = "hello"
+	}
 }
 
 var hostDetails struct {
@@ -1350,7 +1354,7 @@ func listen(listener, controlListener net.Listener, err error) {
 	mainLog.Info("--> Listening on port: ", config.Global().ListenPort)
 	mainLog.Info("--> PID: ", hostDetails.PID)
 
-	mainRouter.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	mainRouter.HandleFunc("/"+config.Global().HealthCheckEndpointName, func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Tiki")
 	})
 
