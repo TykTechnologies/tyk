39277b10f5959fb714c0b118c87ffda59520b419
Leonid Bugaev <leonsbox@gmail.com>
2019-01-18 19:03:44 +0100

    Move API schema to Tyk repo (#2006)
    Having it in analytics repo cause a lot of issues, since we constantly forgot to update it, when adding new fields.
    
    In addition to adding a schema, this PR also ensure automatic tests, which run schema validation against current API definition.
    
    Also, added `DummyAPI` method, which returns pre-populated API definition record, with initialized nested fields. This method used for both schema tests, and for the Dashboard to generate example JSON object of an API.
    
    Manually merged based on f9e008e2fdcb89b9d8a7c50622dc3638936d4afd

---
+++ apidef/api_definition_test.go
@@ -0,0 +1,24 @@
+package apidef
+
+import (
+	"testing"
+
+	schema "github.com/xeipuuv/gojsonschema"
+)
+
+func TestSchema(t *testing.T) {
+	schemaLoader := schema.NewBytesLoader([]byte(Schema))
+
+	spec := DummyAPI()
+	goLoader := schema.NewGoLoader(spec)
+	result, err := schema.Validate(schemaLoader, goLoader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !result.Valid() {
+		for _, err := range result.Errors() {
+			t.Error(err)
+		}
+	}
+}
