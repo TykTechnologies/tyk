cb5ffbf8d8d63eeae8e75eefbc1d389518fcca46
Leonid Bugaev <leonsbox@gmail.com>
2019-02-27 22:44:42 +0100

    Gofmt fixes

--- control_api.go
+++ control_api.go
@@ -1,100 +1,100 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    pprof_http "net/http/pprof"
+	"fmt"
+	"net/http"
+	pprof_http "net/http/pprof"
 
-    "github.com/lonelycode/osin"
+	"github.com/lonelycode/osin"
 
-    "github.com/TykTechnologies/tyk/cli"
-    "github.com/TykTechnologies/tyk/config"
+	"github.com/TykTechnologies/tyk/cli"
+	"github.com/TykTechnologies/tyk/config"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 // Set up default Tyk control API endpoints - these are global, so need to be added first
 func loadAPIEndpoints(router *mux.Router) {
-    hostname := config.Global().HostName
-    if config.Global().ControlAPIHostname != "" {
-        hostname = config.Global().ControlAPIHostname
-    }
+	hostname := config.Global().HostName
+	if config.Global().ControlAPIHostname != "" {
+		hostname = config.Global().ControlAPIHostname
+	}
 
-    if router == nil {
-        router = defaultProxyMux.router(config.Global().ControlAPIPort)
-        if router == nil {
-            log.Error("Can't find control API router")
-            return
-        }
-    }
+	if router == nil {
+		router = defaultProxyMux.router(config.Global().ControlAPIPort)
+		if router == nil {
+			log.Error("Can't find control API router")
+			return
+		}
+	}
 
-    if hostname != "" {
-        router = router.Host(hostname).Subrouter()
-        mainLog.Info("Control API hostname set: ", hostname)
-    }
+	if hostname != "" {
+		router = router.Host(hostname).Subrouter()
+		mainLog.Info("Control API hostname set: ", hostname)
+	}
 
-    if *cli.HTTPProfile {
-        router.HandleFunc("/debug/pprof/profile", pprof_http.Profile)
-        router.HandleFunc("/debug/pprof/{_:.*}", pprof_http.Index)
-    }
+	if *cli.HTTPProfile {
+		router.HandleFunc("/debug/pprof/profile", pprof_http.Profile)
+		router.HandleFunc("/debug/pprof/{_:.*}", pprof_http.Index)
+	}
 
-    router.HandleFunc("/"+config.Global().HealthCheckEndpointName, func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello")
-    })
+	router.HandleFunc("/"+config.Global().HealthCheckEndpointName, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello")
+	})
 
-    r := mux.NewRouter()
-    r.MethodNotAllowedHandler = MethodNotAllowedHandler{}
+	r := mux.NewRouter()
+	r.MethodNotAllowedHandler = MethodNotAllowedHandler{}
 
-    router.PathPrefix("/tyk/").Handler(http.StripPrefix("/tyk",
-        stripSlashes(checkIsAPIOwner(controlAPICheckClientCertificate("/gateway/client", InstrumentationMW(r)))),
-    ))
+	router.PathPrefix("/tyk/").Handler(http.StripPrefix("/tyk",
+		stripSlashes(checkIsAPIOwner(controlAPICheckClientCertificate("/gateway/client", InstrumentationMW(r)))),
+	))
 
-    mainLog.Info("Initialising Tyk REST API Endpoints")
+	mainLog.Info("Initialising Tyk REST API Endpoints")
 
-    // set up main API handlers
-    r.HandleFunc("/reload/group", groupResetHandler).Methods("GET")
-    r.HandleFunc("/reload", resetHandler(nil)).Methods("GET")
+	// set up main API handlers
+	r.HandleFunc("/reload/group", groupResetHandler).Methods("GET")
+	r.HandleFunc("/reload", resetHandler(nil)).Methods("GET")
 
-    if !isRPCMode() {
-        r.HandleFunc("/org/keys", orgHandler).Methods("GET")
-        r.HandleFunc("/org/keys/{keyName:[^/]*}", orgHandler).Methods("POST", "PUT", "GET", "DELETE")
-        r.HandleFunc("/keys/policy/{keyName}", policyUpdateHandler).Methods("POST")
-        r.HandleFunc("/keys/create", createKeyHandler).Methods("POST")
-        r.HandleFunc("/apis", apiHandler).Methods("GET", "POST", "PUT", "DELETE")
-        r.HandleFunc("/apis/{apiID}", apiHandler).Methods("GET", "POST", "PUT", "DELETE")
-        r.HandleFunc("/health", healthCheckhandler).Methods("GET")
-        r.HandleFunc("/oauth/clients/create", createOauthClient).Methods("POST")
-        r.HandleFunc("/oauth/clients/{apiID}/{keyName:[^/]*}", oAuthClientHandler).Methods("PUT")
-        r.HandleFunc("/oauth/refresh/{keyName}", invalidateOauthRefresh).Methods("DELETE")
-        r.HandleFunc("/cache/{apiID}", invalidateCacheHandler).Methods("DELETE")
-    } else {
-        mainLog.Info("Node is slaved, REST API minimised")
-    }
+	if !isRPCMode() {
+		r.HandleFunc("/org/keys", orgHandler).Methods("GET")
+		r.HandleFunc("/org/keys/{keyName:[^/]*}", orgHandler).Methods("POST", "PUT", "GET", "DELETE")
+		r.HandleFunc("/keys/policy/{keyName}", policyUpdateHandler).Methods("POST")
+		r.HandleFunc("/keys/create", createKeyHandler).Methods("POST")
+		r.HandleFunc("/apis", apiHandler).Methods("GET", "POST", "PUT", "DELETE")
+		r.HandleFunc("/apis/{apiID}", apiHandler).Methods("GET", "POST", "PUT", "DELETE")
+		r.HandleFunc("/health", healthCheckhandler).Methods("GET")
+		r.HandleFunc("/oauth/clients/create", createOauthClient).Methods("POST")
+		r.HandleFunc("/oauth/clients/{apiID}/{keyName:[^/]*}", oAuthClientHandler).Methods("PUT")
+		r.HandleFunc("/oauth/refresh/{keyName}", invalidateOauthRefresh).Methods("DELETE")
+		r.HandleFunc("/cache/{apiID}", invalidateCacheHandler).Methods("DELETE")
+	} else {
+		mainLog.Info("Node is slaved, REST API minimised")
+	}
 
-    r.HandleFunc("/debug", traceHandler).Methods("POST")
+	r.HandleFunc("/debug", traceHandler).Methods("POST")
 
-    r.HandleFunc("/keys", keyHandler).Methods("POST", "PUT", "GET", "DELETE")
-    r.HandleFunc("/keys/{keyName:[^/]*}", keyHandler).Methods("POST", "PUT", "GET", "DELETE")
-    r.HandleFunc("/certs", certHandler).Methods("POST", "GET")
-    r.HandleFunc("/certs/{certID:[^/]*}", certHandler).Methods("POST", "GET", "DELETE")
-    r.HandleFunc("/oauth/clients/{apiID}", oAuthClientHandler).Methods("GET", "DELETE")
-    r.HandleFunc("/oauth/clients/{apiID}/{keyName:[^/]*}", oAuthClientHandler).Methods("GET", "DELETE")
-    r.HandleFunc("/oauth/clients/{apiID}/{keyName}/tokens", oAuthClientTokensHandler).Methods("GET")
+	r.HandleFunc("/keys", keyHandler).Methods("POST", "PUT", "GET", "DELETE")
+	r.HandleFunc("/keys/{keyName:[^/]*}", keyHandler).Methods("POST", "PUT", "GET", "DELETE")
+	r.HandleFunc("/certs", certHandler).Methods("POST", "GET")
+	r.HandleFunc("/certs/{certID:[^/]*}", certHandler).Methods("POST", "GET", "DELETE")
+	r.HandleFunc("/oauth/clients/{apiID}", oAuthClientHandler).Methods("GET", "DELETE")
+	r.HandleFunc("/oauth/clients/{apiID}/{keyName:[^/]*}", oAuthClientHandler).Methods("GET", "DELETE")
+	r.HandleFunc("/oauth/clients/{apiID}/{keyName}/tokens", oAuthClientTokensHandler).Methods("GET")
 
-    mainLog.Debug("Loaded API Endpoints")
+	mainLog.Debug("Loaded API Endpoints")
 }
 
 func controlAPICheckClientCertificate(certLevel string, next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if config.Global().Security.ControlAPIUseMutualTLS {
-            if err := CertificateManager.ValidateRequestCertificate(config.Global().Security.Certificates.ControlAPI, r); err != nil {
-                doJSONWrite(w, http.StatusForbidden, apiError(err.Error()))
-                return
-            }
-        }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if config.Global().Security.ControlAPIUseMutualTLS {
+			if err := CertificateManager.ValidateRequestCertificate(config.Global().Security.Certificates.ControlAPI, r); err != nil {
+				doJSONWrite(w, http.StatusForbidden, apiError(err.Error()))
+				return
+			}
+		}
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
 
 // checkIsAPIOwner will ensure that the accessor of the tyk API has the
@@ -102,56 +102,56 @@ func controlAPICheckClientCertificate(certLevel string, next http.Handler) http.
 // client and the owner and is set in the tyk.conf file. This should
 // never be made public!
 func checkIsAPIOwner(next http.Handler) http.Handler {
-    secret := config.Global().Secret
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        tykAuthKey := r.Header.Get("X-Tyk-Authorization")
-        if tykAuthKey != secret {
-            // Error
-            mainLog.Warning("Attempted administrative access with invalid or missing key!")
+	secret := config.Global().Secret
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tykAuthKey := r.Header.Get("X-Tyk-Authorization")
+		if tykAuthKey != secret {
+			// Error
+			mainLog.Warning("Attempted administrative access with invalid or missing key!")
 
-            doJSONWrite(w, http.StatusForbidden, apiError("Forbidden"))
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+			doJSONWrite(w, http.StatusForbidden, apiError("Forbidden"))
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func generateOAuthPrefix(apiID string) string {
-    return "oauth-data." + apiID + "."
+	return "oauth-data." + apiID + "."
 }
 
 // Create API-specific OAuth handlers and respective auth servers
 func addOAuthHandlers(spec *APISpec, muxer *mux.Router) *OAuthManager {
-    apiAuthorizePath := spec.Proxy.ListenPath + "tyk/oauth/authorize-client{_:/?}"
-    clientAuthPath := spec.Proxy.ListenPath + "oauth/authorize{_:/?}"
-    clientAccessPath := spec.Proxy.ListenPath + "oauth/token{_:/?}"
+	apiAuthorizePath := spec.Proxy.ListenPath + "tyk/oauth/authorize-client{_:/?}"
+	clientAuthPath := spec.Proxy.ListenPath + "oauth/authorize{_:/?}"
+	clientAccessPath := spec.Proxy.ListenPath + "oauth/token{_:/?}"
 
-    serverConfig := osin.NewServerConfig()
-    serverConfig.ErrorStatusCode = http.StatusForbidden
-    serverConfig.AllowedAccessTypes = spec.Oauth2Meta.AllowedAccessTypes
-    serverConfig.AllowedAuthorizeTypes = spec.Oauth2Meta.AllowedAuthorizeTypes
-    serverConfig.RedirectUriSeparator = config.Global().OauthRedirectUriSeparator
+	serverConfig := osin.NewServerConfig()
+	serverConfig.ErrorStatusCode = http.StatusForbidden
+	serverConfig.AllowedAccessTypes = spec.Oauth2Meta.AllowedAccessTypes
+	serverConfig.AllowedAuthorizeTypes = spec.Oauth2Meta.AllowedAuthorizeTypes
+	serverConfig.RedirectUriSeparator = config.Global().OauthRedirectUriSeparator
 
-    prefix := generateOAuthPrefix(spec.APIID)
-    storageManager := getGlobalStorageHandler(prefix, false)
-    storageManager.Connect()
-    osinStorage := &RedisOsinStorageInterface{storageManager, spec.SessionManager} //TODO: Needs storage manager from APISpec
+	prefix := generateOAuthPrefix(spec.APIID)
+	storageManager := getGlobalStorageHandler(prefix, false)
+	storageManager.Connect()
+	osinStorage := &RedisOsinStorageInterface{storageManager, spec.SessionManager} //TODO: Needs storage manager from APISpec
 
-    osinServer := TykOsinNewServer(serverConfig, osinStorage)
+	osinServer := TykOsinNewServer(serverConfig, osinStorage)
 
-    oauthManager := OAuthManager{spec, osinServer}
-    oauthHandlers := OAuthHandlers{oauthManager}
+	oauthManager := OAuthManager{spec, osinServer}
+	oauthHandlers := OAuthHandlers{oauthManager}
 
-    muxer.Handle(apiAuthorizePath, checkIsAPIOwner(allowMethods(oauthHandlers.HandleGenerateAuthCodeData, "POST")))
-    muxer.HandleFunc(clientAuthPath, allowMethods(oauthHandlers.HandleAuthorizePassthrough, "GET", "POST"))
-    muxer.HandleFunc(clientAccessPath, allowMethods(oauthHandlers.HandleAccessRequest, "GET", "POST"))
+	muxer.Handle(apiAuthorizePath, checkIsAPIOwner(allowMethods(oauthHandlers.HandleGenerateAuthCodeData, "POST")))
+	muxer.HandleFunc(clientAuthPath, allowMethods(oauthHandlers.HandleAuthorizePassthrough, "GET", "POST"))
+	muxer.HandleFunc(clientAccessPath, allowMethods(oauthHandlers.HandleAccessRequest, "GET", "POST"))
 
-    return &oauthManager
+	return &oauthManager
 }
 
 func addBatchEndpoint(spec *APISpec, muxer *mux.Router) {
-    mainLog.Debug("Batch requests enabled for API")
-    apiBatchPath := spec.Proxy.ListenPath + "tyk/batch/"
-    batchHandler := BatchRequestHandler{API: spec}
-    muxer.HandleFunc(apiBatchPath, batchHandler.HandleBatchRequest)
+	mainLog.Debug("Batch requests enabled for API")
+	apiBatchPath := spec.Proxy.ListenPath + "tyk/batch/"
+	batchHandler := BatchRequestHandler{API: spec}
+	muxer.HandleFunc(apiBatchPath, batchHandler.HandleBatchRequest)
 }
--- proxy_muxer.go
+++ proxy_muxer.go
@@ -1,246 +1,246 @@
 package main
 
 import (
-    "context"
-    "crypto/tls"
-    "fmt"
-    "net"
-    "net/http"
-    "strconv"
-    "sync"
-    "time"
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
-    "github.com/TykTechnologies/tyk/config"
+	"github.com/TykTechnologies/tyk/config"
 
-    "golang.org/x/net/http2"
+	"golang.org/x/net/http2"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 // handleWrapper's only purpose is to allow router to be dynamically replaced
 type handleWrapper struct {
-    router *mux.Router
+	router *mux.Router
 }
 
 func (h *handleWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    AddNewRelicInstrumentation(NewRelicApplication, h.router)
+	AddNewRelicInstrumentation(NewRelicApplication, h.router)
 
-    // make request body to be nopCloser and re-readable before serve it through chain of middlewares
-    nopCloseRequestBody(r)
+	// make request body to be nopCloser and re-readable before serve it through chain of middlewares
+	nopCloseRequestBody(r)
 
-    h.router.ServeHTTP(w, r)
+	h.router.ServeHTTP(w, r)
 }
 
 type proxy struct {
-    listener   net.Listener
-    port       int
-    protocol   string
-    httpServer *http.Server
+	listener   net.Listener
+	port       int
+	protocol   string
+	httpServer *http.Server
 }
 
 type proxyMux struct {
-    sync.RWMutex
-    proxies []*proxy
+	sync.RWMutex
+	proxies []*proxy
 }
 
 var defaultProxyMux = &proxyMux{}
 
 func (m *proxyMux) getProxy(listenPort int) *proxy {
-    if listenPort == 0 {
-        listenPort = config.Global().ListenPort
-    }
+	if listenPort == 0 {
+		listenPort = config.Global().ListenPort
+	}
 
-    for _, p := range m.proxies {
-        if p.port == listenPort {
-            return p
-        }
-    }
+	for _, p := range m.proxies {
+		if p.port == listenPort {
+			return p
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func (m *proxyMux) router(listenPort int) *mux.Router {
-    if proxy := m.getProxy(listenPort); proxy != nil {
-        return proxy.httpServer.Handler.(*handleWrapper).router
-    }
+	if proxy := m.getProxy(listenPort); proxy != nil {
+		return proxy.httpServer.Handler.(*handleWrapper).router
+	}
 
-    return nil
+	return nil
 }
 
 func (m *proxyMux) updateRouter(listenPort int, router *mux.Router) {
-    if proxy := m.getProxy(listenPort); proxy != nil {
-        router.SkipClean(config.Global().HttpServerOptions.SkipURLCleaning)
-        proxy.httpServer.Handler.(*handleWrapper).router = router
-    } else {
-        if err := m.serve(listenPort, ""); err != nil {
-            return
-        }
+	if proxy := m.getProxy(listenPort); proxy != nil {
+		router.SkipClean(config.Global().HttpServerOptions.SkipURLCleaning)
+		proxy.httpServer.Handler.(*handleWrapper).router = router
+	} else {
+		if err := m.serve(listenPort, ""); err != nil {
+			return
+		}
 
-        m.updateRouter(listenPort, router)
-    }
+		m.updateRouter(listenPort, router)
+	}
 }
 
 func (m *proxyMux) serve(listenPort int, protocol string) error {
-    listener, err := m.generateListener(listenPort, protocol)
-    if err != nil {
-        mainLog.WithError(err).Error("Can't start server")
-        return err
-    }
+	listener, err := m.generateListener(listenPort, protocol)
+	if err != nil {
+		mainLog.WithError(err).Error("Can't start server")
+		return err
+	}
 
-    readTimeout := 120 * time.Second
-    writeTimeout := 120 * time.Second
+	readTimeout := 120 * time.Second
+	writeTimeout := 120 * time.Second
 
-    if config.Global().HttpServerOptions.ReadTimeout > 0 {
-        readTimeout = time.Duration(config.Global().HttpServerOptions.ReadTimeout) * time.Second
-    }
+	if config.Global().HttpServerOptions.ReadTimeout > 0 {
+		readTimeout = time.Duration(config.Global().HttpServerOptions.ReadTimeout) * time.Second
+	}
 
-    if config.Global().HttpServerOptions.WriteTimeout > 0 {
-        writeTimeout = time.Duration(config.Global().HttpServerOptions.WriteTimeout) * time.Second
-    }
+	if config.Global().HttpServerOptions.WriteTimeout > 0 {
+		writeTimeout = time.Duration(config.Global().HttpServerOptions.WriteTimeout) * time.Second
+	}
 
-    proxy := m.getProxy(listenPort)
-    if proxy.httpServer != nil {
-        return nil
-    }
+	proxy := m.getProxy(listenPort)
+	if proxy.httpServer != nil {
+		return nil
+	}
 
-    router := mux.NewRouter()
-    router.SkipClean(config.Global().HttpServerOptions.SkipURLCleaning)
+	router := mux.NewRouter()
+	router.SkipClean(config.Global().HttpServerOptions.SkipURLCleaning)
 
-    addr := config.Global().ListenAddress + ":" + strconv.Itoa(listenPort)
-    proxy.httpServer = &http.Server{
-        Addr:         addr,
-        ReadTimeout:  readTimeout,
-        WriteTimeout: writeTimeout,
-        Handler:      &handleWrapper{router},
-    }
+	addr := config.Global().ListenAddress + ":" + strconv.Itoa(listenPort)
+	proxy.httpServer = &http.Server{
+		Addr:         addr,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		Handler:      &handleWrapper{router},
+	}
 
-    if config.Global().CloseConnections {
-        proxy.httpServer.SetKeepAlivesEnabled(false)
-    }
+	if config.Global().CloseConnections {
+		proxy.httpServer.SetKeepAlivesEnabled(false)
+	}
 
-    go proxy.httpServer.Serve(listener)
+	go proxy.httpServer.Serve(listener)
 
-    mainLog.Warning("Started a new server on ", addr)
+	mainLog.Warning("Started a new server on ", addr)
 
-    return nil
+	return nil
 }
 
 func (m *proxyMux) generateListener(listenPort int, protocol string) (l net.Listener, err error) {
-    m.Lock()
-    defer m.Unlock()
+	m.Lock()
+	defer m.Unlock()
 
-    // Default protocol
-    if protocol == "" {
-        if config.Global().HttpServerOptions.UseSSL {
-            protocol = "https"
-        } else {
-            protocol = "http"
-        }
-    }
+	// Default protocol
+	if protocol == "" {
+		if config.Global().HttpServerOptions.UseSSL {
+			protocol = "https"
+		} else {
+			protocol = "http"
+		}
+	}
 
-    if listenPort == 0 {
-        listenPort = config.Global().ListenPort
-    }
+	if listenPort == 0 {
+		listenPort = config.Global().ListenPort
+	}
 
-    // Special case for tests
-    if listenPort == -1 {
-        listenPort = 0
-    }
+	// Special case for tests
+	if listenPort == -1 {
+		listenPort = 0
+	}
 
-    for _, p := range m.proxies {
-        if p.port == listenPort {
-            if p.protocol != protocol {
-                return nil, fmt.Errorf("Can't use port `%d` for protocol `%s`. Listener with `%s` protocol already exist", p.port, protocol, p.protocol)
-            }
+	for _, p := range m.proxies {
+		if p.port == listenPort {
+			if p.protocol != protocol {
+				return nil, fmt.Errorf("Can't use port `%d` for protocol `%s`. Listener with `%s` protocol already exist", p.port, protocol, p.protocol)
+			}
 
-            return p.listener, nil
-        }
-    }
+			return p.listener, nil
+		}
+	}
 
-    listenAddress := config.Global().ListenAddress
+	listenAddress := config.Global().ListenAddress
 
-    targetPort := listenAddress + ":" + strconv.Itoa(listenPort)
+	targetPort := listenAddress + ":" + strconv.Itoa(listenPort)
 
-    if httpServerOptions := config.Global().HttpServerOptions; httpServerOptions.UseSSL {
-        mainLog.Info("--> Using SSL (https)")
+	if httpServerOptions := config.Global().HttpServerOptions; httpServerOptions.UseSSL {
+		mainLog.Info("--> Using SSL (https)")
 
-        tlsConfig := tls.Config{
-            GetCertificate:     dummyGetCertificate,
-            ServerName:         httpServerOptions.ServerName,
-            MinVersion:         httpServerOptions.MinVersion,
-            ClientAuth:         tls.NoClientCert,
-            InsecureSkipVerify: httpServerOptions.SSLInsecureSkipVerify,
-            CipherSuites:       getCipherAliases(httpServerOptions.Ciphers),
-        }
+		tlsConfig := tls.Config{
+			GetCertificate:     dummyGetCertificate,
+			ServerName:         httpServerOptions.ServerName,
+			MinVersion:         httpServerOptions.MinVersion,
+			ClientAuth:         tls.NoClientCert,
+			InsecureSkipVerify: httpServerOptions.SSLInsecureSkipVerify,
+			CipherSuites:       getCipherAliases(httpServerOptions.Ciphers),
+		}
 
-        if httpServerOptions.EnableHttp2 {
-            tlsConfig.NextProtos = append(tlsConfig.NextProtos, http2.NextProtoTLS)
-        }
+		if httpServerOptions.EnableHttp2 {
+			tlsConfig.NextProtos = append(tlsConfig.NextProtos, http2.NextProtoTLS)
+		}
 
-        tlsConfig.GetConfigForClient = getTLSConfigForClient(&tlsConfig, listenPort)
+		tlsConfig.GetConfigForClient = getTLSConfigForClient(&tlsConfig, listenPort)
 
-        l, err = tls.Listen("tcp", targetPort, &tlsConfig)
-    } else if config.Global().HttpServerOptions.UseLE_SSL {
+		l, err = tls.Listen("tcp", targetPort, &tlsConfig)
+	} else if config.Global().HttpServerOptions.UseLE_SSL {
 
-        mainLog.Info("--> Using SSL LE (https)")
+		mainLog.Info("--> Using SSL LE (https)")
 
-        GetLEState(&LE_MANAGER)
+		GetLEState(&LE_MANAGER)
 
-        conf := tls.Config{
-            GetCertificate: LE_MANAGER.GetCertificate,
-        }
-        conf.GetConfigForClient = getTLSConfigForClient(&conf, listenPort)
+		conf := tls.Config{
+			GetCertificate: LE_MANAGER.GetCertificate,
+		}
+		conf.GetConfigForClient = getTLSConfigForClient(&conf, listenPort)
 
-        l, err = tls.Listen("tcp", targetPort, &conf)
-    } else {
-        mainLog.WithField("port", targetPort).Info("--> Standard listener (http)")
-        l, err = net.Listen("tcp", targetPort)
-    }
+		l, err = tls.Listen("tcp", targetPort, &conf)
+	} else {
+		mainLog.WithField("port", targetPort).Info("--> Standard listener (http)")
+		l, err = net.Listen("tcp", targetPort)
+	}
 
-    if err == nil {
-        // Handle 0 port, when it assigned after listener created
-        _, portS, _ := net.SplitHostPort(l.Addr().String())
-        port, _ := strconv.Atoi(portS)
-        m.proxies = append(m.proxies, &proxy{
-            listener: l,
-            port:     port,
-            protocol: protocol,
-        })
-    } else {
-        panic(err.Error())
-    }
+	if err == nil {
+		// Handle 0 port, when it assigned after listener created
+		_, portS, _ := net.SplitHostPort(l.Addr().String())
+		port, _ := strconv.Atoi(portS)
+		m.proxies = append(m.proxies, &proxy{
+			listener: l,
+			port:     port,
+			protocol: protocol,
+		})
+	} else {
+		panic(err.Error())
+	}
 
-    return l, err
+	return l, err
 }
 
 func (m *proxyMux) cleanup(usedPorts []int) {
-    m.Lock()
-    defer m.Unlock()
+	m.Lock()
+	defer m.Unlock()
 
-    activeProxies := []*proxy{}
-    for _, p := range m.proxies {
-        found := false
-        for _, port := range usedPorts {
-            if port == p.port {
-                found = true
-                break
-            }
-        }
+	activeProxies := []*proxy{}
+	for _, p := range m.proxies {
+		found := false
+		for _, port := range usedPorts {
+			if port == p.port {
+				found = true
+				break
+			}
+		}
 
-        if found {
-            activeProxies = append(activeProxies, p)
-            continue
-        }
+		if found {
+			activeProxies = append(activeProxies, p)
+			continue
+		}
 
-        mainLog.Infof("Found unused listener at port %d, shutting down", p.port)
-        if p.httpServer != nil {
-            ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-            p.httpServer.Shutdown(ctx)
-            cancel()
-        } else {
-            p.listener.Close()
-        }
-    }
+		mainLog.Infof("Found unused listener at port %d, shutting down", p.port)
+		if p.httpServer != nil {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			p.httpServer.Shutdown(ctx)
+			cancel()
+		} else {
+			p.listener.Close()
+		}
+	}
 
-    m.proxies = activeProxies
+	m.proxies = activeProxies
 }
