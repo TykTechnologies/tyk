70ea043c31d54da727446ae8e4e95d209ff5b2c6
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2019-03-08 15:51:30 -0300

    storage: avoid MGET call when no keys are specified (#2156)
    Fixes #2155 
    
    `GetKeysAndValuesWithFilter` lists keys and runs `MGET`(https://redis.io/commands/mget) using these keys. This patch avoids the `MGET` call when the list is empty.
    
    This method is only called by the `oauth_manager.go` code.

--- storage/redis_cluster.go
+++ storage/redis_cluster.go
@@ -360,6 +360,9 @@ func (r RedisCluster) GetKeysAndValuesWithFilter(filter string) map[string]strin
 	}
 
 	keys, _ := redis.Strings(sessionsInterface, err)
+	if len(keys) == 0 {
+		return nil
+	}
 	valueObj, err := r.singleton().Do("MGET", sessionsInterface.([]interface{})...)
 	values, err := redis.Strings(valueObj, err)
 	if err != nil {
