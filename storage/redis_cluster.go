bcb2def8e02cc6de76f2ac5b96c38865ae9ea02f
Leonid Bugaev <leonsbox@gmail.com>
2019-03-13 22:49:54 +0100

    Add support for specifying Redis Timeout (#2166)
    Added new variable `storage.timeout`
    
    Fix https://github.com/TykTechnologies/tyk/issues/2165

--- storage/redis_cluster.go
+++ storage/redis_cluster.go
@@ -114,15 +114,24 @@ func NewRedisClusterPool(isCache bool) *rediscluster.RedisCluster {
 		log.Info("--> Using clustered mode")
 	}
 
+	timeout := 5 * time.Second
+
+	if cfg.Timeout > 0 {
+		timeout = time.Duration(cfg.Timeout) * time.Second
+	}
+
 	poolConf := rediscluster.PoolConfig{
-		MaxIdle:       maxIdle,
-		MaxActive:     maxActive,
-		IdleTimeout:   240 * time.Second,
-		Database:      cfg.Database,
-		Password:      cfg.Password,
-		IsCluster:     cfg.EnableCluster,
-		UseTLS:        cfg.UseSSL,
-		TLSSkipVerify: cfg.SSLInsecureSkipVerify,
+		MaxIdle:        maxIdle,
+		MaxActive:      maxActive,
+		IdleTimeout:    240 * time.Second,
+		ConnectTimeout: timeout,
+		ReadTimeout:    timeout,
+		WriteTimeout:   timeout,
+		Database:       cfg.Database,
+		Password:       cfg.Password,
+		IsCluster:      cfg.EnableCluster,
+		UseTLS:         cfg.UseSSL,
+		TLSSkipVerify:  cfg.SSLInsecureSkipVerify,
 	}
 
 	seed_redii := []map[string]string{}
