TykTechnologies/tyk

commit 54fb5fa675a822d905837794edd8ec2476c72a8c

Improve async session updates  (#1757)
Fixes #1758 

Current solution for session updates when async is enabled is just to spawn a new goroutine per redis operation. 

This change allows setting a configurable pool of workers to handle the session updates - with the slower parts of the session update such as json marshalling moved into the pool. Default value if unset is currently 15 and worker pool will gracefully exit on gateway shutdown.

Also changed the dummySessionManager in policy_test.go pass by ref to stop go vet failing because of passed mutex.

auth_manager.go
@@ -4,6 +4,8 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/satori/go.uuid"
@@ -35,7 +37,11 @@ type SessionHandler interface {
 	Sessions(filter string) []string
 	Store() storage.Handler
 	ResetQuota(string, *user.SessionState)
+	Stop()
 }
+
+const sessionPoolDefaultSize = 50
+const sessionBufferDefaultSize = 1000
 
 // DefaultAuthorisationManager implements AuthorisationHandler,
 // requires a storage.Handler to interact with key store
@@ -44,8 +50,22 @@ type DefaultAuthorisationManager struct {
 }
 
 type DefaultSessionManager struct {
-	store       storage.Handler
-	asyncWrites bool
+	store                    storage.Handler
+	asyncWrites              bool
+	disableCacheSessionState bool
+	updateChan               chan *SessionUpdate
+	poolSize                 int
+	shouldStop               uint32
+	poolWG                   sync.WaitGroup
+	bufferSize               int
+	keyPrefix                string
+}
+
+type SessionUpdate struct {
+	isHashed bool
+	keyVal   string
+	session  *user.SessionState
+	ttl      int64
 }
 
 func (b *DefaultAuthorisationManager) Init(store storage.Handler) {
@@ -86,6 +106,71 @@ func (b *DefaultSessionManager) Init(store storage.Handler) {
 	b.asyncWrites = config.Global().UseAsyncSessionWrite
 	b.store = store
 	b.store.Connect()
+
+	if b.asyncWrites {
+		// check pool size in config and set to 50 if unset
+		b.poolSize = config.Global().SessionUpdatePoolSize
+		if b.poolSize <= 0 {
+			b.poolSize = sessionPoolDefaultSize
+		}
+		//check size for channel buffer and set to 1000 if unset
+		b.bufferSize = config.Global().SessionUpdateBufferSize
+		if b.bufferSize <= 0 {
+			b.bufferSize = sessionBufferDefaultSize
+		}
+
+		log.WithField("SessionManager poolsize", b.poolSize).Debug("Session update async pool size")
+
+		b.updateChan = make(chan *SessionUpdate, b.bufferSize)
+
+		b.keyPrefix = b.store.GetKeyPrefix()
+
+		//start worker pool
+		atomic.SwapUint32(&b.shouldStop, 0)
+		for i := 0; i < b.poolSize; i++ {
+			b.poolWG.Add(1)
+			go b.updateWorker()
+		}
+	}
+}
+
+func (b *DefaultSessionManager) updateWorker() {
+	defer b.poolWG.Done()
+
+	for u := range b.updateChan {
+
+		v, err := json.Marshal(u.session)
+		if err != nil {
+			log.WithError(err).Error("Error marshalling session for async session update")
+			continue
+		}
+
+		if u.isHashed {
+			u.keyVal = b.keyPrefix + u.keyVal
+			err := b.store.SetRawKey(u.keyVal, string(v), u.ttl)
+			if err != nil {
+				log.WithError(err).Error("Error updating hashed key")
+			}
+			continue
+
+		}
+
+		err = b.store.SetKey(u.keyVal, string(v), u.ttl)
+		if err != nil {
+			log.WithError(err).Error("Error updating key")
+		}
+	}
+}
+
+func (b *DefaultSessionManager) Stop() {
+	if atomic.LoadUint32(&b.shouldStop) == 0 {
+		// flag to stop adding data to chan
+		atomic.SwapUint32(&b.shouldStop, 1)
+		// close update channel
+		close(b.updateChan)
+		// wait for workers to finish
+		b.poolWG.Wait()
+	}
 }
 
 func (b *DefaultSessionManager) Store() storage.Handler {
@@ -111,26 +196,35 @@ func (b *DefaultSessionManager) ResetQuota(keyName string, session *user.Session
 // UpdateSession updates the session state in the storage engine
 func (b *DefaultSessionManager) UpdateSession(keyName string, session *user.SessionState,
 	resetTTLTo int64, hashed bool) error {
-	v, _ := json.Marshal(session)
-
-	if hashed {
-		keyName = b.store.GetKeyPrefix() + keyName
-	}
 
 	// async update and return if needed
 	if b.asyncWrites {
-		if hashed {
-			go b.store.SetRawKey(keyName, string(v), resetTTLTo)
+		if atomic.LoadUint32(&b.shouldStop) > 0 {
 			return nil
 		}
 
-		go b.store.SetKey(keyName, string(v), resetTTLTo)
+		sessionUpdate := &SessionUpdate{
+			isHashed: hashed,
+			keyVal:   keyName,
+			session:  session,
+			ttl:      resetTTLTo,
+		}
+
+		// send sessionupdate object through channel to pool
+		b.updateChan <- sessionUpdate
+
 		return nil
 	}
 
+	v, err := json.Marshal(session)
+	if err != nil {
+		log.Error("Error marshalling session for sync update")
+		return err
+	}
+
 	// sync update
-	var err error
 	if hashed {
+		keyName = b.store.GetKeyPrefix() + keyName
 		err = b.store.SetRawKey(keyName, string(v), resetTTLTo)
 	} else {
 		err = b.store.SetKey(keyName, string(v), resetTTLTo)

api_definition.go
@@ -830,6 +830,11 @@ func (a *APISpec) Init(authStore, sessionStore, healthStore, orgStore storage.Ha
 	a.OrgSessionManager.Init(orgStore)
 }
 
+func (a *APISpec) StopSessionManagerPool() {
+	a.SessionManager.Stop()
+	a.OrgSessionManager.Stop()
+}
+
 func (a *APISpec) getURLStatus(stat URLStatus) RequestStatus {
 	switch stat {
 	case Ignored:

cli/lint/schema.go
@@ -491,6 +491,12 @@ const confSchema = `{
 	"optimisations_use_async_session_write": {
 		"type": "boolean"
 	},
+	"session_update_pool_size":{
+		"type": "integer"
+	},
+	"session_update_buffer_size":{
+		"type": "integer"
+	},
 	"pid_file_location": {
 		"type": "string"
 	},

config/config.go
@@ -212,6 +212,8 @@ type Config struct {
 	AnalyticsConfig                   AnalyticsConfigConfig                 `json:"analytics_config"`
 	HealthCheck                       HealthCheckConfig                     `json:"health_check"`
 	UseAsyncSessionWrite              bool                                  `json:"optimisations_use_async_session_write"`
+	SessionUpdatePoolSize             int                                   `json:"session_update_pool_size"`
+	SessionUpdateBufferSize           int                                   `json:"session_update_buffer_size"`
 	AllowMasterKeys                   bool                                  `json:"allow_master_keys"`
 	HashKeys                          bool                                  `json:"hash_keys"`
 	HashKeyFunction                   string                                `json:"hash_key_function"`

main.go
@@ -1004,6 +1004,15 @@ func main() {
 		analytics.Stop()
 	}
 
+	// if using async session writes stop workers
+	if config.Global().UseAsyncSessionWrite {
+		DefaultOrgStore.Stop()
+		for i := range apiSpecs {
+			apiSpecs[i].StopSessionManagerPool()
+		}
+
+	}
+
 	// write pprof profiles
 	writeProfiles()
 

policy_test.go
@@ -43,7 +43,7 @@ type dummySessionManager struct {
 	DefaultSessionManager
 }
 
-func (dummySessionManager) UpdateSession(key string, sess *user.SessionState, ttl int64, hashed bool) error {
+func (*dummySessionManager) UpdateSession(key string, sess *user.SessionState, ttl int64, hashed bool) error {
 	return nil
 }
 

session_manager.go
@@ -48,7 +48,8 @@ func (l *SessionLimiter) doRollingWindowWrite(key, rateLimiterKey, rateLimiterSe
 		// and another subtraction because of the preemptive limit
 		subtractor = 2
 	}
-
+	// The test TestRateLimitForAPIAndRateLimitAndQuotaCheck
+	// will only work with ththese two lines here
 	//log.Info("break: ", (int(currentSession.Rate) - subtractor))
 	if ratePerPeriodNow > int(currentSession.Rate)-subtractor {
 		// Set a sentinel value with expire

commit 50c107862b164efe4851ff800289d8bc0a8dd694

Fix goroutine leak with async session update
Session update worker pool was startng for every "DefaultSessionStore" object. And it was happening for every API initialization. So if you have 1000 APIs, and your worker pool is 100, each API reload will trigger at least 100k goroutines.

This change maintains single worker pool which initilized only once.

Issue itself was introduced by https://github.com/TykTechnologies/tyk/pull/1757

Fix https://github.com/TykTechnologies/tyk/issues/2236

auth_manager.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
@@ -43,6 +42,72 @@ type SessionHandler interface {
 const sessionPoolDefaultSize = 50
 const sessionBufferDefaultSize = 1000
 
+type sessionUpdater struct {
+	store      storage.Handler
+	once       sync.Once
+	updateChan chan *SessionUpdate
+	poolSize   int
+	bufferSize int
+	keyPrefix  string
+}
+
+var defaultSessionUpdater *sessionUpdater
+
+func init() {
+	defaultSessionUpdater = &sessionUpdater{}
+}
+
+func (s *sessionUpdater) Init(store storage.Handler) {
+	s.once.Do(func() {
+		s.store = store
+		// check pool size in config and set to 50 if unset
+		s.poolSize = config.Global().SessionUpdatePoolSize
+		if s.poolSize <= 0 {
+			s.poolSize = sessionPoolDefaultSize
+		}
+		//check size for channel buffer and set to 1000 if unset
+		s.bufferSize = config.Global().SessionUpdateBufferSize
+		if s.bufferSize <= 0 {
+			s.bufferSize = sessionBufferDefaultSize
+		}
+
+		log.WithField("pool_size", s.poolSize).Debug("Session update async pool size")
+
+		s.updateChan = make(chan *SessionUpdate, s.bufferSize)
+
+		s.keyPrefix = s.store.GetKeyPrefix()
+
+		for i := 0; i < s.poolSize; i++ {
+			go s.updateWorker()
+		}
+	})
+}
+
+func (s *sessionUpdater) updateWorker() {
+	for u := range s.updateChan {
+		v, err := json.Marshal(u.session)
+		if err != nil {
+			log.WithError(err).Error("Error marshalling session for async session update")
+			continue
+		}
+
+		if u.isHashed {
+			u.keyVal = s.keyPrefix + u.keyVal
+			err := s.store.SetRawKey(u.keyVal, string(v), u.ttl)
+			if err != nil {
+				log.WithError(err).Error("Error updating hashed key")
+			}
+			continue
+
+		}
+
+		err = s.store.SetKey(u.keyVal, string(v), u.ttl)
+		if err != nil {
+			log.WithError(err).Error("Error updating key")
+		}
+	}
+}
+
 // DefaultAuthorisationManager implements AuthorisationHandler,
 // requires a storage.Handler to interact with key store
 type DefaultAuthorisationManager struct {
@@ -53,12 +118,6 @@ type DefaultSessionManager struct {
 	store                    storage.Handler
 	asyncWrites              bool
 	disableCacheSessionState bool
-	updateChan               chan *SessionUpdate
-	poolSize                 int
-	shouldStop               uint32
-	poolWG                   sync.WaitGroup
-	bufferSize               int
-	keyPrefix                string
 }
 
 type SessionUpdate struct {
@@ -114,68 +173,7 @@ func (b *DefaultSessionManager) Init(store storage.Handler) {
 	}
 
 	if b.asyncWrites {
-		// check pool size in config and set to 50 if unset
-		b.poolSize = config.Global().SessionUpdatePoolSize
-		if b.poolSize <= 0 {
-			b.poolSize = sessionPoolDefaultSize
-		}
-		//check size for channel buffer and set to 1000 if unset
-		b.bufferSize = config.Global().SessionUpdateBufferSize
-		if b.bufferSize <= 0 {
-			b.bufferSize = sessionBufferDefaultSize
-		}
-
-		log.WithField("SessionManager poolsize", b.poolSize).Debug("Session update async pool size")
-
-		b.updateChan = make(chan *SessionUpdate, b.bufferSize)
-
-		b.keyPrefix = b.store.GetKeyPrefix()
-
-		//start worker pool
-		atomic.SwapUint32(&b.shouldStop, 0)
-		for i := 0; i < b.poolSize; i++ {
-			b.poolWG.Add(1)
-			go b.updateWorker()
-		}
-	}
-}
-
-func (b *DefaultSessionManager) updateWorker() {
-	defer b.poolWG.Done()
-
-	for u := range b.updateChan {
-
-		v, err := json.Marshal(u.session)
-		if err != nil {
-			log.WithError(err).Error("Error marshalling session for async session update")
-			continue
-		}
-
-		if u.isHashed {
-			u.keyVal = b.keyPrefix + u.keyVal
-			err := b.store.SetRawKey(u.keyVal, string(v), u.ttl)
-			if err != nil {
-				log.WithError(err).Error("Error updating hashed key")
-			}
-			continue
-
-		}
-
-		err = b.store.SetKey(u.keyVal, string(v), u.ttl)
-		if err != nil {
-			log.WithError(err).Error("Error updating key")
-		}
-	}
-}
-
-func (b *DefaultSessionManager) Stop() {
-	if atomic.LoadUint32(&b.shouldStop) == 0 {
-		// flag to stop adding data to chan
-		atomic.SwapUint32(&b.shouldStop, 1)
-		// close update channel
-		close(b.updateChan)
-		// wait for workers to finish
-		b.poolWG.Wait()
+		defaultSessionUpdater.Init(store)
 	}
 }
 
@@ -215,10 +213,6 @@ func (b *DefaultSessionManager) UpdateSession(keyName string, session *user.Sess
 
 	// async update and return if needed
 	if b.asyncWrites {
-		if atomic.LoadUint32(&b.shouldStop) > 0 {
-			return nil
-		}
-
 		sessionUpdate := &SessionUpdate{
 			isHashed: hashed,
 			keyVal:   keyName,
@@ -227,7 +221,7 @@ func (b *DefaultSessionManager) UpdateSession(keyName string, session *user.Sess
 		}
 
 		// send sessionupdate object through channel to pool
-		b.updateChan <- sessionUpdate
+		defaultSessionUpdater.updateChan <- sessionUpdate
 
 		return nil
 	}
@@ -287,6 +281,8 @@ func (b *DefaultSessionManager) SessionDetail(keyName string, hashed bool) (user
 
 	return session, true
 }
+
+func (b *DefaultSessionManager) Stop() {}
 
 // Sessions returns all sessions in the key store that match a filter key (a prefix)
 func (b *DefaultSessionManager) Sessions(filter string) []string {

gateway_test.go
@@ -14,6 +14,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -909,6 +910,31 @@ func TestListenPathTykPrefix(t *testing.T) {
 		Path: "/tyk-foo/",
 		Code: 200,
 	})
+}
+
+func TestReloadGoroutineLeakWithAsyncWrites(t *testing.T) {
+	ts := newTykTestServer()
+	defer ts.Close()
+
+	globalConf := config.Global()
+	globalConf.UseAsyncSessionWrite = true
+	config.SetGlobal(globalConf)
+	defer resetTestConfig()
+
+	buildAndLoadAPI(func(spec *APISpec) {
+		spec.Proxy.ListenPath = "/"
+	})
+
+	before := runtime.NumGoroutine()
+	doReload()
+
+	time.Sleep(100 * time.Millisecond)
+
+	after := runtime.NumGoroutine()
+
+	if before != after {
+		t.Errorf("Goroutine leak, was: %d, after reload: %d", before, after)
+	}
 }
 
 func TestProxyUserAgent(t *testing.T) {