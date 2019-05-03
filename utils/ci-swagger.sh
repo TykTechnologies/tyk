fdd3b68073496e239264eab59f975ea1f04ba442
Lanre Adelowo <yo@lanre.wtf>
2019-05-03 16:07:55 +0100

    update script

--- utils/ci-swagger.sh
+++ utils/ci-swagger.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 swagger2fileName="swagger2.yaml"
+tempOpenAPIFileName="temp-swagger.yml"
+tempUpdatedOpenAPIFileName="temp-swagger2.yml"
 openAPIspecfileName="swagger.yml"
 
 fatal() {
@@ -20,7 +22,7 @@ if [ $? -ne 0 ]; then
 	fatal "swagger spec is invalid... swagger spec is located at $swagger2fileName"
 fi
 
-api-spec-converter --from=swagger_2 --to=openapi_3 --syntax=yaml "$swagger2fileName" > "$openAPIspecfileName"
+api-spec-converter --from=swagger_2 --to=openapi_3 --syntax=yaml "$swagger2fileName" > "$tempOpenAPIFileName"
 
 if [ $? -ne 0 ]; then
 	fatal "could not convert swagger2.0 spec to opeenapi 3.0"
@@ -28,6 +30,26 @@ fi
 
 ## clean up
 rm "$swagger2fileName"
+
+## If running this on macOS, you might need to change sed to gsed
+
+sed -n '1,/components:/p' $openAPIspecfileName > $tempUpdatedOpenAPIFileName
+
+if [ $? -ne 0 ]; then
+	fatal "replace operation failed step 1"
+fi
+
+## lineToStartReplaceFrom=$(grep -n components: temp-swagger.yml | awk '{split($0,a,":components:"); print a[1]}')
+## I don't seem to be able to use the above variable in the sed command below,
+## so the line is hardcoded, if you happen to change the swagger routes, make
+# sure to update the line number with the line immediately the one that houses "components:"
+
+sed -n '851,/components:/p' $openAPIspecfileName >> $tempUpdatedOpenAPIFileName
+if [ $? -ne 0 ]; then
+	fatal "replace operation failed"
+fi
+
+mv $tempUpdatedOpenAPIFileName $openAPIspecfileName
 
 ## Ideally, CI should push $openAPIspecfileName to GitHub
 ## but for now, it can be committed by users and pushed alonside their changes.
