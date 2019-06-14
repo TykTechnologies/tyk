3d1e1d2737461516a9747e5f9360f25e329e3eb6
furkansenharputlu <furkan_senharputlu@hotmail.com>
2019-06-14 21:21:58 +0300

    Remove the first test run without -race flag

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -41,30 +41,17 @@ i=0
 go get -t
 
 # build Go-plugin used in tests
-go build -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins || fatal "building Go-plugin failed"
+go build -race -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins || fatal "building Go-plugin failed"
 
 # need to do per-pkg because go test doesn't support a single coverage
 # profile for multiple pkgs
 for pkg in $PKGS; do
 	for opts in "${MATRIX[@]}"; do
-		show go test -v -timeout $TEST_TIMEOUT -coverprofile=test-$i.cov $opts $pkg \
+		show go test -race -v -timeout $TEST_TIMEOUT -coverprofile=test-$i.cov $opts $pkg \
 			|| fatal "go test errored"
 		let i++ || true
 	done
 done
-
-if [[ ! $LATEST_GO ]]; then
-	echo "Skipping race, checks, and coverage report"
-	exit 0
-fi
-
-# build Go-plugin used in tests but with race support
-mv ./test/goplugins/goplugins.so ./test/goplugins/goplugins_old.so
-go build -race -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins \
-    || fatal "building Go-plugin with race failed"
-
-go test -race -v -timeout $TEST_TIMEOUT $PKGS || fatal "go test -race failed"
-mv ./test/goplugins/goplugins_old.so ./test/goplugins/goplugins.so
 
 for opts in "${MATRIX[@]}"; do
 	show go vet $opts $PKGS || fatal "go vet errored"
