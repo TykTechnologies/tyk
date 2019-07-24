a343ec1e8a40251452b1091e38688d63c940e41f
gernest <geofreyernest@live.com>
2019-07-24 11:55:07 +0300

    add listem_port  and protocol schema

--- apidef/schema.go
+++ apidef/schema.go
@@ -279,6 +279,12 @@ const Schema = `{
         "domain": {
             "type": "string"
         },
+        "listen_port": {
+            "type": "number"
+        },
+        "protocol": {
+            "type": "string"
+        },
         "certificates": {
             "type": ["array", "null"]
         },
