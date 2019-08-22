db50cb322ae8a05c5e70e9e4aa77b6bf8a30d548
gernest <geofreyernest@live.com>
2019-08-22 15:19:11 +0300

    fix test

--- coprocess/grpc/coprocess_grpc_test.go
+++ coprocess/grpc/coprocess_grpc_test.go
@@ -256,7 +256,7 @@ func startTykWithGRPC() (*gateway.Test, *grpc.Server) {
 }
 
 func TestMain(m *testing.M) {
-	os.Exit(gateway.InitTestMain(m))
+	os.Exit(gateway.InitTestMain(context.Background(), m))
 }
 
 func TestGRPCDispatch(t *testing.T) {
