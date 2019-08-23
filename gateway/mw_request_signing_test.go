8f448b012a422214d8ecd1068c91beff9caab6d9
Komal Sukhani <komaldsukhani@gmail.com>
2019-08-23 18:33:40 +0530

    Added test cases for RSA request signing and headerList

--- gateway/mw_request_signing_test.go
+++ gateway/mw_request_signing_test.go
@@ -1,8 +1,12 @@
 package gateway
 
 import (
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/TykTechnologies/tyk/test"
 	"github.com/TykTechnologies/tyk/user"
@@ -10,15 +14,23 @@ import (
 
 var algoList = [4]string{"hmac-sha1", "hmac-sha256", "hmac-sha384", "hmac-sha512"}
 
-func generateSpec(algo string) {
+func generateSession(algo, data string) string {
 	sessionKey := CreateSession(func(s *user.SessionState) {
-		s.HMACEnabled = true
-		s.HmacSecret = "9879879878787878"
+		if strings.HasPrefix(algo, "rsa") {
+			s.RSACertificateId = data
+			s.RSAEnabled = true
+		} else {
+			s.HmacSecret = data
+			s.HMACEnabled = true
+		}
 
 		s.AccessRights = map[string]user.AccessDefinition{"protected": {APIID: "protected", Versions: []string{"v1"}}}
-
 	})
 
+	return sessionKey
+}
+
+func generateSpec(algo string, data string, sessionKey string, headerList []string) {
 	BuildAndLoadAPI(func(spec *APISpec) {
 		spec.APIID = "protected"
 		spec.Name = "protected api"
@@ -35,10 +47,19 @@ func generateSpec(algo string) {
 		spec.VersionData.Versions["v1"] = version
 	}, func(spec *APISpec) {
 		spec.Proxy.ListenPath = "/test"
+		spec.UseKeylessAccess = true
 		spec.RequestSigning.IsEnabled = true
 		spec.RequestSigning.KeyId = sessionKey
-		spec.RequestSigning.Secret = "9879879878787878"
+
+		if strings.HasPrefix(algo, "rsa") {
+			spec.RequestSigning.CertificateId = data
+		} else {
+			spec.RequestSigning.Secret = data
+		}
 		spec.RequestSigning.Algorithm = algo
+		if headerList != nil {
+			spec.RequestSigning.HeaderList = headerList
+		}
 
 		version := spec.VersionData.Versions["v1"]
 		json.Unmarshal([]byte(`{
@@ -58,15 +79,16 @@ func generateSpec(algo string) {
 
 }
 
-func TestRequestSigning(t *testing.T) {
+func TestHMACRequestSigning(t *testing.T) {
 	ts := StartTest()
 	defer ts.Close()
+	secret := "9879879878787878"
 
 	for _, algo := range algoList {
 		name := "Test with " + algo
 		t.Run(name, func(t *testing.T) {
-
-			generateSpec(algo)
+			sessionKey := generateSession(algo, secret)
+			generateSpec(algo, secret, sessionKey, nil)
 
 			ts.Run(t, []test.TestCase{
 				{Path: "/test/by_name", Code: 200},
@@ -74,8 +96,39 @@ func TestRequestSigning(t *testing.T) {
 		})
 	}
 
+	t.Run("Valid Custom headerList", func(t *testing.T) {
+		algo := "hmac-sha1"
+		headerList := []string{"foo", "date"}
+
+		sessionKey := generateSession(algo, secret)
+		generateSpec(algo, secret, sessionKey, headerList)
+
+		refDate := "Mon, 02 Jan 2006 15:04:05 MST"
+		tim := time.Now().Format(refDate)
+
+		headers := map[string]string{"foo": "bar", "date": tim}
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Code: 200, Headers: headers},
+		}...)
+	})
+
+	t.Run("Invalid Custom headerList", func(t *testing.T) {
+		algo := "hmac-sha1"
+		headerList := []string{"foo"}
+
+		sessionKey := generateSession(algo, secret)
+		generateSpec(algo, secret, sessionKey, headerList)
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Code: 200},
+		}...)
+	})
+
 	t.Run("Invalid algorithm", func(t *testing.T) {
-		generateSpec("random")
+		algo := "hmac-123"
+		sessionKey := generateSession(algo, secret)
+		generateSpec(algo, secret, sessionKey, nil)
 
 		ts.Run(t, []test.TestCase{
 			{Path: "/test/by_name", Code: 500},
@@ -83,12 +136,91 @@ func TestRequestSigning(t *testing.T) {
 	})
 
 	t.Run("Invalid Date field", func(t *testing.T) {
-		generateSpec("hmac-sha1")
+		algo := "hmac-sha1"
+		sessionKey := generateSession(algo, secret)
+		generateSpec(algo, secret, sessionKey, nil)
 
 		headers := map[string]string{"date": "Mon, 02 Jan 2006 15:04:05 GMT"}
 
 		ts.Run(t, []test.TestCase{
 			{Path: "/test/by_name", Headers: headers, Code: 400},
+		}...)
+	})
+}
+
+func TestRSARequestSigning(t *testing.T) {
+	ts := StartTest()
+	defer ts.Close()
+
+	_, _, combinedPem, cert := genServerCertificate()
+	privCertId, _ := CertificateManager.Add(combinedPem, "")
+	defer CertificateManager.Delete(privCertId)
+
+	x509Cert, _ := x509.ParseCertificate(cert.Certificate[0])
+	pubDer, _ := x509.MarshalPKIXPublicKey(x509Cert.PublicKey)
+	pubPem := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: pubDer})
+	pubCertId, _ := CertificateManager.Add(pubPem, "")
+	defer CertificateManager.Delete(pubCertId)
+
+	name := "Test with rsa-sha256"
+	t.Run(name, func(t *testing.T) {
+		algo := "rsa-sha256"
+		sessionKey := generateSession(algo, pubCertId)
+		generateSpec(algo, privCertId, sessionKey, nil)
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Code: 200},
+		}...)
+	})
+
+	t.Run("Invalid algorithm", func(t *testing.T) {
+		algo := "rsa-123"
+		sessionKey := generateSession(algo, pubCertId)
+		generateSpec(algo, privCertId, sessionKey, nil)
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Code: 500},
+		}...)
+	})
+
+	t.Run("Invalid Date field", func(t *testing.T) {
+		algo := "rsa-sha256"
+		sessionKey := generateSession(algo, pubCertId)
+		generateSpec(algo, privCertId, sessionKey, nil)
+
+		headers := map[string]string{"date": "Mon, 02 Jan 2006 15:04:05 GMT"}
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Headers: headers, Code: 400},
+		}...)
+	})
+
+	t.Run("Custom headerList", func(t *testing.T) {
+		algo := "rsa-sha256"
+		headerList := []string{"foo", "date"}
+
+		sessionKey := generateSession(algo, pubCertId)
+		generateSpec(algo, privCertId, sessionKey, headerList)
+
+		refDate := "Mon, 02 Jan 2006 15:04:05 MST"
+		tim := time.Now().Format(refDate)
+
+		headers := map[string]string{"foo": "bar", "date": tim}
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Code: 200, Headers: headers},
+		}...)
+	})
+
+	t.Run("Non-existing Custom headers", func(t *testing.T) {
+		algo := "rsa-sha256"
+		headerList := []string{"foo"}
+
+		sessionKey := generateSession(algo, pubCertId)
+		generateSpec(algo, privCertId, sessionKey, headerList)
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/by_name", Code: 200},
 		}...)
 	})
 }
