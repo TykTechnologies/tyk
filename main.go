7444a6e8c19e5c4dd8ecd67557395f4f9cce3aed
Ahmet Soormally <ahmet@mangomm.co.uk>
2019-08-30 17:56:03 +0100

    optimise copyBuffer

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/TykTechnologies/tyk/gateway"
+import (
+	"github.com/TykTechnologies/tyk/gateway"
+)
 
 func main() {
+	//f, err := os.Create("trace-copybuffer.out")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer f.Close()
+	//
+	//err = trace.Start(f)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer trace.Stop()
+
 	gateway.Start()
 }
