27e2f33b258df90370b1758014145a5281a41f84
Ahmet Soormally <ahmet@mangomm.co.uk>
2019-09-01 09:41:19 +0100

    stop allocating memory when ranging over rxPaths

--- main.go
+++ main.go
@@ -5,17 +5,5 @@ import (
 )
 
 func main() {
-	//f, err := os.Create("trace-copybuffer.out")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer f.Close()
-	//
-	//err = trace.Start(f)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer trace.Stop()
-
 	gateway.Start()
 }
