32f36a9d4e54151bb13ee30e554ba36da4645b1f
Ahmet Soormally <ahmet@mangomm.co.uk>
2019-09-03 20:04:15 +0100

    WIP

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/TykTechnologies/tyk/gateway"
+	"github.com/pkg/profile"
 )
 
 func main() {
+	defer profile.Start(profile.ProfilePath("."), profile.MemProfile, profile.MemProfileRate(1)).Stop()
 	gateway.Start()
 }
