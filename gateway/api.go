e6d91b140e62dc4fc18b101f15a95613217865c4
Leonid Bugaev <leonsbox@gmail.com>
2019-09-27 09:42:24 +0300

    API should respond with real key ID (#2569)
    Fix https://github.com/TykTechnologies/tyk/issues/2523
    Fix https://github.com/TykTechnologies/tyk-analytics/issues/1471
    
    (cherry picked from commit c0d6dcbd10c51d189f26d3ca1f8f698447bf9557)

--- gateway/api.go
+++ gateway/api.go
@@ -330,7 +330,6 @@ func handleAddOrUpdate(keyName string, r *http.Request, isHashed bool) (interfac
 
 	// get original session in case of update and preserve fields that SHOULD NOT be updated
 	originalKey := user.SessionState{}
-	originalKeyName := keyName
 	if r.Method == http.MethodPut {
 		found := false
 		for apiID := range newSession.AccessRights {
@@ -407,7 +406,7 @@ func handleAddOrUpdate(keyName string, r *http.Request, isHashed bool) (interfac
 	})
 
 	response := apiModifyKeySuccess{
-		Key:    originalKeyName,
+		Key:    keyName,
 		Status: "ok",
 		Action: action,
 	}
