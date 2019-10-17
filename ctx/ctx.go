a9da8e745246c1e32e401330c57d690784c75a8c
Matias Insaurralde <matias@insaurral.de>
2019-10-17 18:57:47 -0300

    ctx: inject API definition when using Go plugins

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/TykTechnologies/tyk/apidef"
+
 	"github.com/TykTechnologies/tyk/storage"
 	"github.com/TykTechnologies/tyk/user"
 )
@@ -29,6 +31,7 @@ const (
 	ThrottleLevelLimit
 	Trace
 	CheckLoopLimits
+	Definition
 )
 
 func setContext(r *http.Request, ctx context.Context) {
@@ -76,4 +79,17 @@ func GetSession(r *http.Request) *user.SessionState {
 
 func SetSession(r *http.Request, s *user.SessionState, token string, scheduleUpdate bool) {
 	ctxSetSession(r, s, token, scheduleUpdate)
+}
+
+func SetDefinition(r *http.Request, s *apidef.APIDefinition) {
+	ctx := r.Context()
+	ctx = context.WithValue(ctx, Definition, s)
+	setContext(r, ctx)
+}
+
+func GetDefinition(r *http.Request) *apidef.APIDefinition {
+	if v := r.Context().Value(Definition); v != nil {
+		return v.(*apidef.APIDefinition)
+	}
+	return nil
 }
