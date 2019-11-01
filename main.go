bf70670ba551125f9dc83421a05752328c635406
Ahmet Soormally <ahmet@mangomm.co.uk>
2019-11-01 15:56:08 -0700

    removing proviling

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"github.com/TykTechnologies/tyk/gateway"
-	"github.com/pkg/profile"
 )
 
 func main() {
-	defer profile.Start(profile.ProfilePath("."), profile.MemProfile, profile.MemProfileRate(1)).Stop()
 	gateway.Start()
 }
