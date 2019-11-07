acf82084a3d5a7c0a7739a44957daa86da9408a4
Matias Insaurralde <matias@insaurral.de>
2019-11-07 12:16:12 -0300

    ctx: inject API definition when using Go plugins (#2624)
    This is a potential solution for allowing API definition access from Go plugins:
    ```go
    package main
    
    import (
    	"fmt"
    	"net/http"
    
    	"github.com/TykTechnologies/tyk/ctx"
    )
    
    func main() {}
    
    func MyCustomPlugin(w http.ResponseWriter, r *http.Request) {
    	fmt.Println("MyCustomPlugin is called")
    	apidef := ctx.GetDefinition(r)
    	fmt.Println("apidef=", apidef)
    }
    
    ```

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/TykTechnologies/tyk/apidef"
+
 	"github.com/TykTechnologies/tyk/storage"
 	"github.com/TykTechnologies/tyk/user"
 )
@@ -78,4 +80,17 @@ func GetSession(r *http.Request) *user.SessionState {
 
 func SetSession(r *http.Request, s *user.SessionState, token string, scheduleUpdate bool) {
 	ctxSetSession(r, s, token, scheduleUpdate)
+}
+
+func SetDefinition(r *http.Request, s *apidef.APIDefinition) {
+	ctx := r.Context()
+	ctx = context.WithValue(ctx, Definition, s)
+	setContext(r, ctx)
+}
+
+func GetDefinition(r *http.Request) *apidef.APIDefinition {
+	if v := r.Context().Value(Definition); v != nil {
+		return v.(*apidef.APIDefinition)
+	}
+	return nil
 }
