bb85e3ad22451e87573e4e607be4a5ef657482d2
gernest <geofreyernest@live.com>
2020-01-06 15:54:46 +0300

    remove Hand interface

--- storage/storage.go
+++ storage/storage.go
@@ -98,40 +98,6 @@ type WebHook interface {
 	Connect() bool
 }
 
-// Handler is a standard interface to a storage backend, used by
-// AuthorisationManager to read and write key values to the backend
-type Hand interface {
-	GetKey(string) (string, error) // Returned string is expected to be a JSON object (user.SessionState)
-	GetMultiKey([]string) ([]string, error)
-	GetRawKey(string) (string, error)
-	SetKey(string, string, int64) error // Second input string is expected to be a JSON object (user.SessionState)
-	SetRawKey(string, string, int64) error
-	SetExp(string, int64) error   // Set key expiration
-	GetExp(string) (int64, error) // Returns expiry of a key
-	GetKeys(string) []string
-	DeleteKey(string) bool
-	DeleteAllKeys() bool
-	DeleteRawKey(string) bool
-	Connect() bool
-	GetKeysAndValues() map[string]string
-	GetKeysAndValuesWithFilter(string) map[string]string
-	DeleteKeys([]string) bool
-	Decrement(string)
-	IncrememntWithExpire(string, int64) int64
-	SetRollingWindow(key string, per int64, val string, pipeline bool) (int, []interface{})
-	GetRollingWindow(key string, per int64, pipeline bool) (int, []interface{})
-	GetSet(string) (map[string]string, error)
-	AddToSet(string, string)
-	AppendToSet(string, string)
-	GetAndDeleteSet(string) []interface{}
-	RemoveFromSet(string, string)
-	DeleteScanMatch(string) bool
-	GetKeyPrefix() string
-	AddToSortedSet(string, string, float64)
-	GetSortedSetRange(string, string, string) ([]string, []float64, error)
-	RemoveSortedSetRange(string, string, string) error
-}
-
 type Oauth interface {
 	GetKey(string) (string, error)
 	GetRawKey(string) (string, error)
