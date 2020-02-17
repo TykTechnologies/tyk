430e8b229e95d8d7751728169e0f35ecef49b8d5
Leonid Bugaev <leonsbox@gmail.com>
2020-02-17 21:32:20 +0300

    Fix memory leak (#2879)
    We stored HTTP handler inside API definition, which probably introduced some circular dependency, confusing the GC.
    Handler access itself was needed for the looping, so even if API not loaded to the router (internal), it still could be accessed.
    
    Now handler access moved to the map, where key is API id.
    And map is updated in the same maneer as apisByID

--- gateway/api_loader.go
+++ gateway/api_loader.go
@@ -8,6 +8,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
@@ -517,15 +518,15 @@ func (d *DummyProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		var handler http.Handler
 		if r.URL.Hostname() == "self" {
-			if d.SH.Spec.middlewareChain != nil {
-				handler = d.SH.Spec.middlewareChain.ThisHandler
+			if h, found := apisHandlesByID.Load(d.SH.Spec.APIID); found {
+				handler = h.(http.Handler)
 			}
 		} else {
 			ctxSetVersionInfo(r, nil)
 
 			if targetAPI := fuzzyFindAPI(r.URL.Hostname()); targetAPI != nil {
-				if targetAPI.middlewareChain != nil {
-					handler = targetAPI.middlewareChain.ThisHandler
+				if h, found := apisHandlesByID.Load(targetAPI.APIID); found {
+					handler = h.(http.Handler)
 				}
 			} else {
 				handler := ErrorHandler{*d.SH.Base()}
@@ -588,7 +589,7 @@ func fuzzyFindAPI(search string) *APISpec {
 	return nil
 }
 
-func loadHTTPService(spec *APISpec, apisByListen map[string]int, gs *generalStores, muxer *proxyMux) {
+func loadHTTPService(spec *APISpec, apisByListen map[string]int, gs *generalStores, muxer *proxyMux) http.Handler {
 	port := config.Global().ListenPort
 	if spec.ListenPort != 0 {
 		port = spec.ListenPort
@@ -610,12 +611,9 @@ func loadHTTPService(spec *APISpec, apisByListen map[string]int, gs *generalStor
 	}
 
 	chainObj := processSpec(spec, apisByListen, gs, router, logrus.NewEntry(log))
-	apisMu.Lock()
-	spec.middlewareChain = chainObj
-	apisMu.Unlock()
 
 	if chainObj.Skip {
-		return
+		return chainObj.ThisHandler
 	}
 
 	if !chainObj.Open {
@@ -623,6 +621,8 @@ func loadHTTPService(spec *APISpec, apisByListen map[string]int, gs *generalStor
 	}
 
 	router.Handle(chainObj.ListenOn, chainObj.ThisHandler)
+
+	return chainObj.ThisHandler
 }
 
 func loadTCPService(spec *APISpec, gs *generalStores, muxer *proxyMux) {
@@ -664,6 +664,7 @@ func loadApps(specs []*APISpec) {
 	mainLog.Info("Loading API configurations.")
 
 	tmpSpecRegister := make(map[string]*APISpec)
+	tmpSpecHandles := new(sync.Map)
 
 	// sort by listen path from longer to shorter, so that /foo
 	// doesn't break /foo-bar
@@ -711,7 +712,7 @@ func loadApps(specs []*APISpec) {
 					mainLog.Infof("Intialized tracer  api_name=%q", spec.Name)
 				}
 			}
-			loadHTTPService(spec, apisByListen, &gs, muxer)
+			tmpSpecHandles.Store(spec.APIID, loadHTTPService(spec, apisByListen, &gs, muxer))
 		case "tcp", "tls":
 			loadTCPService(spec, &gs, muxer)
 		}
@@ -728,6 +729,8 @@ func loadApps(specs []*APISpec) {
 	}
 
 	apisByID = tmpSpecRegister
+	apisHandlesByID = tmpSpecHandles
+
 	apisMu.Unlock()
 
 	mainLog.Debug("Checker host list")
