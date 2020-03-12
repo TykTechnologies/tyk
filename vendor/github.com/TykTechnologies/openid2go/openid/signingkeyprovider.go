ef5159ebd7b882715b2860160baef41eaae11cc7
Leonid Bugaev <leonsbox@gmail.com>
2020-03-12 20:18:09 +0300

    Fix OpenID EDCA issue (#2925)
    Fix https://github.com/TykTechnologies/tyk/issues/2790

--- vendor/github.com/TykTechnologies/openid2go/openid/signingkeyprovider.go
+++ vendor/github.com/TykTechnologies/openid2go/openid/signingkeyprovider.go
@@ -1,8 +1,9 @@
 package openid
 
 import (
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"net/http"
 	"sync"
 )
@@ -44,13 +45,33 @@ func (s *signingKeyProvider) refreshSigningKeys(issuer string) error {
 	return nil
 }
 
+func parsePublicKey(data []byte) (interface{}, error) {
+	input := data
+	block, _ := pem.Decode(data)
+	if block != nil {
+		input = block.Bytes
+	}
+	var pub interface{}
+	var err error
+	pub, err = x509.ParsePKIXPublicKey(input)
+	if err != nil {
+		cert, err0 := x509.ParseCertificate(input)
+		if err0 != nil {
+			return nil, err0
+		}
+		pub = cert.PublicKey
+		err = nil
+	}
+	return pub, err
+}
+
 func (s *signingKeyProvider) getSigningKey(issuer string, kid string) (interface{}, error) {
 	lock.RLock()
 	sk := findKey(s.jwksMap, issuer, kid)
 	lock.RUnlock()
 
 	if sk != nil {
-		parsed, pErr := jwt.ParseRSAPublicKeyFromPEM(sk)
+		parsed, pErr := parsePublicKey(sk)
 		if pErr != nil {
 			return sk, nil
 		}
@@ -71,7 +92,7 @@ func (s *signingKeyProvider) getSigningKey(issuer string, kid string) (interface
 		return nil, &ValidationError{Code: ValidationErrorKidNotFound, Message: fmt.Sprintf("The jwk set retrieved for the issuer %v does not contain a key identifier %v.", issuer, kid), HTTPStatus: http.StatusUnauthorized}
 	}
 
-	parsed, pErr := jwt.ParseRSAPublicKeyFromPEM(sk)
+	parsed, pErr := parsePublicKey(sk)
 	if pErr != nil {
 		return sk, nil
 	}
