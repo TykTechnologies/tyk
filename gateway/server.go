f0c65e5d988ed36f244096ef9f83ce31bb1eb8a8
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2020-03-16 19:10:33 +0300

    Override oauth errors with global config (#2927)
    This PR defines a new config variable: `override_messages` to override error messages and codes. Example usage:
    ```
    "override_messages": {
            "oauth.auth_field_missing" : {
                "code": 401,
                "message": "Token is not authorised"
            }
        }
    ```
    The following are the configurable errors for now:
    ```
    "auth.auth_field_missing"
    "auth.key_not_found"
    
    "oauth.auth_field_missing"
    "oauth.auth_field_malformed"
    "oauth.key_not_found"
    "oauth.client_deleted"
    ```
    
    Also, it is possible to override only error code or only error message. Like this:
     ```
    "override_messages": {
            "oauth.auth_field_missing" : {
                "code": 401,
            }
        }
    ```
    Fixes https://github.com/TykTechnologies/tyk/issues/2900
    
    (cherry picked from commit 54a18fc3dcb32633a7b86b14ca17f576f8882684)

--- gateway/server.go
+++ gateway/server.go
@@ -907,6 +907,8 @@ func initialiseSystem(ctx context.Context) error {
 		config.SetGlobal(globalConf)
 	}
 
+	overrideTykErrors()
+
 	if os.Getenv("TYK_LOGLEVEL") == "" && !*cli.DebugMode {
 		level := strings.ToLower(config.Global().LogLevel)
 		switch level {
