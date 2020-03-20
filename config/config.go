9a2eb9d3d61235f4f47534458477ab6626812bb4
Nirav Uchat <nuchat@cisco.com>
2020-03-19 21:11:00 -0700

    reset cookie on redirect

--- config/config.go
+++ config/config.go
@@ -415,8 +415,9 @@ type Config struct {
 	DynamicAPIConnTimeout int `json:"dynamic_api_timeout_milli_sec"`
 
 	//Cisco - JWT URL Redirect
-	EnableRedirect bool   `json:"enable_redirect"`
-	RedirectURL    string `json:"redirect_url"`
+	RedirectResetCookie string `json:"redirect_reset_cookie"`
+	EnableRedirect      bool   `json:"enable_redirect"`
+	RedirectURL         string `json:"redirect_url"`
 }
 
 // GetEventTriggers returns event triggers. There was a typo in the json tag.
