492de2d3846dc74241aa1d2dc2368cae3ea94194
tbuchaillot <tombuchaillot89@gmail.com>
2020-04-01 13:01:37 -0300

    optimizing oauth clients and certificates lists

--- gateway/api.go
+++ gateway/api.go
@@ -1927,7 +1927,7 @@ func getApiClients(apiID string) ([]ExtendedOsinClientInterface, apiStatusMessag
 
 	clientData := []ExtendedOsinClientInterface{}
 	if apiSpec.UseOauth2 {
-		clientData, err := apiSpec.OAuthManager.OsinServer.Storage.GetClients(filterID, apiSpec.OrgID, true)
+		clientData, err = apiSpec.OAuthManager.OsinServer.Storage.GetClients(filterID, apiSpec.OrgID, true)
 		if err != nil {
 			log.WithFields(logrus.Fields{
 				"prefix": "api",
