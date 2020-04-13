9083c1646672809e0f3520bc215516fbc5b89fa4
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2020-04-13 15:12:14 +0300

    Fix unlimited quota and rate-limit inheritance (#3014)
    Fixes https://github.com/TykTechnologies/tyk/issues/3013

--- gateway/util.go
+++ gateway/util.go
@@ -28,3 +28,31 @@ func contains(s []string, i string) bool {
 	}
 	return false
 }
+
+// greaterThanFloat64 checks whether first float64 value is bigger than second float64 value.
+// -1 means infinite and the biggest value.
+func greaterThanFloat64(first, second float64) bool {
+	if first == -1 {
+		return true
+	}
+
+	if second == -1 {
+		return false
+	}
+
+	return first > second
+}
+
+// greaterThanInt64 checks whether first int64 value is bigger than second int64 value.
+// -1 means infinite and the biggest value.
+func greaterThanInt64(first, second int64) bool {
+	if first == -1 {
+		return true
+	}
+
+	if second == -1 {
+		return false
+	}
+
+	return first > second
+}
