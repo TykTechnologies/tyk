9da920267dd0e2db68d62e5ffd50f6cdb8025e3d
Patric Vormstein <pvormstein@googlemail.com>
2020-05-12 17:42:37 +0200

    gql normalization (#3000)
    * add normalization and introspection test
    
    * fix test

--- gateway/mw_graphql_test.go
+++ gateway/mw_graphql_test.go
@@ -48,6 +48,16 @@ func TestGraphQL(t *testing.T) {
 		_, _ = g.Run(t, test.TestCase{Data: request, Code: http.StatusBadRequest})
 	})
 
+	t.Run("Introspection query should successfully work", func(t *testing.T) {
+		request := gql.Request{
+			OperationName: "IntrospectionQuery",
+			Variables:     nil,
+			Query:         gqlIntrospectionQuery,
+		}
+
+		_, _ = g.Run(t, test.TestCase{Data: request, BodyMatch: "__schema", Code: http.StatusOK})
+	})
+
 	spec.UseKeylessAccess = false
 	LoadAPI(spec)
 
@@ -113,3 +123,103 @@ func TestGraphQL(t *testing.T) {
 		_, _ = g.Run(t, test.TestCase{Headers: authHeaderWithDirectKey, Data: request, BodyMatch: "hello", Code: http.StatusOK})
 	})
 }
+
+const gqlIntrospectionQuery = `query IntrospectionQuery {
+  __schema {
+    queryType {
+      name
+    }
+    mutationType {
+      name
+    }
+    subscriptionType {
+      name
+    }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+
+fragment InputValue on __InputValue {
+  name
+  description
+  type {
+    ...TypeRef
+  }
+  defaultValue
+}
+
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`
