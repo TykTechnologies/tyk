b6927da160206de457812f74f3fd0166f9fa682e
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2020-06-17 15:36:35 +0300

    Regenerate coprocess files for max_query_depth (#3153)
    Related to https://github.com/TykTechnologies/tyk/issues/3150
    
    (cherry picked from commit ab734eb2fc22821a2b7aba1db8957559f2eb993a)

--- coprocess/coprocess_object.pb.go
+++ coprocess/coprocess_object.pb.go
@@ -224,11 +224,11 @@ var fileDescriptor_72698a2223f86099 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // DispatcherClient is the client API for Dispatcher service.
 //
@@ -239,10 +239,10 @@ type DispatcherClient interface {
 }
 
 type dispatcherClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewDispatcherClient(cc *grpc.ClientConn) DispatcherClient {
+func NewDispatcherClient(cc grpc.ClientConnInterface) DispatcherClient {
 	return &dispatcherClient{cc}
 }
 
