62b835731c486aaca2d6adbfb0f693d8e3f62555
furkansenharputlu <furkan_senharputlu@hotmail.com>
2020-06-24 17:38:08 +0300

    Not care execution_mode if graphql is disabled

--- gateway/reverse_proxy.go
+++ gateway/reverse_proxy.go
@@ -850,7 +850,7 @@ func (p *ReverseProxy) WrappedServeHTTP(rw http.ResponseWriter, req *http.Reques
 
 	sendRequestToUpstream := func() {
 		begin := time.Now()
-		if p.TykAPISpec.GraphQL.ExecutionMode == apidef.GraphQLExecutionModeExecutionEngine {
+		if p.TykAPISpec.GraphQL.Enabled && p.TykAPISpec.GraphQL.ExecutionMode == apidef.GraphQLExecutionModeExecutionEngine {
 
 			if p.TykAPISpec.GraphQLExecutor.Engine == nil {
 				err = errors.New("execution engine is nil")
