280df86ca339242565147f003ec5cfe3f8486864
Alok G Singh <alephnull@hcoop.net>
2020-07-14 14:56:05 +0530

    Fix lint

--- images/plugin-compiler/test/foo-plugin.go
+++ images/plugin-compiler/test/foo-plugin.go
@@ -5,6 +5,7 @@ import (
 )
 
 // AddFooBarHeader adds custom "Foo: Bar" header to the request
+// nolint
 func AddFooBarHeader(rw http.ResponseWriter, r *http.Request) {
 	r.Header.Add("Foo", "Bar")
 }
