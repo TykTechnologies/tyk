201250105b35ba3031191df9b51f08447dbd116d
sredny buitrago <sredny.buitrago@gmail.com>
2020-07-22 15:19:45 -0500

    gofmt files

--- gateway/auth_manager.go
+++ gateway/auth_manager.go
@@ -339,7 +339,7 @@ func generateToken(orgID, keyID string) string {
 	keyID = strings.TrimPrefix(keyID, orgID)
 	log.Info("Key ID:", keyID)
 	token, err := storage.GenerateToken(orgID, keyID, config.Global().HashKeyFunction)
-log.Info("hashed:", token)
+
 	if err != nil {
 		log.WithFields(logrus.Fields{
 			"prefix": "auth-mgr",
