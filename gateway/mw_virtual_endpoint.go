bd7a6f3ae2caae045c04609103a0a1105231a24d
Matias Insaurralde <matias@insaurral.de>
2020-07-23 03:17:19 -0400

    mw_virtual_endpoint: use map[string]interface{} for JSVM session metadata

--- gateway/mw_virtual_endpoint.go
+++ gateway/mw_virtual_endpoint.go
@@ -43,7 +43,7 @@ type ResponseObject struct {
 
 type VMResponseObject struct {
 	Response    ResponseObject
-	SessionMeta map[string]string
+	SessionMeta map[string]interface{}
 }
 
 // DynamicMiddleware is a generic middleware that will execute JS code before continuing
@@ -226,7 +226,7 @@ func (d *VirtualEndpoint) ServeHTTPForCache(w http.ResponseWriter, r *http.Reque
 
 	// Save the sesison data (if modified)
 	if vmeta.UseSession {
-		newMeta := mapStrsToIfaces(newResponseData.SessionMeta)
+		newMeta := newResponseData.SessionMeta
 		if !reflect.DeepEqual(session.GetMetaData(), newMeta) {
 			session.SetMetaData(newMeta)
 			ctxSetSession(r, session, "", true)
