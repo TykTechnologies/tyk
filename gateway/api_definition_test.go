d2427a8f952ed700b8ee1b92ea4946701291627a
gernest <geofreyernest@live.com>
2020-08-31 15:15:16 +0300

    Fix reload test timeouts
    Majority of the tests for reloads were operating under false assumptions
    The ReloadTicker will  immediately trigger a reload, however queuing
    happens in a separate goroutine so its cricial to synchronize the
    operations to make sure reload happens.
    
    This change adds ReloadTestCase, which exposes methods to work with
    reloads in tests. This covers all cases of ensuring queue happened and
    also ensuring reloads happened.

--- gateway/api_definition_test.go
+++ gateway/api_definition_test.go
@@ -481,6 +481,8 @@ func TestWhitelistMethodWithAdditionalMiddleware(t *testing.T) {
 }
 
 func TestSyncAPISpecsDashboardSuccess(t *testing.T) {
+	ReloadTestCase.Enable()
+	defer ReloadTestCase.Disable()
 	// Test Dashboard
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/system/apis" {
@@ -513,8 +515,7 @@ func TestSyncAPISpecsDashboardSuccess(t *testing.T) {
 	}
 	handleRedisEvent(&msg, handled, wg.Done)
 
-	// Since we already know that reload is queued
-	ReloadTick <- time.Time{}
+	ReloadTestCase.TickOk(t)
 
 	// Wait for the reload to finish, then check it worked
 	wg.Wait()
@@ -779,6 +780,8 @@ func BenchmarkGetVersionFromRequest(b *testing.B) {
 }
 
 func TestSyncAPISpecsDashboardJSONFailure(t *testing.T) {
+	ReloadTestCase.Enable()
+	defer ReloadTestCase.Disable()
 	// Test Dashboard
 	callNum := 0
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -818,8 +821,7 @@ func TestSyncAPISpecsDashboardJSONFailure(t *testing.T) {
 	}
 	handleRedisEvent(&msg, handled, wg.Done)
 
-	// Since we already know that reload is queued
-	ReloadTick <- time.Time{}
+	ReloadTestCase.TickOk(t)
 
 	// Wait for the reload to finish, then check it worked
 	wg.Wait()
@@ -833,11 +835,10 @@ func TestSyncAPISpecsDashboardJSONFailure(t *testing.T) {
 
 	var wg2 sync.WaitGroup
 	wg2.Add(1)
+	ReloadTestCase.Reset()
 	handleRedisEvent(&msg, handled, wg2.Done)
 
-	// Since we already know that reload is queued
-	ReloadTick <- time.Time{}
-
+	ReloadTestCase.TickOk(t)
 	// Wait for the reload to finish, then check it worked
 	wg2.Wait()
 	apisMu.RLock()
