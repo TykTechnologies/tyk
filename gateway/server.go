19787f0c6f1a41680b2de9ab70d47db8af728cf2
Patric Vormstein <pvormstein@googlemail.com>
2020-10-23 12:21:58 +0200

    remove deprecated optimisations_use_async_session_write (TT-580) (#3366)
    * remove deprecated optimisations_use_async_session_write
    
    * remove unused struct
    
    * remove more unused code and config references
    
    (cherry picked from commit 4275e5417fc84ffaae64f0eefdd2ea2b3f5c6f0b)

--- gateway/server.go
+++ gateway/server.go
@@ -1293,15 +1293,6 @@ func Start() {
 		analytics.Stop()
 	}
 
-	// if using async session writes stop workers
-	if config.Global().UseAsyncSessionWrite {
-		DefaultOrgStore.Stop()
-		for i := range apiSpecs {
-			apiSpecs[i].StopSessionManagerPool()
-		}
-
-	}
-
 	// write pprof profiles
 	writeProfiles()
 
