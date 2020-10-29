97eb7e4adc6feb023ef926b65aa70aca6d3d3e45
sredny buitrago <sredny.buitrago@gmail.com>
2020-10-28 23:27:19 -0500

    on delete key by username then loop over all apis to update properly the key

--- user/session.go
+++ user/session.go
@@ -116,6 +116,10 @@ type SessionState struct {
 	keyHash string
 }
 
+func NewSessionState() *SessionState {
+	return &SessionState{}
+}
+
 func (s *SessionState) SetAccessRights(accessRights map[string]AccessDefinition) {
 	s.Mutex.Lock()
 	s.AccessRights = accessRights
