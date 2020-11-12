493640a48391c63170475c6341b940f5dc334d47
Patric Vormstein <pvormstein@googlemail.com>
2020-11-12 16:03:26 +0100

    temp: hanging websocket test

--- headers/headers.go
+++ headers/headers.go
@@ -43,6 +43,8 @@ const (
 const (
 	Upgrade              = "Upgrade"
 	SecWebSocketProtocol = "Sec-WebSocket-Protocol"
+	SecWebSocketVersion  = "Sec-WebSocket-Version"
+	SecWebSocketKey      = "Sec-WebSocket-Key"
 )
 
 // Gateway's custom response headers
