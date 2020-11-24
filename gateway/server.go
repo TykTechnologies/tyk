a1a739e40cf3f267ad0c28c2756ad52ffd6eeb0e
Lanre Adelowo <yo@lanre.wtf>
2020-11-24 17:30:01 +0100

    log as much errors with vault and consul

--- gateway/server.go
+++ gateway/server.go
@@ -1120,6 +1120,7 @@ func kvStore(value string) (string, error) {
 		key := strings.TrimPrefix(value, "consul://")
 		log.Debugf("Retrieving %s from consul", key)
 		if err := setUpConsul(); err != nil {
+			log.WithError(err).Error(err)
 			// Return value as is. If consul cannot be set up
 			return value, nil
 		}
@@ -1131,6 +1132,7 @@ func kvStore(value string) (string, error) {
 		key := strings.TrimPrefix(value, "vault://")
 		log.Debugf("Retrieving %s from vault", key)
 		if err := setUpVault(); err != nil {
+			log.WithError(err).Error(err)
 			// Return value as is If vault cannot be set up
 			return value, nil
 		}
