34447b7440c6f22884258a247f99ddd49525aead
gernest <geofreyernest@live.com>
2020-12-16 13:16:26 +0300

    retry once

--- rpc/rpc_client.go
+++ rpc/rpc_client.go
@@ -449,7 +449,7 @@ func FuncClientSingleton(funcName string, request interface{}) (result interface
 		result, err = funcClientSingleton.CallTimeout(funcName, request, GlobalRPCCallTimeout)
 		return nil
 	}, backoff.WithMaxRetries(
-		backoff.NewExponentialBackOff(), 2,
+		backoff.NewExponentialBackOff(), 1,
 	))
 	if be != nil {
 		err = be
