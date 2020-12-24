885e36d74b22f82801f84258627311dc5ba91f89
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2020-12-25 02:30:06 +0300

    [TT-1284] Load gateway health check to control port (#3434)
    * Load gateway health check to control port
    
    * Expand gateway health check test
    
    (cherry picked from commit 0395ad6567013918af23d2b69c18317d592e2174)

--- gateway/server.go
+++ gateway/server.go
@@ -414,6 +414,8 @@ func loadControlAPIEndpoints(muxer *mux.Router) {
 		}
 	}
 
+	muxer.HandleFunc("/"+config.Global().HealthCheckEndpointName, liveCheckHandler)
+
 	r := mux.NewRouter()
 	muxer.PathPrefix("/tyk/").Handler(http.StripPrefix("/tyk",
 		stripSlashes(checkIsAPIOwner(controlAPICheckClientCertificate("/gateway/client", InstrumentationMW(r)))),
