f12fdcddbb3faa5ccd85363ed9798f28eb22df0b
Patric Vormstein <pvormstein@googlemail.com>
2021-01-13 14:58:34 +0100

    add ability to add httpclient

--- apidef/graphql_config_adapter_test.go
+++ apidef/graphql_config_adapter_test.go
@@ -2,9 +2,11 @@ package apidef
 
 import (
 	"encoding/json"
+	"net/http"
 	"testing"
 
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/graphql_datasource"
+	"github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/httpclient"
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/rest_datasource"
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/plan"
 	"github.com/jensneuse/graphql-go-tools/pkg/graphql"
@@ -13,26 +15,27 @@ import (
 )
 
 func TestGraphQLConfigAdapter_EngineConfigV2(t *testing.T) {
-	run := func(t *testing.T, inputJSON string) (*graphql.EngineV2Configuration, error) {
+	run := func(t *testing.T, inputJSON string, httpClient *http.Client) (*graphql.EngineV2Configuration, error) {
 		var gqlConfig GraphQLConfig
 		err := json.Unmarshal([]byte(inputJSON), &gqlConfig)
 		require.NoError(t, err)
 
 		adapter := NewGraphQLConfigAdapter(gqlConfig)
+		adapter.SetHttpClient(httpClient)
 		return adapter.EngineConfigV2()
 	}
 
 	runWithError := func(inputJSON string, expectedErr error) func(t *testing.T) {
 		return func(t *testing.T) {
-			_, err := run(t, inputJSON)
+			_, err := run(t, inputJSON, nil)
 			assert.Error(t, err)
 			assert.Equal(t, expectedErr, err)
 		}
 	}
 
-	runWithoutError := func(inputJSON string, expectedEngineV2ConfigBuilder func(t *testing.T) *graphql.EngineV2Configuration) func(t *testing.T) {
+	runWithoutError := func(inputJSON string, httpClient *http.Client, expectedEngineV2ConfigBuilder func(t *testing.T) *graphql.EngineV2Configuration) func(t *testing.T) {
 		return func(t *testing.T) {
-			engineV2Conf, err := run(t, inputJSON)
+			engineV2Conf, err := run(t, inputJSON, httpClient)
 			expectedEngineV2Config := expectedEngineV2ConfigBuilder(t)
 
 			assert.NoError(t, err)
@@ -40,12 +43,14 @@ func TestGraphQLConfigAdapter_EngineConfigV2(t *testing.T) {
 		}
 	}
 
+	httpClient := &http.Client{}
+
 	t.Run("should return error when provided config is not v2",
 		runWithError(graphqlEngineV1ConfigJson, ErrUnsupportedGraphQLConfigVersion),
 	)
 
 	t.Run("should convert graphql v2 config to engine config v2",
-		runWithoutError(graphqlEngineV2ConfigJson,
+		runWithoutError(graphqlEngineV2ConfigJson, httpClient,
 			func(t *testing.T) *graphql.EngineV2Configuration {
 				schema, err := graphql.NewSchemaFromString("type Query { rest: String, gql: String }")
 				require.NoError(t, err)
@@ -68,7 +73,9 @@ func TestGraphQLConfigAdapter_EngineConfigV2(t *testing.T) {
 								FieldNames: []string{"rest"},
 							},
 						},
-						Factory: &rest_datasource.Factory{},
+						Factory: &rest_datasource.Factory{
+							Client: httpclient.NewNetHttpClient(httpClient),
+						},
 						Custom: rest_datasource.ConfigJSON(rest_datasource.Configuration{
 							Fetch: rest_datasource.FetchConfiguration{
 								URL:    "https://rest.example.com",
@@ -83,7 +90,9 @@ func TestGraphQLConfigAdapter_EngineConfigV2(t *testing.T) {
 								FieldNames: []string{"gql"},
 							},
 						},
-						Factory: &graphql_datasource.Factory{},
+						Factory: &graphql_datasource.Factory{
+							Client: httpclient.NewNetHttpClient(httpClient),
+						},
 						Custom: graphql_datasource.ConfigJson(graphql_datasource.Configuration{
 							Fetch: graphql_datasource.FetchConfiguration{
 								URL:        "https://graphql.example.com",
