835e90e499491adc49e947ddb1ab8cd7cbc5d248
tbuchaillot <tombuchaillot89@gmail.com>
2021-02-10 12:09:55 -0300

    fixing rpc emergency mode logic in middleware

--- gateway/middleware.go
+++ gateway/middleware.go
@@ -692,21 +692,25 @@ func (t BaseMiddleware) CheckSessionAndIdentityForValidKey(originalKey *string, 
 		return session.Clone(), true
 	}
 
-	// Only search in RPC if the storage is RPCStorageHandler and it's not in emergency mode
-	if _, ok := t.Spec.AuthManager.Store().(*RPCStorageHandler); ok && !rpc.IsEmergencyMode() {
-		t.Logger().Debug("Querying authstore")
-		// 2. If not there, get it from the AuthorizationHandler
-		session, found = t.Spec.AuthManager.KeyAuthorised(key)
-		if !found && storage.TokenOrg(key) != t.Spec.OrgID {
-			//treat it as a custom key
-			key = generateToken(t.Spec.OrgID, key)
-			cacheKey = key
-			if t.Spec.GlobalConfig.HashKeys {
-				cacheKey = storage.HashStr(cacheKey)
-			}
-			session, found = t.Spec.AuthManager.KeyAuthorised(key)
-		}
+
+	if _, ok := t.Spec.AuthManager.Store().(*RPCStorageHandler); ok && rpc.IsEmergencyMode() {
+		return session.Clone(), false
 	}
+
+	// Only search in RPC if it's not in emergency mode
+	t.Logger().Debug("Querying authstore")
+	// 2. If not there, get it from the AuthorizationHandler
+	session, found = t.Spec.AuthManager.KeyAuthorised(key)
+	if !found && storage.TokenOrg(key) != t.Spec.OrgID {
+		//treat it as a custom key
+		key = generateToken(t.Spec.OrgID, key)
+		cacheKey = key
+		if t.Spec.GlobalConfig.HashKeys {
+			cacheKey = storage.HashStr(cacheKey)
+		}
+		session, found = t.Spec.AuthManager.KeyAuthorised(key)
+	}
+
 
 	if found {
 		// update value of originalKey, as for custom-keys it might get updated (the key is generated again using alias)
