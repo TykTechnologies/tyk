90eca5495ddcacc0c45993f8cdd32fc7b32d8934
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2021-02-18 19:37:46 +0300

    Move CORS handler from chain to middleware to cover oauth2 endpoints (#3462)
    (cherry picked from commit fd1440d5e97ac090e3621e4309192026f1cbfa78)

--- gateway/server.go
+++ gateway/server.go
@@ -40,7 +40,6 @@ import (
 	"github.com/evalphobia/logrus_sentry"
 	graylogHook "github.com/gemnasium/logrus-graylog-hook"
 	"github.com/gorilla/mux"
-	"github.com/justinas/alice"
 	"github.com/lonelycode/osin"
 	newrelic "github.com/newrelic/go-agent"
 	"github.com/rs/cors"
@@ -677,7 +676,7 @@ func createResponseMiddlewareChain(spec *APISpec, responseFuncs []apidef.Middlew
 	spec.ResponseChain = responseChain
 }
 
-func handleCORS(chain *[]alice.Constructor, spec *APISpec) {
+func handleCORS(router *mux.Router, spec *APISpec) {
 
 	if spec.CORS.Enable {
 		mainLog.Debug("CORS ENABLED")
@@ -692,7 +691,7 @@ func handleCORS(chain *[]alice.Constructor, spec *APISpec) {
 			Debug:              spec.CORS.Debug,
 		})
 
-		*chain = append(*chain, c.Handler)
+		router.Use(c.Handler)
 	}
 }
 
