9c53859a71eab89605de6a0ba01f3d664b1ad5a3
Patric Vormstein <pvormstein@googlemail.com>
2021-02-26 14:01:08 +0100

    fix missing field arguments for gql data source (#3476)
    https://tyktech.atlassian.net/browse/TT-1695
    https://tyktech.atlassian.net/browse/TT-1691
    
    
    ## Types of changes
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test coverage to functionality)

--- apidef/adapter/graphql_config_adapter.go
+++ apidef/adapter/graphql_config_adapter.go
@@ -5,8 +5,6 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/jensneuse/graphql-go-tools/pkg/ast"
-	"github.com/jensneuse/graphql-go-tools/pkg/astparser"
 	graphqlDataSource "github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/graphql_datasource"
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/httpclient"
 	restDataSource "github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/rest_datasource"
@@ -21,6 +19,7 @@ var ErrUnsupportedGraphQLConfigVersion = errors.New("provided version of GraphQL
 type GraphQLConfigAdapter struct {
 	config     apidef.GraphQLConfig
 	httpClient *http.Client
+	schema     *graphql.Schema
 }
 
 func NewGraphQLConfigAdapter(config apidef.GraphQLConfig) GraphQLConfigAdapter {
@@ -32,12 +31,13 @@ func (g *GraphQLConfigAdapter) EngineConfigV2() (*graphql.EngineV2Configuration,
 		return nil, ErrUnsupportedGraphQLConfigVersion
 	}
 
-	schema, err := graphql.NewSchemaFromString(g.config.Schema)
+	var err error
+	g.schema, err = graphql.NewSchemaFromString(g.config.Schema)
 	if err != nil {
 		return nil, err
 	}
 
-	conf := graphql.NewEngineV2Configuration(schema)
+	conf := graphql.NewEngineV2Configuration(g.schema)
 
 	fieldConfigs := g.engineConfigV2FieldConfigs()
 	datsSources, err := g.engineConfigV2DataSources()
@@ -62,6 +62,10 @@ func (g *GraphQLConfigAdapter) engineConfigV2FieldConfigs() (planFieldConfigs pl
 
 		planFieldConfigs = append(planFieldConfigs, planFieldConfig)
 	}
+
+	generatedArgs := g.schema.GetAllFieldArguments(graphql.NewSkipReservedNamesFunc())
+	generatedArgsAsLookupMap := graphql.CreateTypeFieldArgumentsLookupMap(generatedArgs)
+	g.engineConfigV2Arguments(&planFieldConfigs, generatedArgsAsLookupMap)
 
 	return planFieldConfigs
 }
@@ -134,6 +138,45 @@ func (g *GraphQLConfigAdapter) engineConfigV2DataSources() (planDataSources []pl
 	return planDataSources, err
 }
 
+func (g *GraphQLConfigAdapter) engineConfigV2Arguments(fieldConfs *plan.FieldConfigurations, generatedArgs map[graphql.TypeFieldLookupKey]graphql.TypeFieldArguments) {
+	for i := range *fieldConfs {
+		if len(generatedArgs) == 0 {
+			return
+		}
+
+		lookupKey := graphql.CreateTypeFieldLookupKey((*fieldConfs)[i].TypeName, (*fieldConfs)[i].FieldName)
+		currentArgs, ok := generatedArgs[lookupKey]
+		if !ok {
+			continue
+		}
+
+		(*fieldConfs)[i].Arguments = g.createArgumentConfigurationsForArgumentNames(currentArgs.ArgumentNames)
+		delete(generatedArgs, lookupKey)
+	}
+
+	for _, genArgs := range generatedArgs {
+		*fieldConfs = append(*fieldConfs, plan.FieldConfiguration{
+			TypeName:  genArgs.TypeName,
+			FieldName: genArgs.FieldName,
+			Arguments: g.createArgumentConfigurationsForArgumentNames(genArgs.ArgumentNames),
+		})
+	}
+}
+
+func (g *GraphQLConfigAdapter) createArgumentConfigurationsForArgumentNames(argumentNames []string) plan.ArgumentsConfigurations {
+	argConfs := plan.ArgumentsConfigurations{}
+	for _, argName := range argumentNames {
+		argConf := plan.ArgumentConfiguration{
+			Name:       argName,
+			SourceType: plan.FieldArgumentSource,
+		}
+
+		argConfs = append(argConfs, argConf)
+	}
+
+	return argConfs
+}
+
 func (g *GraphQLConfigAdapter) SetHttpClient(httpClient *http.Client) {
 	g.httpClient = httpClient
 }
@@ -170,106 +213,24 @@ func (g *GraphQLConfigAdapter) convertHttpHeadersToEngineV2Headers(apiDefHeaders
 }
 
 func (g *GraphQLConfigAdapter) determineChildNodes(planDataSources []plan.DataSourceConfiguration) error {
-	schema, report := astparser.ParseGraphqlDocumentString(g.config.Schema)
-	if report.HasErrors() {
-		return report
-	}
 	for i := range planDataSources {
 		for j := range planDataSources[i].RootNodes {
 			typeName := planDataSources[i].RootNodes[j].TypeName
 			for k := range planDataSources[i].RootNodes[j].FieldNames {
 				fieldName := planDataSources[i].RootNodes[j].FieldNames[k]
-				children := g.findFieldChildren(typeName, fieldName, &schema, planDataSources)
+				typeFields := g.schema.GetAllNestedFieldChildrenFromTypeField(typeName, fieldName, graphql.NewIsDataSourceConfigV2RootFieldSkipFunc(planDataSources))
+
+				children := make([]plan.TypeField, 0)
+				for _, tf := range typeFields {
+					childNode := plan.TypeField{
+						TypeName:   tf.TypeName,
+						FieldNames: tf.FieldNames,
+					}
+					children = append(children, childNode)
+				}
 				planDataSources[i].ChildNodes = append(planDataSources[i].ChildNodes, children...)
 			}
 		}
 	}
 	return nil
-}
-
-func (g *GraphQLConfigAdapter) findFieldChildren(typeName, fieldName string, schema *ast.Document, dataSources []plan.DataSourceConfiguration) []plan.TypeField {
-	fields := g.nodeFieldRefs(typeName, schema)
-	if len(fields) == 0 {
-		return nil
-	}
-	for _, ref := range fields {
-		if fieldName == schema.FieldDefinitionNameString(ref) {
-			fieldTypeName := schema.FieldDefinitionTypeNode(ref).NameString(schema)
-			childNodes := []plan.TypeField{}
-			g.findNestedFieldChildren(fieldTypeName, schema, dataSources, &childNodes)
-			return childNodes
-		}
-	}
-
-	return nil
-}
-
-func (g *GraphQLConfigAdapter) findNestedFieldChildren(typeName string, schema *ast.Document, dataSources []plan.DataSourceConfiguration, childNodes *[]plan.TypeField) {
-	fields := g.nodeFieldRefs(typeName, schema)
-	if len(fields) == 0 {
-		return
-	}
-	for _, ref := range fields {
-		fieldName := schema.FieldDefinitionNameString(ref)
-		if g.isRootField(typeName, fieldName, dataSources) {
-			continue
-		}
-		g.putChildNode(childNodes, typeName, fieldName)
-		fieldTypeName := schema.FieldDefinitionTypeNode(ref).NameString(schema)
-		g.findNestedFieldChildren(fieldTypeName, schema, dataSources, childNodes)
-	}
-	return
-}
-
-func (g *GraphQLConfigAdapter) nodeFieldRefs(typeName string, schema *ast.Document) []int {
-	node, exists := schema.Index.FirstNodeByNameStr(typeName)
-	if !exists {
-		return nil
-	}
-	var fields []int
-	switch node.Kind {
-	case ast.NodeKindObjectTypeDefinition:
-		fields = schema.ObjectTypeDefinitions[node.Ref].FieldsDefinition.Refs
-	case ast.NodeKindInterfaceTypeDefinition:
-		fields = schema.InterfaceTypeDefinitions[node.Ref].FieldsDefinition.Refs
-	default:
-		return nil
-	}
-
-	return fields
-}
-
-func (g *GraphQLConfigAdapter) isRootField(typeName, fieldName string, dataSources []plan.DataSourceConfiguration) bool {
-	for i := range dataSources {
-		for j := range dataSources[i].RootNodes {
-			if typeName != dataSources[i].RootNodes[j].TypeName {
-				continue
-			}
-			for k := range dataSources[i].RootNodes[j].FieldNames {
-				if fieldName == dataSources[i].RootNodes[j].FieldNames[k] {
-					return true
-				}
-			}
-		}
-	}
-	return false
-}
-
-func (g *GraphQLConfigAdapter) putChildNode(nodes *[]plan.TypeField, typeName, fieldName string) {
-	for i := range *nodes {
-		if typeName != (*nodes)[i].TypeName {
-			continue
-		}
-		for j := range (*nodes)[i].FieldNames {
-			if fieldName == (*nodes)[i].FieldNames[j] {
-				return
-			}
-		}
-		(*nodes)[i].FieldNames = append((*nodes)[i].FieldNames, fieldName)
-		return
-	}
-	*nodes = append(*nodes, plan.TypeField{
-		TypeName:   typeName,
-		FieldNames: []string{fieldName},
-	})
 }
