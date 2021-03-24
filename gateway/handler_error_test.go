30a026d8e7c9d575bd0f563c1c07e9bb89064027
Leonid Bugaev <leonsbox@gmail.com>
2021-03-24 15:33:33 +0300

    [TT-1680] Fix content type detection when charset used
    Now only first part of the header value is used to detect content type.
    E.g. we just split by ";"
    
    Also re-factored test, and now istead of having http server causing panic (and messing with logs) it just has target url which points to non existing port.

--- gateway/handler_error_test.go
+++ gateway/handler_error_test.go
@@ -34,21 +34,26 @@ func TestHandleError_text_xml(t *testing.T) {
 	ts := StartTest()
 	defer ts.Close()
 
-	// Simulate 500 error
-	h := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		panic("I should fail!")
-	}))
-	defer h.Close()
-
 	BuildAndLoadAPI(func(spec *APISpec) {
 		spec.Proxy.ListenPath = "/"
-		spec.Proxy.TargetURL = h.URL
+		spec.Proxy.TargetURL = "http://localhost:66666"
 	})
 	ts.Run(t, test.TestCase{
 		Path: "/",
 		Code: http.StatusInternalServerError,
 		Headers: map[string]string{
 			headers.ContentType: headers.TextXML,
+		},
+		BodyMatchFunc: func(b []byte) bool {
+			return strings.TrimSpace(expect) == string(bytes.TrimSpace(b))
+		},
+	})
+
+	ts.Run(t, test.TestCase{
+		Path: "/",
+		Code: http.StatusInternalServerError,
+		Headers: map[string]string{
+			headers.ContentType: headers.TextXML + "; charset=UTF-8",
 		},
 		BodyMatchFunc: func(b []byte) bool {
 			return strings.TrimSpace(expect) == string(bytes.TrimSpace(b))
