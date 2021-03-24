dcdfa761a8de07dd40ef131cf4e124fab537a5ab
gernest <geofreyernest@live.com>
2021-03-24 09:41:25 +0300

    update zipkin tests

--- trace/openzipkin/config_test.go
+++ trace/openzipkin/config_test.go
@@ -1,39 +1,15 @@
 package openzipkin
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/TykTechnologies/tyk/config"
 )
 
 func TestLoad(t *testing.T) {
-	dir, err := ioutil.TempDir("", "tyk")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-
-	f := filepath.Join(dir, "zipkin.json")
-	err = ioutil.WriteFile(f, []byte(`{
-			"tracing": {
-			  "enabled": true,
-			  "name": "zipkin",
-			  "options": {
-				"reporter": {
-				  "url": "http:localhost:9411/api/v2/spans"
-				}
-			  }
-			}
-		  }
-		  `), 0600)
-	if err != nil {
-		t.Fatal(err)
-	}
+	f := "testdata/zipkin.json"
 	var c config.Config
-	err = config.Load([]string{f}, &c)
+	err := config.Load([]string{f}, &c)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -45,5 +21,4 @@ func TestLoad(t *testing.T) {
 	if z.Reporter.URL != u {
 		t.Errorf("expected %q got %q", u, z.Reporter.URL)
 	}
-
 }
