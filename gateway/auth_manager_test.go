43cdce1f524cc3a769bdada597c30845eb51c64b
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2021-03-30 22:29:34 +0300

    [TT-1542] Add fallback hash key functions to catch function changes (#3505)
    Added new `hash_key_function_fallback` array option to specify list of hash function which Gateway needs to check if key with default hash key function not found. 
    
    This functionality needed only if you change default hashing algorithm, but your token information does not hold information about hashing function like custom keys, certificates, or basic auth tokens.

--- gateway/auth_manager_test.go
+++ gateway/auth_manager_test.go
@@ -1,8 +1,13 @@
 package gateway
 
 import (
+	"crypto/x509"
 	"net/http"
 	"testing"
+
+	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/TykTechnologies/tyk/certs"
+	"github.com/TykTechnologies/tyk/headers"
 
 	"github.com/TykTechnologies/tyk/storage"
 
@@ -100,5 +105,109 @@ func TestAuthenticationAfterUpdateKey(t *testing.T) {
 
 	t.Run("HashKeys=true", func(t *testing.T) {
 		assert(true)
+	})
+}
+
+func TestHashKeyFunctionChanged(t *testing.T) {
+	_, _, combinedPEM, _ := genServerCertificate()
+	serverCertID, _ := CertificateManager.Add(combinedPEM, "")
+	defer CertificateManager.Delete(serverCertID, "")
+
+	_, _, _, clientCert := genCertificate(&x509.Certificate{})
+	clientCertID := certs.HexSHA256(clientCert.Certificate[0])
+	client := GetTLSClient(nil, nil)
+
+	globalConf := config.Global()
+	globalConf.HttpServerOptions.UseSSL = true
+	globalConf.HttpServerOptions.SSLCertificates = []string{serverCertID}
+	globalConf.HashKeys = true
+	globalConf.HashKeyFunction = "murmur64"
+	globalConf.LocalSessionCache.DisableCacheSessionState = true
+	config.SetGlobal(globalConf)
+
+	defer ResetTestConfig()
+
+	g := StartTest()
+	defer g.Close()
+
+	api := BuildAndLoadAPI(func(spec *APISpec) {
+		spec.Proxy.ListenPath = "/"
+		spec.UseKeylessAccess = false
+		spec.AuthConfigs = map[string]apidef.AuthConfig{
+			authTokenType: {UseCertificate: true},
+		}
+	})[0]
+
+	globalConf = config.Global()
+
+	testChangeHashFunc := func(t *testing.T, authHeader map[string]string, client *http.Client, failCode int) {
+		_, _ = g.Run(t, test.TestCase{Headers: authHeader, Client: client, Code: http.StatusOK})
+
+		globalConf.HashKeyFunction = "sha256"
+		config.SetGlobal(globalConf)
+
+		_, _ = g.Run(t, test.TestCase{Headers: authHeader, Client: client, Code: failCode})
+
+		globalConf.HashKeyFunctionFallback = []string{"murmur64"}
+		config.SetGlobal(globalConf)
+
+		_, _ = g.Run(t, test.TestCase{Headers: authHeader, Client: client, Code: http.StatusOK})
+
+		// Reset
+		globalConf.HashKeyFunction = "murmur64"
+		globalConf.HashKeyFunctionFallback = nil
+		config.SetGlobal(globalConf)
+	}
+
+	t.Run("custom key", func(t *testing.T) {
+		const customKey = "custom-key"
+
+		session := CreateStandardSession()
+		session.AccessRights = map[string]user.AccessDefinition{"test": {
+			APIID: "test", Versions: []string{"v1"},
+		}}
+
+		_, _ = g.Run(t, test.TestCase{AdminAuth: true, Method: http.MethodPost, Path: "/tyk/keys/" + customKey,
+			Data: session, Client: client, Code: http.StatusOK})
+
+		testChangeHashFunc(t, map[string]string{headers.Authorization: customKey}, client, http.StatusForbidden)
+	})
+
+	t.Run("basic auth key", func(t *testing.T) {
+		api.UseBasicAuth = true
+		LoadAPI(api)
+		globalConf = config.Global()
+
+		session := CreateStandardSession()
+		session.BasicAuthData.Password = "password"
+		session.AccessRights = map[string]user.AccessDefinition{"test": {
+			APIID: "test", Versions: []string{"v1"},
+		}}
+
+		_, _ = g.Run(t, test.TestCase{AdminAuth: true, Method: http.MethodPost, Path: "/tyk/keys/user",
+			Data: session, Client: client, Code: http.StatusOK})
+
+		authHeader := map[string]string{"Authorization": genAuthHeader("user", "password")}
+
+		testChangeHashFunc(t, authHeader, client, http.StatusUnauthorized)
+
+		api.UseBasicAuth = false
+		LoadAPI(api)
+		globalConf = config.Global()
+	})
+
+	t.Run("client certificate", func(t *testing.T) {
+		session := CreateStandardSession()
+		session.Certificate = clientCertID
+		session.BasicAuthData.Password = "password"
+		session.AccessRights = map[string]user.AccessDefinition{"test": {
+			APIID: "test", Versions: []string{"v1"},
+		}}
+
+		_, _ = g.Run(t, test.TestCase{AdminAuth: true, Method: http.MethodPost, Path: "/tyk/keys/create",
+			Data: session, Client: client, Code: http.StatusOK})
+
+		client = GetTLSClient(&clientCert, nil)
+		testChangeHashFunc(t, nil, client, http.StatusForbidden)
 	})
 }
