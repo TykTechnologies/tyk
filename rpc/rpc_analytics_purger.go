fc79fb691a73db8c0d3c47831d1c1e90ab869ece
Sredny M <sredny.buitrago@gmail.com>
2021-04-13 09:13:31 -0500

    TT-1683 added config for purge analytics interval (#3526)
    * TT-1683 added config for purge analytics interval
    
    * ra goimports on rpc_analytics_purger.go
    
    * added purge_interval to lintern schema
    
    Co-authored-by: Tomas Buchaillot <tombuchaillot89@gmail.com>
    (cherry picked from commit 423577535aea2f720677da62891f010b7bf75f7a)

--- rpc/rpc_analytics_purger.go
+++ rpc/rpc_analytics_purger.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/TykTechnologies/tyk/config"
+
 	msgpack "gopkg.in/vmihailenco/msgpack.v2"
 
 	"github.com/TykTechnologies/tyk/storage"
@@ -89,7 +91,9 @@ func (r *Purger) Connect() {
 // PurgeLoop starts the loop that will pull data out of the in-memory
 // store and into RPC.
 func (r Purger) PurgeLoop(ctx context.Context) {
-	tick := time.NewTicker(10 * time.Second)
+	interval := time.Duration(config.Global().AnalyticsConfig.PurgeInterval)
+	tick := time.NewTicker(interval * time.Second)
+
 	for {
 		select {
 		case <-ctx.Done():
