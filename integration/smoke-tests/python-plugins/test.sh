78c8fd63b5f1ac2554a10b9c0ac5118d24242cd2
Alok G Singh <alephnull@hcoop.net>
2021-06-29 18:32:10 +0530

    dc project is dirname implicitly

--- integration/smoke-tests/python-plugins/test.sh
+++ integration/smoke-tests/python-plugins/test.sh
@@ -17,7 +17,7 @@ EOF
 [[ -z $1 ]] && usage $0
 export tag=$1
 
-docker-compose build && docker-compose up -d -p pp
+docker-compose build && docker-compose up -d
 sleep 2 # Wait to start
 curl http://localhost:8080/pyplugin/headers | jq -e '.headers.Foo == "Bar"'
-docker-compose down -p pp
+docker-compose down
