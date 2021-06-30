6a71584f6c0bbd36ea0e01b0288e04e3975c1d00
Alok G Singh <alok@tyk.io>
2021-06-30 15:01:38 +0530

    releng: [TD-350,TD-388,TD-387] implement forward ports from master, test upgrades, python bindings (#3577)
     add  .g/w/sync-automation.yml
    
    :robot: Beep, boop, this PR has been generated by [automation](https://github.com/TykTechnologies/gromit/blob/master/cmd/policy.go) because commits on `master`  need to be propagated to other branches  as per the current repository policy.
    
    ## Policy for tyk
    Commmits affecting these files will be ported as listed below.
    - bin/unlock-agent.sh
    - .goreleaser.yml
    - Dockerfileg.std
    - Dockerfile.slim
    - aws/byol.pkr.hcl
    - .github/workflows/release.yml
    - .github/workflows/del-env.yml
    - integration/terraform/outputs.tf
    - install/before_install.sh
    - install/post_install.sh
    - install/post_remove.sh
    - images/*
    
    ### Backports
    
    | Source branch | Destination branch |
    |---------------|--------------------|
    | release-3.0.5 | releng/release-3-lts |
    | release-3.1.2 | releng/release-3.1 |
    | release-3.2.0 | releng/release-3.2 |
    
    The destination branches have to be manually merged to their respective ancestors.
    
    
    
    ### Forward ports
    
    No forward port branch for release engineering has been configured on tyk.
    
    
    Thank you for reading this far. Since this PR is generated by automation, if there are inconsistencies in the information provided above, they are bugs and merging this PR will probably do some damage. Please log a jira in the TD project citing this PR.

--- install/before_install.sh
+++ install/before_install.sh
@@ -2,12 +2,12 @@
 echo "Creating user and group..."
 GROUPNAME="tyk"
 USERNAME="tyk"
-OLD_DIR="/opt/tyk-gateway"
-NEW_DIR="/opt/tyk"
 
 getent group "$GROUPNAME" >/dev/null || groupadd -r "$GROUPNAME"
 getent passwd "$USERNAME" >/dev/null || useradd -r -g "$GROUPNAME" -M -s /sbin/nologin -c "Tyk service user" "$USERNAME"
-if [ -d ${OLD_DIR} ]; then
-    echo "Found legacy directory $OLDDIR. This will be moved to $NEW_DIR, no further action is required on your part."
-    mv $OLD_DIR $NEW_DIR && ln $NEW_DIR $OLD_DIR
+
+# This stopped being a symlink in PR #3569
+if [ -L /opt/tyk-gateway/coprocess/python/proto ]; then
+    echo "Removing legacy python protobuf symlink"
+    rm /opt/tyk-gateway/coprocess/python/proto
 fi
