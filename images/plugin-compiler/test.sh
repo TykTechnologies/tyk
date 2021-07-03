ea58a8a23e34eddb5d70ae1419927738ba39ccb3
Leonid Bugaev <leonsbox@gmail.com>
2021-07-03 09:37:31 +0300

    Fix Go plugins
    TBD

--- images/plugin-compiler/test.sh
+++ images/plugin-compiler/test.sh
@@ -1,26 +1,23 @@
-#!/usr/bin/env zsh
-
+#!/bin/bash
+set -eo pipefail
 function usage {
     local progname=$1
     cat <<EOF
 Usage:
 $progname <tag>
-
 Builds the plugin in testplugin using the supplied tag and tests it in the corresponding gw image.
 Requires docker compose.
 EOF
     exit 1
 }
-
-TRAPINT() {
-    print Tearing down test env
-    docker-compose -f test.yml down
-}
-
 [[ -z $1 ]] && usage $0
 export tag=$1
-
-rm -v testplugin/*.so
+docker-compose down
+rm -fv testplugin/*.so || true
 docker run --rm -v `pwd`/testplugin:/plugin-source tykio/tyk-plugin-compiler:${tag} testplugin.so
-(cd testplugin && cp testplugin.so testplugin-${tag}.so)
-docker-compose -f test.yml up
+docker-compose up -d
+sleep 2 # Wait for init
+docker-compose logs
+
+curl -vvv http://localhost:8080/goplugin/headers
+curl http://localhost:8080/goplugin/headers | jq -e '.headers.Foo == "Bar"'
--- images/plugin-compiler/testplugin/main.go
+++ images/plugin-compiler/testplugin/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"net/http"
+	"html/template"
+
+	"gopkg.in/Masterminds/sprig.v2"
+
+	"github.com/TykTechnologies/tyk/ctx"
+	"github.com/TykTechnologies/tyk/log"
 )
+
+var logger = log.Get()
 
 // AddFooBarHeader adds custom "Foo: Bar" header to the request
 //nolint:deadcode
 func AddFooBarHeader(rw http.ResponseWriter, r *http.Request) {
 	r.Header.Add("Foo", "Bar")
+	logger.Info("Test")
+
+	api := ctx.GetDefinition(r)
+	if api != nil {
+		logger.Info("API Definition", api)
+	}
+
+	// Set up variables and template.
+	tpl := `Hello {{.Name | trim | lower}}`
+
+	// Get the Sprig function map.
+	template.Must(template.New("test").Funcs(sprig.FuncMap()).Parse(tpl))
 }
 
 func main() {}
