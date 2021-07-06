b061102dc606d6a700f4f0036ba5cb3a8db96a54
Leonid Bugaev <leonsbox@gmail.com>
2021-07-06 13:21:26 +0300

    Fix/lts go plugins (#3611)
    ## Description
    TBD
    
    ## Related Issue
    <!-- This project only accepts pull requests related to open issues -->
    <!-- If suggesting a new feature or change, please discuss it in an issue first -->
    <!-- If fixing a bug, there should be an issue describing it with steps to reproduce -->
    <!-- Please link to the issue here -->
    
    ## Motivation and Context
    <!-- Why is this change required? What problem does it solve? -->
    
    ## How This Has Been Tested
    <!-- Please describe in detail how you tested your changes -->
    <!-- Include details of your testing environment, and the tests you ran to see how your change affects other areas of
    the code, etc. -->
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    <!-- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test coverage to functionality)
    
    ## Checklist
    <!-- Go over all the following points, and put an `x` in all the boxes that apply -->
    <!-- If you're unsure about any of these, don't hesitate to ask; we're here to help! -->
    - [ ] Make sure you are requesting to **pull a topic/feature/bugfix branch** (right side). If pulling from your own
          fork, don't request your `master`!
    - [ ] Make sure you are making a pull request against the **`master` branch** (left side). Also, you should start
          *your branch* off *our latest `master`*.
    - [ ] My change requires a change to the documentation.
      - [ ] If you've changed APIs, describe what needs to be updated in the documentation.
      - [ ] If new config option added, ensure that it can be set via ENV variable
    - [ ] I have updated the documentation accordingly.
    - [ ] Modules and vendor dependencies have been updated; run `go mod tidy && go mod vendor`
    - [ ] When updating library version must provide reason/explanation for this update.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    - [ ] Check your code additions will not fail linting checks:
      - [ ] `go fmt -s`
      - [ ] `go vet`

--- images/plugin-compiler/data/build.sh
+++ images/plugin-compiler/data/build.sh
@@ -18,11 +18,38 @@ if [ -z "$plugin_name" ]; then
     exit 1
 fi
 
-# Handle if plugin has own vendor folder, and ignore error if not
+
 yes | cp -r $PLUGIN_SOURCE_PATH/* $PLUGIN_BUILD_PATH || true
-yes | cp -r $PLUGIN_BUILD_PATH/vendor $GOPATH/src || true \
+
+
+cd $PLUGIN_BUILD_PATH
+
+# Handle if plugin has own vendor folder, and ignore error if not
+[ -f go.mod ] && [ ! -d ./vendor ] && GO111MODULE=on go mod vendor
+# Ensure that go modules not used
+rm -rf go.mod
+
+# We do not needd to care whicch version of Tyk vendored in plugin, since we going to use version inside compiler
+rm -rf $PLUGIN_BUILD_PATH/vendor/github.com/TykTechnologies/tyk
+
+# Copy plugin vendored pkgs to GOPATH
+yes | cp -rf $PLUGIN_BUILD_PATH/vendor/* $GOPATH/src || true \
         && rm -rf $PLUGIN_BUILD_PATH/vendor
 
-cd $PLUGIN_BUILD_PATH \
-    && go build -buildmode=plugin -ldflags "-pluginpath=$plugin_path" -o $plugin_name \
-    && mv $plugin_name $PLUGIN_SOURCE_PATH
+# Ensure that GW package versions have priorities
+
+# We can't just copy Tyk dependencies on top of plugin dependencies, since different package versions have different file structures
+# First we need to find which deps GW already has, remove this folders, and after copy fresh versions from GW
+
+# github.com and rest of packages have different nesting levels, so have to handle it separately
+ls -d $TYK_GW_PATH/vendor/github.com/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | xargs -d '\n' rm -rf
+ls -d $TYK_GW_PATH/vendor/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | grep -v github | xargs -d '\n' rm -rf
+
+
+# Copy GW dependencies
+yes | cp -rf $TYK_GW_PATH/vendor/* $GOPATH/src
+rm -rf $TYK_GW_PATH/vendor
+
+
+go build -buildmode=plugin -ldflags "-pluginpath=$plugin_path" -o $plugin_name \
+&& mv $plugin_name $PLUGIN_SOURCE_PATH
