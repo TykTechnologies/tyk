bf2782051d1e12f488dedb0638186ba01085e048
Alok G Singh <alephnull@hcoop.net>
2021-07-06 20:02:17 +0530

    remove legacy symlink for coprocess bindings

--- install/before_install.sh
+++ install/before_install.sh
@@ -5,3 +5,9 @@ USERNAME="tyk"
 
 getent group "$GROUPNAME" >/dev/null || groupadd -r "$GROUPNAME"
 getent passwd "$USERNAME" >/dev/null || useradd -r -g "$GROUPNAME" -M -s /sbin/nologin -c "Tyk service user" "$USERNAME"
+
+# This stopped being a symlink in PR #3569
+if [ -L /opt/tyk-gateway/coprocess/python/proto ]; then
+    echo "Removing legacy python protobuf symlink"
+    rm /opt/tyk-gateway/coprocess/python/proto
+fi
