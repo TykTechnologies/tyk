7931d56e1e282ed6bd35df32e97d5255ff665d9a
sredny buitrago <sredny.buitrago@gmail.com>
2021-07-18 21:48:21 -0500

    use token instead key id

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -61,8 +61,8 @@ func ctxSetSession(r *http.Request, s *user.SessionState, token string, schedule
 
 	ctx := r.Context()
 	ctx = context.WithValue(ctx, SessionData, s)
-	ctx = context.WithValue(ctx, AuthToken, s.KeyID)
-
+//	ctx = context.WithValue(ctx, AuthToken, s.KeyID)
+	ctx = context.WithValue(ctx, AuthToken, token)
 	if scheduleUpdate {
 		ctx = context.WithValue(ctx, UpdateSession, true)
 	}
