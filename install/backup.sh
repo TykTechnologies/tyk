3268ac09d402b0f2127eba83e5ccb1c23dda9a15
Alok G Singh <alephnull@hcoop.net>
2021-07-27 12:40:54 +0530

    typo

--- install/backup.sh
+++ install/backup.sh
@@ -7,5 +7,4 @@ mkdir -p /tmp/tyk-backups
 if command -V systemctl >/dev/null 2>&1; then
     cp /lib/systemd/system/${SERVICE_NAME}.service /tmp/tyk-backups
     echo Backed up systemd service file
-    fi
 fi
