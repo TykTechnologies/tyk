584b3e2dc5dd29e9406cf091d77db0008fbe4dba
Leonid Bugaev <leonsbox@gmail.com>
2021-07-28 14:02:21 +0300

    Reduce API mutex locking (#3618)
    At the moment, it locks for the whole period of getting APIs from Dash/RPC and parsing the output.
    In the context of Legacy Cloud it can be a few seconds.
    
    Mutex should be locked only when we swap the global variable.
    
    Potentially should fix https://tyktech.atlassian.net/browse/TD-123
    
    <!-- Please describe in detail how you tested your changes -->
    <!-- Include details of your testing environment, and the tests you ran to see how your change affects other areas of
    the code, etc. -->
    
    <!-- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test coverage to functionality)
    
    <!-- Go over all the following points, and put an `x` in all the boxes that apply -->
    <!-- If you're unsure about any of these, don't hesitate to ask; we're here to help! -->
    - [ ] Make sure you are requesting to **pull a topic/feature/bugfix branch** (right side). If pulling from your own
          fork, don't request your `master`!
    - [ ] Make sure you are making a pull request against the **`master` branch** (left side). Also, you should start
          *your branch* off *our latest `master`*.
    - [ ] My change requires a change to the documentation.
      - [ ] If you've changed APIs, describe what needs to be updated in the documentation.
      - [ ] If new config option added, ensure that it can be set via ENV variable
    - [ ] I have updated the documentation accordingly.
    - [ ] Modules and vendor dependencies have been updated; run `go mod tidy && go mod vendor`
    - [ ] When updating library version must provide reason/explanation for this update.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    - [ ] Check your code additions will not fail linting checks:
      - [ ] `go fmt -s`
      - [ ] `go vet`

--- gateway/server.go
+++ gateway/server.go
@@ -281,8 +281,7 @@ func buildConnStr(resource string) string {
 
 func syncAPISpecs() (int, error) {
 	loader := APIDefinitionLoader{}
-	apisMu.Lock()
-	defer apisMu.Unlock()
+
 	var s []*APISpec
 	if config.Global().UseDBAppConfigs {
 		connStr := buildConnStr("/system/apis")
@@ -328,11 +327,14 @@ func syncAPISpecs() (int, error) {
 		}
 		filter = append(filter, v)
 	}
+
+	apisMu.Lock()
 	apiSpecs = filter
-
+	apiLen := len(apiSpecs)
 	tlsConfigCache.Flush()
+	apisMu.Unlock()
 
-	return len(apiSpecs), nil
+	return apiLen, nil
 }
 
 func syncPolicies() (count int, err error) {
