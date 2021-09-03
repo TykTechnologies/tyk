aca56f7af88efb09fce91ebfd3f63ee70d4ea1ee
Milan Vasic <milan.vasic.su@gmail.com>
2021-09-03 11:05:35 +0200

    remove unused app

--- gateway/util.go
+++ gateway/util.go
@@ -99,15 +99,3 @@ func removeDuplicateStr(strSlice []string) []string {
 	}
 	return list
 }
-// remove duplicate int values from int slice
-func removeDuplicateInt(intSlice []int) []int {
-	allKeys := make(map[int]bool)
-	list := []int{}
-	for _, item := range intSlice {
-		if _, value := allKeys[item]; !value {
-			allKeys[item] = true
-			list = append(list, item)
-		}
-	}
-	return list
-}
