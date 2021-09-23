14f5b5da1d168f4d2db750a648773c9b34155f2a
Leonid Bugaev <leonsbox@gmail.com>
2021-09-23 10:56:39 +0300

    Releng/r307 plugins v2 (#3679)
    Fix plugin compiler

---
+++ integration/smoke-tests/plugin-compiler/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eo pipefail
+
+function usage {
+    local progname=$1
+    cat <<EOF
+Usage:
+$progname <tag>
+
+Builds the plugin in testplugin using the supplied tag and tests it in the corresponding gw image.
+Requires docker compose.
+EOF
+    exit 1
+}
+
+[[ -z $1 ]] && usage $0
+export tag=$1
+
+rm -fv testplugin/*.so || true
+docker-compose down 
+docker run --rm -v `pwd`/testplugin:/plugin-source tykio/tyk-plugin-compiler:${tag} testplugin.so
+docker-compose up -d
+sleep 2 # Wait for init
+curl -vvv http://localhost:8080/goplugin/headers
+docker-compose logs
+curl http://localhost:8080/goplugin/headers | jq -e '.headers.Foo == "Bar"'
+docker-compose down 
