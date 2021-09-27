b08146fa2e9d5e024f6de53ec78b0c58b0dc6cec
Leonid Bugaev <leonsbox@gmail.com>
2021-09-27 20:46:32 +0300

    More fixes

--- gateway/coprocess_python_stub.go
+++ gateway/coprocess_python_stub.go
@@ -7,9 +7,12 @@ package gateway
 import (
 	"errors"
 
+	"github.com/TykTechnologies/tyk/apidef"
 	"github.com/TykTechnologies/tyk/config"
 	"github.com/TykTechnologies/tyk/coprocess"
 )
+
+var GatewayFireSystemEvent func(name apidef.TykEvent, meta interface{})
 
 func NewPythonDispatcher(conf config.Config) (dispatcher coprocess.Dispatcher, err error) {
 	return nil, errors.New("python support not compiled")
