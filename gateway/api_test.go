4b4a83ab0d86069f6bffd0cc69024199804163f9
Milan Vasic <milan.vasic.su@gmail.com>
2021-09-27 09:57:34 +0200

    test improved

--- gateway/api_test.go
+++ gateway/api_test.go
@@ -93,6 +93,9 @@ func TestPolicyAPI(t *testing.T) {
 	_, _ = ts.Run(t, test.TestCase{
 		Path: "/tyk/policies/default-test", AdminAuth: true, Method: "POST", Data: defaultTestPol, BodyMatch: `{"key":"default-test","status":"ok","action":"added"}`,
 	})
+	_, _ = ts.Run(t, test.TestCase{
+		Path: "/tyk/policies/default-test", AdminAuth: true, Method: "GET", BodyMatch: `{"status":"error","message":"Policy not found"}`, Code: http.StatusNotFound,
+	})
 	ts.Gw.DoReload()
 	_, _ = ts.Run(t, test.TestCase{
 		Path: "/tyk/policies/default-test", AdminAuth: true, Method: "GET", Code: http.StatusOK,
