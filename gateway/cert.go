eac6ee913dab7a7ef13e40ce8b70d0051c946328
sredny buitrago <sredny.buitrago@gmail.com>
2021-09-28 11:40:12 -0500

    TT-311 wait for redis connection to be available

--- gateway/cert.go
+++ gateway/cert.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
+	"github.com/TykTechnologies/tyk/storage"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -288,6 +289,15 @@ func (gw *Gateway) getTLSConfigForClient(baseConfig *tls.Config, listenPort int)
 		certNameMap[certData.Name] = &cert
 	}
 
+	if len(gwConfig.HttpServerOptions.SSLCertificates) > 0 {
+		// ensure that we are connected to redis
+		for {
+			if storage.Connected() {
+				break
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
 	for _, cert := range gw.CertificateManager.List(gwConfig.HttpServerOptions.SSLCertificates, certs.CertificatePrivate) {
 		if cert != nil {
 			serverCerts = append(serverCerts, *cert)
