bd80c3082456dbefa66e38d6001f8b3ec5f746ee
Sergey Petrunin <spetrunin@users.noreply.github.com>
2021-10-06 18:06:11 +0300

    fix graphql middleware tests (#3695)
    * fix graphql middleware tests
    
    * move body match func into helper

--- gateway/mw_graphql_test.go
+++ gateway/mw_graphql_test.go
@@ -1,6 +1,7 @@
 package gateway
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 	"testing"
@@ -168,6 +169,18 @@ func TestGraphQLMiddleware_RequestValidation(t *testing.T) {
 }
 
 func TestGraphQLMiddleware_EngineMode(t *testing.T) {
+	assertReviewsSubgraphResponse := func(t *testing.T) func(bytes []byte) bool {
+		return func(bytes []byte) bool {
+			expected := `{"data":{"_entities":[{"__typename":"User","reviews":[{"body":"A highly effective form of birth control."},{"body":"Fedoras are one of the most fashionable hats around and can look great with a variety of outfits."}]}]}}`
+			var body json.RawMessage
+			assert.NoError(t, json.Unmarshal(bytes, &body))
+
+			compactBody, err := json.Marshal(body)
+			assert.NoError(t, err)
+
+			return assert.Equal(t, expected, string(compactBody))
+		}
+	}
 
 	t.Run("on invalid graphql config version", func(t *testing.T) {
 		g := StartTest(nil)
@@ -266,9 +279,9 @@ func TestGraphQLMiddleware_EngineMode(t *testing.T) {
 				}
 
 				_, _ = g.Run(t, test.TestCase{
-					Data:      request,
-					BodyMatch: `{"data":{"\_entities":\[{"reviews":\[{"body":"A highly effective form of birth control."},{"body":"Fedoras are one of the most fashionable hats around and can look great with a variety of outfits."}\]}\]}}`,
-					Code:      http.StatusOK,
+					Data:          request,
+					BodyMatchFunc: assertReviewsSubgraphResponse(t),
+					Code:          http.StatusOK,
 				})
 			})
 		})
@@ -306,9 +319,9 @@ func TestGraphQLMiddleware_EngineMode(t *testing.T) {
 				}
 
 				_, _ = g.Run(t, test.TestCase{
-					Data:      request,
-					BodyMatch: `{"data":{"\_entities":\[{"reviews":\[{"body":"A highly effective form of birth control."},{"body":"Fedoras are one of the most fashionable hats around and can look great with a variety of outfits."}\]}\]}}`,
-					Code:      http.StatusOK,
+					Data:          request,
+					BodyMatchFunc: assertReviewsSubgraphResponse(t),
+					Code:          http.StatusOK,
 				})
 			})
 		})
