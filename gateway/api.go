7adb1fcbb1caa7573eab889109984646dcb3e9d1
Sredny M <sredny.buitrago@gmail.com>
2021-10-14 15:43:50 -0400

    TT-3529 recreate session on update MDCB  (#3694)
    * recreate session on update (mdcb)
    
    * gofmt
    
    * on GetKey check that it belongs to our org
    
    * add test for getSessionAndCreate on rpc when a key is updated
    
    * removed debug log
    
    Co-authored-by: Tomas Buchaillot <tombuchaillot89@gmail.com>

--- gateway/api.go
+++ gateway/api.go
@@ -613,10 +613,15 @@ func (gw *Gateway) handleGetAllKeys(filter string) (interface{}, int) {
 	return sessionsObj, http.StatusOK
 }
 
-func (gw *Gateway) handleAddKey(keyName, hashedName, sessionString, apiID string) {
+func (gw *Gateway) handleAddKey(keyName, hashedName, sessionString, apiID string, orgId string) {
 	sess := &user.SessionState{}
 	json.Unmarshal([]byte(sessionString), sess)
 	sess.LastUpdated = strconv.Itoa(int(time.Now().Unix()))
+
+	if sess.OrgID != orgId {
+		return
+	}
+
 	var err error
 	if gw.GetConfig().HashKeys {
 		err = gw.GlobalSessionManager.UpdateSession(hashedName, sess, 0, true)
