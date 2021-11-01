d20eda66299c2d402228d49fd4da70ca97ea8f04
furkansenharputlu <furkan_senharputlu@hotmail.com>
2021-11-01 20:45:43 +0300

    Fix SSE when web sockets are enabled

--- gateway/reverse_proxy.go
+++ gateway/reverse_proxy.go
@@ -1652,11 +1652,6 @@ func (p *ReverseProxy) IsUpgrade(req *http.Request) (bool, string) {
 		return false, ""
 	}
 
-	EncodeAccept := strings.ToLower(strings.TrimSpace(req.Header.Get(headers.Accept)))
-	if EncodeAccept == "text/event-stream" {
-		return true, ""
-	}
-
 	connection := strings.ToLower(strings.TrimSpace(req.Header.Get(headers.Connection)))
 	if connection != "upgrade" {
 		return false, ""
