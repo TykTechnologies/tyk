8847a482235e57a8fdeb13bf9321469572c0ab07
Alok G Singh <alephnull@hcoop.net>
2021-11-22 12:50:24 +0530

    Update plugin build script with 3.0.x fixes
    This is required on master, release-3-lts and release-4.0

--- images/plugin-compiler/data/build.sh
+++ images/plugin-compiler/data/build.sh
@@ -3,6 +3,7 @@
 set -xe
 
 plugin_name=$1
+plugin_path=$(date +%s)-$plugin_name
 
 function usage() {
     cat <<EOF
@@ -17,26 +18,38 @@ if [ -z "$plugin_name" ]; then
     exit 1
 fi
 
-# Plugin's vendor folder, has precendence over the cached vendor'd dependencies from tyk
+
 yes | cp -r $PLUGIN_SOURCE_PATH/* $PLUGIN_BUILD_PATH || true
 
+
 cd $PLUGIN_BUILD_PATH
-# if plugin has go.mod
-[ -f go.mod ] && [ ! -d vendor ] && go mod vendor
-rm go.mod
+
+# Handle if plugin has own vendor folder, and ignore error if not
+[ -f go.mod ] && [ ! -d ./vendor ] && GO111MODULE=on go mod vendor
+# Ensure that go modules not used
+rm -rf go.mod
+
+# We do not needd to care whicch version of Tyk vendored in plugin, since we going to use version inside compiler
+rm -rf $PLUGIN_BUILD_PATH/vendor/github.com/TykTechnologies/tyk
+
+# Copy plugin vendored pkgs to GOPATH
+yes | cp -rf $PLUGIN_BUILD_PATH/vendor/* $GOPATH/src || true \
+        && rm -rf $PLUGIN_BUILD_PATH/vendor
 
 # Ensure that GW package versions have priorities
 
 # We can't just copy Tyk dependencies on top of plugin dependencies, since different package versions have different file structures
 # First we need to find which deps GW already has, remove this folders, and after copy fresh versions from GW
-# ls -d /tmp/vendor/*
 
 # github.com and rest of packages have different nesting levels, so have to handle it separately
-# ls -d /tmp/vendor/github.com/*/* | sed "s|/tmp/vendor|$PLUGIN_BUILD_PATH/vendor|g" | xargs -d '\n' rm -rf
-# ls -d /tmp/vendor/*/* | sed "s|/tmp/vendor|$PLUGIN_BUILD_PATH/vendor|g" | grep -v github | xargs -d '\n' rm -rf
+ls -d $TYK_GW_PATH/vendor/github.com/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | xargs -d '\n' rm -rf
+ls -d $TYK_GW_PATH/vendor/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | grep -v github | xargs -d '\n' rm -rf
+
 
 # Copy GW dependencies
-# yes | cp -rf /tmp/vendor/* $PLUGIN_BUILD_PATH/vendor
+yes | cp -rf $TYK_GW_PATH/vendor/* $GOPATH/src
+rm -rf $TYK_GW_PATH/vendor
 
-go build -buildmode=plugin -o $plugin_name \
-    && mv $plugin_name $PLUGIN_SOURCE_PATH
+
+go build -buildmode=plugin -ldflags "-pluginpath=$plugin_path" -o $plugin_name \
+&& mv $plugin_name $PLUGIN_SOURCE_PATH
