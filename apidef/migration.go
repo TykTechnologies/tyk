14d62e3b4b2bc4ab904cc52e69d1d1f76b4ce08d
furkansenharputlu <furkan_senharputlu@hotmail.com>
2021-12-14 02:27:42 +0300

    Implement new versioning migrator

---
+++ apidef/migration.go
@@ -0,0 +1,62 @@
+package apidef
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+func (a *APIDefinition) MigrateVersioning() (versions []APIDefinition, err error) {
+	if a.VersionDefinition.Enabled || len(a.VersionDefinition.Versions) != 0 {
+		return nil, errors.New("not migratable - new versioning is enabled")
+	}
+
+	if a.VersionData.NotVersioned && len(a.VersionData.Versions) > 1 {
+		return nil, errors.New("not migratable - if not versioned, there should be 1 version info in versions map")
+	}
+
+	for vName, vInfo := range a.VersionData.Versions {
+		if a.VersionData.DefaultVersion == vName {
+			continue
+		}
+
+		newAPI := *a
+		newAPI.APIID = ""
+		newAPI.Id = ""
+		newAPI.VersionDefinition = VersionDefinition{}
+		newAPI.VersionData.NotVersioned = true
+		newAPI.VersionData.DefaultVersion = ""
+
+		if vInfo.OverrideTarget != "" {
+			newAPI.Proxy.ListenPath = vInfo.OverrideTarget
+		} else { // listen path of each version should be unique
+			newAPI.Proxy.ListenPath = strings.TrimSuffix(newAPI.Proxy.ListenPath, "/") + "-" + url.QueryEscape(vName) + "/"
+		}
+
+		versions = append(versions, newAPI)
+		delete(a.VersionData.Versions, vName)
+
+		a.VersionDefinition.Versions = append(a.VersionDefinition.Versions, VersionMap{Name: vName, Id: ""})
+	}
+
+	a.VersionDefinition.Enabled = !a.VersionData.NotVersioned
+	if a.VersionDefinition.Enabled {
+		a.VersionDefinition.Default = Self
+		a.VersionDefinition.Name = a.VersionData.DefaultVersion
+	}
+
+	defaultVersionInfo := a.VersionData.Versions[a.VersionData.DefaultVersion]
+
+	if defaultVersionInfo.OverrideTarget != "" {
+		a.Proxy.ListenPath = defaultVersionInfo.OverrideTarget
+		defaultVersionInfo.OverrideTarget = ""
+	}
+
+	a.VersionData.Versions[""] = defaultVersionInfo
+	delete(a.VersionData.Versions, a.VersionData.DefaultVersion)
+
+	a.VersionData.DefaultVersion = ""
+	a.VersionData.NotVersioned = true
+
+	return
+}
