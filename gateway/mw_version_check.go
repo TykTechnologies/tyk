d3a7ee0dd394a5a01956524bfc2a3ae7b05d0a04
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2021-12-14 12:20:39 +0300

    Add changes to new versioning (#3780)

--- gateway/mw_version_check.go
+++ gateway/mw_version_check.go
@@ -37,6 +37,31 @@ func (v *VersionCheck) DoMockReply(w http.ResponseWriter, meta interface{}) {
 
 // ProcessRequest will run any checks on the request on the way through the system, return an error to have the chain fail
 func (v *VersionCheck) ProcessRequest(w http.ResponseWriter, r *http.Request, _ interface{}) (error, int) {
+	targetVersion := v.Spec.getVersionFromRequest(r)
+	if targetVersion == "" {
+		targetVersion = v.Spec.VersionDefinition.Default
+	}
+
+	if v.Spec.VersionDefinition.Enabled && targetVersion != apidef.Self && targetVersion != v.Spec.VersionDefinition.Name {
+		var subVersionID string
+		for _, v := range v.Spec.VersionDefinition.Versions {
+			if v.Name == targetVersion {
+				subVersionID = v.Id
+				break
+			}
+		}
+
+		handler, _, found := v.Gw.findInternalHttpHandlerByNameOrID(subVersionID)
+		if !found {
+			return errors.New(string(VersionDoesNotExist)), http.StatusNotFound
+		}
+
+		sanitizeProxyPaths(v.Spec, r)
+
+		handler.ServeHTTP(w, r)
+		return nil, mwStatusRespond
+	}
+
 	// Check versioning, blacklist, whitelist and ignored status
 	requestValid, stat := v.Spec.RequestValid(r)
 	if !requestValid {
@@ -54,26 +79,6 @@ func (v *VersionCheck) ProcessRequest(w http.ResponseWriter, r *http.Request, _ 
 	}
 
 	versionInfo, _ := v.Spec.Version(r)
-	if !v.Spec.VersionData.NotVersioned && versionInfo.APIID != "" {
-		handler, targetAPI, found := v.Gw.findInternalHttpHandlerByNameOrID(versionInfo.APIID)
-		if !found {
-			return errors.New("couldn't detect target"), http.StatusNotFound
-		}
-
-		// Remove cached version info
-		ctxSetVersionInfo(r, nil)
-
-		// Find and cache version info for target API
-		_, status := targetAPI.Version(r)
-		if status != StatusOk {
-			return errors.New(string(status)), http.StatusForbidden
-		}
-
-		sanitizeProxyPaths(v.Spec, r)
-
-		handler.ServeHTTP(w, r)
-		return nil, mwStatusRespond
-	}
 	versionPaths := v.Spec.RxPaths[versionInfo.Name]
 	whiteListStatus := v.Spec.WhiteListEnabled[versionInfo.Name]
 
