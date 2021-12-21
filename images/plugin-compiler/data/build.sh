b72221e6810e065903890e36cf4a9b7c3eb40510
Jeff <jeffy.mathew100@gmail.com>
2021-12-21 13:51:46 +0530

    use GOPATH for building

--- images/plugin-compiler/data/build.sh
+++ images/plugin-compiler/data/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -xe
 plugin_name=$1
 plugin_id=$2
 
 PLUGIN_SOURCE_PATH=${PLUGIN_SOURCE_PATH:-/plugin-source}
-PLUGIN_BUILD_PATH="/go/src/${plugin_name%.*}$plugin_id"
+PLUGIN_BUILD_PATH="/go/src/plugin_${plugin_name%.*}$plugin_id"
 
 function usage() {
     cat <<EOF
@@ -21,30 +21,37 @@ if [ -z "$plugin_name" ]; then
 fi
 
 mkdir -p $PLUGIN_BUILD_PATH
-# Plugin's vendor folder, has precendence over the cached vendor'd dependencies from tyk
+# Plugin's vendor folder, has precedence over the cached vendor'd dependencies from tyk
 yes | cp -r $PLUGIN_SOURCE_PATH/* $PLUGIN_BUILD_PATH || true
 
 cd $PLUGIN_BUILD_PATH
 
-if [ ! -f go.mod ]; then
-    echo 'Please use gomodules.'
-    exit 1
-fi
+# Handle if plugin has own vendor folder, and ignore error if not
+[ -f go.mod ] && [ ! -d ./vendor ] && GO111MODULE=on go mod vendor
+# Ensure that go modules not used
+rm -rf go.mod
 
-# replace gw with local version
-#echo "replace github.com/TykTechnologies/tyk => /__w/tyk/tyk" >> go.mod
+# We do not need to care which version of Tyk vendored in plugin, since we going to use version inside compiler
+rm -rf $PLUGIN_BUILD_PATH/vendor/github.com/TykTechnologies/tyk
+
+# Copy plugin vendored pkgs to GOPATH
+yes | cp -rf $PLUGIN_BUILD_PATH/vendor/* $GOPATH/src || true \
+        && rm -rf $PLUGIN_BUILD_PATH/vendor
 
 # Ensure that GW package versions have priorities
 
 # We can't just copy Tyk dependencies on top of plugin dependencies, since different package versions have different file structures
 # First we need to find which deps GW already has, remove this folders, and after copy fresh versions from GW
-# ls -d /tmp/vendor/*
-## Handle if plugin has own vendor folder, and ignore error if not
-#
-#[ -d ./vendor ] && echo 'Found vendor directory, ignoring'
+
+# github.com and rest of packages have different nesting levels, so have to handle it separately
+ls -d $TYK_GW_PATH/vendor/github.com/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | xargs -d '\n' rm -rf
+ls -d $TYK_GW_PATH/vendor/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | grep -v github | xargs -d '\n' rm -rf
 
 # Copy GW dependencies
-# yes | cp -rf /tmp/vendor/* $PLUGIN_BUILD_PATH/vendor
+yes | cp -rf $TYK_GW_PATH/vendor/* $GOPATH/src
+rm -rf $TYK_GW_PATH/vendor
 
-go build -trimpath -buildmode=plugin -o $plugin_name \
+rm /go/src/modules.txt
+
+GO111MODULE=off go build -buildmode=plugin -o $plugin_name \
     && mv $plugin_name $PLUGIN_SOURCE_PATH
