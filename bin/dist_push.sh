f9ba8b01fc2799fd8b7c0ea99c78d8a8c29cd857
Alok G Singh <alephnull@hcoop.net>
2022-01-13 16:02:52 +0530

    el7 support for plugin compiler

--- bin/dist_push.sh
+++ bin/dist_push.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 : ${ORGDIR:="/go/src/github.com/TykTechnologies"}
 : ${SOURCEBINPATH:="${ORGDIR}/tyk"}
-: ${DEBVERS:="ubuntu/xenial ubuntu/bionic debian/jessie debian/stretch debian/buster"}
-: ${RPMVERS:="el/7 el/8"}
+: ${DEBVERS:="ubuntu/xenial"}
+: ${RPMVERS:="el/7"}
 : ${PKGNAME:="tyk-gateway"}
+: ${PC_TARGET:="tyk-gateway-unstable"}
 
 echo "Set version number"
 : ${VERSION:=$(perl -n -e'/v(\d+).(\d+).(\d+)/'' && print "$1\.$2\.$3"' version.go)}
 
 RELEASE_DIR="$SOURCEBINPATH/build"
-export PACKAGECLOUDREPO=$PC_TARGET
 
 cd $RELEASE_DIR/
 
-for arch in i386 amd64 arm64
+for arch in amd64 arm64
 do
     debName="${PKGNAME}_${VERSION}_${arch}.deb"
     rpmName="$PKGNAME-$VERSION-1.${arch/amd64/x86_64}.rpm"
@@ -21,12 +21,12 @@ do
     for ver in $DEBVERS
     do
         echo "Pushing $debName to PackageCloud $ver"
-        package_cloud push tyk/$PACKAGECLOUDREPO/$ver $debName
+        package_cloud push tyk/$PC_TARGET/$ver $debName
     done
 
     for ver in $RPMVERS
     do
         echo "Pushing $rpmName to PackageCloud $ver"
-        package_cloud push tyk/$PACKAGECLOUDREPO/$ver $rpmName
+        package_cloud push tyk/$PC_TARGET/$ver $rpmName
     done
 done
