600e434f425c055160c3a254b8171d80d9705740
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-03-07 13:09:12 -0300

    internal: unused moved files cleanup

--- install/init_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Init local setup to domain: $1"
-sudo /opt/tyk-gateway/install/setup.sh --dashboard=1 --listenport=8080 --redishost=localhost --redisport=6379 --domain=$1 --mongo=mongodb://localhost/tyk_analytics
