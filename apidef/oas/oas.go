005863a62d1e539f42a3646adf72e4187dbe28e9
Jeff <jeffy.mathew100@gmail.com>
2022-03-17 18:10:18 +0530

    Add ValidateOASObject method
    [changelog]
    added: Add ValidateOASObject method to validate OAS definition against mulitple OAS versions

--- apidef/oas/oas.go
+++ apidef/oas/oas.go
@@ -2,12 +2,77 @@ package oas
 
 import (
 	"encoding/json"
+	logger "github.com/TykTechnologies/tyk/log"
+	"github.com/xeipuuv/gojsonschema"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/TykTechnologies/tyk/apidef"
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
 const ExtensionTykAPIGateway = "x-tyk-api-gateway"
+
+var log = logger.Get()
+var oasJSONSchemas map[string][]byte
+
+func init() {
+	oasJSONSchemas = make(map[string][]byte)
+	baseDir := "./schema/"
+	files, err := os.ReadDir(baseDir)
+
+	if err != nil {
+		log.WithError(err).Error("error while listing schema files")
+		return
+	}
+
+	for _, fileInfo := range files {
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		oasVersion := strings.TrimSuffix(fileInfo.Name(), ".json")
+		file, err := os.Open(baseDir + fileInfo.Name())
+		if err != nil {
+			log.WithError(err).Error("error while loading oas json schema")
+			continue
+		}
+
+		oasJSONSchema, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.WithError(err).Error("error while reading schema file")
+			continue
+		}
+
+		oasJSONSchemas[oasVersion] = oasJSONSchema
+	}
+
+}
+
+func ValidateOASObject(documentBody []byte, oasVersion string) (bool, []string) {
+	schemaLoader := gojsonschema.NewBytesLoader(oasJSONSchemas[oasVersion])
+	documentLoader := gojsonschema.NewBytesLoader(documentBody)
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+
+	if err != nil {
+		log.WithError(err).Errorln("error while validating document")
+		return false, nil
+	}
+
+	if !result.Valid() {
+		log.Error("OAS object validation failed, most likely malformed input")
+		validationErrs := result.Errors()
+		var errs = make([]string, len(validationErrs))
+		for i, validationErr := range validationErrs {
+			errStr := validationErr.String()
+			errs[i] = errStr
+		}
+		return false, errs
+	}
+
+	return true, nil
+}
 
 type OAS struct {
 	openapi3.T
--- apidef/oas/oas_test.go
+++ apidef/oas/oas_test.go
@@ -35,3 +35,345 @@ func TestOAS(t *testing.T) {
 
 	assert.Equal(t, api.AuthConfigs, converted.AuthConfigs)
 }
+
+func TestValidateOASObject(t *testing.T) {
+	var validOAS3Definition = []byte(`{
+	  "openapi": "3.0.0",
+	  "info": {
+		"version": "1.0.0",
+		"title": "Swagger Petstore",
+		"license": {
+		  "name": "MIT"
+		}
+	  },
+	  "servers": [
+		{
+		  "url": "http://petstore.swagger.io/v1"
+		}
+	  ],
+	  "paths": {
+		"/pets": {
+		  "get": {
+			"summary": "List all pets",
+			"operationId": "listPets",
+			"tags": [
+			  "pets"
+			],
+			"parameters": [
+			  {
+				"name": "limit",
+				"in": "query",
+				"description": "How many items to return at one time (max 100)",
+				"required": false,
+				"schema": {
+				  "type": "integer",
+				  "format": "int32"
+				}
+			  }
+			],
+			"responses": {
+			  "200": {
+				"description": "A paged array of pets",
+				"headers": {
+				  "x-next": {
+					"description": "A link to the next page of responses",
+					"schema": {
+					  "type": "string"
+					}
+				  }
+				},
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Pets"
+					}
+				  }
+				}
+			  },
+			  "default": {
+				"description": "unexpected error",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Error"
+					}
+				  }
+				}
+			  }
+			}
+		  },
+		  "post": {
+			"summary": "Create a pet",
+			"operationId": "createPets",
+			"tags": [
+			  "pets"
+			],
+			"responses": {
+			  "201": {
+				"description": "Null response"
+			  },
+			  "default": {
+				"description": "unexpected error",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Error"
+					}
+				  }
+				}
+			  }
+			}
+		  }
+		},
+		"/pets/{petId}": {
+		  "get": {
+			"summary": "Info for a specific pet",
+			"operationId": "showPetById",
+			"tags": [
+			  "pets"
+			],
+			"parameters": [
+			  {
+				"name": "petId",
+				"in": "path",
+				"required": true,
+				"description": "The id of the pet to retrieve",
+				"schema": {
+				  "type": "string"
+				}
+			  }
+			],
+			"responses": {
+			  "200": {
+				"description": "Expected response to a valid request",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Pet"
+					}
+				  }
+				}
+			  },
+			  "default": {
+				"description": "unexpected error",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Error"
+					}
+				  }
+				}
+			  }
+			}
+		  }
+		}
+	  },
+	  "components": {
+		"schemas": {
+		  "Pet": {
+			"type": "object",
+			"required": [
+			  "id",
+			  "name"
+			],
+			"properties": {
+			  "id": {
+				"type": "integer",
+				"format": "int64"
+			  },
+			  "name": {
+				"type": "string"
+			  },
+			  "tag": {
+				"type": "string"
+			  }
+			}
+		  },
+		  "Pets": {
+			"type": "array",
+			"items": {
+			  "$ref": "#/components/schemas/Pet"
+			}
+		  },
+		  "Error": {
+			"type": "object",
+			"required": [
+			  "code",
+			  "message"
+			],
+			"properties": {
+			  "code": {
+				"type": "integer",
+				"format": "int32"
+			  },
+			  "message": {
+				"type": "string"
+			  }
+			}
+		  }
+		}
+	  }
+	}`)
+
+	var invalidOAS3Definition = []byte(`{
+	  "openapi": "3.0.0",
+	  "info": {
+		"version": "1.0.0",
+		"title": "Swagger Petstore",
+		"license": {
+		  "name": "MIT"
+		}
+	  },
+	  "servers": [
+		{
+		  "url": "http://petstore.swagger.io/v1"
+		}
+	  ],
+	  "paths": {
+		"/pets": {
+		  "get": {
+			"summary": "List all pets",
+			"operationId": "listPets",
+			"tags": "pets",
+			"parameters": [
+			  {
+				"name": "limit",
+				"in": "query",
+				"description": "How many items to return at one time (max 100)",
+				"required": false,
+				"schema": {
+				  "type": "integer",
+				  "format": "int32"
+				}
+			  }
+			]
+		  },
+		  "post": {
+			"summary": "Create a pet",
+			"operationId": "createPets",
+			"tags": [
+			  "pets"
+			],
+			"responses": {
+			  "201": {
+				"description": "Null response"
+			  },
+			  "default": {
+				"description": "unexpected error",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Error"
+					}
+				  }
+				}
+			  }
+			}
+		  }
+		},
+		"/pets/{petId}": {
+		  "get": {
+			"summary": "Info for a specific pet",
+			"operationId": "showPetById",
+			"tags": [
+			  "pets"
+			],
+			"parameters": [
+			  {
+				"name": "petId",
+				"in": "path",
+				"required": true,
+				"description": "The id of the pet to retrieve",
+				"schema": {
+				  "type": "string"
+				}
+			  }
+			],
+			"responses": {
+			  "200": {
+				"description": "Expected response to a valid request",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Pet"
+					}
+				  }
+				}
+			  },
+			  "default": {
+				"description": "unexpected error",
+				"content": {
+				  "application/json": {
+					"schema": {
+					  "$ref": "#/components/schemas/Error"
+					}
+				  }
+				}
+			  }
+			}
+		  }
+		}
+	  },
+	  "components": {
+		"schemas": {
+		  "Pet": {
+			"type": "object",
+			"required": [
+			  "id",
+			  "name"
+			],
+			"properties": {
+			  "id": {
+				"type": "integer",
+				"format": "int64"
+			  },
+			  "name": {
+				"type": "string"
+			  },
+			  "tag": {
+				"type": "string"
+			  }
+			}
+		  },
+		  "Pets": {
+			"type": "array",
+			"items": {
+			  "$ref": "#/components/schemas/Pet"
+			}
+		  },
+		  "Error": {
+			"type": "object",
+			"required": [
+			  "code",
+			  "message"
+			],
+			"properties": {
+			  "code": {
+				"type": "integer",
+				"format": "int32"
+			  },
+			  "message": {
+				"type": "string"
+			  }
+			}
+		  }
+		}
+	  }
+	}`)
+
+	t.Run("valid OAS object", func(t *testing.T) {
+		isValid, errs := ValidateOASObject(validOAS3Definition, "3.0")
+		assert.True(t, isValid)
+		assert.Nil(t, errs)
+	})
+
+	t.Run("invalid OAS object", func(t *testing.T) {
+		isValid, errs := ValidateOASObject(invalidOAS3Definition, "3.0")
+		expectedErrs := []string{
+			"paths./pets.get: responses is required",
+			"paths./pets.get.tags: Invalid type. Expected: array, given: string",
+		}
+		assert.False(t, isValid)
+		assert.Equal(t, expectedErrs, errs)
+	})
+}
