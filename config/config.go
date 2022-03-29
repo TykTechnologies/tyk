69971cb329ef83746973042b0fa09cb6d6c51175
Sredny M <sredny.buitrago@gmail.com>
2022-03-29 21:31:53 +0200

    TT-4934 make ctx.SetSession backwards compatible (#3967)
    <!-- Provide a general summary of your changes in the Title above -->
    
    ## Description
    In order to be fully backward compatible, then updated the signature of `ctx.SetSession` function, now it will infer from where to get the configuration if keys should be hashed or not. When a gw starts then it will override the function `Global func() Config` that resides inside the config package, in this way it will take automatically the configurtion of the current gateway. 
    
    ## Related Issue
    [ticket](https://tyktech.atlassian.net/browse/TT-4934)
    
    ## Motivation and Context
    Unblock release
    
    ## How This Has Been Tested
    - Ran GW
    - Created a key and consume api
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    <!-- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test coverage to functionality)
    
    ## Checklist
    <!-- Go over all the following points, and put an `x` in all the boxes that apply -->
    <!-- If you're unsure about any of these, don't hesitate to ask; we're here to help! -->
    - [x] Make sure you are requesting to **pull a topic/feature/bugfix branch** (right side). If pulling from your own
          fork, don't request your `master`!
    - [x] Make sure you are making a pull request against the **`master` branch** (left side). Also, you should start
          *your branch* off *our latest `master`*.
    - [ ] My change requires a change to the documentation.
      - [ ] If you've changed APIs, describe what needs to be updated in the documentation.
      - [ ] If new config option added, ensure that it can be set via ENV variable
    - [ ] I have updated the documentation accordingly.
    - [ ] Modules and vendor dependencies have been updated; run `go mod tidy && go mod vendor`
    - [ ] When updating library version must provide reason/explanation for this update.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    - [x] Check your code additions will not fail linting checks:
      - [x] `go fmt -s`
      - [ ] `go vet`
    
    (cherry picked from commit 61ca289b6e82f0ca5be1f8e0f7422fba4d8ae8b7)

--- config/config.go
+++ config/config.go
@@ -1080,6 +1080,9 @@ type TykEventHandler interface {
 	HandleEvent(EventMessage)
 }
 
+// Global function that will return the config of the gw running
+var Global func() Config
+
 func WriteConf(path string, conf *Config) error {
 	bs, err := json.MarshalIndent(conf, "", "    ")
 	if err != nil {
