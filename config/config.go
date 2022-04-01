a83821fa1accef8caa661cda8d14bedd4afeddff
sredny buitrago <sredny.buitrago@gmail.com>
2022-04-01 16:48:12 +0200

    separate configs by spaces

--- config/config.go
+++ config/config.go
@@ -320,6 +320,7 @@ type SlaveOptionsConfig struct {
 
 	// RPCCertCacheExpiration defines the expiration time of the rpc cache that stores the certificates, defined in seconds
 	RPCCertCacheExpiration float32 `json:"rpc_cert_cache_expiration"`
+
 	// RPCKeysCacheExpiration defines the expiration time of the rpc cache that stores the keys, defined in seconds
 	RPCGlobalCacheExpiration float32 `json:"rpc_global_cache_expiration""`
 }
