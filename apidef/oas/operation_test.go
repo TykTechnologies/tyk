32793f28621c541d0e319649e5ffa02f561a6848
Tit Petric <black@scene-si.org>
2022-04-04 11:13:19 +0200

    [TT-4870] OAS migrate regex path fragments (#3975)
    * Add migration of regex paths from tyk to OAS
    * Add full OAS Paths test case for regex
    
    Co-authored-by: Jeffy Mathew <jeffy.mathew100@gmail.com>
    Co-authored-by: Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
    
    [changelog]
    added: Migration of regex path fragments to the new OAS definition
    
    (cherry picked from commit 29ea729cca66858df44ef32fd1638a22f8998ae1)

--- apidef/oas/operation_test.go
+++ apidef/oas/operation_test.go
@@ -1,6 +1,7 @@
 package oas
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/TykTechnologies/tyk/apidef"
@@ -9,6 +10,8 @@ import (
 )
 
 func TestOAS_PathsAndOperations(t *testing.T) {
+	t.Parallel()
+
 	const operationId = "userGET"
 	const existingOperationId = "userPOST"
 
@@ -62,4 +65,137 @@ func TestOAS_PathsAndOperations(t *testing.T) {
 	}
 
 	assert.Equal(t, expCombinedPaths, convertedOAS.Paths)
+}
+
+func TestOAS_PathsAndOperationsRegex(t *testing.T) {
+	t.Parallel()
+
+	expectedOperationID := "users/[a-z]+/[0-9]+$GET"
+	expectedPath := "/users/{customRegex1}/{customRegex2}"
+
+	var oas OAS
+	oas.Paths = openapi3.Paths{}
+
+	_ = oas.getOperationID("/users/[a-z]+/[0-9]+$", "GET")
+
+	expectedPathItems := openapi3.Paths{
+		expectedPath: &openapi3.PathItem{
+			Get: &openapi3.Operation{
+				OperationID: expectedOperationID,
+			},
+			Parameters: []*openapi3.ParameterRef{
+				{
+					Value: &openapi3.Parameter{
+						Schema: &openapi3.SchemaRef{
+							Value: &openapi3.Schema{
+								Type:    "string",
+								Pattern: "[a-z]+",
+							},
+						},
+						Name:     "customRegex1",
+						In:       "path",
+						Required: true,
+					},
+				},
+				{
+					Value: &openapi3.Parameter{
+						Schema: &openapi3.SchemaRef{
+							Value: &openapi3.Schema{
+								Type:    "string",
+								Pattern: "[0-9]+$",
+							},
+						},
+						Name:     "customRegex2",
+						In:       "path",
+						Required: true,
+					},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, expectedPathItems, oas.Paths, "expected path item differs")
+}
+
+func TestOAS_RegexOperationIDs(t *testing.T) {
+	t.Parallel()
+
+	type test struct {
+		input  string
+		method string
+		want   string
+	}
+
+	tests := []test{
+		{"/.+", "GET", ".+GET"},
+		{"/.*", "GET", ".*GET"},
+		{"/[^a]*", "GET", "[^a]*GET"},
+		{"/foo$", "GET", "foo$GET"},
+		{"/group/.+", "GET", "group/.+GET"},
+		{"/group/.*", "GET", "group/.*GET"},
+		{"/group/[^a]*", "GET", "group/[^a]*GET"},
+		{"/group/foo$", "GET", "group/foo$GET"},
+		{"/group/[^a]*/.*", "GET", "group/[^a]*/.*GET"},
+	}
+
+	for i, tc := range tests {
+		var oas OAS
+		oas.Paths = openapi3.Paths{
+			tc.input: {
+				Get: &openapi3.Operation{},
+			},
+		}
+		got := oas.getOperationID(tc.input, tc.method)
+		assert.Equalf(t, tc.want, got, "test %d: expected operationID %v, got %v", i, tc.want, got)
+	}
+}
+
+func TestOAS_RegexPaths(t *testing.T) {
+	t.Parallel()
+
+	type test struct {
+		input  string
+		want   string
+		params int
+	}
+
+	tests := []test{
+		{"/.+", "/{customRegex1}", 1},
+		{"/.*", "/{customRegex1}", 1},
+		{"/[^a]*", "/{customRegex1}", 1},
+		{"/foo$", "/{customRegex1}", 1},
+		{"/group/.+", "/group/{customRegex1}", 1},
+		{"/group/.*", "/group/{customRegex1}", 1},
+		{"/group/[^a]*", "/group/{customRegex1}", 1},
+		{"/group/foo$", "/group/{customRegex1}", 1},
+		{"/group/[^a]*/.*", "/group/{customRegex1}/{customRegex2}", 2},
+	}
+
+	for i, tc := range tests {
+		var oas OAS
+		oas.Paths = openapi3.Paths{
+			tc.input: {
+				Get: &openapi3.Operation{},
+			},
+		}
+		_ = oas.getOperationID(tc.input, "GET")
+
+		p, ok := oas.Paths[tc.want]
+		assert.Truef(t, ok, "test %d: path doesn't exist in OAS: %v", i, tc.want)
+		assert.Lenf(t, p.Parameters, tc.params, "test %d: expected %d parameters, got %d", i, tc.params, len(p.Parameters))
+
+		// rebuild original link
+		got := tc.want
+		for _, param := range p.Parameters {
+			assert.NotNilf(t, param.Value, "test %d: missing value", i)
+			assert.NotNilf(t, param.Value.Schema, "test %d: missing schema", i)
+			assert.NotNilf(t, param.Value.Schema.Value, "test %d: missing schema value", i)
+
+			assert.Truef(t, strings.HasPrefix(param.Value.Name, "customRegex"), "test %d: invalid name %v", i, param.Value.Name)
+
+			got = strings.ReplaceAll(got, "{"+param.Value.Name+"}", param.Value.Schema.Value.Pattern)
+		}
+
+		assert.Equalf(t, tc.input, got, "test %d: rebuilt link, expected %v, got %v", i, tc.input, got)
+	}
 }
