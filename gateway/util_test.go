66b0628229387286170211e00ec9b62afc8b7b5c
Tit Petric <tit.petric@monotek.net>
2022-04-12 10:47:57 +0200

    Fix: deduplicate versions and methods, fix incorrect policy test (#4002)
    Co-authored-by: Jeffy Mathew <jeffy.mathew100@gmail.com>

---
+++ gateway/util_test.go
@@ -0,0 +1,27 @@
+package gateway
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAppendIfMissingUniqueness(t *testing.T) {
+	t.Parallel()
+
+	// DNA TAGC Append M
+	after := strings.Split("CTTCGGTTGTCAAGGAACTGTTG", "")
+	after = appendIfMissing(after, strings.Split("MCTGAACGTGCATGCATGCATGGTCATGCATGTTTGTGCATAAAATGTGAGATGAGAAA", "")...)
+
+	// DNA TAGC + M (in order as it appears)
+	want := strings.Split("CTGAM", "")
+
+	assert.Equal(t, want, after)
+
+	// Append some alphabet things
+	after = appendIfMissing(after, "A", "B", "C", "D", "E", "F", "E", "F", "E", "F")
+	want = append(want, "B", "D", "E", "F")
+
+	assert.Equal(t, want, after)
+}
