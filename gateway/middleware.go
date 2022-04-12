ee21b663c0416a34b4e41c376262579a45c03247
Tit Petric <tit.petric@monotek.net>
2022-04-12 10:47:57 +0200

    Fix: deduplicate versions and methods, fix incorrect policy test (#4002)
    Co-authored-by: Jeffy Mathew <jeffy.mathew100@gmail.com>
    (cherry picked from commit 97139e02167a298b4007d25008f7714aab22fb75)

--- gateway/middleware.go
+++ gateway/middleware.go
@@ -390,7 +390,7 @@ func (t BaseMiddleware) ApplyPolicies(session *user.SessionState) error {
 							for ai, au := range r.AllowedURLs {
 								if u.URL == au.URL {
 									found = true
-									r.AllowedURLs[ai].Methods = append(au.Methods, u.Methods...)
+									r.AllowedURLs[ai].Methods = appendIfMissing(au.Methods, u.Methods...)
 								}
 							}
 
@@ -556,7 +556,7 @@ func (t BaseMiddleware) ApplyPolicies(session *user.SessionState) error {
 	// set tags
 	session.Tags = []string{}
 	for tag := range tags {
-		session.Tags = append(session.Tags, tag)
+		session.Tags = appendIfMissing(session.Tags, tag)
 	}
 
 	if len(policies) == 0 {
