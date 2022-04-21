afeb09808ebec2d3fad74d7840f240b5d8a34b65
tbuchaillot <tombuchaillot89@gmail.com>
2022-04-21 11:25:18 +0200

    fmting

--- gateway/dashboard_register_test.go
+++ gateway/dashboard_register_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_BuildDashboardConnStr(t *testing.T){
+func Test_BuildDashboardConnStr(t *testing.T) {
 	ts := StartTest(func(globalConf *config.Config) {
 		globalConf.DisableDashboardZeroConf = false
 		globalConf.DBAppConfOptions.ConnectionString = ""
@@ -16,8 +16,8 @@ func Test_BuildDashboardConnStr(t *testing.T){
 	defer ts.Close()
 
 	//we trigger a go routine here to simulate a redis zeroconf
-	go func(){
-		time.Sleep(1*time.Second)
+	go func() {
+		time.Sleep(1 * time.Second)
 		cfg := ts.Gw.GetConfig()
 		cfg.DBAppConfOptions.ConnectionString = "http://localhost"
 		ts.Gw.SetConfig(cfg)
