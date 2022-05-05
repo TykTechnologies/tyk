8f926a008a2303881402b5ffca957d0e4121d148
Tit Petric <tit.petric@monotek.net>
2022-05-05 18:20:59 +0200

    Improve CI tests
    - Run go vet over whole repo, once
    - Avoid bash no-ops in favor of 'set -e'
    - Remove no-op stdout redirects
    - Fail fast with -failfast flag
    - No caching with -count=1 flag

--- bin/ci-tests.sh
+++ bin/ci-tests.sh
@@ -2,46 +2,33 @@
 
 TEST_TIMEOUT=15m
 
-# print a command and execute it
-show() {
-	echo "$@" >&2
-	eval "$@"
-}
-
-fatal() {
-	echo "$@" >&2
-	exit 1
-}
-
 PKGS="$(go list ./...)"
 
 export PKG_PATH=${GOPATH}/src/github.com/TykTechnologies/tyk
 
+# exit on non-zero exit from go test/vet
+set -e
+
+# run go vet overall to fail early on violations
+echo "Running go vet"
+go vet ./...
+
 # build Go-plugin used in tests
-go build -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins || fatal "building Go-plugin failed"
+echo "Building go plugin"
+go build -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins
 
 for pkg in ${PKGS}; do
     tags=""
-
-    # TODO: Remove skipRace variable after solving race conditions in tests.
-    skipRace=false
-    if [[ ${pkg} == *"grpc" ]]; then
-        skipRace=true
-    elif [[ ${pkg} == *"goplugin" ]]; then
-        skipRace=true
+    if [[ ${pkg} == *"goplugin" ]]; then
         tags="-tags 'goplugin'"
     fi
 
-    race="-race"
-
-    if [[ ${skipRace} = true ]]; then
-        race=""
-    fi
     coveragefile=`echo "$pkg" | awk -F/ '{print $NF}'`
-    show go test ${race} -timeout ${TEST_TIMEOUT} -v -coverprofile=${coveragefile}.cov ${pkg} ${tags} || fatal "Test Failed"
-    show go vet ${tags} ${pkg} || fatal "go vet errored"
+
+    echo go test -race -count=1 -timeout ${TEST_TIMEOUT} -failfast -v -coverprofile=${coveragefile}.cov ${pkg} ${tags}
+    go test -race -count=1 -timeout ${TEST_TIMEOUT} -failfast -v -coverprofile=${coveragefile}.cov ${pkg} ${tags}
 done
 
 # run rpc tests separately
 rpc_tests='SyncAPISpecsRPC|OrgSessionWithRPCDown'
-show go test -timeout ${TEST_TIMEOUT} -v -coverprofile=gateway-rpc.cov github.com/TykTechnologies/tyk/gateway -p 1 -run '"'${rpc_tests}'"' || fatal "Test Failed"
+go test -count=1 -timeout ${TEST_TIMEOUT} -v -coverprofile=gateway-rpc.cov github.com/TykTechnologies/tyk/gateway -p 1 -run '"'${rpc_tests}'"'
