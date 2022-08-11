48963d6de04729e81d2ce0ef9bd16c5dae53bd98
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-08-11 16:48:19 -0300

    fix region output

--- ci/terraform/credentials/outputs.tf
+++ ci/terraform/credentials/outputs.tf
@@ -9,12 +9,23 @@ data "terraform_remote_state" "infra" {
   }
 }
 
+data "terraform_remote_state" "base" {
+  backend = "remote"
+
+  config = {
+    organization = "Tyk"
+    workspaces = {
+      name = "base-prod"
+    }
+  }
+}
+
 output "cd" {
   value       = data.terraform_remote_state.infra.outputs.cd
   description = "Service account for continuous deployment"
 }
 
 output "region" {
-  value       = data.terraform_remote_state.infra.outputs.region
+  value       = data.terraform_remote_state.base.outputs.region
   description = "Region in which the env is running"
 }
