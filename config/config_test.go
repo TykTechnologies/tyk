0e5a5cda2cbedf271d744162ab05717baf64685e
Burak Sekili <buraksekili@gmail.com>
2022-08-22 12:10:06 +0300

    TT-5980 Add custom environment variable decoder for '.ports_whitelist' (#4166)
    * Add custom environment variable decoder for '.ports_whitelist' field in tyk.conf
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    
    * replace t.Fatal with assert
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    
    * 2 unused vars deleted and a few tests added
    
    * example added
    
    * modifying comment with the EV format
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    Co-authored-by: Tomas Buchaillot <tombuchaillot89@gmail.com>
    Co-authored-by: mativm02 <matipvp02@gmail.com>
    Co-authored-by: Matias Sidler <83959431+mativm02@users.noreply.github.com>
    (cherry picked from commit e1145d0cdcab1793899ee67ac68967b01a0daece)

--- config/config_test.go
+++ config/config_test.go
@@ -287,7 +287,7 @@ func TestLoad_tracing(t *testing.T) {
 	})
 }
 
-func TestCustomEnvDecoders(t *testing.T) {
+func TestCustomCertsDataDecoder(t *testing.T) {
 	var c Config
 	os.Setenv("TYK_GW_HTTPSERVEROPTIONS_CERTIFICATES", "[{\"domain_name\":\"testCerts\"}]")
 	err := envconfig.Process("TYK_GW", &c)
@@ -298,4 +298,56 @@ func TestCustomEnvDecoders(t *testing.T) {
 	assert.Len(t, c.HttpServerOptions.Certificates, 1, "TYK_GW_HTTPSERVEROPTIONS_CERTIFICATES should have len 1")
 	assert.Equal(t, "testCerts", c.HttpServerOptions.Certificates[0].Name, "TYK_GW_HTTPSERVEROPTIONS_CERTIFICATES domain_name should be equals to testCerts")
 
+}
+
+func TestPortsWhiteListDecoder(t *testing.T) {
+	var c Config
+
+	//testing invalid value
+	err := os.Setenv("TYK_GW_PORTWHITELIST", "invalid-value")
+	assert.NoError(t, err)
+
+	httpWhiteList, ok := c.PortWhiteList["http"]
+	assert.False(t, ok)
+	assert.Empty(t, httpWhiteList)
+
+	tlsWhiteList, ok := c.PortWhiteList["tls"]
+	assert.False(t, ok)
+	assert.Empty(t, tlsWhiteList)
+
+	//testing empty value
+	err = os.Setenv("TYK_GW_PORTWHITELIST", "")
+	assert.NoError(t, err)
+
+	httpWhiteList, ok = c.PortWhiteList["http"]
+	assert.False(t, ok)
+	assert.Empty(t, httpWhiteList)
+
+	tlsWhiteList, ok = c.PortWhiteList["tls"]
+	assert.False(t, ok)
+	assert.Empty(t, tlsWhiteList)
+
+	//testing real value
+	err = os.Setenv("TYK_GW_PORTWHITELIST", `{"http":{"ranges":[{"from":8000,"to":9000}]},"tls":{"ports":[6000,6015]}}`)
+	assert.NoError(t, err)
+
+	err = envconfig.Process("TYK_GW", &c)
+	assert.NoError(t, err)
+
+	httpWhiteList, ok = c.PortWhiteList["http"]
+	assert.True(t, ok, "expected to have http key in PortWhiteList")
+
+	assert.Len(t, httpWhiteList.Ports, 0, "http should have 0 Ports")
+	assert.Len(t, httpWhiteList.Ranges, 1, "http should have 1 Ranges")
+
+	assert.Equal(t, 8000, httpWhiteList.Ranges[0].From, "http Range From should be equals to 8000")
+	assert.Equal(t, 9000, httpWhiteList.Ranges[0].To, "http Range To should be equals to 9000")
+
+	tlsWhiteList, ok = c.PortWhiteList["tls"]
+	assert.True(t, ok, "expected to have tls key in PortWhiteList")
+	assert.Len(t, tlsWhiteList.Ports, 2, "tls should have 2 Ports")
+	assert.Len(t, tlsWhiteList.Ranges, 0, "tls should have 0 Ranges")
+
+	assert.Contains(t, tlsWhiteList.Ports, 6000, "tls should have 6000 port")
+	assert.Contains(t, tlsWhiteList.Ports, 6015, "tls should have 6015 port")
 }
