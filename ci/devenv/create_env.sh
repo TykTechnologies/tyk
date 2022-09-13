52c9bb658e8d9c2ca8b520b6f5397b7df1ea308b
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-09-13 17:30:08 -0300

    adapt and test script

--- ci/devenv/create_env.sh
+++ ci/devenv/create_env.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-ls
-# pushd terraform/environment
-# terraform init
-# terraform workspace new ${CLUSTER}
-# terraform workspace show
-# echo "CALL - Generate templates on EFS"
-# echo "CALL - Make input var files"
-# terraform apply -auto-approve -var-file=master.tfvars
-# popd
+pushd repo/ci/terraform/environment
+terraform init
+terraform workspace new ${CLUSTER}
+terraform workspace show
+echo "CALL - Generate templates on EFS"
+echo "CALL - Make input var files"
+terraform apply -auto-approve -var-file=master.tfvars
+popd
