ee6d5408bce76833ad14f60cf456e63e826eb2c1
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-09-13 17:08:44 -0300

    comment script

--- ci/devenv/create_env.sh
+++ ci/devenv/create_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ls
-pushd terraform/environment
-terraform init
-terraform workspace new ${CLUSTER}
-terraform workspace show
-echo "CALL - Generate templates on EFS"
-echo "CALL - Make input var files"
-terraform apply -auto-approve -var-file=master.tfvars
-popd
+# pushd terraform/environment
+# terraform init
+# terraform workspace new ${CLUSTER}
+# terraform workspace show
+# echo "CALL - Generate templates on EFS"
+# echo "CALL - Make input var files"
+# terraform apply -auto-approve -var-file=master.tfvars
+# popd
