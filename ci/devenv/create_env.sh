07dee0e7983afc725d0163a3897845fce8627421
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-09-13 17:45:21 -0300

    change pushd for cd

--- ci/devenv/create_env.sh
+++ ci/devenv/create_env.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-pushd /repo/ci/terraform/environment
+cd /repo/ci/terraform/environment
 terraform init
 terraform workspace new ${CLUSTER}
 terraform workspace show
 echo "CALL - Generate templates on EFS"
 echo "CALL - Make input var files"
 terraform apply -auto-approve -var-file=master.tfvars
-popd
