a63323df30969ca4895eb791409f586fb210e16a
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-10-04 09:24:31 -0300

    Test

--- ci/devenv/create_env.sh
+++ ci/devenv/create_env.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd /repo/ci/terraform/environment
+#cd ../terraform/environment
+ls
 terraform init
-terraform workspace new ${CLUSTER}
+terraform workspace select kikitest3
 terraform workspace show
-echo "CALL - Generate templates on EFS"
-echo "CALL - Make input var files"
+# echo "CALL - Generate templates on EFS"
+# echo "CALL - Make input var files"
 terraform apply -auto-approve -var-file=master.tfvars
