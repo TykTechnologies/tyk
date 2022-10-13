539935ed6a4f45772b9b650aa9a5e8fc19a460eb
jeff <jeffy.mathew100@gmail.com>
2022-10-13 14:19:40 +0530

    add logic to validate external OAuth JWT. refactor jwt middleware for reuse.
    [changelog]
    internal: add logic to validate external OAuth JWT.

--- gateway/mw_external_oauth.go
+++ gateway/mw_external_oauth.go
@@ -1,8 +1,13 @@
 package gateway
 
 import (
+	"encoding/base64"
 	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/pmylund/go-cache"
 	"net/http"
+	"time"
 
 	"github.com/TykTechnologies/tyk/apidef"
 	"github.com/TykTechnologies/tyk/user"
@@ -10,6 +15,7 @@ import (
 
 type ExternalOAuthMiddleware struct {
 	BaseMiddleware
+	jwkCache *cache.Cache
 }
 
 func (k *ExternalOAuthMiddleware) Name() string {
@@ -51,7 +57,7 @@ func (k *ExternalOAuthMiddleware) ProcessRequest(w http.ResponseWriter, r *http.
 	provider := k.Spec.ExternalOAuth.Providers[0]
 
 	if provider.JWT.Enabled {
-		valid, identifier, err = k.jwt(token)
+		valid, identifier, err = k.jwt(token, provider.JWT)
 	} else if provider.Introspection.Enabled {
 		valid, identifier, err = k.introspection(token)
 	} else {
@@ -80,8 +86,65 @@ func (k *ExternalOAuthMiddleware) ProcessRequest(w http.ResponseWriter, r *http.
 
 // jwt makes access token validation without making a network call and validates access token locally.
 // The access token should be JWT type.
-func (k *ExternalOAuthMiddleware) jwt(accessToken string) (bool, string, error) {
-	return false, "", errors.New("jwt validation not implemented yet")
+func (k *ExternalOAuthMiddleware) jwt(accessToken string, jwtValidation apidef.JWTValidation) (bool, string, error) {
+	parser := jwt.NewParser(jwt.WithoutClaimsValidation())
+	// Verify the token
+	token, err := parser.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if err := assertSigningMethod(k.Spec.JWTSigningMethod, token); err != nil {
+			return nil, err
+		}
+
+		val, err := k.getSecretFromJWKOrConfig()
+		if err != nil {
+			k.Logger().WithError(err).Error("Couldn't get token")
+			return nil, err
+		}
+
+		return parseJWTKey(k.Spec.JWTSigningMethod, val)
+	})
+
+	if err != nil || (token != nil && !token.Valid) {
+		return false, "", fmt.Errorf("invalid token: %w", err)
+	}
+
+	if jwtErr := timeValidateJWTClaims(token.Claims.(jwt.MapClaims), jwtValidation.ExpiresAtValidationSkew,
+		jwtValidation.IssuedAtValidationSkew, jwtValidation.NotBeforeValidationSkew); jwtErr != nil {
+		return false, "", fmt.Errorf("key not authorized: %w", jwtErr)
+	}
+
+	userId, err := getUserIdFromClaim(token.Claims.(jwt.MapClaims), jwtValidation.IdentityBaseField)
+
+	if err != nil {
+		return false, "", err
+	}
+
+	return true, userId, nil
+}
+
+// getSecretFromJWKOrConfig gets the secret to verify jwt signature from API definition
+// or from JWK set if config is set to a URL
+func (k *ExternalOAuthMiddleware) getSecretFromJWKOrConfig() (interface{}, error) {
+	if k.jwkCache == nil {
+		JWKCache = cache.New(240*time.Second, 30*time.Second)
+	}
+
+	// is it a JWK URL?
+	if httpScheme.MatchString(k.Spec.JWTSource) {
+		return getJWK(k.Spec.JWTSource, k.Spec.APIID, k.Gw.GetConfig().JWTSSLInsecureSkipVerify, k.jwkCache)
+	}
+
+	decodedCert, err := base64.StdEncoding.DecodeString(k.Spec.JWTSource)
+	if err != nil {
+		return nil, err
+	}
+
+	// is decoded a JWK url too?
+	if httpScheme.MatchString(string(decodedCert)) {
+		return getJWK(string(decodedCert), k.Spec.APIID, k.Gw.GetConfig().JWTSSLInsecureSkipVerify, k.jwkCache)
+	}
+
+	return decodedCert, nil
 }
 
 // introspection makes an introspection request to third-party provider to check whether the access token is valid or not.
