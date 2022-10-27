4e230bf4e203aba6224ac7f01b31d76b0957be4d
Furkan Senharputlu <furkan_senharputlu@hotmail.com>
2022-10-27 11:03:38 +0300

    [TT-6933] Implement introspection cache OAS conversion (#4369)
    * Implement introspection cache OAS conversion
    
    * Make caching omittable
    
    (cherry picked from commit 2dd015b8d9bda756e300bbe49e131a9f3b306558)

--- apidef/oas/security.go
+++ apidef/oas/security.go
@@ -412,11 +412,12 @@ func (j *JWTValidation) ExtractTo(jwt *apidef.JWTValidation) {
 }
 
 type Introspection struct {
-	Enabled           bool   `bson:"enabled" json:"enabled"`
-	URL               string `bson:"url" json:"url"`
-	ClientID          string `bson:"clientId" json:"clientId"`
-	ClientSecret      string `bson:"clientSecret" json:"clientSecret"`
-	IdentityBaseField string `bson:"identityBaseField,omitempty" json:"identityBaseField,omitempty"`
+	Enabled           bool                `bson:"enabled" json:"enabled"`
+	URL               string              `bson:"url" json:"url"`
+	ClientID          string              `bson:"clientId" json:"clientId"`
+	ClientSecret      string              `bson:"clientSecret" json:"clientSecret"`
+	IdentityBaseField string              `bson:"identityBaseField,omitempty" json:"identityBaseField,omitempty"`
+	Cache             *IntrospectionCache `bson:"cache,omitempty" json:"cache,omitempty"`
 }
 
 func (i *Introspection) Fill(intros apidef.Introspection) {
@@ -425,6 +426,15 @@ func (i *Introspection) Fill(intros apidef.Introspection) {
 	i.ClientID = intros.ClientID
 	i.ClientSecret = intros.ClientSecret
 	i.IdentityBaseField = intros.IdentityBaseField
+
+	if i.Cache == nil {
+		i.Cache = &IntrospectionCache{}
+	}
+
+	i.Cache.Fill(intros.Cache)
+	if ShouldOmit(i.Cache) {
+		i.Cache = nil
+	}
 }
 
 func (i *Introspection) ExtractTo(intros *apidef.Introspection) {
@@ -433,6 +443,25 @@ func (i *Introspection) ExtractTo(intros *apidef.Introspection) {
 	intros.ClientID = i.ClientID
 	intros.ClientSecret = i.ClientSecret
 	intros.IdentityBaseField = i.IdentityBaseField
+
+	if i.Cache != nil {
+		i.Cache.ExtractTo(&intros.Cache)
+	}
+}
+
+type IntrospectionCache struct {
+	Enabled bool  `bson:"enabled" json:"enabled"`
+	Timeout int64 `bson:"timeout" json:"timeout"`
+}
+
+func (c *IntrospectionCache) Fill(cache apidef.IntrospectionCache) {
+	c.Enabled = cache.Enabled
+	c.Timeout = cache.Timeout
+}
+
+func (c *IntrospectionCache) ExtractTo(cache *apidef.IntrospectionCache) {
+	cache.Enabled = c.Enabled
+	cache.Timeout = c.Timeout
 }
 
 type ExternalOAuth struct {
