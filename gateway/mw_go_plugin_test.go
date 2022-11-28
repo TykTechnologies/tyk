b273af449319bf76d5eff64e3f0b3b16374018d2
Sredny M <sredny.buitrago@gmail.com>
2022-11-28 13:39:02 -0500

    Ensure that version used my plugin loader has the correct syntax (#4417) (#4427) (#4432)
    * ensure that version used my plugin loader has the correct syntax
    
    * allow goplugin loader to load plugin without the v prefix in version

--- gateway/mw_go_plugin_test.go
+++ gateway/mw_go_plugin_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGoPluginFromTykVersion(t *testing.T) {
+func TestGetGoPluginNameFromTykVersion(t *testing.T) {
 	t.Parallel()
 
 	m := GoPluginMiddleware{
@@ -32,22 +32,66 @@ func TestGoPluginFromTykVersion(t *testing.T) {
 	// plugin with clean version in filename (no -rc16).
 	expectVersion := "v4.1.0"
 
-	for _, version := range []string{expectVersion, expectVersion + "-rc16"} {
+	versions := []struct {
+		version, expectedVersion string
+	}{
+		{
+			version:         expectVersion,
+			expectedVersion: expectVersion,
+		},
+		{
+			version:         expectVersion + "-rc16",
+			expectedVersion: expectVersion,
+		},
+		{
+			version:         "4.1.0",
+			expectedVersion: "4.1.0",
+		},
+	}
+	for _, version := range versions {
 		testcases = append(testcases, []testCase{
-			{version, "plugin.so", fmt.Sprintf("./plugin_%v_%v_%v.so", expectVersion, goos, goarch)},
-			{version, "/some/path/plugin.so", fmt.Sprintf("/some/path/plugin_%v_%v_%v.so", expectVersion, goos, goarch)},
-			{version, "/some/path/plugin", fmt.Sprintf("/some/path/plugin_%v_%v_%v.so", expectVersion, goos, goarch)},
-			{version, "./plugin.so", fmt.Sprintf("./plugin_%v_%v_%v.so", expectVersion, goos, goarch)},
+			{version.version, "plugin.so", fmt.Sprintf("./plugin_%v_%v_%v.so", version.expectedVersion, goos, goarch)},
+			{version.version, "/some/path/plugin.so", fmt.Sprintf("/some/path/plugin_%v_%v_%v.so", version.expectedVersion, goos, goarch)},
+			{version.version, "/some/path/plugin", fmt.Sprintf("/some/path/plugin_%v_%v_%v.so", version.expectedVersion, goos, goarch)},
+			{version.version, "./plugin.so", fmt.Sprintf("./plugin_%v_%v_%v.so", version.expectedVersion, goos, goarch)},
 		}...)
 	}
 
-	for idx, tc := range testcases {
-		t.Run(fmt.Sprintf("Test case: %d", idx), func(t *testing.T) {
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("GW version:%v-Plugin Name:%v", tc.version, tc.inferredName), func(t *testing.T) {
 			t.Parallel()
 
 			m.Path = tc.userDefinedName
-			newPluginPath := m.goPluginFromTykVersion(tc.version)
+			newPluginPath := m.getPluginNameFromTykVersion(tc.version)
 			assert.Equal(t, tc.inferredName, newPluginPath)
+		})
+	}
+}
+
+func TestGetPrefixedVersion(t *testing.T) {
+	version := getPrefixedVersion("v4.1.0")
+	expectedVersion := "v4.1.0"
+	assert.Equal(t, expectedVersion, version)
+
+	testCases := []struct {
+		name, version, expectedVersion string
+	}{
+		{
+			name:            "version with the prefix V",
+			version:         "v4.1.0",
+			expectedVersion: "v4.1.0",
+		},
+		{
+			name:            "version without the prefix v",
+			version:         "4.1.0",
+			expectedVersion: "v4.1.0",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			version := getPrefixedVersion(tc.version)
+			assert.Equal(t, tc.expectedVersion, version)
 		})
 	}
 }
