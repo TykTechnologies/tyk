4fff813b9b4e3bd2d9654182b1628f24688aee13
Kofo Okesola <okesolakofo@gmail.com>
2022-11-28 10:01:39 +0100

    fixed issue with perist graphql and non empty listen path (#4414)

--- gateway/mw_persist_graphql_operation_test.go
+++ gateway/mw_persist_graphql_operation_test.go
@@ -25,6 +25,13 @@ query country($code: ID!){
   }
 }`
 
+const testGQLQueryCountryCode = `
+query country($countryCode: ID!){
+  country(code: $countryCode){
+    code
+  }
+}`
+
 const testQueryContinentCode = `
 query continent($code: ID!) {
   continent(code: $code){
@@ -223,6 +230,55 @@ func TestGraphqlPersist_Variables(t *testing.T) {
 			return q.Query == testQueryContinentCode && string(q.Variables) == `{"code":"AF"}`
 		}},
 	)
+	assert.NoError(t, err)
+}
+
+func TestGraphQLPersist_VariablesListenPath(t *testing.T) {
+	ts := StartTest(nil)
+	t.Cleanup(func() {
+		ts.Close()
+	})
+	ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
+		spec.Name = "rest-graph-listen-path"
+		spec.OrgID = "default"
+		spec.Proxy.ListenPath = "/test/"
+		spec.Proxy.TargetURL = TestHttpAny
+		spec.EnableContextVars = true
+		spec.VersionData.NotVersioned = false
+		spec.VersionData.Versions["Default"] = apidef.VersionInfo{
+			Name:             "Default",
+			Expires:          "3000-01-02 00:00",
+			UseExtendedPaths: true,
+			ExtendedPaths: apidef.ExtendedPathsSet{
+				PersistGraphQL: []apidef.PersistGraphQLMeta{
+					{
+						Path:      "/getCountryByCode/{countryCode}",
+						Method:    "GET",
+						Operation: testGQLQueryCountryCode,
+						Variables: map[string]interface{}{
+							"countryCode": "$path.countryCode",
+						},
+					},
+				},
+			},
+		}
+	})
+
+	_, err := ts.Run(t,
+		test.TestCase{Path: "/test/getCountryByCode/NG", Method: "GET", BodyMatchFunc: func(bytes []byte) bool {
+			var testResp TestHttpResponse
+			if err := json.Unmarshal(bytes, &testResp); err != nil {
+				return false
+			}
+			// Get query and variables
+			var q GraphQLRequest
+			if err := json.Unmarshal([]byte(testResp.Body), &q); err != nil {
+				return false
+			}
+			return q.Query == testGQLQueryCountryCode && string(q.Variables) == `{"countryCode":"NG"}`
+		}},
+	)
+
 	assert.NoError(t, err)
 }
 
