320ce63b6c46befd2dee432b14f680a9fce06a65
Tit Petric <tit.petric@monotek.net>
2022-12-01 10:34:25 +0100

    Add latency analytics to cache hit (#4461)

--- gateway/mw_redis_cache.go
+++ gateway/mw_redis_cache.go
@@ -147,6 +147,8 @@ type cacheOptions struct {
 
 // ProcessRequest will run any checks on the request on the way through the system, return an error to have the chain fail
 func (m *RedisCacheMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Request, _ interface{}) (error, int) {
+	t1 := time.Now()
+
 	var stat RequestStatus
 	var cacheKeyRegex string
 	var cacheMeta *EndPointCacheMeta
@@ -258,7 +260,8 @@ func (m *RedisCacheMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Req
 
 	// Record analytics
 	if !m.Spec.DoNotTrack {
-		m.sh.RecordHit(r, analytics.Latency{}, newRes.StatusCode, newRes)
+		ms := DurationToMillisecond(time.Since(t1))
+		m.sh.RecordHit(r, analytics.Latency{Total: int64(ms)}, newRes.StatusCode, newRes)
 	}
 
 	// Stop any further execution after we wrote cache out
