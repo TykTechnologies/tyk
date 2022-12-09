36368a139f27b643ac84f1d45a65a82acf443ab9
Burak Sezer <burak.sezer.developer@gmail.com>
2022-12-09 13:07:43 +0300

    [TT-6419] Querying for __typename in any graph causes a 502 error (#4501)
    This PR fixes an edge case in proxy-only mode. When you run the
    following query:
    
    ```graphql
    query { __typename }
    ```
    
    The GW panics and returns 502 for the request because the recent version
    of graphql-go-tools can handle this query without hitting the upstream.
    It maintains the type names internally.
    
    See https://github.com/TykTechnologies/graphql-go-tools/pull/323 for
    further info.
    
    (cherry picked from commit 6a32af5d5b5457426ffd961f3be6c12d9d671f6e)

--- gateway/mw_graphql_test.go
+++ gateway/mw_graphql_test.go
@@ -1,6 +1,7 @@
 package gateway
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"strings"
@@ -202,6 +203,31 @@ func TestGraphQLMiddleware_EngineMode(t *testing.T) {
 				{Data: countries1, BodyMatch: `"There was a problem proxying the request`, Code: http.StatusInternalServerError},
 			}...)
 		})
+	})
+
+	t.Run("Inspect __typename without hitting the upstream", func(t *testing.T) {
+		// See TT-6419
+		g := StartTest(nil)
+		defer g.Close()
+
+		g.Gw.BuildAndLoadAPI(func(spec *APISpec) {
+			spec.UseKeylessAccess = true
+			spec.Proxy.ListenPath = "/"
+			spec.GraphQL.Enabled = true
+			spec.GraphQL.ExecutionMode = apidef.GraphQLExecutionModeExecutionEngine
+			spec.GraphQL.Version = apidef.GraphQLConfigVersion2
+		})
+
+		request := gql.Request{
+			Variables: nil,
+			Query:     "query { __typename }",
+		}
+
+		expectedBody := []byte(`{"data":{"__typename":"Query"}}`)
+		_, _ = g.Run(t, test.TestCase{Data: request, BodyMatchFunc: func(body []byte) bool {
+			return bytes.Equal(expectedBody, body)
+		},
+			Code: http.StatusOK})
 	})
 
 	t.Run("graphql engine v2", func(t *testing.T) {
