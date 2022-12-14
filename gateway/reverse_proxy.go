40fd93c8299d88d1cce2ab9f38df364620a1162d
sredny buitrago <sredny.buitrago@gmail.com>
2022-12-13 21:03:37 -0400

    changed signature of method seek

--- gateway/reverse_proxy.go
+++ gateway/reverse_proxy.go
@@ -1717,7 +1717,7 @@ func (n *nopCloserBuffer) Read(p []byte) (int, error) {
 }
 
 // Seek seeks within the buffer
-func (n *nopCloserBuffer) Seek(offset int64, whence int64) (int64, error) {
+func (n *nopCloserBuffer) Seek(offset int64, whence int) (int64, error) {
 	if whence != io.SeekStart {
 		return 0, errors.New("invalid seek method, only supporting SeekStart")
 	}
