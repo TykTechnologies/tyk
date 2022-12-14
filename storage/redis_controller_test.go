48fb36e35d38516ae253c0ff09704844f442db3d
sredny buitrago <sredny.buitrago@gmail.com>
2022-12-14 13:05:36 -0400

    group /tyk imports

--- storage/redis_controller_test.go
+++ storage/redis_controller_test.go
@@ -5,11 +5,10 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/TykTechnologies/tyk/config"
 	"github.com/TykTechnologies/tyk/test"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/TykTechnologies/tyk/config"
 )
 
 func TestRecoverLoop(t *testing.T) {
