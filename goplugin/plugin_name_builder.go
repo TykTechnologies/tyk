0a068dd33c595176bcfa494ae72906f39a5f0c1d
Leonid Bugaev <leonsbox@gmail.com>
2022-12-22 21:24:51 +0300

    Merging to release-4.3: TT-7263 make response goplugins use the plugin's naming convention (#4569) (#4574)
    TT-7263 make response goplugins use the plugin's naming convention
    (#4569)
    
    <!-- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    some refactor so goplugins use the same logic even if they are
    middleware or response handlers. Now response goplugins use as well the
    same naming convention created previously for middleware goplugins
    
    ## Related Issue
    
    https://tyktech.atlassian.net/browse/TT-7263
    
    ## Motivation and Context
    
    <!-- Why is this change required? What problem does it solve? -->
    
    ## How This Has Been Tested
    
    Created bundle with response plugin. For this example the name of the
    file is bundle-versioned.zip,. With manifest as:
    
    ```
    {
      "file_list":[
        "response-plugin_v4.3.0_darwin_amd64.so"
      ],
      "custom_middleware":{
        "pre":null,
        "post":null,
        "post_key_auth":null,
        "auth_check":{
          "name":"",
          "path":"",
          "require_session":false,
          "raw_body_only":false
        },
        "response":[
          {
            "name":"MyPluginResponse",
            "path":"response-plugin.so",
            "require_session":false,
            "raw_body_only":false
          }
        ],
        "driver":"goplugin",
        "id_extractor":{
          "extract_from":"",
          "extract_with":"",
          "extractor_config":null
        }
      },
      "checksum":"84b78de895e1019621e4575ea6ffe03a",
      "signature":""
    }
    ```
    
    - Served the bundle with a file server
    - Ran Gw with bundler enabled
    - Created api that consumes the bundler. In the raw editor set:
    "custom_middleware_bundle": "bundle-versioned.zip",
    - Consume the api, check that response is modified. You should see a log
    like:
    
    ```
    WARN plugin file middleware/bundles/cee0c48a9bd56a47ad7da5520285fb77/response-plugin.so doesn't exist
    INFO Loaded Go response plugin: MyPluginResponse mwPath=middleware/bundles/cee0c48a9bd56a47ad7da5520285fb77/response-plugin.so mwSymbolName=MyPluginResponse
    ```
    
    You can use this as plugin:
    
    ```
    package main
    
    import (
       "bytes"
       "io/ioutil"
       "net/http"
    )
    
    // MyPluginResponse intercepts response from upstream
    func MyPluginResponse(rw http.ResponseWriter, res *http.Response, req *http.Request) {
       // add a header to our response object
       res.Header.Add("X-Response-Added", "resp-added")
    
       // overwrite our response body
       var buf bytes.Buffer
       buf.Write([]byte(`{"message":"Hi! I'm a response plugin"}`))
       res.Body = ioutil.NopCloser(&buf)
    
    }
    
    func main() {}
    ```
    
    Also added tests.
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test
    coverage to functionality)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply -->
    <!-- If there are no documentation updates required, mark the item as
    checked. -->
    <!-- Raise up any additional concerns not covered by the checklist. -->
    
    - [ ] I ensured that the documentation is up to date
    - [ ] I explained why this PR updates go.mod in detail with reasoning
    why it's required
    - [ ] I would like a code coverage CI quality gate exception and have
    explained why
    
    Co-authored-by: Sredny M <sredny.buitrago@gmail.com>

---
+++ goplugin/plugin_name_builder.go
@@ -0,0 +1,64 @@
+package goplugin
+
+import (
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+// pluginStorage defaults to FileSystemStorage
+var pluginStorage storage
+
+// GetPluginFileNameToLoad check which file to load based on name, tyk version, os and architecture
+// but it also takes care of returning the name of the file that exists
+func GetPluginFileNameToLoad(path string, version string) string {
+	if !pluginStorage.fileExist(path) {
+		// if the exact name doesn't exist then try to load it using tyk version
+		newPath := getPluginNameFromTykVersion(version, path)
+
+		prefixedVersion := getPrefixedVersion(version)
+
+		if !pluginStorage.fileExist(newPath) && version != prefixedVersion {
+			// if the file doesn't exist yet, then lets try with version in the format: v.x.x.x
+			newPath = getPluginNameFromTykVersion(prefixedVersion, path)
+		}
+		path = newPath
+	}
+
+	return path
+}
+
+// getPluginNameFromTykVersion builds a name of plugin based on tyk version
+// os and architecture. The structure of the plugin name looks like:
+// {plugin-dir}/{plugin-name}_{GW-version}_{OS}_{arch}.so
+// it doesn't check if the file exist
+func getPluginNameFromTykVersion(version string, path string) string {
+	if path == "" {
+		return ""
+	}
+
+	pluginDir := filepath.Dir(path)
+	// remove plugin extension to have the plugin's clean name
+	pluginName := strings.TrimSuffix(filepath.Base(path), ".so")
+	os := runtime.GOOS
+	architecture := runtime.GOARCH
+
+	// sanitize away `-rc15` suffixes (remove `-*`) from version
+	vs := strings.Split(version, "-")
+	if len(vs) > 0 {
+		version = vs[0]
+	}
+
+	newPluginName := strings.Join([]string{pluginName, version, os, architecture}, "_")
+	newPluginPath := pluginDir + "/" + newPluginName + ".so"
+
+	return newPluginPath
+}
+
+// getPrefixedVersion receives a version and check that it has the prefix 'v' otherwise, it adds it
+func getPrefixedVersion(version string) string {
+	if !strings.HasPrefix(version, "v") {
+		version = "v" + version
+	}
+	return version
+}
