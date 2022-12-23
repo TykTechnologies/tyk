6789a976676ecf5658148bb895ae103131c0df5f
Leonid Bugaev <leonsbox@gmail.com>
2022-12-23 21:18:09 +0300

    Merging to release-4: TT-7351 changed precedence on how plugins are picked (#4576) (#4579)
    TT-7351 changed precedence on how plugins are picked (#4576)
    
    <!-- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    Changed precedence on how goplugins are picked to be loaded. It used to
    be:
    
    1. exact name provided by user
    2. new naming convention with unprefixed gw version
    3. new naming convention
    
    Now it was modified to be:
    
    1. New naming convention
    2. New naminf convention with unprefixed gw version
    3. exact name provided by user
    
    In this way it will always attempt to load a compatible plugin and will
    avoid crashes when is found a file that is not intended to be used by
    that version of gw
    
    ## Related Issue
    
    https://tyktech.atlassian.net/browse/TT-7351
    
    ## Motivation and Context
    
    <!-- Why is this change required? What problem does it solve? -->
    
    ## How This Has Been Tested
    
    - Added unit tests
    - Manual testing. In a single directory have 3 plugins files which name
    is: `myplugin.so`,
    `myplugin_v4.3.0_darwin_amd64.so`,`myplugin_4.3.0_darwin_amd64.so`
    - Create an api that uses a goplugin which name is `myplugin.so`
    - The plugin `myplugin_v4.3.0_darwin_amd64.so` should be picked
    - Remove the file `myplugin_v4.3.0_darwin_amd64.so` (if you use bundles
    then remove any local cache, check the directory set in the gw config
    `middleware_path`+bundles)
    - Restart the gateway , the file `myplugin_4.3.0_darwin_amd64.so` should
    be loaded
    - Remove the file `myplugin_4.3.0_darwin_amd64.so` and restart the
    gateway
    - The file `myplugin.so` should be loaded
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test
    coverage to functionality)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply -->
    <!-- If there are no documentation updates required, mark the item as
    checked. -->
    <!-- Raise up any additional concerns not covered by the checklist. -->
    
    - [ ] I ensured that the documentation is up to date
    - [ ] I explained why this PR updates go.mod in detail with reasoning
    why it's required
    - [ ] I would like a code coverage CI quality gate exception and have
    explained why
    
    Co-authored-by: Tomas Buchaillot <tombuchaillot89@gmail.com>
    
    Co-authored-by: Sredny M <sredny.buitrago@gmail.com>

---
+++ gateway/mw_go_plugin_test.go
@@ -0,0 +1,18 @@
+package gateway
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestLoadPlugin test the function to load a middleware goplugin
+// ToDo: find out how to successfully load a plugin for testing
+func TestLoadPlugin(t *testing.T) {
+	plugin := GoPluginMiddleware{
+		Path: "/any-fake-path",
+	}
+
+	pluginLoaded := plugin.loadPlugin()
+	assert.Equal(t, false, pluginLoaded)
+}
