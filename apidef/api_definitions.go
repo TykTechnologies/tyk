c8160ad31252471e1b2bb3147acaede9ca1e21f6
Jeffy Mathew <jeffy.mathew100@gmail.com>
2023-02-01 09:21:05 +0530

    [TT-7362] Fix validate JSON middleware not loading in worker gateway. (#4719)
    https://tyktech.atlassian.net/browse/TT-7362
    
    caused by:  https://github.com/TykTechnologies/tyk/pull/4099
    
    (cherry picked from commit 455fc922043216cb88452223112d1e93baf13bc3)

--- apidef/api_definitions.go
+++ apidef/api_definitions.go
@@ -342,7 +342,7 @@ type ValidatePathMeta struct {
 	Path        string                  `bson:"path" json:"path"`
 	Method      string                  `bson:"method" json:"method"`
 	Schema      map[string]interface{}  `bson:"-" json:"schema"`
-	SchemaB64   string                  `bson:"schema_b64" json:"-"`
+	SchemaB64   string                  `bson:"schema_b64" json:"schema_b64,omitempty"`
 	SchemaCache gojsonschema.JSONLoader `bson:"-" json:"-"`
 	// Allows override of default 422 Unprocessible Entity response code for validation errors.
 	ErrorResponseCode int `bson:"error_response_code" json:"error_response_code"`
