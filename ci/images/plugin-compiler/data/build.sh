95b944893103693a3a5f9ef0ca99fb983059bbea
Leonid Bugaev <leonsbox@gmail.com>
2023-02-01 21:30:14 +0300

    More fixes

--- ci/images/plugin-compiler/data/build.sh
+++ ci/images/plugin-compiler/data/build.sh
@@ -48,15 +48,17 @@ go list -m -f '{{ if not .Main }}{{ .Path }} {{ .Version }}{{ end }}' all > depe
 
 # for any shared dependency, pin the version to Tyk gateway version
 awk 'NR==FNR{seen[$1]=$2; next} seen[$1] && seen[$1] != $2' $PLUGIN_SOURCE_PATH/dependencies.txt $TYK_GW_PATH/dependencies.txt | while read PKG VER; do
-  go get $PKG@$VER
+  go mod edit -replace $PKG=$PKG@$VER
 done
 
 go mod edit -replace github.com/TykTechnologies/tyk=$TYK_GW_PATH
 
 # Run a final go mod tidy to make sure that the go.sum entries are correct for
 # all replaced dependencies.
-go mod tidy
-#go mod vendor
+# go mod tidy
+# go mod vendor
+
+echo $GOARCH $GOOS
 
 # set appropriate X-build gcc binary for arm64.
 if [[ $GOARCH == "arm64" ]] && [[ $GOOS == "linux" ]] ; then
