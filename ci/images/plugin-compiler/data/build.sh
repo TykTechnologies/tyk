826e2eecf37d0d03374706403e09a95300a37aa4
Asutosh <asutosh@tyk.io>
2023-02-02 11:06:37 +0530

    Slightly modify the build script.
    We now update the cache as we replace the dependencies,
    also do a final verify before build.

--- ci/images/plugin-compiler/data/build.sh
+++ ci/images/plugin-compiler/data/build.sh
@@ -36,25 +36,34 @@ fi
 echo "Building plugin: $plugin_name"
 
 cd $TYK_GW_PATH
-go mod download
-go list -m -f '{{ if not .Main }}{{ .Path }} {{ .Version }}{{ end }}' all > dependencies.txt
+# get all gateway dependencies(path and versions)
+go list -mod=readonly -m -f '{{ if not .Main }}{{ .Path }} {{ .Version }}{{ end }}' all > dependencies.txt
 
 cd $PLUGIN_SOURCE_PATH
-go get
+go mod download
 
-# Get plugin dependencies
-go list -m -f '{{ if not .Main }}{{ .Path }} {{ .Version }}{{ end }}' all > dependencies.txt
+# Get all plugin dependencies(just the paths)
+go list -mod=mod -m -f '{{ if not .Main }}{{ .Path }}{{ end }}' all > dependencies.txt
 
-# for any shared dependency, pin the version to Tyk gateway version
-awk 'NR==FNR{seen[$1]=$2; next} seen[$1] && seen[$1] != $2' $PLUGIN_SOURCE_PATH/dependencies.txt $TYK_GW_PATH/dependencies.txt | while read PKG VER; do
-  go mod edit -replace $PKG=$PKG@$VER
-done
+
+# For any shared dependencies, use the version used in gateway, also download and
+# update the cache and go.sum for the replaced pkg.
+while IFS="" read -r dep
+do
+  if FULL_DEP=$(grep -E "^$dep[[:blank:]]+" $TYK_GW_PATH/dependencies.txt); then
+    VER=${FULL_DEP#* }
+    go mod edit -replace $dep="$dep"@"$VER"
+    go mod download "$dep"@"$VER"
+  fi
+done < $PLUGIN_SOURCE_PATH/dependencies.txt
 
 go mod edit -replace github.com/TykTechnologies/tyk=$TYK_GW_PATH
 
-# Run a final go mod tidy to make sure that the go.sum entries are correct for
-# all replaced dependencies.
-go mod tidy
+# Do a final verification - this updates all the go.sum entries to current.
+go mod verify
+
+rm $PLUGIN_SOURCE_PATH/dependencies.txt
+rm $TYK_GW_PATH/dependencies.txt
 
 CC=$(go env CC)
 # set appropriate X-build gcc binary for arm64.
@@ -62,5 +71,4 @@ if [[ $GOARCH == "arm64" ]] && [[ $GOOS == "linux" ]] ; then
     CC=aarch64-linux-gnu-gcc
 fi
 
-CGO_ENABLED=1 GOOS=$GOOS GOARCH=$GOARCH CC=$CC  go build -buildmode=plugin -o $plugin_name
-
+CGO_ENABLED=1 GOOS=$GOOS GOARCH=$GOARCH CC=$CC go build -buildmode=plugin -o $plugin_name
