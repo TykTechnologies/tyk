c1515b98c23dacf9ce24f0d3436d30a5002ae05b
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-13 10:48:32 -0300

    implement missing methods of objectID

--- apidef/api_definitions.go
+++ apidef/api_definitions.go
@@ -170,6 +170,18 @@ func (j ObjectId) GetBSON() (interface{}, error) {
 	return bson.ObjectId(j), nil
 }
 
+func (j ObjectId) TimeStamp() time.Time {
+	return j.Time()
+}
+
+func (j ObjectId) MarshalText() ([]byte, error) {
+	panic("not implemented")
+}
+
+func (j ObjectId) UnmarshalText([]byte) error {
+	panic("not implemented")
+}
+
 func ObjectIdHex(hex string) ObjectId {
 	return ObjectId(bson.ObjectIdHex(hex))
 }
