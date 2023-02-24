b3d5eb9ab26f7b88cb749ac3bce7c669b773bc84
Leonid Bugaev <leonsbox@gmail.com>
2023-02-24 16:05:00 +0300

    Merging to release-4-lts: [TT-7959] Add option to skip ClientCA announcement during mTLS (#4762)
    [TT-7959] Add option to skip ClientCA announcement during mTLS (#4762)
    
    At the moment we announce ClientCA to the client, which is a default Go
    way to do mTLS, and it also gives client a hint which certificate to
    use, if it has many.
    
    However if you have a lot of certificates in ClientCA, like 100, it can
    overflow TLS handshake buffer of some clients (for example default JVM
    buffer is 32kb).
    
    This PR adds an option
    `http_server_options.skip_client_ca_announcement`, which stops
    announcing ClientCAs during handshake. However, if you just pass empty
    ClientCA, and at the same time set TLS mode to
    `RequireAndVerifyClientCert` it will error, because non empty ClientCA
    required for default mTLS verification, which is built in to Go TLS
    library. Which means that we have to "disable" default client cert
    validation by setting TLS mode to `RequestClientCert`, and implement our
    "own" verification. Thankfully Go provides TLS hook
    `VerifyPeerCertificate` where you can add custom validation. It does not
    have access to client hello message, and other dynamic variables, so it
    needs to use a closure (this way is recommended by Go team). Inside this
    check duplicate exactly the same code which Go TLS library has, but
    instead of depending on `config.ClientCA` it use certificate pool passed
    via closure.
    
    mTLS unit test now runs 2 times, with and without clientCA announcement.
    
    I was afraid to make new way "default" because mTLS is weird, and better
    have a lot of options to tune it.
    
    But despite new config variable is added, this PR still can count as
    bugfix.
    
    ## Motivation and Context
    
    We have a client who is hitting overflow issue, without a way to
    overcome it. And number of used certificates only will grow.
    
    
    ## Types of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test
    coverage to functionality)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply -->
    <!-- If there are no documentation updates required, mark the item as
    checked. -->
    <!-- Raise up any additional concerns not covered by the checklist. -->
    
    - [ ] I ensured that the documentation is up to date
    - [ ] I explained why this PR updates go.mod in detail with reasoning
    why it's required
    - [ ] I would like a code coverage CI quality gate exception and have
    explained why
    
    ---------
    
    Co-authored-by: jeff <jeffy.mathew100@gmail.com>

--- gateway/cert.go
+++ gateway/cert.go
@@ -272,6 +272,30 @@ var tlsConfigCache = cache.New(60*time.Second, 60*time.Minute)
 
 var tlsConfigMu sync.Mutex
 
+func getClientValidator(helloInfo *tls.ClientHelloInfo, certPool *x509.CertPool) func([][]byte, [][]*x509.Certificate) error {
+	return func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+		if len(rawCerts) == 0 {
+			return errors.New("x509: missing client certificate")
+		}
+
+		cert, certErr := x509.ParseCertificate(rawCerts[0])
+		if certErr != nil {
+			return certErr
+		}
+
+		opts := x509.VerifyOptions{
+			Roots:         certPool,
+			CurrentTime:   time.Now(),
+			Intermediates: x509.NewCertPool(),
+			KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		}
+
+		_, err := cert.Verify(opts)
+
+		return err
+	}
+}
+
 func (gw *Gateway) getTLSConfigForClient(baseConfig *tls.Config, listenPort int) func(hello *tls.ClientHelloInfo) (*tls.Config, error) {
 	gwConfig := gw.GetConfig()
 	// Supporting legacy certificate configuration
@@ -438,8 +462,17 @@ func (gw *Gateway) getTLSConfigForClient(baseConfig *tls.Config, listenPort int)
 			newConfig.ClientAuth = domainRequireCert[""]
 		}
 
+		if gwConfig.HttpServerOptions.SkipClientCAAnnouncement {
+			if newConfig.ClientAuth == tls.RequireAndVerifyClientCert {
+				newConfig.VerifyPeerCertificate = getClientValidator(hello, newConfig.ClientCAs)
+			}
+			newConfig.ClientCAs = x509.NewCertPool()
+			newConfig.ClientAuth = tls.RequestClientCert
+		}
+
 		// Cache the config
 		tlsConfigCache.Set(hello.ServerName+listenPortStr, newConfig, cache.DefaultExpiration)
+
 		return newConfig, nil
 	}
 }
