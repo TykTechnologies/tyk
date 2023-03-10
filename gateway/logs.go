f3404513a1ef7d1b781fc5cd41ac459de8d1e5b0
Tit Petric <tit@tyk.io>
2023-03-10 20:05:14 +0100

    Add log variables to global

---
+++ gateway/logs.go
@@ -0,0 +1,9 @@
+package gateway
+
+var (
+	apiLog       = log.WithField("prefix", "api")
+	coprocessLog = log.WithField("prefix", "api")
+	pythonLog    = log.WithField("prefix", "api")
+	hostCheckLog = log.WithField("prefix", "api")
+	webhookLog   = log.WithField("prefix", "api")
+)
