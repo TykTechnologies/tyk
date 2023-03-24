c58634231a824a5ce58536060d8a01ede3285550
Tit Petric <tit@tyk.io>
2023-03-24 11:30:46 +0100

    Add new config flag to enable distributed tracing

--- config/config.go
+++ config/config.go
@@ -491,6 +491,8 @@ type NewRelicConfig struct {
 	AppName string `json:"app_name"`
 	// New Relic License key
 	LicenseKey string `json:"license_key"`
+	// Enable distributed tracing
+	EnableDistributedTracing bool `json:"enable_distributed_tracing"`
 }
 
 type Tracer struct {
