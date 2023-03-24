a3234b385cf0a1382812b4b3afb4acbc6dba1a68
jeff <jeffy.mathew100@gmail.com>
2023-03-24 21:49:59 +0530

    remove TestEncodeForDB, TestAPIDefinition_GenerateAPIID

--- apidef/api_definition_test.go
+++ apidef/api_definition_test.go
@@ -1,9 +1,6 @@
 package apidef
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,52 +22,6 @@ func TestSchema(t *testing.T) {
 			t.Error(err)
 		}
 	}
-}
-
-func TestEncodeForDB(t *testing.T) {
-	t.Run("EncodeForDB persist schema objects from extended path", func(t *testing.T) {
-		spec := DummyAPI()
-		spec.EncodeForDB()
-		var schemaNotEmpty bool
-		for _, version := range spec.VersionData.Versions {
-			for _, validateObj := range version.ExtendedPaths.ValidateJSON {
-				schemaNotEmpty = schemaNotEmpty || (validateObj.Schema != nil)
-			}
-		}
-		assert.True(t, schemaNotEmpty, "expected EncodeForDB to persist schema objects")
-	})
-}
-
-func TestDecodeFromDB(t *testing.T) {
-	t.Run("json schema validation middleware", func(t *testing.T) {
-		apiDef := DummyAPI()
-		var (
-			bodySchema map[string]interface{}
-			v1         = "v1"
-			v1B64      = base64.StdEncoding.EncodeToString([]byte(v1))
-		)
-		err := json.Unmarshal([]byte(`{"$schema":"http://json-schema.org/draft-04/schema#","properties":{"id":{"type":"integer"}},"required":["id"],"type":"object"}`),
-			&bodySchema)
-		assert.NoError(t, err)
-		apiDef.VersionData.Versions[v1] = VersionInfo{
-			ExtendedPaths: ExtendedPathsSet{
-				ValidateJSON: []ValidatePathMeta{
-					{
-						Path:   "/",
-						Method: http.MethodPost,
-						Schema: bodySchema,
-					},
-				},
-			},
-		}
-		apiDef.EncodeForDB()
-		copyAPIDef := apiDef
-		copyAPIDef.DecodeFromDB()
-
-		assert.Equal(t, apiDef.VersionData.Versions[v1B64].ExtendedPaths.ValidateJSON[0].Schema,
-			copyAPIDef.VersionData.Versions[v1].ExtendedPaths.ValidateJSON[0].Schema)
-		assert.Empty(t, copyAPIDef.VersionData.Versions[v1].ExtendedPaths.ValidateJSON[0].SchemaB64)
-	})
 }
 
 func TestSchemaGraphqlConfig(t *testing.T) {
@@ -113,12 +64,6 @@ func TestAPIDefinition_DecodeFromDB_AuthDeprecation(t *testing.T) {
 		"jwt":       spec.Auth,
 	})
 
-}
-
-func TestAPIDefinition_GenerateAPIID(t *testing.T) {
-	a := APIDefinition{}
-	a.GenerateAPIID()
-	assert.NotEmpty(t, a.APIID)
 }
 
 func TestAPIDefinition_GetScopeClaimName(t *testing.T) {
