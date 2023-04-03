778c8a9336034f0bedb2f811a8abb27884b0699c
Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
2023-04-03 11:32:34 +0300

    Update main.tf

--- repo-policy/main.tf
+++ repo-policy/main.tf
@@ -53,6 +53,11 @@ module "tyk" {
 	convos    = "false",
 	source_branch  = "release-4-lts",
 	required_tests = ["Go 1.15 Redis 5","1.15","1.15-el7"]},
+{ branch    = "release-4.0.13",
+	reviewers = "0",
+	convos    = "false",
+	source_branch  = "release-4-lts",
+	required_tests = ["Go 1.15 Redis 5","1.15","1.15-el7"]},
 { branch    = "release-4.3",
 	reviewers = "0",
 	convos    = "false",
@@ -87,6 +92,11 @@ module "tyk" {
 	reviewers = "0",
 	convos    = "false",
 	source_branch  = "master",
+	required_tests = ["Go 1.16 Redis 5","1.16","1.16-el7"]},
+{ branch    = "release-5.0.1",
+	reviewers = "0",
+	convos    = "false",
+	source_branch  = "release-5-lts",
 	required_tests = ["Go 1.16 Redis 5","1.16","1.16-el7"]},
 ]
 }
