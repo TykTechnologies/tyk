5ef17390ec3c5bbc9a697551e694fb4bf022e0cf
Bender <policy@gromit>
2023-06-16 08:40:27 +0000

    [CI]: Syncing release-5.0.2

--- ci/images/plugin-compiler/data/build.sh
+++ ci/images/plugin-compiler/data/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-GATEWAY_VERSION=$(perl -n -e'/v(\d+).(\d+).(\d+)/'' && print "v$1\.$2\.$3"' $TYK_GW_PATH/gateway/version.go)
+GATEWAY_VERSION=$(echo $GITHUB_TAG | perl -n -e'/v(\d+).(\d+).(\d+)/'' && print "v$1\.$2\.$3"')
 
 # Plugin compiler arguments:
 #
@@ -25,8 +25,10 @@ GOOS=${3:-$(go env GOOS)}
 GOARCH=${4:-$(go env GOARCH)}
 
 # Some defaults that can be overriden with env
+WORKSPACE_ROOT=$(dirname $TYK_GW_PATH)
+
 PLUGIN_SOURCE_PATH=${PLUGIN_SOURCE_PATH:-"/plugin-source"}
-PLUGIN_BUILD_PATH=${PLUGIN_BUILD_PATH:-"/opt/plugin_${plugin_name%.*}$plugin_id"}
+PLUGIN_BUILD_PATH=${PLUGIN_BUILD_PATH:-"${WORKSPACE_ROOT}/plugin_${plugin_name%.*}$plugin_id"}
 
 function usage() {
     cat <<EOF
@@ -42,23 +44,45 @@ if [ -z "$plugin_name" ]; then
     exit 1
 fi
 
+CC=$(go env CC)
+
+if [[ $BASE_IMAGE == *"golang-cross"* ]] && [[ $GOARCH == "arm64" ]] && [[ $GOOS == "linux" ]] ; then
+	CC=aarch64-linux-gnu-gcc
+fi
+
 # if arch and os present then update the name of file with those params
 if [[ $GOOS != "" ]] && [[ $GOARCH != "" ]]; then
   plugin_name="${plugin_name%.*}_${GATEWAY_VERSION}_${GOOS}_${GOARCH}.so"
 fi
 
 # Copy plugin source into plugin build folder.
+set -x
 
 mkdir -p $PLUGIN_BUILD_PATH
 yes | cp -r $PLUGIN_SOURCE_PATH/* $PLUGIN_BUILD_PATH || true
+
+# Create worspace
+cd $WORKSPACE_ROOT
+
+go work init ./tyk
+go work use ./$(basename $PLUGIN_BUILD_PATH)
+
+echo "---"
+cat go.work
+echo "---"
+
 cd $PLUGIN_BUILD_PATH
+
+set +x
 
 # Dump settings for inspection
 
 echo "PLUGIN_BUILD_PATH: ${PLUGIN_BUILD_PATH}"
 echo "PLUGIN_SOURCE_PATH: ${PLUGIN_SOURCE_PATH}"
+echo "CC: ${CC}"
 echo "plugin_name: ${plugin_name}"
 
 set -x
-CGO_ENABLED=1 GOOS=$GOOS GOARCH=$GOARCH go build -buildmode=plugin -o $plugin_name
+CC=$CC CGO_ENABLED=1 GOOS=$GOOS GOARCH=$GOARCH go build -buildmode=plugin -trimpath -o $plugin_name
 mv $plugin_name $PLUGIN_SOURCE_PATH
+rm -f $WORKSPACE_ROOT/go.work
