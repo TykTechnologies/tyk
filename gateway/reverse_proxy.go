27ce6c97becf7c18f7207ab4b260847ea6e7ea3d
Leonid Bugaev <leonsbox@gmail.com>
2023-06-29 09:29:08 +0300

    Update reverse_proxy.go

--- gateway/reverse_proxy.go
+++ gateway/reverse_proxy.go
@@ -1338,7 +1338,7 @@ func (p *ReverseProxy) WrappedServeHTTP(rw http.ResponseWriter, req *http.Reques
 		p.TykAPISpec.HTTPTransportCreated = time.Now()
 
 		if p.TykAPISpec.HTTPTransport != nil {
-			p.TykAPISpec.HTTPTransport.transport.CloseIdleConnections()
+			oldTransport.CloseIdleConnections()
 		}
 	}
 
