4318a168fdd458bdfb864811b8c07bfaff66fc50
Tit Petric <tit.petric@monotek.net>
2023-06-29 22:31:56 +0200

    [TT-9223] revert and update plugin compiler (#5247)
    Co-authored-by: Tit Petric <tit@tyk.io>

--- ci/images/plugin-compiler/data/build.sh
+++ ci/images/plugin-compiler/data/build.sh
@@ -55,20 +55,49 @@ if [[ $GOOS != "" ]] && [[ $GOARCH != "" ]]; then
   plugin_name="${plugin_name%.*}_${GATEWAY_VERSION}_${GOOS}_${GOARCH}.so"
 fi
 
-# Copy plugin source into plugin build folder.
+set -x
 mkdir -p $PLUGIN_BUILD_PATH
+# Plugin's vendor folder, has precedence over the cached vendor'd dependencies from tyk
 yes | cp -r $PLUGIN_SOURCE_PATH/* $PLUGIN_BUILD_PATH || true
 
-# Go to plugin build path
 cd $PLUGIN_BUILD_PATH
+
+# Handle if plugin has own vendor folder, and ignore error if not
+[ -f go.mod ] && [ ! -d ./vendor ] && GO111MODULE=on go mod vendor
+# Ensure that go modules not used
+rm -rf go.mod
+
+# We do not need to care which version of Tyk vendored in plugin, since we going to use version inside compiler
+rm -rf $PLUGIN_BUILD_PATH/vendor/github.com/TykTechnologies/tyk
+
+# Copy plugin vendored pkgs to GOPATH
+yes | cp -rf $PLUGIN_BUILD_PATH/vendor/* $GOPATH/src || true \
+        && rm -rf $PLUGIN_BUILD_PATH/vendor
+
+# Ensure that GW package versions have priorities
+
+# We can't just copy Tyk dependencies on top of plugin dependencies, since different package versions have different file structures
+# First we need to find which deps GW already has, remove this folders, and after copy fresh versions from GW
+
+# github.com and rest of packages have different nesting levels, so have to handle it separately
+ls -d $TYK_GW_PATH/vendor/github.com/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | xargs -d '\n' rm -rf
+ls -d $TYK_GW_PATH/vendor/*/* | sed "s|$TYK_GW_PATH/vendor|$GOPATH/src|g" | grep -v github | xargs -d '\n' rm -rf
+
+# Copy GW dependencies
+yes | cp -rf $TYK_GW_PATH/vendor/* $GOPATH/src
+rm -rf $TYK_GW_PATH/vendor
+rm /go/src/modules.txt
+
+set +x
 
 # Dump settings for inspection
 
 echo "PLUGIN_BUILD_PATH: ${PLUGIN_BUILD_PATH}"
 echo "PLUGIN_SOURCE_PATH: ${PLUGIN_SOURCE_PATH}"
+echo "CC: ${CC}"
+echo "plugin_name: ${plugin_name}"
 
 set -x
-CC=$CC CGO_ENABLED=1 GOOS=$GOOS GOARCH=$GOARCH go build -buildmode=plugin -o $plugin_name
-set +x
 
+GO111MODULE=off CC=$CC GOOS=$GOOS GOARCH=$GOARCH go build -buildmode=plugin -o $plugin_name
 mv $plugin_name $PLUGIN_SOURCE_PATH
