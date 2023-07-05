cfee300cfa5660ac9a6ba2142680753bda276b67
Leonid Bugaev <leonsbox@gmail.com>
2023-07-05 20:45:42 +0300

    Backport #5231

--- gateway/reverse_proxy.go
+++ gateway/reverse_proxy.go
@@ -1203,9 +1203,18 @@ func (p *ReverseProxy) WrappedServeHTTP(rw http.ResponseWriter, req *http.Reques
 	}
 
 	if createTransport {
+		oldTransport := p.TykAPISpec.HTTPTransport.transport
+ 		// Prevent new idle connections to be generated.
+ 		oldTransport.DisableKeepAlives = true
+
 		_, timeout := p.CheckHardTimeoutEnforced(p.TykAPISpec, req)
 		p.TykAPISpec.HTTPTransport = p.httpTransport(timeout, rw, req, outreq)
 		p.TykAPISpec.HTTPTransportCreated = time.Now()
+
+
+ 		if p.TykAPISpec.HTTPTransport != nil {
+ 			oldTransport.CloseIdleConnections()
+ 		}		
 	}
 
 	roundTripper = p.TykAPISpec.HTTPTransport
