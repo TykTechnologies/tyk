5108c043993d71819e5961085fa72a80c84f8eaa
Kofo Okesola <okesolakofo@gmail.com>
2023-07-25 09:22:57 +0100

    set span status

--- internal/graphql/otel_graphql_engine.go
+++ internal/graphql/otel_graphql_engine.go
@@ -4,10 +4,6 @@ import (
 	"context"
 	"sync"
 
-	"go.opentelemetry.io/otel/codes"
-
-	"github.com/sirupsen/logrus"
-
 	"github.com/TykTechnologies/graphql-go-tools/pkg/engine/plan"
 	"github.com/TykTechnologies/graphql-go-tools/pkg/engine/resolve"
 	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
@@ -18,7 +14,6 @@ import (
 
 type OtelGraphqlEngineV2 struct {
 	mutex          sync.Mutex
-	logger         *logrus.Entry
 	traceContext   context.Context
 	tracerProvider otel.TracerProvider
 
@@ -42,10 +37,9 @@ func (o *OtelGraphqlEngineV2) Normalize(operation *graphql.Request) error {
 	defer span.End()
 	err := o.engine.Normalize(operation)
 	if err != nil {
-		span.SetStatus(codes.Error, "request normalization failed")
+		span.SetStatus(otel.SPAN_STATUS_ERROR, "request normalization failed")
 		return err
 	}
-	span.SetStatus(codes.Ok, "success")
 	return nil
 }
 
@@ -55,10 +49,9 @@ func (o *OtelGraphqlEngineV2) ValidateForSchema(operation *graphql.Request) erro
 	defer span.End()
 	err := o.engine.ValidateForSchema(operation)
 	if err != nil {
-		span.SetStatus(codes.Error, "request validation failed")
+		span.SetStatus(otel.SPAN_STATUS_ERROR, "request validation failed")
 		return err
 	}
-	span.SetStatus(codes.Ok, "success")
 	return nil
 }
 
@@ -73,13 +66,12 @@ func (o *OtelGraphqlEngineV2) Plan(postProcessor *postprocess.Processor, operati
 	var operationName = "GeneratePlan"
 	_, span := o.tracerProvider.Tracer().Start(o.traceContext, operationName)
 	defer span.End()
-	plan, err := o.engine.Plan(postProcessor, operation, report)
+	p, err := o.engine.Plan(postProcessor, operation, report)
 	if err != nil {
-		span.SetStatus(codes.Error, "failed to generate plan")
+		span.SetStatus(otel.SPAN_STATUS_ERROR, "failed to generate plan")
 		return nil, err
 	}
-	span.SetStatus(codes.Ok, "success")
-	return plan, nil
+	return p, nil
 }
 
 func (o *OtelGraphqlEngineV2) Resolve(resolveContext *resolve.Context, planResult plan.Plan, writer resolve.FlushWriter) error {
@@ -88,10 +80,9 @@ func (o *OtelGraphqlEngineV2) Resolve(resolveContext *resolve.Context, planResul
 	defer span.End()
 	resolveContext.Context = ctx
 	if err := o.engine.Resolve(resolveContext, planResult, writer); err != nil {
-		span.SetStatus(codes.Error, "failed to resolve")
+		span.SetStatus(otel.SPAN_STATUS_ERROR, "failed to resolve")
 		return err
 	}
-	span.SetStatus(codes.Ok, "success")
 	return nil
 }
 
@@ -103,10 +94,9 @@ func (o *OtelGraphqlEngineV2) InputValidation(operation *graphql.Request) error 
 	_, span := o.tracerProvider.Tracer().Start(o.traceContext, operationName)
 	defer span.End()
 	if err := o.engine.InputValidation(operation); err != nil {
-		span.SetStatus(codes.Error, "failed input validation")
+		span.SetStatus(otel.SPAN_STATUS_ERROR, "failed input validation")
 		return err
 	}
-	span.SetStatus(codes.Ok, "success")
 	return nil
 }
 
@@ -115,10 +105,9 @@ func (o *OtelGraphqlEngineV2) Execute(inCtx context.Context, operation *graphql.
 	defer span.End()
 	o.SetContext(ctx)
 	if err := o.engine.Execute(inCtx, operation, writer, options...); err != nil {
-		span.SetStatus(codes.Error, "failed to execute")
+		span.SetStatus(otel.SPAN_STATUS_ERROR, "failed to execute")
 		return err
 	}
-	span.SetStatus(codes.Ok, "success")
 	return nil
 }
 
