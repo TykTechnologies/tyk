c30e0a6b72aaa5ec316444d34e35fcecaa9719f0
Tit Petric <tit@tyk.io>
2023-08-01 19:09:33 +0200

    Add test case to confirm goroutine leaks

---
+++ gateway/testutil_test.go
@@ -0,0 +1,22 @@
+package gateway
+
+import (
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestStartTestLeaks(t *testing.T) {
+	t.Logf("Started with %d goroutines", runtime.NumGoroutine())
+
+	ts := StartTest(nil)
+	ts.Close()
+
+	tick := func() {
+		runtime.GC()
+		time.Sleep(time.Second)
+		t.Logf("Tick with %d goroutines", runtime.NumGoroutine())
+	}
+
+	tick()
+}
