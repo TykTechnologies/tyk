9fef29a302e865d398113c8b3aa2b2fc11469b3d
Tit Petric <tit@tyk.io>
2023-08-08 21:45:10 +0200

    Fix tests behind a v52 build tag

---
+++ gateway/mw_graphql_v52_test.go
@@ -0,0 +1,70 @@
+//go:build v52
+// +build v52
+
+package gateway
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	gql "github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
+	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/TykTechnologies/tyk/config"
+	"github.com/TykTechnologies/tyk/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGraphQLMiddleware_OpenTelemetry(t *testing.T) {
+	g := StartTest(nil)
+	defer g.Close()
+
+	spec := BuildAPI(func(spec *APISpec) {
+		spec.UseKeylessAccess = true
+		spec.Proxy.ListenPath = "/"
+		spec.GraphQL.Enabled = true
+		spec.GraphQL.ExecutionMode = apidef.GraphQLExecutionModeProxyOnly
+	})[0]
+
+	t.Run("fail input validation with otel tracing active", func(t *testing.T) {
+		local := StartTest(func(globalConf *config.Config) {
+			globalConf.OpenTelemetry.Enabled = true
+		})
+		defer local.Close()
+		testSpec := BuildAPI(func(spec *APISpec) {
+			spec.GraphQL.ExecutionMode = apidef.GraphQLExecutionModeProxyOnly
+			spec.GraphQL.Schema = gqlCountriesSchema
+			spec.GraphQL.Version = apidef.GraphQLConfigVersion2
+			spec.Proxy.TargetURL = testGraphQLProxyUpstream
+			spec.Proxy.ListenPath = "/"
+			spec.GraphQL.Enabled = true
+		})[0]
+
+		local.Gw.LoadAPI(testSpec)
+
+		_, err := local.Run(
+			t,
+			test.TestCase{
+				Path:   "/",
+				Method: http.MethodPost,
+				Data: gql.Request{
+					Query:     gqlContinentQueryVariable,
+					Variables: []byte(`{"code":null}`),
+				},
+				Code: 400,
+			},
+			test.TestCase{
+				Path:   "/",
+				Method: http.MethodPost,
+				Data: gql.Request{
+					Query:     gqlStateQueryVariable,
+					Variables: []byte(`{"filter":{"code":{"eq":"filterString"}}}`),
+				},
+				Code: 400,
+				BodyMatchFunc: func(i []byte) bool {
+					return strings.Contains(string(i), `Validation for variable \"filter\" failed`)
+				},
+			})
+		assert.NoError(t, err)
+	})
+}
--- gateway/otel_test.go
+++ gateway/otel_test.go
@@ -2,3 +2,161 @@
 // +build v52
 
 package gateway
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/TykTechnologies/tyk/config"
+	"github.com/TykTechnologies/tyk/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGateway_afterConfSetup(t *testing.T) {
+
+	tests := []struct {
+		name           string
+		initialConfig  config.Config
+		expectedConfig config.Config
+	}{
+		{
+			name: "slave options test",
+			initialConfig: config.Config{
+				SlaveOptions: config.SlaveOptionsConfig{
+					UseRPC: true,
+				},
+			},
+			expectedConfig: config.Config{
+				SlaveOptions: config.SlaveOptionsConfig{
+					UseRPC:                   true,
+					GroupID:                  "ungrouped",
+					CallTimeout:              30,
+					PingTimeout:              60,
+					KeySpaceSyncInterval:     10,
+					RPCCertCacheExpiration:   3600,
+					RPCGlobalCacheExpiration: 30,
+				},
+				AnalyticsConfig: config.AnalyticsConfigConfig{
+					PurgeInterval: 10,
+				},
+				HealthCheckEndpointName: "hello",
+			},
+		},
+		{
+			name: "opentelemetry options test",
+			initialConfig: config.Config{
+				OpenTelemetry: otel.Config{
+					Enabled: true,
+				},
+			},
+			expectedConfig: config.Config{
+				OpenTelemetry: otel.Config{
+					Enabled:            true,
+					Exporter:           "grpc",
+					Endpoint:           "localhost:4317",
+					ResourceName:       "tyk-gateway",
+					SpanProcessorType:  "batch",
+					ConnectionTimeout:  1,
+					ContextPropagation: "tracecontext",
+					Sampling: otel.Sampling{
+						Type: "AlwaysOn",
+					},
+				},
+				AnalyticsConfig: config.AnalyticsConfigConfig{
+					PurgeInterval: 10,
+				},
+				HealthCheckEndpointName: "hello",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gw := NewGateway(tt.initialConfig, context.Background())
+			gw.afterConfSetup()
+
+			assert.Equal(t, tt.expectedConfig, gw.GetConfig())
+
+		})
+	}
+}
+
+func TestOpenTelemetry(t *testing.T) {
+	t.Run("Opentelemetry enabled - check if we are sending traces", func(t *testing.T) {
+		otelCollectorMock := httpCollectorMock(t, func(w http.ResponseWriter, r *http.Request) {
+			//check the body
+			body, err := io.ReadAll(r.Body)
+			assert.Nil(t, err)
+
+			assert.NotEmpty(t, body)
+
+			// check the user agent
+			agent, ok := r.Header["User-Agent"]
+			assert.True(t, ok)
+			assert.Len(t, agent, 1)
+			assert.Contains(t, agent[0], "OTLP")
+
+			//check if we are sending the traces to the right endpoint
+			assert.Contains(t, r.URL.Path, "/v1/traces")
+
+			// Here you can check the request and return a response
+			w.WriteHeader(http.StatusOK)
+		}, ":0")
+
+		// Start the server.
+		otelCollectorMock.Start()
+		// Stop the server on return from the function.
+		defer otelCollectorMock.Close()
+
+		ts := StartTest(func(globalConf *config.Config) {
+			globalConf.OpenTelemetry.Enabled = true
+			globalConf.OpenTelemetry.Exporter = "http"
+			globalConf.OpenTelemetry.Endpoint = otelCollectorMock.URL
+			globalConf.OpenTelemetry.SpanProcessorType = "simple"
+		})
+		defer ts.Close()
+		detailedTracing := []bool{true, false}
+		for _, detailed := range detailedTracing {
+			ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
+				spec.APIID = "test"
+				spec.Proxy.ListenPath = "/my-api/"
+				spec.UseKeylessAccess = true
+				spec.DetailedTracing = detailed
+			})
+
+			_, _ = ts.Run(t, test.TestCase{Path: "/my-api/", Code: http.StatusOK})
+			assert.Equal(t, "otel", ts.Gw.TracerProvider.Type())
+		}
+
+	})
+
+	t.Run("Opentelemetry disabled - check if we are not sending traces", func(t *testing.T) {
+
+		otelCollectorMock := httpCollectorMock(t, func(w http.ResponseWriter, r *http.Request) {
+			t.Fail()
+		}, ":0")
+
+		// Start the server.
+		otelCollectorMock.Start()
+		// Stop the server on return from the function.
+		defer otelCollectorMock.Close()
+
+		ts := StartTest(func(globalConf *config.Config) {
+			globalConf.OpenTelemetry.Enabled = false
+			globalConf.OpenTelemetry.Exporter = "http"
+			globalConf.OpenTelemetry.Endpoint = otelCollectorMock.URL
+			globalConf.OpenTelemetry.SpanProcessorType = "simple"
+		})
+		defer ts.Close()
+
+		ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
+			spec.APIID = "test"
+			spec.Proxy.ListenPath = "/my-api/"
+			spec.UseKeylessAccess = true
+		})
+
+		_, _ = ts.Run(t, test.TestCase{Path: "/my-api/", Code: http.StatusOK})
+		assert.Equal(t, "noop", ts.Gw.TracerProvider.Type())
+	})
+}
