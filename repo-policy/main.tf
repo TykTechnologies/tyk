21ff9aad069acf5b7adeb1ba1ffbc0e50b41d8c6
Asutosh <1187055+asutosh@users.noreply.github.com>
2023-08-12 00:43:58 +0530

    Update required checks for release-5.0.5 (#5413)
    The release job name was changed from 1.16 to 1.16-bullseye in
    `release-5-lts' in between 5.0.4 and 5.0.5. This was not updated for
    5.0.5 repo policy.

--- repo-policy/main.tf
+++ repo-policy/main.tf
@@ -74,7 +74,7 @@ module "tyk" {
 	reviewers = "0",
 	convos    = "false",
 	source_branch  = "release-5-lts",
-	required_tests = ["Go 1.16 Redis 5","1.16","1.16-el7"]},
+	required_tests = ["Go 1.16 Redis 5","1.16-bullseye","1.16-el7"]},
 { branch    = "release-5.1",
 	reviewers = "0",
 	convos    = "false",
