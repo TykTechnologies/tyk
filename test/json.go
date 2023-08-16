2ba1bb3e68dae763f6c61c354dda14716c75a3bb
Tit Petric <tit@tyk.io>
2023-08-16 16:50:23 +0200

    Fix sonarcloud issue (typo, t.Helper)

--- test/json.go
+++ test/json.go
@@ -8,8 +8,10 @@ import (
 )
 
 // MarshalJSON returns a closure returning the marshalled json
-// while asserting no error occured during marshalling.
+// while asserting no error occurred during marshalling.
 func MarshalJSON(t testing.TB) func(interface{}) []byte {
+	t.Helper()
+
 	return func(in interface{}) []byte {
 		b, err := json.Marshal(in)
 		require.NoError(t, err)
