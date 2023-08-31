d2f0c4cbcf7b8d4431b848a5871fbb0b4a9fbf27
Jeffy Mathew <jeffy.mathew100@gmail.com>
2023-08-31 10:50:52 +0100

    [TT-9924]remove muxer, proxy from explicitRouteHandler (#5482)
    Remove muxer from explicitRouteHandler
    
    (cherry picked from commit 213ac5b31f61556de7b2299001131bf2b25dfd1d)

--- gateway/api_loader.go
+++ gateway/api_loader.go
@@ -671,7 +671,6 @@ func (gw *Gateway) fuzzyFindAPI(search string) *APISpec {
 type explicitRouteHandler struct {
 	prefix  string
 	handler http.Handler
-	muxer   *proxyMux
 }
 
 func (h *explicitRouteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -679,10 +678,12 @@ func (h *explicitRouteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 		h.handler.ServeHTTP(w, r)
 		return
 	}
-	h.muxer.handle404(w, r)
+
+	w.WriteHeader(http.StatusNotFound)
+	_, _ = fmt.Fprint(w, http.StatusText(http.StatusNotFound))
 }
 
-func explicitRouteSubpaths(prefix string, handler http.Handler, muxer *proxyMux, enabled bool) http.Handler {
+func explicitRouteSubpaths(prefix string, handler http.Handler, enabled bool) http.Handler {
 	// feature is enabled via config option
 	if !enabled {
 		return handler
@@ -700,7 +701,6 @@ func explicitRouteSubpaths(prefix string, handler http.Handler, muxer *proxyMux,
 	return &explicitRouteHandler{
 		prefix:  prefix,
 		handler: handler,
-		muxer:   muxer,
 	}
 }
 
@@ -751,6 +751,7 @@ func (gw *Gateway) loadHTTPService(spec *APISpec, apisByListen map[string]int, g
 		spec.Proxy.ListenPath,
 	}
 
+<<<<<<< HEAD
 	// Register routes for each prefixe
 	for _, prefix := range prefixes {
 		subrouter := router.PathPrefix(prefix).Subrouter()
@@ -766,6 +767,10 @@ func (gw *Gateway) loadHTTPService(spec *APISpec, apisByListen map[string]int, g
 		// Attach handlers
 		subrouter.NewRoute().Handler(httpHandler)
 	}
+=======
+	httpHandler := explicitRouteSubpaths(spec.Proxy.ListenPath, chainObj.ThisHandler, gwConfig.HttpServerOptions.EnableStrictRoutes)
+	subrouter.NewRoute().Handler(httpHandler)
+>>>>>>> 213ac5b3... [TT-9924]remove muxer, proxy from explicitRouteHandler (#5482)
 
 	return chainObj
 }
