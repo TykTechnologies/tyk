b79dd2481f47c41b36c5ff787151e4a891f7fca1
sredny buitrago <sredny.buitrago@gmail.com>
2023-09-04 13:00:08 -0300

    when worker node is on emergency node, do not panic when using a new jwt token

--- gateway/middleware.go
+++ gateway/middleware.go
@@ -816,6 +816,7 @@ func (t BaseMiddleware) CheckSessionAndIdentityForValidKey(originalKey string, r
 	}
 
 	if _, ok := t.Spec.AuthManager.Store().(*RPCStorageHandler); ok && rpc.IsEmergencyMode() {
+		session.KeyID = key
 		return session.Clone(), false
 	}
 
