1abf513790ad4a10d5c4818c47727a543d63aff9
Tit Petric <tit@tyk.io>
2023-10-12 13:53:19 +0200

    Fix smoke tests

--- ci/tests/plugin-compiler/test.sh
+++ ci/tests/plugin-compiler/test.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 function setup {
+	local tag=${1:-"v6.0.0"}
 	# Setup required env vars for docker compose
-	export GATEWAY_IMAGE=${GATEWAY_IMAGE:-"tykio/tyk:${tag}"}
-	export PLUGIN_COMPILER_IMAGE=${GATEWAY_IMAGE:-"tykio/tyk-plugin-compiler:${tag}"}
+	export GATEWAY_IMAGE=${GATEWAY_IMAGE:-"tykio/tyk-gateway:${tag}"}
+	export PLUGIN_COMPILER_IMAGE=${PLUGIN_COMPILER_IMAGE:-"tykio/tyk-plugin-compiler:${tag}"}
 }
-
-setup
 
 set -eo pipefail
 
-function usage {
-    local progname=$1
-    cat <<EOF
-Usage:
-$progname <tag>
-
-Builds the plugin in testplugin using the supplied tag and tests it in the corresponding gw image.
-Requires docker compose.
-EOF
-    exit 1
-}
-
-[[ -z $1 ]] && usage $0
+setup $1
 
 # if params were not sent, then attempt to get them from env vars
 if [[ $GOOS == "" ]] && [[ $GOARCH == "" ]]; then
@@ -33,12 +20,14 @@ compose='docker-compose'
 # composev2 is a client plugin
 [[ $(docker version --format='{{ .Client.Version }}') =~ "20.10" ]] && compose='docker compose'
 
-export tag=$1
-
 trap "$compose down" EXIT
 
 PLUGIN_SOURCE_PATH=$PWD/testplugin
 rm -fv $PLUGIN_SOURCE_PATH/*.so || true
+
+set -x
+echo "Hi"
+
 docker run --rm -v $PLUGIN_SOURCE_PATH:/plugin-source $PLUGIN_COMPILER_IMAGE testplugin.so
 cp $PLUGIN_SOURCE_PATH/*.so $PLUGIN_SOURCE_PATH/testplugin.so 
 
