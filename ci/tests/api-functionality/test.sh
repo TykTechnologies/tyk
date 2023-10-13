283f2dcc787677608675fc465e57a808f32521f3
Tit Petric <tit@tyk.io>
2023-10-13 09:43:50 +0200

    Clean up smoke tests, fix python, update docs, add taskfile

--- ci/tests/api-functionality/test.sh
+++ ci/tests/api-functionality/test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -eo pipefail
+
 function setup {
 	local tag=${1:-"v6.0.0"}
 	# Setup required env vars for docker compose
 	export GATEWAY_IMAGE=${GATEWAY_IMAGE:-"tykio/tyk-gateway:${tag}"}
-}
 
-set -eo pipefail
+	docker pull -q $GATEWAY_IMAGE
+}
 
 setup $1
 
 trap "docker compose down --remove-orphans" EXIT
 
-set -x
-docker compose up -d --wait --force-recreate
+docker compose up -d --wait --force-recreate || { docker compose logs gw; exit 1; }
 
-./api_test.sh || { $compose logs gw; exit 1; }
+./api_test.sh || { docker compose logs gw; exit 1; }
