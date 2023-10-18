05831f1987f8e990d079b24a87701441dc1c8dc3
Tit Petric <tit.petric@monotek.net>
2023-10-18 12:57:06 +0200

    [TT-10286] plugin compiler local tests (#5635)
    This PR extends the local test capabilities of
    `ci/tests/plugin-compiler`.
    
    It adds a Taskfile.yml to test locally. See README.md for instructions.
    
    It moves the test plugins into `testdata/` to be excluded by go tooling
    when doing builds.
    
    ---------
    
    Co-authored-by: Tit Petric <tit@tyk.io>
    
    (cherry picked from commit e4eb539c99065bc3e4f6351e329e1476263fbdb3)

--- ci/tests/plugin-compiler/test.sh
+++ ci/tests/plugin-compiler/test.sh
@@ -17,10 +17,10 @@ setup $1
 trap "docker compose down --remove-orphans" EXIT
 
 # Clean up loose .so files, rebuild plugin and normalize plugin name.
-PLUGIN_SOURCE_PATH=$PWD/testplugin
+PLUGIN_SOURCE_PATH=$PWD/testdata/test-plugin
 rm -fv $PLUGIN_SOURCE_PATH/*.so || true
-docker run --rm -v $PLUGIN_SOURCE_PATH:/plugin-source $PLUGIN_COMPILER_IMAGE testplugin.so
-cp $PLUGIN_SOURCE_PATH/*.so $PLUGIN_SOURCE_PATH/testplugin.so 
+docker run --rm -v $PLUGIN_SOURCE_PATH:/plugin-source $PLUGIN_COMPILER_IMAGE plugin.so
+cp $PLUGIN_SOURCE_PATH/*.so $PLUGIN_SOURCE_PATH/plugin.so 
 
 docker compose up -d --wait --force-recreate || { docker compose logs gw; exit 1; }
 
