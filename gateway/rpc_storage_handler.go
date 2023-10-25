27e79f7eac16dec09d0b1c4c6cae3cdca255cffc
Leonid Bugaev <leonsbox@gmail.com>
2023-10-25 21:47:35 +0300

    Merging to release-5-lts: TT-10069 when pulling keys from RPC check if we have connectivity (#5643)
    TT-10069 when pulling keys from RPC check if we have connectivity (#5643)
    
    <!-- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    When attempting to pull a key from RPC check if the gateway has
    connectivity, otherwise do not try and return the proper err msg. Also
    when a rpc-key is not pulled we display what kind of key is it: oauth
    client, api key, certificate, it defaults to the word "key"
    
    ## Related Issue
    
    https://tyktech.atlassian.net/browse/TT-10069
    
    ## Motivation and Context
    
    Give solution to https://tyktech.atlassian.net/browse/TT-10069
    
    ## How This Has Been Tested
    
    <!-- Please describe in detail how you tested your changes -->
    <!-- Include details of your testing environment, and the tests -->
    <!-- you ran to see how your change affects other areas of the code,
    etc. -->
    <!-- This information is helpful for reviewers and QA. -->
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test
    coverage to functionality)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply -->
    <!-- If there are no documentation updates required, mark the item as
    checked. -->
    <!-- Raise up any additional concerns not covered by the checklist. -->
    
    - [ ] I ensured that the documentation is up to date
    - [ ] I explained why this PR updates go.mod in detail with reasoning
    why it's required
    - [ ] I would like a code coverage CI quality gate exception and have
    explained why

--- gateway/rpc_storage_handler.go
+++ gateway/rpc_storage_handler.go
@@ -214,6 +214,10 @@ func (r *RPCStorageHandler) GetRawKey(keyName string) (string, error) {
 		}
 	}
 
+	if rpc.IsEmergencyMode() {
+		return "", storage.ErrMDCBConnectionLost
+	}
+
 	value, err := rpc.FuncClientSingleton("GetKey", keyName)
 	if err != nil {
 		rpc.EmitErrorEventKv(
