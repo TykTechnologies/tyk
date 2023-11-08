1eaca15e7f0cc9c4e40b66786514303c4f5a5cb4
Kofo Okesola <okesolakofo@gmail.com>
2023-11-08 10:09:17 +0100

    [TT-9476]:fixed flaky tests (#5753)
    <!-- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!-- Describe your changes in detail -->
    This PR fixes a flaky test added to in the Prs for
    [TT-9476](https://tyktech.atlassian.net/browse/TT-9476)
    
    ## Related Issue
    
    <!-- This project only accepts pull requests related to open issues. -->
    <!-- If suggesting a new feature or change, please discuss it in an
    issue first. -->
    <!-- If fixing a bug, there should be an issue describing it with steps
    to reproduce. -->
    <!-- OSS: Please link to the issue here. Tyk: please create/link the
    JIRA ticket. -->
    
    ## Motivation and Context
    
    <!-- Why is this change required? What problem does it solve? -->
    
    ## How This Has Been Tested
    
    <!-- Please describe in detail how you tested your changes -->
    <!-- Include details of your testing environment, and the tests -->
    <!-- you ran to see how your change affects other areas of the code,
    etc. -->
    <!-- This information is helpful for reviewers and QA. -->
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Refactoring or add test (improvements in base code or adds test
    coverage to functionality)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply -->
    <!-- If there are no documentation updates required, mark the item as
    checked. -->
    <!-- Raise up any additional concerns not covered by the checklist. -->
    
    - [ ] I ensured that the documentation is up to date
    - [ ] I explained why this PR updates go.mod in detail with reasoning
    why it's required
    - [ ] I would like a code coverage CI quality gate exception and have
    explained why
    
    
    [TT-9476]:
    https://tyktech.atlassian.net/browse/TT-9476?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- gateway/handler_success_test.go
+++ gateway/handler_success_test.go
@@ -142,7 +142,7 @@ func TestAnalyticRecord_GraphStats(t *testing.T) {
 			checkFunc: func(t *testing.T, record *analytics.AnalyticsRecord) {
 				assert.True(t, record.GraphQLStats.IsGraphQL)
 				assert.False(t, record.GraphQLStats.HasErrors)
-				assert.Equal(t, []string{"hello", "httpMethod"}, record.GraphQLStats.RootFields)
+				assert.ElementsMatch(t, []string{"hello", "httpMethod"}, record.GraphQLStats.RootFields)
 				assert.Equal(t, map[string][]string{}, record.GraphQLStats.Types)
 				assert.Equal(t, analytics.OperationQuery, record.GraphQLStats.OperationType)
 			},
@@ -178,7 +178,7 @@ func TestAnalyticRecord_GraphStats(t *testing.T) {
 			checkFunc: func(t *testing.T, record *analytics.AnalyticsRecord) {
 				assert.True(t, record.GraphQLStats.IsGraphQL)
 				assert.True(t, record.GraphQLStats.HasErrors)
-				assert.Equal(t, []string{"hello", "httpMethod"}, record.GraphQLStats.RootFields)
+				assert.ElementsMatch(t, []string{"hello", "httpMethod"}, record.GraphQLStats.RootFields)
 				assert.Equal(t, map[string][]string{}, record.GraphQLStats.Types)
 				assert.Equal(t, analytics.OperationQuery, record.GraphQLStats.OperationType)
 				assert.Equal(t, `{"in":"hello"}`, record.GraphQLStats.Variables)
@@ -201,7 +201,7 @@ func TestAnalyticRecord_GraphStats(t *testing.T) {
 			checkFunc: func(t *testing.T, record *analytics.AnalyticsRecord) {
 				assert.True(t, record.GraphQLStats.IsGraphQL)
 				assert.True(t, record.GraphQLStats.HasErrors)
-				assert.Equal(t, []string{"hello", "httpMethod"}, record.GraphQLStats.RootFields)
+				assert.ElementsMatch(t, []string{"hello", "httpMethod"}, record.GraphQLStats.RootFields)
 				assert.Equal(t, map[string][]string{}, record.GraphQLStats.Types)
 				assert.Equal(t, analytics.OperationQuery, record.GraphQLStats.OperationType)
 				assert.Equal(t, `{"in":"hello"}`, record.GraphQLStats.Variables)
