4a06c3ae909bfea860a29a0aa8b5370f4c1aa581
Tit Petric <tit@tyk.io>
2023-12-01 15:42:45 +0100

    Add internal/httputil.EncodeRequest

--- internal/httputil/request.go
+++ internal/httputil/request.go
@@ -1,6 +1,10 @@
 package httputil
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/base64"
+	"net/http"
+)
 
 // TransferEncoding gets the header value from the request.
 func TransferEncoding(req *http.Request) string {
@@ -15,4 +19,13 @@ func TransferEncoding(req *http.Request) string {
 // HasTransferEncoding returns true if a transfer encoding header is present.
 func HasTransferEncoding(req *http.Request) bool {
 	return TransferEncoding(req) != ""
+}
+
+// EncodeRequest will write the request out in wire protocol and return
+// it it as a base64 encoded string.
+func EncodeRequest(r *http.Request) string {
+	var asBytes bytes.Buffer
+	r.Write(&asBytes)
+
+	return base64.StdEncoding.EncodeToString(asBytes.Bytes())
 }
