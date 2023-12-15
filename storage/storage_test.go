d7fd4281442b9fe94e4a94a568d5f77bbfc1052b
tbuchaillot <tombuchaillot89@gmail.com>
2023-12-15 12:05:43 +0100

    adding more tests

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -9,19 +9,24 @@ func Test_TokenOrg(t *testing.T) {
 		expectedResult string
 	}{
 		{
-			name:           "long custom key",
+			name:           "long non-b64 key - without orgId ",
 			givenKey:       "testdata-JJNIsqyZViCvcnbX8ouvG7yctsH1irHa2aklAFYC",
 			expectedResult: "",
 		},
 		{
-			name:           "keyID",
+			name:           "b64 key",
 			givenKey:       "eyJvcmciOiI2NDkyZjY2ZTZlYmJjNTZjNmE2YmYwMjIiLCJpZCI6IjEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU5IiwiaCI6Im11cm11cjY0In0=",
 			expectedResult: "6492f66e6ebbc56c6a6bf022",
 		},
 		{
-			name:           "long key - with org",
+			name:           "long non-b64 key - with orgId",
 			givenKey:       "6492f66e6ebbc56c6a6bf022657c162274933214b91ea570",
 			expectedResult: "6492f66e6ebbc56c6a6bf022",
+		},
+		{
+			name:           "short non-b64 key",
+			givenKey:       "6492f66e6e",
+			expectedResult: "",
 		},
 	}
 
