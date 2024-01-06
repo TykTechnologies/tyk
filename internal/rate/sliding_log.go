41bfbe9486b33bc6cd7b077a60b1a479abf2cf8f
Tit Petric <tit@tyk.io>
2024-01-06 18:38:44 +0100

    Use Client() to decrease allocations, heap escape

--- internal/rate/sliding_log.go
+++ internal/rate/sliding_log.go
@@ -2,6 +2,7 @@ package rate
 
 import (
 	"context"
+	"errors"
 	"strconv"
 	"time"
 
@@ -9,37 +10,61 @@ import (
 )
 
 type SlidingLog struct {
-	conn redis.UniversalClient
+	conn     redis.UniversalClient
+	pipeline bool
 
 	// PipelineFn is exposed for black box tests in the same package.
-	PipelineFn func(context.Context, func(redis.Pipeliner) error) ([]redis.Cmder, error)
+	PipelineFn func(context.Context, func(redis.Pipeliner) error) error
 }
 
 type StorageHandler interface {
-	As(interface{}) error
+	// Client returns the redis.UniversalClient or an error if not available.
+	Client() (redis.UniversalClient, error)
 }
+
+var ErrStorageHandler = errors.New("Client doesn't implement StorageHandler")
 
 // NewSlidingLog creates a new SlidingLog instance with a storage.Handler. In case
 // the storage is offline, it's expected to return nil and an error to handle.
-func NewSlidingLog(cluster StorageHandler, pipeline bool) (*SlidingLog, error) {
-	conn := new(redis.UniversalClient)
-	if err := cluster.As(conn); err != nil {
+func NewSlidingLog(client interface{}, pipeline bool) (*SlidingLog, error) {
+	cluster, ok := client.(StorageHandler)
+	if !ok {
+		return nil, ErrStorageHandler
+	}
+
+	conn, err := cluster.Client()
+	if err != nil {
 		return nil, err
 	}
 
-	return NewSlidingLogRedis(*conn, pipeline), nil
+	return NewSlidingLogRedis(conn, pipeline), nil
 }
 
 // NewSlidingLogRedis creates a new SlidingLog instance with a redis.UniversalClient.
 func NewSlidingLogRedis(conn redis.UniversalClient, pipeline bool) *SlidingLog {
 	r := &SlidingLog{
-		conn: conn,
-	}
-	r.PipelineFn = r.conn.TxPipelined
-	if pipeline {
-		r.PipelineFn = r.conn.Pipelined
+		conn:     conn,
+		pipeline: pipeline,
 	}
 	return r
+}
+
+func (r *SlidingLog) ExecPipeline(ctx context.Context, pipeFn func(redis.Pipeliner) error) error {
+	if r.PipelineFn != nil {
+		return r.PipelineFn(ctx, pipeFn)
+	}
+
+	return r.execPipeline(ctx, pipeFn)
+}
+
+func (r *SlidingLog) execPipeline(ctx context.Context, pipeFn func(redis.Pipeliner) error) error {
+	if r.pipeline {
+		_, err := r.conn.Pipelined(ctx, pipeFn)
+		return err
+	}
+
+	_, err := r.conn.TxPipelined(ctx, pipeFn)
+	return err
 }
 
 // SetCount will trim the rolling window log, add an item and return the count of the items in a window before the add.
@@ -52,19 +77,18 @@ func (r *SlidingLog) SetCount(ctx context.Context, now time.Time, keyName string
 		pipe.ZRemRangeByScore(ctx, keyName, "-inf", strconv.Itoa(int(onePeriodAgo.UnixNano())))
 		res = pipe.ZCard(ctx, keyName)
 
-		element := redis.Z{
-			Score: float64(now.UnixNano()),
+		element := &redis.Z{
+			Score:  float64(now.UnixNano()),
+			Member: strconv.Itoa(int(now.UnixNano())),
 		}
 
-		element.Member = strconv.Itoa(int(now.UnixNano()))
-
-		pipe.ZAdd(ctx, keyName, &element)
+		pipe.ZAdd(ctx, keyName, element)
 		pipe.Expire(ctx, keyName, time.Duration(per)*time.Second)
 
 		return nil
 	}
 
-	if _, err := r.PipelineFn(ctx, pipeFn); err != nil {
+	if err := r.ExecPipeline(ctx, pipeFn); err != nil {
 		return 0, err
 	}
 
@@ -84,7 +108,7 @@ func (r *SlidingLog) GetCount(ctx context.Context, now time.Time, keyName string
 		return nil
 	}
 
-	if _, err := r.PipelineFn(ctx, pipeFn); err != nil {
+	if err := r.ExecPipeline(ctx, pipeFn); err != nil {
 		return 0, err
 	}
 
@@ -101,19 +125,20 @@ func (r *SlidingLog) Set(ctx context.Context, now time.Time, keyName string, per
 		pipe.ZRemRangeByScore(ctx, keyName, "-inf", strconv.Itoa(int(onePeriodAgo.UnixNano())))
 		res = pipe.ZRange(ctx, keyName, 0, -1)
 
-		element := redis.Z{
-			Score: float64(now.UnixNano()),
+		element := &redis.Z{
+			Score:  float64(now.UnixNano()),
+			Member: strconv.Itoa(int(now.UnixNano())),
 		}
 
 		element.Member = strconv.Itoa(int(now.UnixNano()))
 
-		pipe.ZAdd(ctx, keyName, &element)
+		pipe.ZAdd(ctx, keyName, element)
 		pipe.Expire(ctx, keyName, time.Duration(per)*time.Second)
 
 		return nil
 	}
 
-	if _, err := r.PipelineFn(ctx, pipeFn); err != nil {
+	if err := r.ExecPipeline(ctx, pipeFn); err != nil {
 		return nil, err
 	}
 
@@ -133,7 +158,7 @@ func (r *SlidingLog) Get(ctx context.Context, now time.Time, keyName string, per
 		return nil
 	}
 
-	if _, err := r.PipelineFn(ctx, pipeFn); err != nil {
+	if err := r.ExecPipeline(ctx, pipeFn); err != nil {
 		return nil, err
 	}
 
---
+++ internal/rate/sliding_log_test.go
@@ -0,0 +1,204 @@
+package rate_test
+
+import (
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/TykTechnologies/tyk/config"
+	"github.com/TykTechnologies/tyk/internal/rate"
+	"github.com/TykTechnologies/tyk/internal/uuid"
+	"github.com/TykTechnologies/tyk/storage"
+)
+
+// TestRollingWindow_GetCount is an integration test that tests counter behaviour.
+func TestRollingWindow_GetCount(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	conf, err := config.New()
+	assert.NoError(t, err)
+
+	conn := storage.NewRedisClusterPool(false, false, *conf)
+
+	for _, tx := range []bool{true, false} {
+		assertGetCount(ctx, t, conn, tx)
+	}
+}
+
+// TestRollingWindow_Get is an integration test that tests log behaviour.
+func TestRollingWindow_Get(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	conf, err := config.New()
+	assert.NoError(t, err)
+
+	conn := storage.NewRedisClusterPool(false, false, *conf)
+
+	for _, tx := range []bool{true, false} {
+		assertGet(ctx, t, conn, tx)
+	}
+}
+
+// TestRollingWindow_pipelinerError is testing that pipeline errors are returned as expected.
+func TestRollingWindow_pipelinerError(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	conf, err := config.New()
+	assert.NoError(t, err)
+
+	rc := storage.NewRedisController(ctx)
+	go rc.ConnectToRedis(ctx, nil, conf)
+
+	timeout, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	connected := rc.WaitConnect(timeout)
+	if !connected {
+		panic("can't connect to redis '" + conf.Storage.Host + "', timeout")
+	}
+
+	rl, err := rate.NewSlidingLog(&storage.RedisCluster{KeyPrefix: "test-cluster", RedisController: rc}, false)
+	assert.NoError(t, err)
+
+	rl.PipelineFn = func(context.Context, func(redis.Pipeliner) error) error {
+		return io.EOF
+	}
+
+	_, err = rl.Get(ctx, time.Now(), "test", 5)
+	assert.Error(t, err, io.EOF)
+
+	_, err = rl.GetCount(ctx, time.Now(), "test", 5)
+	assert.Error(t, err, io.EOF)
+
+	_, err = rl.Set(ctx, time.Now(), "test", 5)
+	assert.Error(t, err, io.EOF)
+
+	_, err = rl.SetCount(ctx, time.Now(), "test", 5)
+	assert.Error(t, err, io.EOF)
+}
+
+const testRequestCount int = 1000
+
+func assertPipelinerError(ctx context.Context, tb testing.TB, conn redis.UniversalClient, tx bool) {
+	rl := rate.NewSlidingLogRedis(conn, tx)
+
+	key, per := uuid.New(), int64(5)
+
+	for i := 0; i < testRequestCount; i++ {
+		now := time.Now()
+		_, err := rl.SetCount(ctx, now, key, per)
+		assert.NoError(tb, err)
+	}
+
+	now := time.Now()
+	count, err := rl.GetCount(ctx, now, key, per)
+
+	assert.NoError(tb, err)
+	assert.Equal(tb, int64(testRequestCount), count)
+}
+
+func assertGetCount(ctx context.Context, tb testing.TB, conn redis.UniversalClient, tx bool) {
+	rl := rate.NewSlidingLogRedis(conn, tx)
+
+	key, per := uuid.New(), int64(5)
+
+	for i := 0; i < testRequestCount; i++ {
+		now := time.Now()
+		_, err := rl.SetCount(ctx, now, key, per)
+		assert.NoError(tb, err)
+	}
+
+	now := time.Now()
+	count, err := rl.GetCount(ctx, now, key, per)
+
+	assert.NoError(tb, err)
+	assert.Equal(tb, int64(testRequestCount), count)
+}
+
+func assertGet(ctx context.Context, tb testing.TB, conn redis.UniversalClient, tx bool) {
+	rl := rate.NewSlidingLogRedis(conn, tx)
+
+	key, per := uuid.New(), int64(5)
+
+	for i := 0; i < testRequestCount; i++ {
+		now := time.Now()
+		_, err := rl.Set(ctx, now, key, per)
+		assert.NoError(tb, err)
+	}
+
+	now := time.Now()
+	count, err := rl.Get(ctx, now, key, per)
+
+	assert.NoError(tb, err)
+	assert.Len(tb, count, testRequestCount)
+}
+
+func assertNew(ctx context.Context, tb testing.TB, conn redis.UniversalClient, tx bool) {
+	rl := rate.NewSlidingLogRedis(conn, tx)
+	assert.NotNil(tb, rl)
+}
+
+func BenchmarkRollingWindow_New(b *testing.B) {
+	ctx := context.Background()
+
+	conf, err := config.New()
+	assert.NoError(b, err)
+
+	conn := storage.NewRedisClusterPool(false, false, *conf)
+
+	b.ResetTimer()
+
+	b.Run("constructor", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			assertNew(ctx, b, conn, false)
+		}
+	})
+}
+
+func BenchmarkRollingWindow_Count(b *testing.B) {
+	ctx := context.Background()
+
+	conf, err := config.New()
+	assert.NoError(b, err)
+
+	conn := storage.NewRedisClusterPool(false, false, *conf)
+
+	b.ResetTimer()
+
+	b.Run("set/get count pipelined", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			assertGetCount(ctx, b, conn, false)
+		}
+	})
+
+	b.Run("set/get count not pipelined", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			assertGetCount(ctx, b, conn, true)
+		}
+	})
+
+	b.Run("set/get pipelined", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			assertGet(ctx, b, conn, false)
+		}
+	})
+
+	b.Run("set/get not pipelined", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			assertGet(ctx, b, conn, true)
+		}
+	})
+}
