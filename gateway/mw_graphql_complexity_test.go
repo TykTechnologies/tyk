480c0a500426914d011cdbff04132b487c11f4c6
Patric Vormstein <pvormstein@googlemail.com>
2024-01-22 13:20:09 +0100

    remove comments

--- gateway/mw_graphql_complexity_test.go
+++ gateway/mw_graphql_complexity_test.go
@@ -249,13 +249,9 @@ func TestGraphQLComplexityMiddleware_DepthLimitExceeded(t *testing.T) {
 }
 
 func TestGraphQLComplexityMiddleware_ProcessRequest_GraphqlLimits(t *testing.T) {
-	/*countriesSchema, err := graphql.NewSchemaFromString(gqlCountriesSchema)
-	require.NoError(t, err)*/
-
 	apiSpec := BuildAPI(func(spec *APISpec) {
 		spec.GraphQL.Enabled = true
 		spec.GraphQL.Schema = gqlCountriesSchema
-		//spec.GraphQLExecutor.Schema = countriesSchema
 	})[0]
 	m := GraphQLComplexityMiddleware{
 		&BaseMiddleware{
