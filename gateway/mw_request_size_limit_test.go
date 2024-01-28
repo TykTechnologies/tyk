934f7c716070f6cb56e9922a330cca856819ea50
furkansenharputlu <furkan_senharputlu@hotmail.com>
2024-01-28 22:54:57 +0300

    Add test for endpoint level request size limit

--- gateway/mw_request_size_limit_test.go
+++ gateway/mw_request_size_limit_test.go
@@ -9,18 +9,34 @@ import (
 	"github.com/TykTechnologies/tyk/test"
 )
 
-func TestRequestSizeLimitGlobalSizeLimit(t *testing.T) {
+func TestRequestSizeLimit(t *testing.T) {
 	ts := StartTest(nil)
 	defer ts.Close()
 
-	ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
+	api := ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
 		UpdateAPIVersion(spec, "v1", func(v *apidef.VersionInfo) {
 			v.GlobalSizeLimit = 1024
 		})
-	})
+	})[0]
 
 	_, _ = ts.Run(t, []test.TestCase{
 		{Method: "POST", Path: "/sample/", Data: strings.Repeat("a", 1024), Code: http.StatusOK},
 		{Method: "POST", Path: "/sample/", Data: strings.Repeat("a", 1025), Code: http.StatusBadRequest},
 	}...)
+
+	t.Run("endpoint level", func(t *testing.T) {
+		lim := apidef.RequestSizeMeta{Method: http.MethodPost, Path: "/get", SizeLimit: 512}
+
+		UpdateAPIVersion(api, "v1", func(v *apidef.VersionInfo) {
+			v.UseExtendedPaths = true
+			v.ExtendedPaths.SizeLimit = append(v.ExtendedPaths.SizeLimit, lim)
+		})
+
+		ts.Gw.LoadAPI(api)
+
+		_, _ = ts.Run(t, []test.TestCase{
+			{Method: http.MethodPost, Path: "/sample/get", Data: strings.Repeat("a", 512), Code: http.StatusOK},
+			{Method: http.MethodPost, Path: "/sample/get", Data: strings.Repeat("a", 513), Code: http.StatusBadRequest},
+		}...)
+	})
 }
