d5301ae5ce00a4f4d4ece9401fa3f0c864d14674
Yaara <letz.yaara@gmail.com>
2024-02-22 10:10:13 +0000

    token exchange mw

--- apidef/api_definitions.go
+++ apidef/api_definitions.go
@@ -704,7 +704,53 @@ type APIDefinition struct {
 	VersionName string `bson:"-" json:"-"`
 
 	DetailedTracing bool `bson:"detailed_tracing" json:"detailed_tracing"`
+
+	// For Toekn Exchange see in the struct below
+	TokenExchangeOptions TokenExchangeOptions `token_exchange_options:"token_exchange_options" bson:"token_exchange_options" json:"token_exchange_options"`
 }
+
+type ActorTokenType string //change to enum or const?
+
+// REQUIRED. The value urn:ietf:params:oauth:grant-type:token-exchange indicates
+// that a token exchange is being performed.
+const GrantType = "urn:ietf:params:oauth:grant-type:token-exchange"
+
+// For Toekn Exchange RFC 8693 https://www.rfc-editor.org/rfc/rfc8693.html
+type TokenExchangeOptions struct {
+	Enable       bool   `bson:"enable json:"enable,omitempty"`
+	ClientID     string `bson:"client_id" json:"client_id,omitempty"`
+	ClientSecret string `bson:"client_secret" json:"client_secret,omitempty"`
+
+	// REQUIRED. A security token that represents the identity of the party on behalf
+	// of whom the request is being made
+	SubjectToken string `bson:"subject_token" json:"subject_token,omitempty"`
+
+	// REQUIRED. An identifier, as described in Section 3, that indicates the type of 
+	// the security token in the subject_token parameter.
+	SubjectTokenType string `bson:"subject_token_type" json:"subject_token_type,omitempty"`
+
+	//GrantType    string `bson:"grant_type" json:"grant_type,omitempty"`
+	Audience     string `bson:"audience" json:"audience,omitempty"`
+
+	// OPTIONAL. A URI that indicates the target service or resource where the
+	//client intends to use the requested security token.
+	// Format: Its value MUST be an absolute URI, as specified by Section 4.3
+	// of [RFC3986].  The URI MUST NOT include a fragment component or a query component.
+	Resource string `bson:"resource" json:"resource,omitempty"`
+
+	// OPTIONAL. A list of space-delimited, case-sensitive strings.  The strings
+	Scope string `bson:"scope" json:"scope,omitempty"`
+
+	// OPTIONAL. A security token that represents the identity of the acting party. Typically, 
+	// this will be the party that is authorized to use the requested security token and act on behalf of the subject.
+	ActorToken string `bson:"actor_token" json:"actor_token,omitempty"`
+
+	// An identifier, as described in Section 3, that indicates the type of the security token
+	// in the actor_token parameter. This is REQUIRED when the actor_token parameter is present
+	// in the request but MUST NOT be included otherwise.
+	ActorTokenTYpe ActorTokenType `bson:"actor_token_type" json:"actor_token_type,omitempty"`
+}
+
 
 type AnalyticsPluginConfig struct {
 	Enabled    bool   `bson:"enable" json:"enable,omitempty"`
