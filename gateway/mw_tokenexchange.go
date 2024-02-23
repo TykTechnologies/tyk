ce5e24548ec8a9f4c53e945b97ea8e440bfcf58b
asoorm <ahmet@mangomm.co.uk>
2024-02-23 13:01:15 +0000

    MVP token exchange

--- gateway/mw_tokenexchange.go
+++ gateway/mw_tokenexchange.go
@@ -1,10 +1,52 @@
 package gateway
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
+
+type TokenExchangeRequest struct {
+	ClientID         string `json:"client_id"`
+	ClientSecret     string `json:"client_secret"`
+	GrantType        string `bson:"grant_type" json:"grant_type"`
+	SubjectToken     string `json:"subject_token,omitempty"`
+	SubjectTokenType string `bson:"subject_token_type" json:"subject_token_type,omitempty"`
+	//GrantType string `bson:"grant_type" json:"grant_type,omitempty"`
+	//Audience  string `bson:"audience" json:"audience,omitempty"`
+
+	// OPTIONAL. A URI that indicates the target service or resource where the
+	//client intends to use the requested security token.
+	// Format: Its value MUST be an absolute URI, as specified by Section 4.3
+	// of [RFC3986].  The URI MUST NOT include a fragment component or a query component.
+	//Resource string `bson:"resource" json:"resource,omitempty"`
+
+	// OPTIONAL. A list of space-delimited, case-sensitive strings.  The strings
+	Scope string `bson:"scope" json:"scope,omitempty"`
+
+	// OPTIONAL. A security token that represents the identity of the acting party. Typically,
+	// this will be the party that is authorized to use the requested security token and act on behalf of the subject.
+	//ActorToken string `bson:"actor_token" json:"actor_token,omitempty"`
+
+	// An identifier, as described in Section 3, that indicates the type of the security token
+	// in the actor_token parameter. This is REQUIRED when the actor_token parameter is present
+	// in the request but MUST NOT be included otherwise.
+	//ActorTokenTYpe ActorTokenType `bson:"actor_token_type" json:"actor_token_type,omitempty"`
+}
+
+type TokenExchangeResponse struct {
+	AccessToken      string `json:"access_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	TokenType        string `json:"token_type"`
+	NotBeforePolicy  int    `json:"not-before-policy"`
+	SessionState     string `json:"session_state"`
+	Scope            string `json:"scope"`
+}
 
 type TokenExchangeMW struct {
 	*BaseMiddleware
@@ -18,74 +60,55 @@ func (k *TokenExchangeMW) EnabledForSpec() bool {
 	return k.Spec.TokenExchangeOptions.Enable
 }
 
+const (
+	// TokenExchangeGrantType is the grant type for token exchange
+	TokenExchangeGrantType = "urn:ietf:params:oauth:grant-type:token-exchange"
+)
+
 func (k *TokenExchangeMW) ProcessRequest(w http.ResponseWriter, r *http.Request, _ interface{}) (error, int) {
 	if ctxGetRequestStatus(r) == StatusOkAndIgnore {
 		return nil, http.StatusOK
 	}
-	fmt.Println("TokenExchangeMW.ProcessRequest")
-	// logger := k.Logger()
-	// var tykId string
 
-	// rawJWT, config := k.getAuthToken(k.getAuthType(), r)
+	subjectToken := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
 
-	// if rawJWT == "" {
-	// 	// No header value, fail
-	// 	logger.Info("Attempted access with malformed header, no JWT auth header found.")
+	ter := TokenExchangeRequest{
+		ClientID:     k.Spec.TokenExchangeOptions.ClientID,
+		ClientSecret: k.Spec.TokenExchangeOptions.ClientSecret,
+		SubjectToken: subjectToken,
+		GrantType:    TokenExchangeGrantType,
+	}
 
-	// 	log.Debug("Looked in: ", config.AuthHeaderName)
-	// 	log.Debug("Raw data was: ", rawJWT)
-	// 	log.Debug("Headers are: ", r.Header)
+	params := url.Values{}
+	params.Set("client_id", ter.ClientID)
+	params.Set("client_secret", ter.ClientSecret)
+	params.Set("subject_token", ter.SubjectToken)
+	params.Set("grant_type", ter.GrantType)
 
-	// 	k.reportLoginFailure(tykId, r)
-	// 	return errors.New("Authorization field missing"), http.StatusBadRequest
-	// }
+	req, err := http.NewRequest(http.MethodPost, k.Spec.TokenExchangeOptions.TokenEndpoint, strings.NewReader(params.Encode()))
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	// // enable bearer token format
-	// rawJWT = stripBearer(rawJWT)
+	c := &http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
 
-	// // Use own validation logic, see below
-	// parser := jwt.NewParser(jwt.WithoutClaimsValidation())
+	defer res.Body.Close()
 
-	// // Verify the token
-	// token, err := parser.Parse(rawJWT, func(token *jwt.Token) (interface{}, error) {
-	// 	// Don't forget to validate the alg is what you expect:
-	// 	if err := assertSigningMethod(k.Spec.JWTSigningMethod, token); err != nil {
-	// 		return nil, err
-	// 	}
+	if res.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(res.Body)
+		log.Error("token exchange failed: %s", string(bodyBytes))
+		return fmt.Errorf("token exchange failed: %s", res.Status), http.StatusInternalServerError
+	}
 
-	// 	val, err := k.getSecretToVerifySignature(r, token)
-	// 	if err != nil {
-	// 		k.Logger().WithError(err).Error("Couldn't get token")
-	// 		return nil, err
-	// 	}
+	tokenResponse := TokenExchangeResponse{}
+	json.NewDecoder(res.Body).Decode(&tokenResponse)
 
-	// 	return parseJWTKey(k.Spec.JWTSigningMethod, val)
-	// })
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", tokenResponse.AccessToken))
 
-	// if err == nil && token.Valid {
-	// 	if jwtErr := k.timeValidateJWTClaims(token.Claims.(jwt.MapClaims)); jwtErr != nil {
-	// 		return errors.New("Key not authorized: " + jwtErr.Error()), http.StatusUnauthorized
-	// 	}
-
-	// 	// Token is valid - let's move on
-
-	// 	// Are we mapping to a central JWT Secret?
-	// 	if k.Spec.JWTSource != "" {
-	// 		return k.processCentralisedJWT(r, token)
-	// 	}
-
-	// 	// No, let's try one-to-one mapping
-	// 	return k.processOneToOneTokenMap(r, token)
-	// }
-
-	// logger.Info("Attempted JWT access with non-existent key.")
-	// k.reportLoginFailure(tykId, r)
-	// if err != nil {
-	// 	logger.WithError(err).Error("JWT validation error")
-	// 	errorDetails := strings.Split(err.Error(), ":")
-	// 	if errorDetails[0] == UnexpectedSigningMethod {
-	// 		return errors.New(MsgKeyNotAuthorizedUnexpectedSigningMethod), http.StatusForbidden
-	// 	}
-	// }
-	return errors.New("Key not authorized"), http.StatusForbidden
+	return nil, http.StatusOK
 }
