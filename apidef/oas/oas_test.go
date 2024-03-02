72a541fdd02e2d395ea29dbe36bdd19802ca3088
sredny buitrago <sredny.buitrago@gmail.com>
2024-03-02 15:14:39 -0300

    Revert "Merging to release-5.2: [TT-10358] Add fallback to default version OAS support (#5679)"
    This reverts commit d11a5c5fd2f6571ca01bcea6004c15a07fdf4634.

--- apidef/oas/oas_test.go
+++ apidef/oas/oas_test.go
@@ -188,6 +188,7 @@ func TestOAS_ExtractTo_ResetAPIDefinition(t *testing.T) {
 		"APIDefinition.RequestSigning.HeaderList[0]",
 		"APIDefinition.RequestSigning.CertificateId",
 		"APIDefinition.RequestSigning.SignatureHeader",
+		"APIDefinition.VersionDefinition.FallbackToDefault",
 		"APIDefinition.VersionData.Versions[0].ExtendedPaths.TransformJQ[0].Filter",
 		"APIDefinition.VersionData.Versions[0].ExtendedPaths.TransformJQ[0].Path",
 		"APIDefinition.VersionData.Versions[0].ExtendedPaths.TransformJQ[0].Method",
