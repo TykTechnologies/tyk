d2c526870517bfbf04e4a26dbf54460681b823f8
jeff <jeffy.mathew100@gmail.com>
2024-03-12 11:47:44 +0100

    remove python plugin smoke tests

--- ci/tests/python-plugins/src/middleware.py
+++
@@ -1,7 +0,0 @@
-from tyk.decorators import *
-from gateway import TykGateway as tyk
-
-@Hook
-def MyFooBarHeader(request, session, metadata, spec):
-  request.add_header('Foo', 'Bar')
-  return request, session, metadata
