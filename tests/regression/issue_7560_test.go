67087cbc5753d25db2c72523febafc77987ca451
Tit Petric <tit@tyk.io>
2024-04-04 16:04:51 +0200

    Add negative test cases

--- tests/regression/issue_7560_test.go
+++ tests/regression/issue_7560_test.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/TykTechnologies/tyk/config"
 	"github.com/TykTechnologies/tyk/gateway"
 	"github.com/TykTechnologies/tyk/test"
@@ -14,27 +12,87 @@ import (
 func Test_Issue7560(t *testing.T) {
 	bundleID := "issue-7560-bundle.zip"
 
-	ts := gateway.StartTest(func(c *config.Config) {
-		c.PublicKeyPath = "testdata/issue-7560-server.pub"
-		c.BundleBaseURL = "file://testdata/"
-	})
-	defer ts.Close()
+	t.Run("Signed bundle should load", func(t *testing.T) {
+		ts := gateway.StartTest(func(c *config.Config) {
+			c.PublicKeyPath = "testdata/issue-7560-server.pub"
+			c.BundleBaseURL = "file://testdata/"
+		})
+		defer ts.Close()
 
-	t.Run("Simple bundle base URL", func(t *testing.T) {
 		specs := ts.Gw.BuildAndLoadAPI(func(spec *gateway.APISpec) {
 			spec.CustomMiddlewareBundle = bundleID
 			spec.UseKeylessAccess = true
 			spec.Proxy.ListenPath = "/test/"
 		})
 		spec := specs[0]
+		_ = spec
 
-		bundle, err := ts.Gw.FetchBundle(spec)
-
-		assert.NotNil(t, bundle)
-		assert.NoError(t, err)
+		//		bundle, err := ts.Gw.FetchBundle(spec)
+		//		assert.NotNil(t, bundle)
+		//		assert.NoError(t, err)
 
 		ts.Run(t, []test.TestCase{
 			{Path: "/test/", Code: http.StatusOK, BodyMatch: `New Request body`},
+		}...)
+	})
+
+	t.Run("Invalid bundle signature should error", func(t *testing.T) {
+		ts := gateway.StartTest(func(c *config.Config) {
+			c.PublicKeyPath = "testdata/issue-7560-server-invalid.pub"
+			c.BundleBaseURL = "file://testdata/"
+		})
+		defer ts.Close()
+
+		specs := ts.Gw.BuildAndLoadAPI(func(spec *gateway.APISpec) {
+			spec.CustomMiddlewareBundle = bundleID
+			spec.UseKeylessAccess = true
+			spec.Proxy.ListenPath = "/test/"
+		})
+		spec := specs[0]
+		_ = spec
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/", Code: http.StatusInternalServerError},
+		}...)
+	})
+
+	t.Run("Signed bundle but no public key should error", func(t *testing.T) {
+		ts := gateway.StartTest(func(c *config.Config) {
+			c.PublicKeyPath = "testdata/issue-7560-server-invalid.pub"
+			c.BundleBaseURL = "file://testdata/"
+		})
+		defer ts.Close()
+
+		specs := ts.Gw.BuildAndLoadAPI(func(spec *gateway.APISpec) {
+			spec.CustomMiddlewareBundle = bundleID
+			spec.UseKeylessAccess = true
+			spec.Proxy.ListenPath = "/test/"
+		})
+		spec := specs[0]
+		_ = spec
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/", Code: http.StatusInternalServerError},
+		}...)
+	})
+
+	t.Run("Full bundle from api spec should error", func(t *testing.T) {
+		ts := gateway.StartTest(func(c *config.Config) {
+			c.PublicKeyPath = "testdata/issue-7560-server.pub"
+			c.BundleBaseURL = ""
+		})
+		defer ts.Close()
+
+		specs := ts.Gw.BuildAndLoadAPI(func(spec *gateway.APISpec) {
+			spec.CustomMiddlewareBundle = "file://testdata/" + bundleID
+			spec.UseKeylessAccess = true
+			spec.Proxy.ListenPath = "/test/"
+		})
+		spec := specs[0]
+		_ = spec
+
+		ts.Run(t, []test.TestCase{
+			{Path: "/test/", Code: http.StatusInternalServerError},
 		}...)
 	})
 }
