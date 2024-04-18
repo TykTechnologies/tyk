157795bd22371a2baacea02bc28fda49de5f3134
sredny buitrago <sredny.buitrago@gmail.com>
2024-04-18 12:26:46 -0400

    print keys in sync

--- gateway/rpc_storage_handler.go
+++ gateway/rpc_storage_handler.go
@@ -1092,6 +1092,7 @@ func (r *RPCStorageHandler) ProcessKeySpaceChanges(keys []string, orgId string) 
 	for _, key := range keys {
 		_, isOauthTokenKey := notRegularKeys[key]
 		if !isOauthTokenKey {
+			log.Infof("\n Key: %v", key)
 			splitKeys := strings.Split(key, ":")
 			_, resetQuota := keysToReset[splitKeys[0]]
 
