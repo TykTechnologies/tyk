d99a5f7903addd44ebac40e37c7306951679e7b7
Andrei <andrei@tyk.io>
2024-05-10 10:41:06 +0300

    TT-11566, fixed unit tests that were using recently disabled cyphers for happy path

--- gateway/cert_go1.10_test.go
+++ gateway/cert_go1.10_test.go
@@ -258,7 +258,7 @@ func TestProxyTransport(t *testing.T) {
 		// force creating new transport on each reque
 		globalConf.MaxConnTime = -1
 
-		globalConf.ProxySSLCipherSuites = []string{"TLS_RSA_WITH_AES_128_CBC_SHA"}
+		globalConf.ProxySSLCipherSuites = []string{"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"}
 		ts.Gw.SetConfig(globalConf)
 		ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
 			spec.Proxy.ListenPath = "/"
@@ -293,7 +293,7 @@ func TestProxyTransport(t *testing.T) {
 		ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
 			spec.Proxy.ListenPath = "/"
 			spec.Proxy.TargetURL = upstream.URL
-			spec.Proxy.Transport.SSLCipherSuites = []string{"TLS_RSA_WITH_AES_128_CBC_SHA"}
+			spec.Proxy.Transport.SSLCipherSuites = []string{"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"}
 		})
 
 		ts.Run(t, test.TestCase{Path: "/", Code: 200})
@@ -327,7 +327,7 @@ func TestProxyTransport(t *testing.T) {
 		ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
 			spec.Proxy.ListenPath = "/"
 			spec.Proxy.TargetURL = upstream.URL
-			spec.Proxy.Transport.SSLCipherSuites = []string{"TLS_RSA_WITH_AES_128_CBC_SHA"}
+			spec.Proxy.Transport.SSLCipherSuites = []string{"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"}
 			// Invalid proxy
 			spec.Proxy.Transport.ProxyURL = upstream.URL
 		})
@@ -358,7 +358,7 @@ func TestProxyTransport(t *testing.T) {
 
 		ts.Gw.BuildAndLoadAPI(func(spec *APISpec) {
 			spec.Proxy.ListenPath = "/"
-			spec.Proxy.Transport.SSLCipherSuites = []string{"TLS_RSA_WITH_AES_128_CBC_SHA"}
+			spec.Proxy.Transport.SSLCipherSuites = []string{"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"}
 			spec.Proxy.Transport.ProxyURL = proxy.URL
 		})
 
