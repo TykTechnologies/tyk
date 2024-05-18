a0832b994dc5a657274ca9e3e4f5f10bc3a9ccba
Leonid Bugaev <leonsbox@gmail.com>
2024-05-18 20:46:42 +0300

    Streaming POC
    Based on https://github.com/TykTechnologies/tyk/pull/6288

--- gateway/testutil.go
+++ gateway/testutil.go
@@ -1301,6 +1301,10 @@ func (s *Test) Close() {
 	s.Gw.ReloadTestCase.StopTicker()
 	s.Gw.GlobalHostChecker.StopPoller()
 
+	if s.Gw.StreamingServer != nil {
+		s.Gw.StreamingServer.Reset()
+	}
+
 	err = s.RemoveApis()
 	if err != nil {
 		log.Error("could not remove apis")
