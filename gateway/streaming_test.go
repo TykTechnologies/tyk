517d90eab2fee4d564bf1f67cfbaeb8438876995
Leonid Bugaev <leonsbox@gmail.com>
2024-05-20 08:34:05 +0300

    Add TYk integration

--- gateway/streaming_test.go
+++ gateway/streaming_test.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/gorilla/websocket"
 	"gopkg.in/yaml.v2"
 
 	"github.com/TykTechnologies/tyk/apidef/oas"
@@ -168,5 +169,127 @@ streams:
 		if line != "hello world" {
 			t.Fatalf("Expected 'hello world', got '%s'", line)
 		}
+	}
+}
+
+func TestAsyncAPIHttp(t *testing.T) {
+	ts := StartTest(func(globalConf *config.Config) {
+		globalConf.Streaming.Enabled = true
+	})
+
+	defer ts.Close()
+
+	const (
+		oasAPIID      = "oas-api-id"
+		oasBasePath   = "/tyk/apis/oas"
+		messageToSend = "hello websocket"
+	)
+
+	streamingConfig := `
+streams:
+  test:
+    input:
+      http_server:
+        path: /post
+
+    output:
+      http_server:
+        path: /get`
+
+	streamingConfigJSON, err := ConvertYAMLToJSON([]byte(streamingConfig))
+	if err != nil {
+		t.Fatalf("Failed to convert YAML to JSON: %v", err)
+	}
+
+	var parsedStreamingConfig map[string]interface{}
+	if err := json.Unmarshal(streamingConfigJSON, &parsedStreamingConfig); err != nil {
+		t.Fatalf("Failed to parse JSON: %v", err)
+	}
+
+	tykExtension := oas.XTykAPIGateway{
+		Info: oas.Info{
+			Name: "oas api",
+			ID:   oasAPIID,
+			State: oas.State{
+				Active: true,
+			},
+		},
+		Upstream: oas.Upstream{
+			URL: TestHttpAny,
+		},
+		Server: oas.Server{
+			// Authentication: &oas.Authentication{
+			// 	Enabled: true,
+			// },
+			ListenPath: oas.ListenPath{
+				Value: "/streaming-api/",
+				Strip: false,
+			},
+		},
+	}
+
+	oasAPI := openapi3.T{
+		OpenAPI: "3.0.3",
+		Info: &openapi3.Info{
+			Title:   "oas doc",
+			Version: "1",
+		},
+		Paths: make(openapi3.Paths),
+	}
+
+	oasAPI.Extensions = map[string]interface{}{
+		oas.ExtensionTykStreaming:  parsedStreamingConfig,
+		oas.ExtensionTykAPIGateway: tykExtension,
+	}
+
+	// Create OAS API
+	_, _ = ts.Run(t, test.TestCase{AdminAuth: true, Method: http.MethodPost, Path: oasBasePath, Data: &oasAPI,
+		BodyMatch: `"action":"added"`, Code: http.StatusOK})
+
+	ts.Gw.DoReload()
+
+	streams := ts.Gw.StreamingServer.Streams()
+
+	if len(streams) != 1 {
+		t.Fatalf("Expected 1 stream, got %d", len(streams))
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	// Create a websocket client
+	dialer := websocket.Dialer{}
+	wsURL := "ws" + strings.TrimPrefix(ts.URL, "http") + "/streaming-api/get/ws"
+	wsConn, _, err := dialer.Dial(wsURL, nil)
+	if err != nil {
+		t.Fatalf("Failed to connect to websocket: %v", err)
+	}
+	defer wsConn.Close()
+
+	// Send message to HTTP input
+	httpClient := &http.Client{}
+	reqBody := strings.NewReader(messageToSend)
+	req, err := http.NewRequest(http.MethodPost, ts.URL+"/streaming-api/post", reqBody)
+	if err != nil {
+		t.Fatalf("Failed to create new HTTP request: %v", err)
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to send message to /post: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code 200, got %d", resp.StatusCode)
+	}
+
+	// Read message from websocket
+	_, p, err := wsConn.ReadMessage()
+	if err != nil {
+		t.Fatalf("Failed to read message from websocket: %v", err)
+	}
+
+	receivedMessage := string(p)
+	if receivedMessage != messageToSend {
+		t.Fatalf("Expected message '%s', got '%s'", messageToSend, receivedMessage)
 	}
 }
