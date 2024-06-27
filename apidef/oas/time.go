df7f90f9da34862db9dbc41887f8db45dc712f4a
Andrei <andrei@tyk.io>
2024-06-27 11:21:24 +0300

    TT-12306, removed aliases from autogenerated godocs and from being examined by golangci-lint part 2

--- apidef/oas/time.go
+++ apidef/oas/time.go
@@ -3,4 +3,5 @@ package oas
 import "github.com/TykTechnologies/tyk/internal/time"
 
 // ReadableDuration is an alias maintained to be used in imports.
+// nolint
 type ReadableDuration = time.ReadableDuration
