bfe525e75a50026fe191c98ea443bb8d1ef17e04
Tit Petric <tit@tyk.io>
2024-07-10 14:22:26 +0200

    Add the benchmark taskfile, use optimized version in json

--- log/log_test.go
+++ log/log_test.go
@@ -4,21 +4,31 @@ import (
 	"errors"
 	"io"
 	"testing"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	// json-logrus is added for benchmarks
+	formatterIndex["json-logrus"] = func() logrus.Formatter {
+		return &logrus.JSONFormatter{
+			TimestampFormat: time.RFC3339,
+		}
+	}
+}
 
 func TestNewFormatter(t *testing.T) {
 	textFormatter, ok := NewFormatter("").(*logrus.TextFormatter)
 	assert.NotNil(t, textFormatter)
 	assert.True(t, ok)
 
-	jsonFormatter, ok := NewFormatter("json").(*logrus.JSONFormatter)
+	jsonFormatter, ok := NewFormatter("json").(*JSONFormatter)
 	assert.NotNil(t, jsonFormatter)
 	assert.True(t, ok)
 
-	jsonExtFormatter, ok := NewFormatter("json-ext").(*JSONFormatter)
+	jsonExtFormatter, ok := NewFormatter("json-logrus").(*logrus.JSONFormatter)
 	assert.NotNil(t, jsonExtFormatter)
 	assert.True(t, ok)
 }
@@ -33,8 +43,8 @@ func BenchmarkFormatter(b *testing.B) {
 	b.Run("json", func(b *testing.B) {
 		benchmarkFormatter(b, NewFormatter("json"))
 	})
-	b.Run("json-ext", func(b *testing.B) {
-		benchmarkFormatter(b, NewFormatter("json-ext"))
+	b.Run("json-logrus", func(b *testing.B) {
+		benchmarkFormatter(b, NewFormatter("json-logrus"))
 	})
 	b.Run("default", func(b *testing.B) {
 		benchmarkFormatter(b, NewFormatter(""))
