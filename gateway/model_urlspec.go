9b04ac9ea1d978ef848395b41fdf15a05eb460a2
Tit Petric <tit.petric@monotek.net>
2024-07-15 13:54:11 +0200

    [TT-12618] Refactor/checkspec findspec (#6415)
    ### **User description**
    Adds a type safe function to get an *URL spec for the request;
    
    https://tyktech.atlassian.net/browse/TT-12618
    ___
    
    ### **PR Type**
    Enhancement, Other
    
    
    ___
    
    ### **Description**
    - Removed the `CheckSpecMatchesStatus` function from
    `api_definition.go`.
    - Added a new `CheckSpecMatchesStatus` function with a deprecation
    notice in `model_apispec.go`.
    - Introduced the `FindSpecMatchesStatus` function to replace the
    deprecated function.
    - Added helper functions `getMatchPathAndMethod`, `modeSpecificSpec`,
    and `matchesMethod` to modularize the code.
    
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>api_definition.go</strong><dd><code>Remove
    `CheckSpecMatchesStatus` function from `APISpec`</code>&nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    gateway/api_definition.go
    
    - Removed the `CheckSpecMatchesStatus` function.
    
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/TykTechnologies/tyk/pull/6415/files#diff-0cf80174bbafb36f6d4f4308ebbd971b2833b76a936bad568220aa1a4ba0ee8b">+0/-118</a>&nbsp;
    </td>
    
    </tr>                    
    
    <tr>
      <td>
        <details>
    <summary><strong>model_apispec.go</strong><dd><code>Add
    `FindSpecMatchesStatus` and helper functions</code>&nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    gateway/model_apispec.go
    
    <li>Added <code>CheckSpecMatchesStatus</code> function with deprecation
    notice.<br> <li> Added <code>FindSpecMatchesStatus</code> function.<br>
    <li> Added <code>getMatchPathAndMethod</code> helper function.<br>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/TykTechnologies/tyk/pull/6415/files#diff-80c49b9bdb411a3d5a4706ec3ff138ef44154d0306040c19eba1cb5559f199d6">+77/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>                    
    
    <tr>
      <td>
        <details>
    <summary><strong>model_urlspec.go</strong><dd><code>Add helper functions
    for URLSpec status matching</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    gateway/model_urlspec.go
    
    <li>Added <code>modeSpecificSpec</code> function.<br> <li> Added
    <code>matchesMethod</code> function.<br>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/TykTechnologies/tyk/pull/6415/files#diff-ed766dd1814a557a1943cd3483c6ef511ad1b8febc7bd59982792d0ab7e23cff">+100/-0</a>&nbsp;
    </td>
    
    </tr>                    
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > 💡 **PR-Agent usage**:
    >Comment `/help` on the PR to get a list of all available PR-Agent tools
    and their descriptions
    
    ---------
    
    Co-authored-by: Tit Petric <tit@tyk.io>

---
+++ gateway/model_urlspec.go
@@ -0,0 +1,98 @@
+package gateway
+
+// modeSpecificSpec returns the respective field of URLSpec if it matches the given mode.
+// Deprecated: Usage should not increase.
+func (u *URLSpec) modeSpecificSpec(mode URLStatus) (interface{}, bool) {
+	switch mode {
+	case Ignored, BlackList, WhiteList:
+		return nil, true
+	case Cached:
+		return &u.CacheConfig, true
+	case Transformed:
+		return &u.TransformAction, true
+	case TransformedJQ:
+		return &u.TransformJQAction, true
+	case HeaderInjected:
+		return &u.InjectHeaders, true
+	case HeaderInjectedResponse:
+		return &u.InjectHeadersResponse, true
+	case TransformedResponse:
+		return &u.TransformResponseAction, true
+	case TransformedJQResponse:
+		return &u.TransformJQResponseAction, true
+	case HardTimeout:
+		return &u.HardTimeout.TimeOut, true
+	case CircuitBreaker:
+		return &u.CircuitBreaker, true
+	case URLRewrite:
+		return u.URLRewrite, true
+	case VirtualPath:
+		return &u.VirtualPathSpec, true
+	case RequestSizeLimit:
+		return &u.RequestSize, true
+	case MethodTransformed:
+		return &u.MethodTransform, true
+	case RequestTracked:
+		return &u.TrackEndpoint, true
+	case RequestNotTracked:
+		return &u.DoNotTrackEndpoint, true
+	case ValidateJSONRequest:
+		return &u.ValidatePathMeta, true
+	case Internal:
+		return &u.Internal, true
+	case GoPlugin:
+		return &u.GoPluginMeta, true
+	case PersistGraphQL:
+		return &u.PersistGraphQL, true
+	default:
+		return nil, false
+	}
+}
+
+// matchesMethod checks if the given method matches the method required by the URLSpec for the current status.
+func (u *URLSpec) matchesMethod(method string) bool {
+	switch u.Status {
+	case Ignored, BlackList, WhiteList:
+		return true
+	case Cached:
+		return method == u.CacheConfig.Method || (u.CacheConfig.Method == SAFE_METHODS && isSafeMethod(method))
+	case Transformed:
+		return method == u.TransformAction.Method
+	case TransformedJQ:
+		return method == u.TransformJQAction.Method
+	case HeaderInjected:
+		return method == u.InjectHeaders.Method
+	case HeaderInjectedResponse:
+		return method == u.InjectHeadersResponse.Method
+	case TransformedResponse:
+		return method == u.TransformResponseAction.Method
+	case TransformedJQResponse:
+		return method == u.TransformJQResponseAction.Method
+	case HardTimeout:
+		return method == u.HardTimeout.Method
+	case CircuitBreaker:
+		return method == u.CircuitBreaker.Method
+	case URLRewrite:
+		return method == u.URLRewrite.Method
+	case VirtualPath:
+		return method == u.VirtualPathSpec.Method
+	case RequestSizeLimit:
+		return method == u.RequestSize.Method
+	case MethodTransformed:
+		return method == u.MethodTransform.Method
+	case RequestTracked:
+		return method == u.TrackEndpoint.Method
+	case RequestNotTracked:
+		return method == u.DoNotTrackEndpoint.Method
+	case ValidateJSONRequest:
+		return method == u.ValidatePathMeta.Method
+	case Internal:
+		return method == u.Internal.Method
+	case GoPlugin:
+		return method == u.GoPluginMeta.Meta.Method
+	case PersistGraphQL:
+		return method == u.PersistGraphQL.Method
+	default:
+		return false
+	}
+}
