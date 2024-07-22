9e4f54d2c15b0909c9be118191237e88c255ae94
Tit Petric <tit@tyk.io>
2024-07-22 20:40:29 +0200

    Add godoc

--- internal/oasutil/paths.go
+++ internal/oasutil/paths.go
@@ -7,14 +7,23 @@ import (
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
+// PathItem holds the path to a particular OAS path item.
 type PathItem struct {
+	// PathItem represents an openapi3.Paths value.
 	*openapi3.PathItem
 
+	// Path is an openapi3.Paths key, the endpoint URL.
 	Path string
 }
 
 var pathParamRegex = regexp.MustCompile(`\{[^}]+\}`)
 
+// SortByPathLength decomposes an openapi3.Paths to a sorted []PathItem.
+// The sorting takes the length of the paths into account, as well as
+// path parameters, sorting them by length descending, and ordering
+// path parameters after the statically defined paths.
+//
+// Check the test function for sorting expectations.
 func SortByPathLength(in openapi3.Paths) []PathItem {
 	// get urls
 	paths := []string{}
