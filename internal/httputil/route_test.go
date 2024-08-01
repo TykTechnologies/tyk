bae2cbc254a72bb526a6ce59e0bb9a326b4b7e49
Tit Petric <tit@tyk.io>
2024-08-01 13:12:44 +0200

    Adjust path regexp to ones from mux

--- internal/httputil/route_test.go
+++ internal/httputil/route_test.go
@@ -7,15 +7,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRouteRegexString(t *testing.T) {
-	assert.Equal(t, "^/users/.*$", RouteRegexString("/users/{id}"))
-	assert.Equal(t, "^/users/.*/profile/[a-zA-Z]+$", RouteRegexString("/users/{id}/profile/{type:[a-zA-Z]+}"))
-	assert.Equal(t, "^/static/.*/assets/.*$", RouteRegexString("/static/{path}/assets/{file}"))
-	assert.Equal(t, "^/items/[0-9]+/details/.*$", RouteRegexString("/items/{itemID:[0-9]+}/details/{detail}"))
+func testPathRegexp(tb testing.TB, in string) string {
+	tb.Helper()
+
+	res, err := GetPathRegexp(in)
+	assert.NoError(tb, err)
+	return res
 }
 
-func TestRouteRegexStringWithRegexCompile(t *testing.T) {
-	pattern := RouteRegexString("/api/v1/users/{userId}/roles/{roleId}")
+func TestGetPathRegexp(t *testing.T) {
+	assert.Equal(t, "^/users/(?P<v0>[^/]+)", testPathRegexp(t, "/users/{id}"))
+	assert.Equal(t, "^/users/(?P<v0>[^/]+)/profile/(?P<v1>[a-zA-Z]+)", testPathRegexp(t, "/users/{id}/profile/{type:[a-zA-Z]+}"))
+	assert.Equal(t, "^/static/(?P<v0>[^/]+)/assets/(?P<v1>[^/]+)", testPathRegexp(t, "/static/{path}/assets/{file}"))
+	assert.Equal(t, "^/items/(?P<v0>[0-9]+)/details/(?P<v1>[^/]+)", testPathRegexp(t, "/items/{itemID:[0-9]+}/details/{detail}"))
+}
+
+func TestGetPathRegexpWithRegexCompile(t *testing.T) {
+	pattern := testPathRegexp(t, "/api/v1/users/{userId}/roles/{roleId}")
 	url := "/api/v1/users/10512/roles/32587"
 
 	matched, err := regexp.MatchString(pattern, url)
