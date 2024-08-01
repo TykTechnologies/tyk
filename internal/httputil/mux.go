6b21f47e960c5876a6bbd4275e34fb79cbadfd35
Tit Petric <tit@tyk.io>
2024-08-01 22:58:45 +0200

    Update mux related path matcher, add streaming tests for coverage

---
+++ internal/httputil/mux.go
@@ -0,0 +1,46 @@
+package httputil
+
+import (
+	"strings"
+
+	"github.com/gorilla/mux"
+
+	"github.com/TykTechnologies/tyk/internal/maps"
+)
+
+// routeCache holds the raw routes as they are mapped to mux regular expressions.
+// e.g. `/foo` becomes `^/foo$` or similar, and parameters get matched and replaced.
+var pathRegexpCache = maps.NewStringMap()
+
+// RouteRegexString will convert a mux route url to a regular expression string.
+// The results for subsequent invocations with the same parameters are cached.
+func GetPathRegexp(pattern string) (string, error) {
+	val, ok := pathRegexpCache.Get(pattern)
+	if ok {
+		return val, nil
+	}
+
+	if IsMuxTemplate(pattern) {
+		dummyRouter := mux.NewRouter()
+		route := dummyRouter.PathPrefix(pattern)
+		result, err := route.GetPathRegexp()
+		if err != nil {
+			return "", err
+		}
+
+		pathRegexpCache.Set(pattern, result)
+		return result, nil
+	}
+
+	if strings.HasPrefix(pattern, "/") {
+		return "^" + pattern, nil
+	}
+	return "^.*" + pattern, nil
+}
+
+// IsMuxTemplate determines if a pattern is a mux template by counting the number of opening and closing braces.
+func IsMuxTemplate(pattern string) bool {
+	openBraces := strings.Count(pattern, "{")
+	closeBraces := strings.Count(pattern, "}")
+	return openBraces > 0 && openBraces == closeBraces
+}
---
+++ internal/httputil/mux_test.go
@@ -0,0 +1,45 @@
+package httputil
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func testPathRegexp(tb testing.TB, in string, want string) string {
+	tb.Helper()
+
+	res, err := GetPathRegexp(in)
+	assert.NoError(tb, err)
+	if want != "" {
+		assert.Equal(tb, want, res)
+	}
+	return res
+}
+
+func TestGetPathRegexp(t *testing.T) {
+	tests := map[string]string{
+		"/users*.":                             "^/users*.",
+		"/users":                               "^/users",
+		"users":                                "^.*users",
+		"/users$":                              "^/users$",
+		"/users/.*":                            "^/users/.*",
+		"/users/{id}":                          "^/users/(?P<v0>[^/]+)",
+		"/users/{id}/profile/{type:[a-zA-Z]+}": "^/users/(?P<v0>[^/]+)/profile/(?P<v1>[a-zA-Z]+)",
+		"/static/{path}/assets/{file}":         "^/static/(?P<v0>[^/]+)/assets/(?P<v1>[^/]+)",
+		"/items/{itemID:[0-9]+}/details/{detail}": "^/items/(?P<v0>[0-9]+)/details/(?P<v1>[^/]+)",
+	}
+
+	for k, v := range tests {
+		testPathRegexp(t, k, v)
+	}
+}
+
+func TestGetPathRegexpWithRegexCompile(t *testing.T) {
+	pattern := testPathRegexp(t, "/api/v1/users/{userId}/roles/{roleId}", "")
+
+	matched, err := regexp.MatchString(pattern, "/api/v1/users/10512/roles/32587")
+	assert.NoError(t, err)
+	assert.True(t, matched, "The URL should match the pattern")
+}
