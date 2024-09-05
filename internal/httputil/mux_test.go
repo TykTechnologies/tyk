fc670bc97bfa04e90fb128d27194387ce7614d8e
Tit Petric <tit@tyk.io>
2024-09-05 20:21:21 +0200

    Remove GetPathRegexp, unused, add cache to PreparePathRegexp

--- internal/httputil/mux_test.go
+++ internal/httputil/mux_test.go
@@ -12,40 +12,9 @@ import (
 func pathRegexp(tb testing.TB, in string, want string) string {
 	tb.Helper()
 
-	res, err := httputil.GetPathRegexp(in)
-	assert.NoError(tb, err)
-	if want != "" {
-		assert.Equal(tb, want, res)
-	}
-	return res
-}
-
-func preparePathRegexp(tb testing.TB, in string, want string) string {
-	tb.Helper()
-
 	res := httputil.PreparePathRegexp(in, true, false)
 	assert.Equal(tb, want, res)
 	return res
-}
-
-func TestGetPathRegexp(t *testing.T) {
-	tests := map[string]string{
-		"/users*.":                             "^/users*.",
-		"/users":                               "^/users",
-		"users":                                "^.*users",
-		"^/test/users":                         "^/test/users",
-		"/users$":                              "^/users$",
-		"/users/.*":                            "^/users/.*",
-		"/users/{id}":                          "^/users/(?P<v0>[^/]+)",
-		"/users/{id}$":                         "^/users/(?P<v0>[^/]+)$",
-		"/users/{id}/profile/{type:[a-zA-Z]+}": "^/users/(?P<v0>[^/]+)/profile/(?P<v1>[a-zA-Z]+)",
-		"/static/{path}/assets/{file}":         "^/static/(?P<v0>[^/]+)/assets/(?P<v1>[^/]+)",
-		"/items/{itemID:[0-9]+}/details/{detail}": "^/items/(?P<v0>[0-9]+)/details/(?P<v1>[^/]+)",
-	}
-
-	for k, v := range tests {
-		pathRegexp(t, k, v)
-	}
 }
 
 func TestPreparePathRegexp(t *testing.T) {
@@ -64,12 +33,12 @@ func TestPreparePathRegexp(t *testing.T) {
 	}
 
 	for k, v := range tests {
-		preparePathRegexp(t, k, v)
+		pathRegexp(t, k, v)
 	}
 }
 
 func TestGetPathRegexpWithRegexCompile(t *testing.T) {
-	pattern := pathRegexp(t, "/api/v1/users/{userId}/roles/{roleId}", "")
+	pattern := pathRegexp(t, "/api/v1/users/{userId}/roles/{roleId}", "^/api/v1/users/([^/]+)/roles/([^/]+)")
 
 	matched, err := regexp.MatchString(pattern, "/api/v1/users/10512/roles/32587")
 	assert.NoError(t, err)
