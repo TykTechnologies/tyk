145b806a6627f174820a89113a4ddb5b8358c973
Tit Petric <tit@tyk.io>
2024-09-05 12:32:37 +0200

    Update granular access mw to support config flags

--- gateway/mw_granular_access.go
+++ gateway/mw_granular_access.go
@@ -3,8 +3,13 @@ package gateway
 import (
 	"errors"
 	"net/http"
+	"slices"
+	"strings"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/TykTechnologies/tyk/internal/httputil"
+	"github.com/TykTechnologies/tyk/regexp"
 )
 
 // GranularAccessMiddleware will check if a URL is specifically enabled for the key
@@ -33,34 +38,96 @@ func (m *GranularAccessMiddleware) ProcessRequest(w http.ResponseWriter, r *http
 		return nil, http.StatusOK
 	}
 
-	urlPaths := []string{
-		m.Spec.StripListenPath(r.URL.Path),
-		r.URL.Path,
+	gwConfig := m.Gw.GetConfig()
+
+	// Hook per-api settings here (m.Spec...)
+	isPrefixMatch := gwConfig.HttpServerOptions.EnablePrefixMatching
+	isSuffixMatch := gwConfig.HttpServerOptions.EnableSuffixMatching
+
+	if isPrefixMatch {
+		urlPaths := []string{
+			m.Spec.StripListenPath(r.URL.Path),
+			r.URL.Path,
+		}
+
+		logger := m.Logger().WithField("paths", urlPaths)
+
+		for _, accessSpec := range sessionVersionData.AllowedURLs {
+			if !slices.Contains(accessSpec.Methods, r.Method) {
+				continue
+			}
+
+			// Append $ if so configured to match end of request path.
+			url := accessSpec.URL
+			if isSuffixMatch && !strings.HasSuffix(url, "$") {
+				url += "$"
+			}
+
+			match, err := httputil.MatchEndpoints(url, urlPaths)
+
+			// unconditional log of err/match/url
+			// if loglevel is set to debug verbosity increases and all requests are logged,
+			// regardless if an error occured or not.
+			if gwConfig.LogLevel == "debug" || err != nil {
+				logger = logger.WithError(err).WithField("pattern", url).WithField("match", match)
+				if err != nil {
+					logger.Error("error matching endpoint")
+				} else {
+					logger.Debug("matching endpoint")
+				}
+			}
+
+			if err != nil || !match {
+				continue
+			}
+			return m.pass()
+		}
+
+		return m.block(logger)
 	}
 
-	logger := m.Logger().WithField("paths", urlPaths)
+	logger := m.Logger().WithField("paths", []string{r.URL.Path})
+
+	// Legacy behaviour (5.5.0 and earlier), wildcard match against full request path.
+	// Fixed error handling in regex compilation to continue to next pattern (block).
+	urlPath := r.URL.Path
 
 	for _, accessSpec := range sessionVersionData.AllowedURLs {
-		url := accessSpec.URL
-
-		match, err := httputil.MatchEndpoints(url, urlPaths)
-
-		// unconditional log of err/match/url...
-		logger.WithError(err).WithField("pattern", url).WithField("match", match).Debug("checking allowed url")
-		if err != nil || !match {
+		if !slices.Contains(accessSpec.Methods, r.Method) {
 			continue
 		}
 
-		// if a path is matched, but isn't matched on method,
-		// then we continue onto the next path for evaluation.
-		for _, method := range accessSpec.Methods {
-			if method == r.Method {
-				return nil, http.StatusOK
-			}
+		url := accessSpec.URL
+
+		// Extends legacy by honoring isSuffixMatch.
+		// Append $ if so configured to match end of request path.
+		if isSuffixMatch && !strings.HasSuffix(url, "$") {
+			url += "$"
+		}
+
+		logger.Debug("Checking: ", urlPath, " Against:", url)
+
+		// Wildcard match (user supplied, as-is)
+		asRegex, err := regexp.Compile(url)
+		if err != nil {
+			logger.WithError(err).Error("Regex error")
+			continue
+		}
+
+		match := asRegex.MatchString(r.URL.Path)
+		if match {
+			return m.pass()
 		}
 	}
 
+	return m.block(logger)
+}
+
+func (m *GranularAccessMiddleware) block(logger *logrus.Entry) (error, int) {
 	logger.Info("Attempted access to unauthorised endpoint (Granular).")
 	return errors.New("Access to this resource has been disallowed"), http.StatusForbidden
+}
 
+func (m *GranularAccessMiddleware) pass() (error, int) {
+	return nil, http.StatusOK
 }
