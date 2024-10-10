acbcc4ac72dbf032ff06ce5341913f99ee1e57d7
Jeffy Mathew <jeffy.mathew100@gmail.com>
2024-10-10 09:15:15 +0200

    add godocs for pathItemHasParameters

--- apidef/oas/default.go
+++ apidef/oas/default.go
@@ -60,6 +60,7 @@ type TykExtensionConfigParams struct {
 	// MockResponse is true if a mocked response is configured.
 	MockResponse *bool
 
+	// pathItemHasParameters is set to true when parameters are defined the same level as of operations within path.
 	pathItemHasParameters bool
 }
 
