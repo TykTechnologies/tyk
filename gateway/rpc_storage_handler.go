e33af0ddd6309f302c5ff0a39c92aba796d20749
Matias <83959431+mativm02@users.noreply.github.com>
2024-10-18 09:16:59 -0300

    moving SetFirstConnection

--- gateway/rpc_storage_handler.go
+++ gateway/rpc_storage_handler.go
@@ -798,8 +798,6 @@ func (r *RPCStorageHandler) CheckForReload(orgId string) bool {
 	log.Debug("[RPC STORE] Check Reload called...")
 	reload, err := rpc.FuncClientSingleton("CheckReload", orgId)
 	if err != nil {
-		forcer := rpc.NewSyncForcer(r.Gw.StorageConnectionHandler, r.buildNodeInfo)
-		forcer.SetFirstConnection(true)
 		rpc.EmitErrorEventKv(
 			rpc.FuncClientSingletonCall,
 			"CheckReload",
@@ -814,6 +812,8 @@ func (r *RPCStorageHandler) CheckForReload(orgId string) bool {
 				r.CheckForReload(orgId)
 			}
 		} else if !strings.Contains(err.Error(), "Cannot obtain response during") {
+			forcer := rpc.NewSyncForcer(r.Gw.StorageConnectionHandler, r.buildNodeInfo)
+			forcer.SetFirstConnection(true)
 			log.Warning("[RPC STORE] RPC Reload Checker encountered unexpected error: ", err)
 		}
 
