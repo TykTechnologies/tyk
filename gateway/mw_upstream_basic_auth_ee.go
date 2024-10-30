bb6498bf556f7bf274d21aaed940fea106c6bd40
Tit Petric <tit@tyk.io>
2024-10-30 16:24:00 +0100

    Revert whitespace

--- gateway/mw_upstream_basic_auth_ee.go
+++ gateway/mw_upstream_basic_auth_ee.go
@@ -9,9 +9,6 @@ import (
 func getUpstreamBasicAuthMw(base *BaseMiddleware) TykMiddleware {
 	spec := base.Spec
 	mwSpec := upstreambasicauth.NewAPISpec(spec.APIID, spec.Name, spec.IsOAS, spec.OAS, spec.UpstreamAuth)
-	upstreamBasicAuthMw := upstreambasicauth.NewMiddleware(
-		base.Gw,
-		base,
-		mwSpec)
+	upstreamBasicAuthMw := upstreambasicauth.NewMiddleware(base.Gw, base, mwSpec)
 	return WrapMiddleware(base, upstreamBasicAuthMw)
 }
