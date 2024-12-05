224ec09949b81349002f1d0ba9c885eb69fa5828
sredny buitrago <sredny@srednys-MacBook-Pro.local>
2024-12-05 12:38:11 -0300

    addressing pr change request in certs

--- certs/manager.go
+++ certs/manager.go
@@ -101,9 +101,7 @@ func NewSlaveCertManager(localStorage, rpcStorage storage.Handler, secret string
 		return err
 	}
 
-	mdcbStorage := storage.NewMdcbStorage(localStorage, rpcStorage, log)
-	mdcbStorage.CallbackOnPullCertificateFromRPC = &callbackOnPullCertFromRPC
-
+	mdcbStorage := storage.NewMdcbStorage(localStorage, rpcStorage, log, callbackOnPullCertFromRPC)
 	cm.storage = mdcbStorage
 	return cm
 }
