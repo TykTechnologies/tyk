cd0965ab6369ab3968304b9e4f006ad7e67c7d71
Burak Sezer <burak.sezer.developer@gmail.com>
2024-12-17 20:06:30 +0300

    [TT-12741] Drop ineffectual assignment to r

--- internal/httpctx/context.go
+++ internal/httpctx/context.go
@@ -33,7 +33,7 @@ var selfLoopingValue = NewValue[bool](ctx.SelfLooping)
 
 // SetSelfLooping updates the request context with a boolean value indicating whether the request is in a self-looping state.
 func SetSelfLooping(r *http.Request, value bool) {
-	r = selfLoopingValue.Set(r, value)
+	selfLoopingValue.Set(r, value)
 }
 
 // IsSelfLooping returns true if the request is flagged as self-looping, indicating it originates and targets the same service.
