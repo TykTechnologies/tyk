fe7463dd36e3e8e9cbdfc3c30eb13045e7ea3948
Kofo Okesola <okesolakofo@gmail.com>
2025-01-21 16:44:33 +0100

    validation schema

--- apidef/oas/linter_test.go
+++ apidef/oas/linter_test.go
@@ -90,6 +90,10 @@ func TestXTykGateway_Lint(t *testing.T) {
 			BasicAuth: nil,
 			OAuth:     nil,
 		}
+
+		settings.Upstream.TLSTransport.MinVersion = "1.2"
+		settings.Upstream.TLSTransport.MaxVersion = "1.2"
+		settings.Upstream.TLSTransport.Ciphers = []string{"TLS_RSA_WITH_RC4_128_SHA"}
 		settings.Middleware.Global.TrafficLogs.RetentionPeriod.Value = ReadableDuration(time.Minute * 10)
 	}
 
