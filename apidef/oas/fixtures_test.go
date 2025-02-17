f8056cfa93c89ac202afb2327138b167b502a7d4
Tit Petric <tit@tyk.io>
2025-02-17 15:09:10 +0100

    Add migration test fixtures

---
+++ apidef/oas/fixtures_test.go
@@ -0,0 +1,139 @@
+package oas_test
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/goccy/go-yaml"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/TykTechnologies/tyk/apidef/oas"
+	"github.com/TykTechnologies/tyk/internal/maps"
+)
+
+// The fixtures.yml file contains a list of migration areas and corresponding test cases,
+// each with a source API definition ("apidef" by default) and its expected OAS migration output.
+//
+// Example fixtures.yml:
+//
+//   fixtures:
+//   - name: ServiceDefinition migrations
+//     tests:
+//       - input:
+//           proxy:
+//             service_discovery:
+//               use_discovery_service: true
+//               cache_disabled: false
+//               cache_timeout: 10
+//         output:
+//           upstream:
+//             serviceDiscovery:
+//               enabled: false
+//
+
+//go:embed fixtures.yml
+var fixtures embed.FS
+
+// FixtureDocument represents the root structure of the fixtures configuration.
+// It contains a collection of migration fixtures.
+type FixtureDocument struct {
+	Fixtures []Fixture `yaml:"fixtures"`
+}
+
+// Fixture represents a single migration test case.
+// It defines a conversion between a classic API definition and its expected OAS migration output.
+type Fixture struct {
+	// Name holds the name of the fixture, e.g. a product area, a feature.
+	Name string
+
+	// Tests hold the individual migration test cases.
+	Tests []FixtureTest `yaml:"tests"`
+}
+
+// Fixture represents a single migration test case.
+type FixtureTest struct {
+	// Source indicates the source definition type, either "apidef" (classic API) or "oas".
+	// Defaults to "apidef" if not specified.
+	Source string `yaml:"source"`
+
+	// Input holds the source API definition's explicitly set fields.
+	Input map[string]any `yaml:"input"`
+
+	// Output holds the expected OAS API definition's explicitly set fields.
+	Output map[string]any `yaml:"output"`
+}
+
+// TestFixtures loads the fixtures configuration from the embedded fixtures.yml,
+// unmarshals the YAML content into a FixtureDocument, and logs the parsed fixtures.
+// This test currently only prints the YAML content for debugging purposes.
+func TestFixtures(t *testing.T) {
+	var (
+		doc FixtureDocument
+
+		encodeJSON = func(in any) []byte {
+			v, err := json.MarshalIndent(in, "", "  ")
+			assert.NoError(t, err)
+			return v
+		}
+	)
+
+	// Read the embedded fixtures.yml file.
+	data, err := fixtures.ReadFile("fixtures.yml")
+	assert.NoError(t, err, "failed to read fixtures.yml from embed FS")
+
+	// Unmarshal the YAML content into a FixtureDocument.
+	err = yaml.Unmarshal(data, &doc)
+	assert.NoError(t, err, "failed to unmarshal fixtures YAML")
+
+	// Optionally, iterate through each fixture and log its details.
+	for _, fixture := range doc.Fixtures {
+		t.Run(fixture.Name, func(t *testing.T) {
+			for idx, tc := range fixture.Tests {
+				t.Run(fmt.Sprint(idx), func(t *testing.T) {
+					var oasMap = make(map[string]any)
+
+					switch tc.Source {
+					default:
+						// Read in input api definition
+						var from apidef.APIDefinition
+						assert.NoError(t, json.Unmarshal(encodeJSON(tc.Input), &from))
+						from.Migrate()
+
+						// Fill oas from api definition, src modified in place.
+						src := &oas.OAS{T: openapi3.T{}}
+						oas.FillOASFromClassicAPIDefinition(&from, src)
+						oasJSON := encodeJSON(src)
+
+						assert.NoError(t, json.Unmarshal(oasJSON, &oasMap))
+
+						// Flatten output
+						oasFlatMap, err := maps.Flatten(oasMap)
+						assert.NoError(t, err)
+
+						// Print output
+						fmt.Println("keys")
+						for k, v := range oasFlatMap {
+							fmt.Println(k, v)
+						}
+
+						// Flatten expected output
+						outputFlatMap, err := maps.Flatten(tc.Output)
+						assert.NoError(t, err)
+
+						// Assert results
+						for k, want := range outputFlatMap {
+							got, ok := oasFlatMap[k]
+
+							assert.True(t, ok, "expected key %s in output", k)
+							assert.Equal(t, want, got)
+						}
+					}
+				})
+			}
+		})
+	}
+}
