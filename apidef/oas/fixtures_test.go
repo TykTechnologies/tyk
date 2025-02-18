3c4411e1210bc5bc2afb9a0a827883feb2ee8f9d
Tit Petric <tit@tyk.io>
2025-02-18 14:01:49 +0100

    If fixtures output is omitted, print detected change from base api definition

--- apidef/oas/fixtures_test.go
+++ apidef/oas/fixtures_test.go
@@ -76,8 +76,20 @@ type FixtureTest struct {
 	// Output holds the expected OAS API definition's explicitly set fields.
 	Output map[string]any `yaml:"output"`
 
+	// Ignores will skip values in source
+	Ignores []FixtureIgnore `yaml:"ignores"`
+
 	// Errors should be configured to assert on errors.
 	Errors FixtureError `yaml:"errors"`
+}
+
+// FixtureIgnore
+type FixtureIgnore struct {
+	// Key is a key to ignore. Supports a wildcard.
+	Key string `yaml:"key"`
+
+	// Values is a list of values to ignore.
+	Values []any `yaml:"values"`
 }
 
 // FixtureError holds configuration for assertion errors in FixtureTest.
@@ -120,17 +132,95 @@ func Fixtures(tb testing.TB) *FixtureDocument {
 	return doc
 }
 
+var encodeJSON = func(tb testing.TB, in any) []byte {
+	tb.Helper()
+	v, err := json.MarshalIndent(in, "", "  ")
+	assert.NoError(tb, err)
+	return v
+}
+
+func createOAS(tb testing.TB, patch any) *oas.OAS {
+	tb.Helper()
+
+	var def = &oas.OAS{
+		T: openapi3.T{
+			Info: &openapi3.Info{},
+		},
+	}
+
+	if patch != nil {
+		assert.NoError(tb, json.Unmarshal(encodeJSON(tb, patch), &def))
+	}
+
+	return def
+}
+
+func createClassic(tb testing.TB, patch any) *apidef.APIDefinition {
+	tb.Helper()
+
+	var def = &apidef.APIDefinition{
+		VersionData: apidef.VersionData{
+			Versions: map[string]apidef.VersionInfo{
+				"v1": {
+					GlobalSizeLimit:         0,
+					GlobalSizeLimitDisabled: false,
+				},
+			},
+		},
+	}
+
+	if patch != nil {
+		assert.NoError(tb, json.Unmarshal(encodeJSON(tb, patch), &def))
+	}
+
+	def.Migrate()
+
+	return def
+}
+
+func migrateClassic(tb testing.TB, def *apidef.APIDefinition) (*oas.OAS, error) {
+	src := &oas.OAS{T: openapi3.T{Info: &openapi3.Info{}}}
+	_, err := oas.FillOASFromClassicAPIDefinition(def, src)
+	return src, err
+}
+
+func migrateOAS(tb testing.TB, def *oas.OAS) (*apidef.APIDefinition, error) {
+	src := &apidef.APIDefinition{}
+	def.ExtractTo(src)
+	return src, nil
+}
+
+func flatMap(tb testing.TB, src any, base map[string]any) map[string]any {
+	result := map[string]any{}
+	err := json.Unmarshal(encodeJSON(tb, src), &result)
+	assert.NoError(tb, err)
+
+	result, err = Flatten(result)
+	assert.NoError(tb, err)
+
+	for k, v := range base {
+		if vv, ok := result[k]; ok && strval(vv) == strval(v) {
+			delete(result, k)
+		}
+	}
+
+	return result
+}
+
 // TestFixtures loads the fixtures configuration from the embedded fixtures.yml,
 // unmarshals the YAML content into a FixtureDocument, and logs the parsed fixtures.
 // This test currently only prints the YAML content for debugging purposes.
 func TestFixtures(t *testing.T) {
-	encodeJSON := func(in any) []byte {
-		v, err := json.MarshalIndent(in, "", "  ")
-		assert.NoError(t, err)
-		return v
-	}
-
 	doc := Fixtures(t)
+
+	// Base migration gives us an initial state.
+	oasEmpty := createOAS(t, nil)
+	oasEmptyClassic, _ := migrateOAS(t, oasEmpty)
+	oasEmptyMap := flatMap(t, oasEmptyClassic, nil)
+
+	classicEmpty := createClassic(t, nil)
+	classicEmptyOAS, _ := migrateClassic(t, classicEmpty)
+	classicEmptyMap := flatMap(t, classicEmptyOAS, nil)
 
 	// Optionally, iterate through each fixture and log its details.
 	for _, fixture := range doc.Fixtures {
@@ -141,71 +231,90 @@ func TestFixtures(t *testing.T) {
 					name = fmt.Sprintf("case %d", idx)
 				}
 				t.Run(name, func(t *testing.T) {
-					var result = make(map[string]any)
+					var (
+						err    error
+						skip   map[string]any = nil
+						result                = make(map[string]any)
+					)
 
 					switch tc.Source {
 					case "oas":
-						// Read in input api definition
-						var from *oas.OAS = &oas.OAS{T: openapi3.T{Info: &openapi3.Info{}}}
-						assert.NoError(t, json.Unmarshal(encodeJSON(tc.Input), &from))
-
-						// Fill oas from api definition, src modified in place.
-						src := &apidef.APIDefinition{}
-						from.ExtractTo(src)
+						var dest *apidef.APIDefinition
+						def := createOAS(t, tc.Input)
+						dest, err = migrateOAS(t, def)
 
 						assert.False(t, tc.Errors.Enabled, "OAS migrations to classic don't support error=true")
-						assert.NoError(t, json.Unmarshal(encodeJSON(src), &result))
+
+						if len(tc.Output) == 0 {
+							skip = oasEmptyMap
+						}
+
+						result = flatMap(t, dest, skip)
 					default:
-						// Read in input api definition
-						var from apidef.APIDefinition
-						from.VersionData = apidef.VersionData{
-							Versions: map[string]apidef.VersionInfo{
-								"v1": {
-									GlobalSizeLimit:         0,
-									GlobalSizeLimitDisabled: false,
-								},
-							},
+						var dest *oas.OAS
+						def := createClassic(t, tc.Input)
+						dest, err = migrateClassic(t, def)
+
+						if len(tc.Output) == 0 {
+							skip = classicEmptyMap
 						}
 
-						assert.NoError(t, json.Unmarshal(encodeJSON(tc.Input), &from))
-						from.Migrate()
-
-						// Fill oas from api definition, src modified in place.
-						src := &oas.OAS{T: openapi3.T{Info: &openapi3.Info{}}}
-						_, err := oas.FillOASFromClassicAPIDefinition(&from, src)
-
-						if tc.Errors.Enabled {
-							t.Log("Checking errors enabled, want=", tc.Errors.Want)
-							if tc.Errors.Want {
-								assert.Error(t, err)
-							} else {
-								assert.NoError(t, err)
-							}
-						}
-
-						assert.NoError(t, json.Unmarshal(encodeJSON(src), &result))
+						result = flatMap(t, dest, skip)
 					}
 
-					resultMap, err := Flatten(result)
-					assert.NoError(t, err)
+					if tc.Errors.Enabled {
+						t.Logf("Checking errors enabled, want=%v", tc.Errors.Want)
+						if tc.Errors.Want {
+							assert.Error(t, err)
+						} else {
+							assert.NoError(t, err)
+						}
+					}
 
 					// Print debug output
-					if isDebug {
-						keys := slices.Sorted(maps.Keys(resultMap))
+					if isDebug || len(tc.Output) == 0 {
+						keys := slices.Sorted(maps.Keys(result))
+
+						fmt.Printf("Ignores: %s\n", encodeJSON(t, tc.Ignores))
 
 						for _, k := range keys {
-							v := resultMap[k]
-							t.Logf("- %s \"%v\"", k, v)
+							raw := result[k]
+							v := strval(raw)
+
+							shouldIgnore := func() bool {
+								for _, ign := range tc.Ignores {
+									var matchedValue bool
+
+									for _, ignval := range ign.Values {
+										if v == strval(ignval) {
+											matchedValue = true
+											break
+										}
+									}
+
+									matchedKey := ign.Key == "" || strings.Contains(k, ign.Key)
+									return matchedValue && matchedKey
+								}
+								return false
+							}()
+							if shouldIgnore {
+								continue
+							}
+
+							t.Logf("- %s \"%s\"", k, v)
 						}
 					}
 
 					// Flatten expected output
-					want, err := Flatten(tc.Output)
-					assert.NoError(t, err)
+					want := map[string]any{}
+					if len(tc.Output) > 0 {
+						want, err = Flatten(tc.Output)
+						assert.NoError(t, err)
+					}
 
 					// Assert results
 					for k, want := range want {
-						got, ok := resultMap[k]
+						got, ok := result[k]
 
 						assert.True(t, ok, "expected key %s in output", k)
 						assert.Equal(t, want, got, "expected key %s=%s, got %s", k, want, got)
@@ -215,4 +324,8 @@ func TestFixtures(t *testing.T) {
 			}
 		})
 	}
+}
+
+func strval(in any) string {
+	return fmt.Sprintf("%v", in)
 }
