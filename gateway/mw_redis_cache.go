01b512dcc0e11339638946132845c0531a4cb65d
Edson Michaque <edson@michaque.com>
2025-03-10 15:40:43 +0200

    add testing logs to gateway

--- gateway/mw_redis_cache.go
+++ gateway/mw_redis_cache.go
@@ -201,11 +201,16 @@ func (m *RedisCacheMiddleware) ProcessRequest(w http.ResponseWriter, r *http.Req
 			cacheOnlyResponseCodes = cacheMeta.CacheOnlyResponseCodes
 		}
 
+		log.Printf("CACHE META: %#+v", cacheMeta)
+
 		// override api level Timout by endpoint level if provided
 		if cacheMeta.Timeout > 0 {
 			timeout = cacheMeta.Timeout
 		}
 	}
+
+	log.Printf("RESPONSE CODES: %#+v", cacheOnlyResponseCodes)
+	log.Printf("TIMEOUT: %#+v", timeout)
 
 	ctxSetCacheOptions(r, &cacheOptions{
 		key:                    key,
