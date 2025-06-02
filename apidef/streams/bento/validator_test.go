ec28444bbad50d8b2a35f2e269c66a80ab752e99
Burak Sezer <burak.sezer.developer@gmail.com>
2025-06-02 16:35:58 +0300

    [TT-14731] Rename EnableAllConfigValidator to EnableAllExperimentalConfigValidator

--- apidef/streams/bento/validator_test.go
+++ apidef/streams/bento/validator_test.go
@@ -88,8 +88,8 @@ func TestValidateBentoConfiguration(t *testing.T) {
 }
 
 func TestValidateBentoConfiguration_NewEnableAllConfigValidator(t *testing.T) {
-	// The purpose of this test is to make SonarCloud happy. EnableAllConfigValidator validates everything.
-	validator := NewEnableAllConfigValidator()
+	// The purpose of this test is to make SonarCloud happy. EnableAllExperimentalConfigValidator validates everything.
+	validator := NewEnableAllExperimentalConfigValidator()
 	require.NotNil(t, validator)
 
 	validDocumentWithAdditionalProperties := []byte(`{
