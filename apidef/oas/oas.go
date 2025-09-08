451d4ed48f4b56e9c7b901e286b796d8976e5ee6
Kofo Okesola <okesolakofo@gmail.com>
2025-09-08 09:16:15 +0100

    adderd normalize logic

--- apidef/oas/oas.go
+++ apidef/oas/oas.go
@@ -470,6 +470,25 @@ func (s *OAS) Validate(ctx context.Context, opts ...openapi3.ValidationOption) e
 	return errors.Join(validationErr, securityErr)
 }
 
+// Normalize converts the OAS api to a normalized state.
+// it does this by fixing backwards compatibility issues
+// and copying fields values necessary to work
+// any logic to ensure the Tyk oas API is backwards compatible with previous versions of the gateway
+// should be placed in here.
+func (s *OAS) Normalize() {
+	// copy the values of the new JWT validation
+	jwtConfiguration := s.GetJWTConfiguration()
+	if len(jwtConfiguration.BasePolicyClaims) > 0 {
+		jwtConfiguration.PolicyFieldName = jwtConfiguration.BasePolicyClaims[0]
+	}
+	if len(jwtConfiguration.SubjectClaims) > 0 {
+		jwtConfiguration.IdentityBaseField = jwtConfiguration.SubjectClaims[0]
+	}
+	if len(jwtConfiguration.Scopes.Claims) > 0 {
+		jwtConfiguration.Scopes.ClaimName = jwtConfiguration.Scopes.Claims[0]
+	}
+}
+
 // validateSecurity verifies that existing Security Requirement Objects has Security Schemes declared in the Security
 // Schemes under the Components Object. This function closes gap in validation provided by OAS.Validate func.
 func (s *OAS) validateSecurity() error {
