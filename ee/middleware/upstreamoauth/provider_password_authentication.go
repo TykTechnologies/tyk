2e92f68609dcaa57d51b6087b90087517a0dbc6d
Andrei <andrei@tyk.io>
2025-09-26 18:07:33 +0300

    TT-15860, fixed upstream oauth password auth config

--- ee/middleware/upstreamoauth/provider_password_authentication.go
+++ ee/middleware/upstreamoauth/provider_password_authentication.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 
 	"golang.org/x/oauth2"
+
+	"github.com/TykTechnologies/tyk/internal/httpclient"
 )
 
 func (cache *PasswordClient) ObtainToken(ctx context.Context) (*oauth2.Token, error) {
 	cfg := newOAuth2PasswordConfig(cache.mw)
+
+	// Use external services HTTP client if configured
+	if httpClient := cache.getHTTPClient(); httpClient != nil {
+		ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+	}
+
 	return cfg.PasswordCredentialsToken(ctx, cache.mw.Spec.UpstreamAuth.OAuth.PasswordAuthentication.Username, cache.mw.Spec.UpstreamAuth.OAuth.PasswordAuthentication.Password)
 }
 
@@ -22,4 +30,24 @@ func (cache *PasswordClient) GetToken(r *http.Request) (string, error) {
 	}
 
 	return getToken(r, cacheKey, obtainTokenFunc, secret, extraMetadata, cache.mw.passwordStorageHandler)
+}
+
+// getHTTPClient creates an HTTP client with external services configuration if available
+func (cache *PasswordClient) getHTTPClient() *http.Client {
+	gwConfig := cache.mw.Gw.GetConfig()
+	if gwConfig.ExternalServices.OAuth.MTLS.Enabled ||
+		gwConfig.ExternalServices.OAuth.Proxy.Enabled ||
+		gwConfig.ExternalServices.Global.Enabled {
+
+		// Create HTTP client factory
+		factory := httpclient.NewExternalHTTPClientFactory(&gwConfig.ExternalServices, cache.mw.Gw.GetCertificateManager())
+
+		// Try to create OAuth client
+		if client, err := factory.CreateOAuthClient(); err == nil {
+			return client
+		}
+	}
+
+	// Return nil to use default HTTP client
+	return nil
 }
