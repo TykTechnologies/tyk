32dd92b6f328bd49c1c24f3b14f4e4ef3b0d1779
Yaroslav Kotsur <yarikkotsur@gmail.com>
2025-10-14 14:43:49 +0200

    add tests to skip sonar

--- gateway/policy_test.go
+++ gateway/policy_test.go
@@ -1413,3 +1413,54 @@ func TestLoadPoliciesFromDashboardLoadBalancerDrain(t *testing.T) {
 		})
 	}
 }
+
+func Test_ensurePolicyId(t *testing.T) {
+	objectId := persistentmodel.NewObjectID()
+
+	for _, tc := range []struct {
+		name           string
+		input          *user.Policy
+		expectedResult bool
+		expectedId     string
+	}{
+		{
+			name:           "skips if id is provided and MID is invalid",
+			input:          &user.Policy{ID: "my-custom-id"},
+			expectedResult: true,
+			expectedId:     "my-custom-id",
+		},
+		{
+			name:           "skips if id is provided and MID is valid",
+			input:          &user.Policy{MID: objectId, ID: "my-custom-id"},
+			expectedResult: true,
+			expectedId:     "my-custom-id",
+		},
+		{
+			name:           "returns false if is is not provided and MID is invalid",
+			input:          &user.Policy{ID: "", MID: "invalid"},
+			expectedResult: false,
+			expectedId:     "",
+		},
+		{
+			name:           "returns true and sets id if ID is not defined and MID is valid",
+			input:          &user.Policy{ID: "", MID: objectId},
+			expectedResult: true,
+			expectedId:     objectId.Hex(),
+		},
+		{
+			name:           "returns false if provided policy is nil",
+			input:          nil,
+			expectedResult: false,
+			expectedId:     "",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			res := ensurePolicyId(tc.input)
+			assert.Equal(t, tc.expectedResult, res)
+
+			if tc.input != nil {
+				assert.Equal(t, tc.expectedId, tc.input.ID)
+			}
+		})
+	}
+}
