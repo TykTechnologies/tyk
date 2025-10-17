882b6a6571a08b96f84ec05743bd86fb61d17ab0
Jeffy Mathew <jeffy.mathew100@gmail.com>
2025-10-17 16:16:36 +0200

    add alias packages

---
+++ pkg/alias/gateway/gateway.go
@@ -0,0 +1,29 @@
+package gateway
+
+import (
+	"github.com/TykTechnologies/tyk/gateway"
+	"github.com/TykTechnologies/tyk/internal/event"
+)
+
+const (
+	EventBreakerTriggered  = event.BreakerTriggered
+	EventRateLimitExceeded = event.RateLimitExceeded
+	EH_WebHook             = event.WebHookHandler
+	TestHttpAny            = gateway.TestHttpAny
+)
+
+type (
+	Test                    = gateway.Test
+	TestConfig              = gateway.TestConfig
+	OASSchemaResponse       = gateway.OASSchemaResponse
+	APIAllCertificateBasics = gateway.APIAllCertificateBasics
+)
+
+var (
+	LoopingUrl            = gateway.LoopingUrl
+	StartTest             = gateway.StartTest
+	InitTestMain          = gateway.InitTestMain
+	CreateStandardSession = gateway.CreateStandardSession
+	APILoopingName        = gateway.APILoopingName
+	EnableTestDNSMock     = gateway.EnableTestDNSMock
+)
