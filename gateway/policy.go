50393192bc1c36dfd0519ebe708ef5dac9384a7d
Yaroslav Kotsur <yarikkotsur@gmail.com>
2025-11-13 13:28:26 +0100

    bechmarks

--- gateway/policy.go
+++ gateway/policy.go
@@ -3,18 +3,17 @@ package gateway
 import (
 	"encoding/json"
 	"errors"
-	"github.com/TykTechnologies/tyk/header"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 
-	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
-
-	"github.com/TykTechnologies/tyk/rpc"
-
 	"github.com/sirupsen/logrus"
 
+	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
+
+	"github.com/TykTechnologies/tyk/header"
+	"github.com/TykTechnologies/tyk/rpc"
 	"github.com/TykTechnologies/tyk/user"
 )
 
