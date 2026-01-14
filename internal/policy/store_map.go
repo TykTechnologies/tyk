d7dca9a9830d71e85a362022a0c11da8d739eedc
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-01-14 16:45:21 +0100

    fix anti corruption layer tests

--- internal/policy/store_map.go
+++ internal/policy/store_map.go
@@ -1,6 +1,12 @@
 package policy
 
 import (
+	"maps"
+	"slices"
+
+	"github.com/samber/lo"
+
+	"github.com/TykTechnologies/tyk/internal/model"
 	"github.com/TykTechnologies/tyk/user"
 )
 
@@ -11,33 +17,37 @@ type StoreMap struct {
 
 // NewStoreMap returns a new policy.StoreMap.
 func NewStoreMap(policies map[string]user.Policy) *StoreMap {
-	if len(policies) == 0 {
-		policies = make(map[string]user.Policy)
-	}
-
-	return &StoreMap{
+	res := &StoreMap{
 		policies: policies,
 	}
+
+	if res.policies == nil {
+		res.policies = make(map[string]user.Policy)
+	}
+
+	return res
 }
 
 // PolicyIDs returns a list policy IDs in the store.
 // It will return nil if no policies exist.
-func (s *StoreMap) PolicyIDs() []string {
-	if len(s.policies) == 0 {
-		return nil
-	}
-
-	policyIDs := make([]string, 0, len(s.policies))
-	for _, val := range s.policies {
-		policyIDs = append(policyIDs, val.ID)
-	}
-	return policyIDs
+func (s *StoreMap) PolicyIDs() []model.PolicyID {
+	return lo.Map(slices.Collect(maps.Values(s.policies)), func(pol user.Policy, _ int) model.PolicyID {
+		return model.NewAnyPolicyId(pol.OrgID, pol.ID)
+	})
 }
 
 // PolicyByID returns a policy by ID.
-func (s *StoreMap) PolicyByID(id string) (user.Policy, bool) {
-	v, ok := s.policies[id]
-	return v, ok
+func (s *StoreMap) PolicyByID(id model.PolicyID) (user.Policy, bool) {
+	switch id := id.(type) {
+	case model.PolicyDbId:
+		v, ok := s.policies[id.String()]
+		return v, ok
+	case model.AnyPolicyId:
+		v, ok := s.policies[id.Id()]
+		return v, ok
+	default:
+		panic("unexpected sealed type")
+	}
 }
 
 // PolicyCount returns the number of policies in the store.
