51979c37f5670c7e660ba5007a33654917b5bc88
lghiur <laurentiu.ghiur@gmail.com>
2026-01-28 14:57:05 +0200

    wip

--- apidef/oas/mcp_primitive.go
+++ apidef/oas/mcp_primitive.go
@@ -19,8 +19,6 @@ func (m *MCPPrimitive) extractTransformResponseBodyTo(ep *apidef.ExtendedPathsSe
 
 // ensureNotBypassingOverride validates that the caller passed *MCPPrimitive and not *Operation.
 // This development helper catches accidental usage of &primitive.Operation which would bypass overrides.
-//
-//nolint:unused // Called from gateway package
 func ensureNotBypassingOverride(v interface{}) {
 	if _, ok := v.(*Operation); ok {
 		panic("BUG: Extracting Operation directly instead of MCPPrimitive - bypasses overrides!")
