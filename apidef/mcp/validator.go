13a2a65194aec21d1dfe83afc8209ce96ce8523b
lghiur <laurentiu.ghiur@gmail.com>
2026-01-30 09:56:33 +0200

    removed useless schema

--- apidef/mcp/validator.go
+++ apidef/mcp/validator.go
@@ -28,7 +28,6 @@ const (
 	keyRequired                 = "required"
 	mcpSchemaVersionNotFoundFmt = "schema not found for version %q"
 	ExtensionTykAPIGateway      = "x-tyk-api-gateway"
-	ExtensionTykMCP             = "x-tyk-mcp"
 )
 
 var (
@@ -63,14 +62,6 @@ func loadMCPSchema() error {
 
 		xTykAPIGwSchemaWithoutDefs := jsonparser.Delete(xTykAPIGwSchema, keyDefinitions)
 
-		// Load x-tyk-mcp extension schema
-		xTykMCPSchema, err := schemaDir.ReadFile(fmt.Sprintf("schema/%s.json", ExtensionTykMCP))
-		if err != nil {
-			return fmt.Errorf("%s loading failed: %w", ExtensionTykMCP, err)
-		}
-
-		xTykMCPSchemaWithoutDefs := jsonparser.Delete(xTykMCPSchema, keyDefinitions)
-
 		mcpJSONSchemas = make(map[string][]byte)
 		members, err := schemaDir.ReadDir("schema")
 		if err != nil {
@@ -91,9 +82,6 @@ func loadMCPSchema() error {
 			if strings.HasSuffix(fileName, fmt.Sprintf("%s.json", ExtensionTykAPIGateway)) {
 				continue
 			}
-			if strings.HasSuffix(fileName, fmt.Sprintf("%s.json", ExtensionTykMCP)) {
-				continue
-			}
 
 			var data []byte
 			data, err = schemaDir.ReadFile(filepath.Join("schema/", fileName))
@@ -110,23 +98,8 @@ func loadMCPSchema() error {
 				return err
 			}
 
-			// Inject x-tyk-mcp extension as property
-			data, err = jsonparser.Set(data, xTykMCPSchemaWithoutDefs, keyProperties, ExtensionTykMCP)
-			if err != nil {
-				return err
-			}
-
 			// Merge x-tyk-api-gateway definitions into schema
 			err = jsonparser.ObjectEach(xTykAPIGwSchema, func(key []byte, value []byte, _ jsonparser.ValueType, _ int) error {
-				data, err = jsonparser.Set(data, value, defsKey, string(key))
-				return err
-			}, keyDefinitions)
-			if err != nil {
-				return err
-			}
-
-			// Merge x-tyk-mcp definitions into schema
-			err = jsonparser.ObjectEach(xTykMCPSchema, func(key []byte, value []byte, _ jsonparser.ValueType, _ int) error {
 				data, err = jsonparser.Set(data, value, defsKey, string(key))
 				return err
 			}, keyDefinitions)
