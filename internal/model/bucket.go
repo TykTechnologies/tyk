8496a198cbacca06a81ae3b1793cf82b6ff577f1
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-02-09 21:57:22 +0100

    add backet tests, removed data race

--- internal/model/bucket.go
+++ internal/model/bucket.go
@@ -13,15 +13,6 @@ var (
 // Bucket interface for interacting with leaky buckets: https://en.wikipedia.org/wiki/Leaky_bucket
 // Underlying mechanism does not implement Leaky_bucket algorithm :'(
 type Bucket interface {
-	// Capacity of the bucket.
-	Capacity() uint
-
-	// Remaining space in the bucket.
-	Remaining() uint
-
-	// Reset returns when the bucket will be drained.
-	Reset() time.Time
-
 	// Add to the bucket. Returns bucket state after adding.
 	Add(uint) (BucketState, error)
 
