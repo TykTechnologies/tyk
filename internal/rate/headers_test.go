249381adadce610b79e3e896493b30a9b7e8a78c
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-02-10 08:51:37 +0100

    header sender unit tests

--- internal/rate/headers_test.go
+++ internal/rate/headers_test.go
@@ -1,9 +1,14 @@
 package rate
 
 import (
+	"net/http"
+	"net/textproto"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/TykTechnologies/tyk/header"
 )
 
 func Test_HeaderSender(t *testing.T) {
@@ -24,10 +29,61 @@ func Test_HeaderSender(t *testing.T) {
 			assert.IsType(t, &rateLimitSender{}, s)
 		})
 	})
+}
 
-	// todo: add cover
-	//t.Run("SendQuotas", func(t *testing.T) {
-	//	s := &quotaSender{}
-	//	s.SendQuotas(nil, nil, "")
-	//})
+func Test_quotaSender(t *testing.T) {
+	t.Run("SendQuotas", func(t *testing.T) {
+		t.Run("sends quotas and creates headers map headers", func(t *testing.T) {
+			dst := http.Response{}
+			qs := &quotaSender{}
+			qs.SendQuotas(&dst, nil, "")
+
+			assert.NotNil(t, dst.Header)
+			assert.Contains(t, dst.Header, textproto.CanonicalMIMEHeaderKey(header.XRateLimitLimit))
+			assert.Contains(t, dst.Header, textproto.CanonicalMIMEHeaderKey(header.XRateLimitRemaining))
+			assert.Contains(t, dst.Header, textproto.CanonicalMIMEHeaderKey(header.XRateLimitReset))
+		})
+	})
+
+	t.Run("SendRateLimits", func(t *testing.T) {
+		t.Run("does nothing and dont fails", func(t *testing.T) {
+			assert.NotPanics(t, func() {
+				qs := &quotaSender{}
+				qs.SendRateLimits(nil, Limits{})
+			})
+		})
+	})
+}
+
+func Test_rateLimitSender(t *testing.T) {
+	t.Run("SendQuotas", func(t *testing.T) {
+		t.Run("sends quotas and creates headers map headers", func(t *testing.T) {
+			assert.NotPanics(t, func() {
+				dst := http.Response{}
+				qs := &rateLimitSender{}
+				qs.SendQuotas(&dst, nil, "")
+				assert.Nil(t, dst.Header)
+			})
+		})
+	})
+
+	t.Run("SendRateLimits", func(t *testing.T) {
+		t.Run("sends quotas and creates headers map headers", func(t *testing.T) {
+			assert.NotPanics(t, func() {
+				rls := &rateLimitSender{}
+
+				hdr := http.Header{}
+
+				rls.SendRateLimits(hdr, Limits{
+					Limit:     200,
+					Remaining: 100,
+					Reset:     time.Second,
+				})
+
+				assert.Equal(t, "200", hdr.Get(header.XRateLimitLimit))
+				assert.Equal(t, "100", hdr.Get(header.XRateLimitRemaining))
+				assert.Equal(t, "1", hdr.Get(header.XRateLimitReset))
+			})
+		})
+	})
 }
