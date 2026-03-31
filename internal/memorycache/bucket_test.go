b4d2521596c25390114c72fd3cb48deff76f7eb3
Vlad Zabolotnyi <vlad.z@tyk.io>
2026-03-31 15:06:33 +0300

    test: update bucket tests with using synctest to avoid flakiness

--- internal/memorycache/bucket_test.go
+++ internal/memorycache/bucket_test.go
@@ -2,6 +2,7 @@ package memorycache_test
 
 import (
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/stretchr/testify/assert"
@@ -11,44 +12,46 @@ import (
 )
 
 func TestBucket(t *testing.T) {
-	// todo: use synctest when moved to go 1.25
-
 	t.Run("Add", func(t *testing.T) {
-		const capacity = 5
+		synctest.Test(t, func(t *testing.T) {
+			const capacity = 5
 
-		bucket := memorycache.NewBucket(capacity, time.Millisecond)
-		for range capacity {
+			bucket := memorycache.NewBucket(capacity, time.Millisecond)
+			for range capacity {
+				_, err := bucket.Add(1)
+				assert.NoError(t, err)
+			}
+
 			_, err := bucket.Add(1)
-			assert.NoError(t, err)
-		}
+			assert.ErrorIs(t, err, model.ErrBucketFull, "blocks if limit exceeded")
 
-		_, err := bucket.Add(1)
-		assert.ErrorIs(t, err, model.ErrBucketFull, "blocks if limit exceeded")
+			// resets time after
+			time.Sleep(time.Millisecond + time.Microsecond)
 
-		// resets time after
-		time.Sleep(time.Millisecond + time.Microsecond)
-
-		_, err = bucket.Add(1)
-		assert.NoError(t, err, "unlocks after timeout")
+			_, err = bucket.Add(1)
+			assert.NoError(t, err, "unlocks after timeout")
+		})
 	})
 
 	t.Run("State", func(t *testing.T) {
-		const capacity = 5
-		bucket := memorycache.NewBucket(capacity, time.Millisecond)
+		synctest.Test(t, func(t *testing.T) {
+			const capacity = 5
+			bucket := memorycache.NewBucket(capacity, time.Millisecond)
 
-		for range capacity {
-			stateOnAdd, err := bucket.Add(1)
-			assert.NoError(t, err)
-			stateOnGet := bucket.State()
-			assert.Equal(t, stateOnGet, stateOnAdd)
-		}
+			for range capacity {
+				stateOnAdd, err := bucket.Add(1)
+				assert.NoError(t, err)
+				stateOnGet := bucket.State()
+				assert.Equal(t, stateOnGet, stateOnAdd)
+			}
 
-		_, err := bucket.Add(1)
-		assert.ErrorIs(t, err, model.ErrBucketFull, "blocks if limit exceeded")
-		assert.True(t, bucket.State().Remaining == 0, "remaining state zero")
+			_, err := bucket.Add(1)
+			assert.ErrorIs(t, err, model.ErrBucketFull, "blocks if limit exceeded")
+			assert.True(t, bucket.State().Remaining == 0, "remaining state zero")
 
-		time.Sleep(time.Millisecond + time.Microsecond)
+			time.Sleep(time.Millisecond + time.Microsecond)
 
-		assert.True(t, bucket.State().Remaining == capacity, "resets state")
+			assert.True(t, bucket.State().Remaining == capacity, "resets state")
+		})
 	})
 }
