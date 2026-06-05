2e5de5c700c4304c2049306f744a3ce87948ac51
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-06-05 08:29:14 +0200

    split interface apply ISP

--- storage/storage.go
+++ storage/storage.go
@@ -18,40 +18,41 @@ var ErrMDCBConnectionLost = errors.New("mdcb connection is lost")
 // Handler is a standard interface to a storage backend, used by
 // AuthorisationManager to read and write key values to the backend
 type Handler interface {
-	GetKey(string) (string, error) // Returned string is expected to be a JSON object (user.SessionState)
-	GetMultiKey([]string) ([]string, error)
-	GetRawKey(string) (string, error)
-	SetKey(string, string, int64) error // Second input string is expected to be a JSON object (user.SessionState)
-	SetRawKey(string, string, int64) error
-	SetExp(string, int64) error   // Set key expiration
-	GetExp(string) (int64, error) // Returns expiry of a key
-	GetKeys(string) []string
-	DeleteKey(string) bool
-	DeleteAllKeys() bool
-	DeleteRawKey(string) bool
-	DeleteRawKeys([]string) bool
-	Connect() bool
-	GetKeysAndValues() map[string]string
-	GetKeysAndValuesWithFilter(string) map[string]string
-	DeleteKeys([]string) bool
-	Decrement(string)
-	IncrememntWithExpire(string, int64) int64
-	SetRollingWindow(key string, per int64, val string, pipeline bool) (int, []interface{})
-	GetRollingWindow(key string, per int64, pipeline bool) (int, []interface{})
-	GetSet(string) (map[string]string, error)
-	AddToSet(string, string)
-	GetAndDeleteSet(string) []interface{}
-	RemoveFromSet(string, string)
-	DeleteScanMatch(string) bool
-	GetKeyPrefix() string
-	AddToSortedSet(string, string, float64)
-	GetSortedSetRange(string, string, string) ([]string, []float64, error)
-	RemoveSortedSetRange(string, string, string) error
-	GetListRange(string, int64, int64) ([]string, error)
-	RemoveFromList(string, string) error
-	AppendToSet(string, string)
-	Exists(string) (bool, error)
-	SetExHandler
+	GetKeyHandler
+	GetMultiKeyHandler
+	GetRawKeyHandler
+	SetKeyHandler
+	SetRawKeyHandler
+	SetExpHandler
+	GetExpHandler
+	GetKeysHandler
+	DeleteKeyHandler
+	DeleteAllKeysHandler
+	DeleteRawKeyHandler
+	DeleteRawKeysHandler
+	ConnectHandler
+	GetKeysAndValuesHandler
+	GetKeysAndValuesWithFilterHandler
+	DeleteKeysHandler
+	DecrementHandler
+	IncrememntWithExpireHandler
+	SetRollingWindowHandler
+	GetRollingWindowHandler
+	GetSetHandler
+	AddToSetHandler
+	GetAndDeleteSetHandler
+	RemoveFromSetHandler
+	DeleteScanMatchHandler
+	GetKeyPrefixHandler
+	AddToSortedSetHandler
+	GetSortedSetRangeHandler
+	RemoveSortedSetRangeHandler
+	GetListRangeHandler
+	RemoveFromListHandler
+	AppendToSetHandler
+	ExistsHandler
+	SetKeyExHandler
+	SetRawKeyExHandler
 }
 
 type AnalyticsHandler interface {
@@ -62,9 +63,144 @@ type AnalyticsHandler interface {
 	GetExp(string) (int64, error) // Returns expiry of a key
 }
 
-type SetExHandler interface {
+type GetKeyHandler interface {
+	GetKey(string) (string, error) // Returned string is expected to be a JSON object (user.SessionState)
+}
+
+type GetMultiKeyHandler interface {
+	GetMultiKey([]string) ([]string, error)
+}
+
+type GetRawKeyHandler interface {
+	GetRawKey(string) (string, error)
+}
+
+type SetKeyHandler interface {
+	SetKey(string, string, int64) error // Second input string is expected to be a JSON object (user.SessionState)
+}
+
+type SetRawKeyHandler interface {
+	SetRawKey(string, string, int64) error
+}
+
+type SetExpHandler interface {
+	SetExp(string, int64) error // Set key expiration
+}
+
+type GetExpHandler interface {
+	GetExp(string) (int64, error) // Returns expiry of a key
+}
+
+type GetKeysHandler interface {
+	GetKeys(string) []string
+}
+
+type DeleteKeyHandler interface {
+	DeleteKey(string) bool
+}
+
+type DeleteAllKeysHandler interface {
+	DeleteAllKeys() bool
+}
+
+type DeleteRawKeyHandler interface {
+	DeleteRawKey(string) bool
+}
+
+type DeleteRawKeysHandler interface {
+	DeleteRawKeys([]string) bool
+}
+
+type ConnectHandler interface {
+	Connect() bool
+}
+
+type GetKeysAndValuesHandler interface {
+	GetKeysAndValues() map[string]string
+}
+
+type GetKeysAndValuesWithFilterHandler interface {
+	GetKeysAndValuesWithFilter(string) map[string]string
+}
+
+type DeleteKeysHandler interface {
+	DeleteKeys([]string) bool
+}
+
+type DecrementHandler interface {
+	Decrement(string)
+}
+
+type IncrememntWithExpireHandler interface {
+	IncrememntWithExpire(string, int64) int64
+}
+
+type SetRollingWindowHandler interface {
+	SetRollingWindow(key string, per int64, val string, pipeline bool) (int, []interface{})
+}
+
+type GetRollingWindowHandler interface {
+	GetRollingWindow(key string, per int64, pipeline bool) (int, []interface{})
+}
+
+type GetSetHandler interface {
+	GetSet(string) (map[string]string, error)
+}
+
+type AddToSetHandler interface {
+	AddToSet(string, string)
+}
+
+type GetAndDeleteSetHandler interface {
+	GetAndDeleteSet(string) []interface{}
+}
+
+type RemoveFromSetHandler interface {
+	RemoveFromSet(string, string)
+}
+
+type DeleteScanMatchHandler interface {
+	DeleteScanMatch(string) bool
+}
+
+type GetKeyPrefixHandler interface {
+	GetKeyPrefix() string
+}
+
+type AddToSortedSetHandler interface {
+	AddToSortedSet(string, string, float64)
+}
+
+type GetSortedSetRangeHandler interface {
+	GetSortedSetRange(string, string, string) ([]string, []float64, error)
+}
+
+type RemoveSortedSetRangeHandler interface {
+	RemoveSortedSetRange(string, string, string) error
+}
+
+type GetListRangeHandler interface {
+	GetListRange(string, int64, int64) ([]string, error)
+}
+
+type RemoveFromListHandler interface {
+	RemoveFromList(string, string) error
+}
+
+type AppendToSetHandler interface {
+	AppendToSet(string, string)
+}
+
+type ExistsHandler interface {
+	Exists(string) (bool, error)
+}
+
+type SetKeyExHandler interface {
 	// SetKeyEx sets key if key already exists.
 	SetKeyEx(string, string, int64) error
+}
+
+type SetRawKeyExHandler interface {
 	// SetRawKeyEx sets raw key if key already exists.
 	SetRawKeyEx(string, string, int64) error
 }
