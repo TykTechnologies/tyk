3b4fdb5af641c1f8586f7db6e173d39cfce49d35
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-06-08 19:04:51 +0200

    remove logs check

--- gateway/ldap_auth_handler_test.go
+++ gateway/ldap_auth_handler_test.go
@@ -1,17 +1,12 @@
 package gateway
 
 import (
-	"strings"
 	"testing"
 
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-
-	logger "github.com/TykTechnologies/tyk/log"
 )
 
 func Test_LDAPStorageHandler(t *testing.T) {
-	const msg = "LDAP storage is READ ONLY"
 
 	newLDAPStorageHandler := func() *LDAPStorageHandler {
 		return &LDAPStorageHandler{}
@@ -20,28 +15,17 @@ func Test_LDAPStorageHandler(t *testing.T) {
 	t.Run("SetKeyEx", func(t *testing.T) {
 		t.Run("does not return error and logs warning", func(t *testing.T) {
 			handler := newLDAPStorageHandler()
-			hook := logger.InjectTestHook(t, logger.WithDestLogger(log))
-
 			err := handler.SetKeyEx("", "", 1)
 			assert.NoError(t, err)
-
-			assert.Equal(t, 1, hook.CountBy(func(entry *logrus.Entry) bool {
-				return entry.Level == logrus.WarnLevel && strings.Contains(entry.Message, msg)
-			}))
 		})
 	})
 
 	t.Run("SetRawKeyEx", func(t *testing.T) {
 		t.Run("does not return error and logs warning", func(t *testing.T) {
 			handler := newLDAPStorageHandler()
-			hook := logger.InjectTestHook(t, logger.WithDestLogger(log))
 
 			err := handler.SetRawKeyEx("", "", 1)
 			assert.NoError(t, err)
-
-			assert.Equal(t, 1, hook.CountBy(func(entry *logrus.Entry) bool {
-				return entry.Level == logrus.WarnLevel && strings.Contains(entry.Message, msg)
-			}))
 		})
 	})
 }
