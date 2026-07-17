85c1d07c77f6db6217b61549e16baaf2ee69fc20
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-07-17 09:54:32 +0200

    fix

--- gateway/api_loader.go
+++ gateway/api_loader.go
@@ -696,7 +696,7 @@ func (d *DummyProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if newURL := ctxGetURLRewriteTarget(r); newURL != nil {
 		r.URL = newURL
 		ctxSetURLRewriteTarget(r, nil)
-		ctxSetUrlRewritten(r, false)
+		ctxSetUrlRewritten(r, true)
 	}
 	if newMethod := ctxGetTransformRequestMethod(r); newMethod != "" {
 		r.Method = newMethod
