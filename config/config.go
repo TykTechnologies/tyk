2578874185d767e9f8428d4b2cfdb352e6b20173
Vlad Zabolotnyi <109525963+vladzabolotnyi@users.noreply.github.com>
2026-08-06 23:31:06 -0700

    docs: update config.KV comment
    Co-authored-by: andyo-tyk <99968932+andyo-tyk@users.noreply.github.com>

--- config/config.go
+++ config/config.go
@@ -1370,7 +1370,8 @@ type Config struct {
 	// global session lifetime, in seconds.
 	GlobalSessionLifetime int64 `bson:"global_session_lifetime" json:"global_session_lifetime"`
 
-	// This section enables the use of the KV capabilities to substitute configuration values.
+	// Configure the Key-Value stores for secure storage of secrets and other configuration values.
+	// From Tyk 5.15.0 the dedicated Consul, Vault and File methods are deprecated in favour of the more flexible Stores approach.
 	// See more details https://tyk.io/docs/tyk-self-managed/#store-configuration-with-key-value-store
 	KV struct {
 		Consul ConsulConfig              `json:"consul"`
