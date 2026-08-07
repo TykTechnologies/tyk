73a2136771b6e34dcec7f635a5981495c08ad089
Vlad Zabolotnyi <vlad.z@tyk.io>
2026-08-07 15:55:37 +0300

    feat: add ee providers to factory and upgrade storage v to 1.5.0

--- gateway/kv_ee.go
+++ gateway/kv_ee.go
@@ -2,16 +2,19 @@
 
 package gateway
 
-import "github.com/TykTechnologies/storage/kv"
+import (
+	"github.com/TykTechnologies/storage/kv"
+	"github.com/TykTechnologies/storage/kv/providers/aws"
+	"github.com/TykTechnologies/storage/kv/providers/azure"
+	"github.com/TykTechnologies/storage/kv/providers/gcp"
+)
 
 func enterpriseKVFactories() map[kv.ProviderType]kv.ProviderFactory {
 	factories := make(map[kv.ProviderType]kv.ProviderFactory)
 
-	// TODO: Uncomment after providers are implemented
-	// factories[kv.AWS] = aws.NewFactory()
-	// factories[kv.Azure] = azure.NewFactory()
-	// factories[kv.GCP] = gcp.NewFactory()
-	// factories[kv.Conjur] = conjur.NewFactory()
+	factories[kv.AWS] = aws.NewFactory()
+	factories[kv.Azure] = azure.NewFactory()
+	factories[kv.GCP] = gcp.NewFactory()
 
 	return factories
 }
