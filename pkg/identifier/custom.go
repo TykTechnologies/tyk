dca07c42a97703e88b672119e1209014d3ed2d86
Dawid Gora <dawid.gora@codilime.com>
2026-09-16 11:17:18 +0200

    chore: fix refactor

--- pkg/identifier/custom.go
+++ pkg/identifier/custom.go
@@ -6,12 +6,28 @@ import (
 	"github.com/TykTechnologies/tyk/pkg/errpack"
 )
 
+// validCustomIdRe holds the characters allowed in any user-defined identifier.
+var validCustomIdRe = regexp.MustCompile(`^[a-zA-Z0-9.\-_~]+$`)
+
 var (
-	validPolicyRe            = regexp.MustCompile(`^[a-zA-Z0-9.\-_~]+$`)
-	validApiRe               = regexp.MustCompile(`^[a-zA-Z0-9.\-_~]+$`)
 	ErrInvalidCustomPolicyId = errpack.Domain("Invalid Policy ID: Allowed characters: a-z, A-Z, 0-9, ., _, -, ~")
 	ErrInvalidCustomApiId    = errpack.Domain("Invalid API ID: Allowed characters: a-z, A-Z, 0-9, ., _, -, ~")
 )
+
+// validateCustomId returns invalidErr when id contains characters outside the
+// allowed set. An empty id is valid: it means the identifier is generated
+// rather than user-defined.
+func validateCustomId(id string, invalidErr error) error {
+	if len(id) == 0 {
+		return nil
+	}
+
+	if !validCustomIdRe.MatchString(id) {
+		return invalidErr
+	}
+
+	return nil
+}
 
 // CustomPolicyId (user-defined-identifier)
 type CustomPolicyId string
@@ -21,15 +37,7 @@ func (c CustomPolicyId) String() string {
 }
 
 func (c CustomPolicyId) Validate() error {
-	if len(c) == 0 {
-		return nil
-	}
-
-	if !validPolicyRe.MatchString(string(c)) {
-		return ErrInvalidCustomPolicyId
-	}
-
-	return nil
+	return validateCustomId(string(c), ErrInvalidCustomPolicyId)
 }
 
 // CustomApiId (user-defined-identifier)
@@ -40,13 +48,5 @@ func (c CustomApiId) String() string {
 }
 
 func (c CustomApiId) Validate() error {
-	if len(c) == 0 {
-		return nil
-	}
-
-	if !validApiRe.MatchString(string(c)) {
-		return ErrInvalidCustomApiId
-	}
-
-	return nil
+	return validateCustomId(string(c), ErrInvalidCustomApiId)
 }
