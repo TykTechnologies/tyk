280e782adb86034b7685c20bca51ce5b7a4d4320
Yaroslav Kotsur <yarikkotsur@gmail.com>
2026-09-23 12:38:17 +0200

    chore: error prioryty

--- internal/graphengine/errors.go
+++ internal/graphengine/errors.go
@@ -2,6 +2,11 @@ package graphengine
 
 import (
 	"errors"
+	"net/http"
+
+	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
+
+	"github.com/TykTechnologies/tyk/header"
 )
 
 var (
@@ -11,3 +16,14 @@ var (
 	ErrIntrospectionDisabled     = errors.New("introspection is disabled")
 	ErrUnknownReverseProxyType   = errors.New("unknown reverse proxy type")
 )
+
+type GraphQlError struct {
+	graphql.Errors
+}
+
+func (e GraphQlError) WriteToResponse(w http.ResponseWriter, statusCode int) error {
+	w.Header().Set(header.ContentType, header.ApplicationJSON)
+	w.WriteHeader(statusCode)
+	_, err := e.WriteResponse(w)
+	return err
+}
